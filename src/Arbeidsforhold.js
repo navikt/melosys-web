e6b452b91caa8a9746d2a5d39d7bce6d0cae9322
Bjølseth, Are <are.bjolseth@nav.no>
2017-09-07 13:26:46 +0200

    EESSI2-237 La til oppslag på Person i Arbeidsforhold

--- src/Arbeidsforhold.js
+++ src/Arbeidsforhold.js
@@ -4,16 +4,20 @@ import Lenkepanel from 'nav-frontend-lenkepanel';
 import './arbeidsforhold.css';
 import {CONTEXT_PATH} from './constants';
 import { connect } from 'react-redux';
+import { hentPerson } from './ducks/person';
 import { hentArbeidsforhold } from './ducks/arbeidsforhold';
 
-const ArbeidsforholdRad = ({data}) => {
+const ArbeidsforholdRad = ({
+   data, person
+}) => {
+  const sammensattNavn = person ? person.personnavn.sammensattNavn: '';
   const fnr = data.arbeidstaker.ident.ident;
   const orgnr = data.arbeidsgiver.orgnummer;
   const linkTo = `${CONTEXT_PATH}/arbeidsforholdet/${fnr}/${orgnr}`;
   const arbeidsavtaler = `, arbeidsavtaler(${data.arbeidsavtale.length})`
   return (
     <Lenkepanel tittelProps="innholdstittel" href={linkTo}>
-      {orgnr}, {data.arbeidsforholdstype}{arbeidsavtaler}
+      {sammensattNavn}, {orgnr}, {data.arbeidsforholdstype}{arbeidsavtaler}
     </Lenkepanel>
   );
 };
@@ -22,11 +26,12 @@ class Arbeidsforhold extends Component {
   componentDidMount() {
 
     let fnr = this.props.match.params.fnr;
+    this.props.hentPerson(fnr);
     this.props.hentArbeidsforhold(fnr);
   }
   render() {
-    const arbeidsforhold = this.props.arbeidsforhold ? this.props.arbeidsforhold : [];
-    const rows = arbeidsforhold.map((item) => <ArbeidsforholdRad key={item.arbeidsforholdID} data={item}/>);
+    const { person, arbeidsforhold } = this.props;
+    const rows = arbeidsforhold.map((item) => <ArbeidsforholdRad key={item.arbeidsforholdID} data={item} person={person}/>);
     return (
       <section className="arbeidsforhold">
         <Panel>
@@ -40,10 +45,14 @@ class Arbeidsforhold extends Component {
 }
 
 
-const mapStateToProps = (state) => ({
-  arbeidsforhold: state.arbeidsforhold.data
-});
+const mapStateToProps = (state) => {
+  return ({
+    person: state.person.data,
+    arbeidsforhold: state.arbeidsforhold.data
+  })
+};
 const mapDispatchToProps = (dispatch) => ({
+  hentPerson: (fnr) => dispatch(hentPerson(fnr)),
   hentArbeidsforhold: (fnr) => dispatch(hentArbeidsforhold(fnr))
 });
 
