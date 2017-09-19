9cd9a3068691a1602824431099b9678ac5f924a1
Bjølseth, Are <are.bjolseth@nav.no>
2017-09-19 08:57:19 +0200

    EESSI2-276 Implementere sok-arbeidsforhold med redux-form API

---
+++ src/moduler/arbeidsforhold/soke-form.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PT from 'prop-types';
+import { Field, reduxForm } from 'redux-form'
+
+//import { Input } from 'nav-frontend-skjema';
+//import Input from './felles-komponenter/skjema/input/input';
+
+import { Hovedknapp } from 'nav-frontend-knapper';
+
+const required = value => (value ? undefined : 'Required');
+const fnr11digits = value => (/^[0-9]{11}$/.test(value) ? value: 'Fnr må ha 11 siffer');
+
+const renderFnrField = ({input, name, label, bredde, type, meta: {touched, error}}) => (
+  <div>
+    <label htmlFor={name} className="skjemaelement__label">{label}</label>
+    <div>
+      <input {...input} type={type}  className={`skjemaelement__input input--${bredde}`}/>
+      {touched && ((error && <div className="skjemaelement__feilmelding">{error}</div>))}
+    </div>
+  </div>
+);
+
+class SokeForm extends Component {
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <div className="search-container">
+        <h2 className="typo-undertittel"><span>Søk på person</span></h2>
+        <form onSubmit={ handleSubmit }>
+          {/*<Input label="Fnr. eller dnr. " bredde="xl" value={this.state.fnr} onChange={handleChange}/>*/}
+          <div className="skjemaelement">
+            <Field name="fnr" type="text" component={renderFnrField} label="Fnr. eller dnr." validate={[required, fnr11digits]} bredde="xl"/>
+          </div>
+          <Hovedknapp>Søk</Hovedknapp>
+        </form>
+      </div>
+    );
+  }
+}
+
+SokeForm.propTypes = {
+  fields: PT.object.isRequired,
+  handleSubmit: PT.func.isRequired
+};
+
+
+SokeForm = reduxForm({
+  form:'sokeform'
+})(SokeForm);
+
+export default SokeForm;
--- src/sok-arbeidsforhold.js
+++ src/sok-arbeidsforhold.js
@@ -1,72 +1,21 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom'
-//import { Route } from 'react-router-dom'
-//import PropTypes from 'prop-types'
-import { Field, reduxForm } from 'redux-form'
 
-import { Input } from 'nav-frontend-skjema';
-//import Input from './felles-komponenter/skjema/input/input';
-import { Hovedknapp } from 'nav-frontend-knapper';
 import './sok-arbeidsforhold.css';
-
-let SokeForm = props => {
-  const { handleSubmit } = props;
-  return (
-      <form onSubmit={ handleSubmit }>
-        {/*<Input label="Fnr. eller dnr. " bredde="xl" value={this.state.fnr} onChange={handleChange}/>*/}
-        <div>
-          <label htmlFor="fnr">Fnr. eller dnr. </label>
-          <Field name="fnr" component="input" type="text" />
-        </div>
-        <Hovedknapp>Søk</Hovedknapp>
-      </form>
-  )
-};
-SokeForm = reduxForm({
-  form:'sokeform'
-})(SokeForm);
+import SokeForm from './moduler/arbeidsforhold/soke-form';
 
 class SokArbeidsforhold extends Component {
   submit = (values) => {
     this.props.history.push('/arbeidsforhold/'+values.fnr);
-  }
+  };
   render() {
     return (
-      <SokeForm onSubmit={this.submit} />
+      <div className="arbeidsforholdContainer">
+        <SokeForm onSubmit={this.submit} />
+      </div>
     );
   }
-  /*
-
-  state = {
-        fnr: ''
-    };
-    onChange = (event) => {
-        this.setState({ fnr: event.target.value });
-    };
-
-
-    onSubmit = (event) => {
-        //console.log('this.refs.fnr', this.state.fnr);
-        event.preventDefault();
-        this.props.history.push('/arbeidsforhold/'+this.state.fnr);
-        // window.location = '/melosys/arbeidsforhold/'+this.state.fnr;
-    };
-
-    render() {
-      return (
-          <ArbeidsforholdContainer>
-            <h2 className="typo-undertittel"><span>Søk på person</span></h2>
-            <form onSubmit={this.onSubmit}>
-                <Input label="Fnr. eller dnr. " bredde="xl" value={this.state.fnr} onChange={this.onChange}/>
-                <Hovedknapp>Søk</Hovedknapp>
-            </form>
-          </ArbeidsforholdContainer>
-        );
-    }
-    */
 }
-const ArbeidsforholdContainer = (props) => (
-  <div className="arbeidsforholdContainer" {...props}/>
-)
+
 export default withRouter(SokArbeidsforhold);
 
