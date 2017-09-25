0032ced7793a82cae5ae861e3267aeffc18f05f6
Bjølseth, Are <are.bjolseth@nav.no>
2017-09-25 13:11:45 +0200

    EESSI2-285 mer config fra .env systemet

--- src/ducks/api.js
+++ src/ducks/api.js
@@ -1,23 +1,23 @@
 //import {CONTEXT_PATH, API_BASE_URL} from '../constants';
 //import { fetchToJson, postAsJson, putAsJson } from './../ducks/utils';
 import { fetchToJson } from './../ducks/utils';
-
+/*
 function erDev() {
   // const url = window.location.href;
   // return url.includes('debug=true') ||  url.includes('localhost:');
   //return false;
   // from .env or .env.local
-  return process.env.NODE_PATH === 'production';
+  return process.env.NODE_PATH !== 'production';
 }
-/*
-const MOCK_API_SERVER = erDev() ? 'http://localhost:3002' : 'https://e34jbsl01783.devillo.no:8443';
-const API_BASE_URL = erDev() ? '/api/' : '/system/melosys/api/';
-const API_MELOSYS_URL = `${MOCK_API_SERVER}${API_BASE_URL}`;
 */
 
 // from .env or .env.local
-console.log('process.env',process.env);
-const API_MELOSYS_URL = `${process.env.REACT_APP_API_SERVER}${process.env.REACT_APP_API_BASE_URL}`;
+const API_SERVER = `${process.env.REACT_APP_API_SERVER}`;
+const API_BASE_URL = `${process.env.REACT_APP_API_BASE_URL}`;
+const API_MELOSYS_URL = `${API_SERVER}${API_BASE_URL}`;
+const API_SAKSBEHANDLER = `${process.env.REACT_APP_API_SAKSBEHANDLER}`;
+const API_SAKSOPPLYSNINGER = `${process.env.REACT_APP_API_SAKSOPPLYSNINGER}`;
+console.log('process.env', process.env);
 
 
 const headers = new Headers({
@@ -26,14 +26,12 @@ const headers = new Headers({
 });
 
 export function hentSaksbehandler() {
-  const URI_SAKSBEHANDLER = `${API_MELOSYS_URL}saksbehandler`;
+  const URI_SAKSBEHANDLER = `${API_MELOSYS_URL}${API_SAKSBEHANDLER}`;
   return fetchToJson(URI_SAKSBEHANDLER, {headers: headers});
 }
 
 export function hentSaksopplysninger(fnr) {
-  const saksopplysningerOgArbeidsforhold = erDev() ? 'saksopplysninger/' : 'arbeidsforhold/';
-  const URI_SAMMENSATT_ARBEIDSFORHOLD = `${API_MELOSYS_URL}${saksopplysningerOgArbeidsforhold}${fnr}`;
-  //const URI_SAMMENSATT_ARBEIDSFORHOLD = `https://e34jbsl01783.devillo.no:8443/system/melosys/api/arbeidsforhold/${fnr}`;
+  const URI_SAMMENSATT_ARBEIDSFORHOLD = `${API_MELOSYS_URL}${API_SAKSOPPLYSNINGER}/${fnr}`;
   return fetchToJson(URI_SAMMENSATT_ARBEIDSFORHOLD, {headers: headers});
 
 }
