b5d94d792413e67780e1b414ebec7f57e3814cb7
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-02 12:50:18 +0200

    Refactored Topplinje to pure flexbox

--- src/components/Topplinje.js
+++ src/components/Topplinje.js
@@ -1,52 +1,45 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { hentSaksbehandler, getSaksbehandlerState } from '../ducks/saksbehandler';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+import {hentSaksbehandler, getSaksbehandlerState} from '../ducks/saksbehandler';
 import * as navLogo from '../resources/images/nav.svg';
-import * as saksbehandlerIkon from '../resources/images/saksbehandler.svg';
-import '../index.css'
 import './topplinje.css'
 
 class Topplinje extends Component {
-  componentDidMount() {
-    this.props.hentSaksbehandler()
-  }
+    componentDidMount() {
+        this.props.hentSaksbehandler()
+    }
 
-  render() {
-    const {saksbehandler: {navn}} = this.props;
-    return (
-        <header className="headerContainer">
-          <div className="topplinje">
-            <div>
-              <div className="logo">
-                <Link to="/" alt="NAV, lenke hovedsiden">
-                  <img
-                      className="headerIkon"
-                      src={navLogo}
-                      alt="To personer på NAV kontor"
-                  />
-                </Link>
-              </div>
-              <div className="headerDivider" />
-            </div>
-            <h4 className="text"><span>Medlemsskap og lovvalgssystem</span></h4>
-            <div className="saksbehandler">
-              <img className="saksbehandlerIkon" src={saksbehandlerIkon} alt="Saksbehandler" />
-              <div className="saksbehandlerTekst">{navn}</div>
-            </div>
-          </div>
-        </header>
-    );
-  }
+    render() {
+        const {saksbehandler: {navn}} = this.props;
+        return (
+            <header className="topplinje">
+                <div className="topplinje__brand">
+                    <Link to="/" alt="NAV, lenke hovedsiden">
+                        <img
+                            className="brand__logo"
+                            src={navLogo}
+                            alt="To personer på NAV kontor"
+                        />
+                    </Link>
+                    <div className="brand__skillelinje"/>
+                    <h4 className="brand__tittel"><span>Medlemsskap og lovvalgssystem</span></h4>
+                </div>
+                <div className="topplinje__bruker">
+                    <div className="bruker__navn">{navn}</div>
+                </div>
+            </header>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-  return ({
-    saksbehandler: getSaksbehandlerState(state)
-  })
+    return ({
+        saksbehandler: getSaksbehandlerState(state)
+    })
 };
 const mapDispatchToProps = (dispatch) => ({
-  hentSaksbehandler: () => dispatch(hentSaksbehandler())
+    hentSaksbehandler: () => dispatch(hentSaksbehandler())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Topplinje);
