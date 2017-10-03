893a068cc4f11287ba54ff8da8b9ec495e2e09ca
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-03 14:52:26 +0200

    Syntaksfiks

--- src/history.js
+++ src/history.js
@@ -1,5 +1,4 @@
 import { createBrowserHistory } from 'history';
-//import { getFodselsnummer } from './bootstrap/fnr-util';
 import { CONTEXT_PATH } from './constants'; // eslint-disable-line
 
 const routerHistory = createBrowserHistory({
