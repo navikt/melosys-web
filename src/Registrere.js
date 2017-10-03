404c468ef84901c23edc6cc7d2455a3535094801
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-03 14:57:47 +0200

    Syntaksfiks

--- src/Registrere.js
+++ src/Registrere.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import ArbeidsgiverForm from './moduler/arbeidsgiver/arbeidsgiver-form';
+
 class Registrere extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            mymessage: '',
-        };
-    }
-    submit = values => {
-        console.log(JSON.stringify(values));
+  constructor (props) {
+    super(props);
+    this.state = {
+      mymessage: '',
     };
-    onComplete = data => {
-        this.setState({
-            mymessage: data,
-        });
-        console.log('onComplete', JSON.stringify(data));
-    };
-    render() {
-        return <ArbeidsgiverForm onComplete={this.onComplete} />;
-    }
+  }
+
+  onComplete = data => {
+    this.setState({
+      mymessage: data,
+    });
+
+    // eslint-disable-next-line
+    console.log('onComplete', JSON.stringify(data));
+  }
+
+  submit = values => {
+    // eslint-disable-next-line
+    console.log(JSON.stringify(values));
+  };
+
+  render () {
+    return <ArbeidsgiverForm onComplete={this.onComplete} />;
+  }
 }
+
 export default Registrere;
