faafc630548e6463d6c34e5a1913b7e69a1acd96
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-05 15:14:24 +0200

    Oppdatert diverse nav-frontend-*-komponenter. Constant.less holder globale variabler. Satt opp første routing for Sok og Saksbehandling.

--- src/routing.js
+++ src/routing.js
@@ -3,6 +3,7 @@ import PT from 'prop-types';
 import { Route, Switch, withRouter } from 'react-router-dom';
 
 import Sok from './sider/sok';
+import Saksbehandling from './sider/saksbehandling';
 import Arbeidsforhold from './sider/Arbeidsforhold';
 import ArbeidsforholdDetalj from './sider/ArbeidsforholdDetalj';
 import Registrere from './sider/Registrere';
@@ -10,6 +11,7 @@ import Registrere from './sider/Registrere';
 const Routing = ({ location }) => (
   <Switch location={location}>
     <Route exact path="/" component={Sok} />
+    <Route exact path="/saksbehandling/:fnr" component={Saksbehandling} />
     <Route path="/arbeidsforhold/:fnr" component={Arbeidsforhold} />
     <Route
       path="/arbeidsforholdet/:fnr/"
