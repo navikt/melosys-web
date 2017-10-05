1b4333a02f66c54922f67ae8077a9b820a48917e
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-05 12:21:28 +0200

    Slått sammen Header og Topplinje til én komponent.

---
+++ src/components/topplinje.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import PT from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import * as navLogo from '../resources/images/nav.svg';
+
+import './topplinje.css';
+
+import {
+  hentSaksbehandler,
+  getSaksbehandlerState,
+} from '../ducks/saksbehandler';
+
+class Topplinje extends Component {
+  componentDidMount() {
+    this.props.hentSaksbehandler();
+  }
+
+  render() {
+    const { saksbehandler: { navn } } = this.props;
+    return (
+      <header className="topplinje">
+        <div className="topplinje__brand">
+          <Link to="/" alt="NAV, lenke hovedsiden">
+            <img
+              className="brand__logo"
+              src={navLogo}
+              alt="To personer på NAV kontor"
+            />
+          </Link>
+          <div className="brand__skillelinje" />
+          <div className="brand__tittel"><span>Medlemsskap og lovvalgssystem</span></div>
+        </div>
+        <div className="topplinje__sok">
+          <input className="sok__felt" />
+          <button className="sok__button" />
+        </div>
+        <div className="topplinje__saksbehandler">
+          <div className="saksbehandler__navn">{navn}</div>
+        </div>
+      </header>
+    );
+  }
+}
+
+Topplinje.propTypes = {
+  hentSaksbehandler: PT.func.isRequired,
+  saksbehandler: PT.object.isRequired,
+};
+
+Topplinje.defaultProps = {
+  saksbehandler: { navn: '' },
+};
+
+const mapStateToProps = state => ({
+  saksbehandler: getSaksbehandlerState(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  hentSaksbehandler: () => dispatch(hentSaksbehandler()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Topplinje);
