ae803d92960fcada75e3471b0569e59414e31bef
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-09 12:49:45 +0200

    Refactor og fjernet material-ui. Bygget egen steg-komponent med faner.

--- src/felles-komponenter/vilkarsvurdering/vilkarsvurdering.js
+++ src/felles-komponenter/vilkarsvurdering/vilkarsvurdering.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Knapp } from 'nav-frontend-knapper';
-import { Checkbox, Radio } from 'nav-frontend-skjema';
-import { Undertittel } from 'nav-frontend-typografi';
-import {
-  Step,
-  Stepper,
-  StepButton,
-} from 'material-ui/Stepper';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Checkbox, Radio, Fieldset } from 'nav-frontend-skjema';
+import { Innholdstittel } from 'nav-frontend-typografi';
 
+import StegVelger from './komponenter/stegVelger';
+import StegFane from './komponenter/stegFane';
 import './vilkarsvurdering.css';
 
 class Vilkarsvurdering extends Component {
-  // Todo 1: Sett steg-innold inn via JSON for enklere feed inn senere.
-  // Todo 2: Bygg om stegvelger og dropp material-ui. Den bygger 100-120kb ekstra og er ikke verdt det.
-  // Todo 3: Sett opp evt. prop types og hook opp mot Redux med dispatch.
+  // Todo 1: Sett opp evt. prop types og hook opp mot Redux med dispatch.
 
   static propTypes = {};
-
   static defaultProps = {};
 
   state = {
@@ -31,81 +23,54 @@ class Vilkarsvurdering extends Component {
     this.setState({ visited: visited.concat(stepIndex) });
   }
 
-  getStepContent = stepIndex => {
-    switch (stepIndex) {
-      case 0:
-        return (
-          <div>
-            <Undertittel type="undertittel">Vurdering</Undertittel>
-            <p>Vurder om søker er</p>
-            <Radio label="Ikke arbeidsakriv / yrkesmottaker" value="ikke_arbeidsaktiv" id="ikke_arbeidsaktiv_checkbox" name="arbeid" />
-            <Radio label="Arbeidstaker" value="arbeidstaker" id="arbeidstaker_checkbox" name="arbeid" />
-            <Radio label="Selvstendig næringsdrivende" value="selvstendig" id="selvstendig_checkbox" name="arbeid" />
-            <Radio label="Både arbeidstaker og selvstendig" value="arbeidstaker_selvstendig" id="iarbeidstaker_selvstendig_checkbox" name="arbeid" />
-          </div>
-        );
-      case 1:
-        return (
-          <div>
-            <Undertittel type="undertittel">Vurdering</Undertittel>
-            <p>Vurder om søker er</p>
-            <Checkbox label="Offentlig tjenesteperson" value="ikke_arbeidsaktiv" id="ikke_arbeidsaktiv_checkbox" name="offentlig" />
-            <Checkbox label="Ansatt på skip" value="arbeidstaker" id="arbeidstaker_checkbox" name="arbeid" />
-            <Checkbox label="Ansatt på sokkel" value="selvstendig" id="selvstendig_checkbox" name="arbeid" />
-            <Checkbox label="Flyvende personell" value="arbeidstaker_selvstendig" id="iarbeidstaker_selvstendig_checkbox" name="arbeid" />
-          </div>
-        );
-      case 2:
-        return '';
-      default:
-        return (
-          <div>
-            <p>Dette er en veileder for vilkårsvurdering som skal hjelpe deg som sakebehandler med å vurdere søknaden. Naviger fanene med punktene øverst</p>
-            <Knapp onClick={() => this.goTo(0)} type="hoved" className="fane__nesteknapp">Begynn</Knapp>
-          </div>
-        );
-    }
-  }
-
-  goTo = step => {
-    const { stepIndex, visited } = this.state;
-    if (visited.indexOf(stepIndex) === -1) {
-      this.setState({ visited: visited.concat(stepIndex) });
-    }
-    this.setState({ stepIndex: step });
-  }
-
-  handleNext = () => {
-    const { stepIndex } = this.state;
-    if (stepIndex < 2) {
-      this.setState({ stepIndex: stepIndex + 1 });
-    }
-  };
-
   render() {
-    const { stepIndex, visited } = this.state;
+    const valg = {
+      visNesteKnapp: true,
+      stegvis: true,
+    };
+
     return (
-      <MuiThemeProvider>
-        <div className="vilkarsvurdering">
-          <Stepper linear={false}>
-            <Step completed={visited.indexOf(0) !== -1} active={stepIndex === 0}>
-              <StepButton onClick={() => this.goTo(0)} />
-            </Step>
-            <Step completed={visited.indexOf(1) !== -1} active={stepIndex === 1}>
-              <StepButton onClick={() => this.goTo(1)} />
-            </Step>
-            <Step completed={visited.indexOf(2) !== -1} active={stepIndex === 2}>
-              <StepButton onClick={() => this.goTo(2)} />
-            </Step>
-          </Stepper>
-          <div className="vilkarsvurdering__fane">
-            {this.getStepContent(stepIndex)}
-            {stepIndex !== null && (
-              <Knapp onClick={this.handleNext} type="hoved" className="fane__nesteknapp">Bekreft og fortsett</Knapp>
-            )}
-          </div>
-        </div>
-      </MuiThemeProvider>
+      <div className="vilkarsvurdering">
+        <StegVelger valg={valg}>
+          <StegFane>
+            <Innholdstittel type="innholdstittel">Vurdering:</Innholdstittel>
+            <Fieldset legend="Vurder om søkeren er:">
+              <Radio id="steg0_ikke_arbeidende" name="arbeidssted" label="Ikke arbeidende / yrkesmottaker" />
+              <Radio id="steg0_arbeidstaker" name="arbeidssted" label="Arbeidstaker" />
+              <Radio id="steg0_selvstendig" name="arbeidssted"label="Selvstendig næringsdrivende" />
+              <Radio id="steg0_arbeidstaker_selvstendig" name="arbeidssted" label="Både arbeidstakende og selvstendig" />
+            </Fieldset>
+          </StegFane>
+          <StegFane>
+            <Innholdstittel type="innholdstittel">Vurdering:</Innholdstittel>
+            <Fieldset legend="Gjelder én eller flere av dissefor søkeren?">
+              <Checkbox id="steg1_ansatt_offentlig" label="Offentlig tjenestepensjon (relevant for 11.3 b)" />
+              <Checkbox id="steg1_ansatt_skip" label="Ansatt på skip (relevant for 11.4" />
+              <Checkbox id="steg1_ansatt_sokkel" label="Ansatt på sokkel (relevant for 11.3 a)" />
+              <Checkbox id="steg1_ansatt_flyvende" label="Flyvende personell (relevant for 11.5)" />
+            </Fieldset>
+          </StegFane>
+          <StegFane>
+            <Innholdstittel type="innholdstittel">Vurdering:</Innholdstittel>
+            <Fieldset legend="Hvor mange land skal søker arbeide/drive virsomhet i?">
+              <Radio id="steg2_land_ett" name="land" label="Ett" />
+              <Radio id="steg2_land_flere" name="land" label="To eller flere" />
+            </Fieldset>
+            <Fieldset legend="Hvor mye av aktiviteten skjed i Norge?">
+              <Radio id="steg2_aktivitet_under25" name="aktivitet" label="Mindre enn 25%" />
+              <Radio id="steg2_aktivitet_over25" name="aktivitet" label="25% eller mer" />
+            </Fieldset>
+            <Fieldset legend="Hvor mange arbeidsgivere har søker?">
+              <Radio id="steg2_arbeidsgivere_en" name="arbeidsgivere" label="Èn" />
+              <Radio id="steg2_arbeidsgivere_fler" name="arbeidsgivere" label="To eller fler" />
+            </Fieldset>
+            <Fieldset legend="Er arbeidsgivere i sammme land eller i ulike land?">
+              <Radio id="steg2_arbeidsgiverfordeling_ettland" name="arbeidsgiverfordeling" label="Samme land" />
+              <Radio id="steg2_arbeidsgiverfordeling_ulikeland" name="arbeidsgiverfordeling" label="Ulike land" />
+            </Fieldset>
+          </StegFane>
+        </StegVelger>
+      </div>
     );
   }
 }
