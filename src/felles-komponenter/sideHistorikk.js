471e0643ac70e6ae958564f0672872cadbc63551
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-11 16:19:18 +0200

    Lagt til margin i bunn for SideOppsummering.

---
+++ src/felles-komponenter/sideHistorikk.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PT from 'prop-types';
+import EkspanderbartPanel from 'nav-frontend-ekspanderbartpanel';
+import { Knapp } from 'nav-frontend-knapper';
+import { Undertittel, Normaltekst } from 'nav-frontend-typografi';
+import { Panel } from 'nav-frontend-paneler';
+import { Container, Row, Column } from 'nav-frontend-grid';
+
+import './sideOppsummering.less';
+
+class SideHistorikk extends Component {
+  static propTypes = {
+    historikk: PT.object,
+  };
+  static defaultProps = {
+    historikk: {
+
+    },
+  };
+
+  render() {
+    return (
+      <div className="tilleggsopplysninger">
+        <Panel className="saksbehandling__soknadSammendrag">
+          Historikk
+        </Panel>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = () => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SideHistorikk);
