bdb6ac4e97d0d19a5d1c4e316ecac0ebf95a8cad
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-11 14:00:28 +0200

    Lagt til kontaktinfo, ektefelle og barn.

--- src/felles-komponenter/personopplysninger.js
+++ src/felles-komponenter/personopplysninger.js
@@ -40,19 +40,66 @@ class Personopplysninger extends Component {
             {/* START ADRESSE */}
             <Row>
               <Column xs="6">
-                <section arial-label="Adresse">
-                  <Undertittel type="undertittel">Adresse:</Undertittel>
-                  <Normaltekst>1234546789234</Normaltekst>
+                <section arial-label="Adresse TPS">
+                  <Undertittel type="undertittel">Adresse (TPS):</Undertittel>
+                  <Normaltekst>Adresseveien 123, 1234 Adresseby, Norge</Normaltekst>
                 </section>
               </Column>
               <Column xs="6">
-                <section arial-label="Adresse">
-                  <Undertittel type="undertittel">Adresse:</Undertittel>
+                <section arial-label="Bostedsadresse søker">
+                  <Undertittel type="undertittel">Bostedsadresse (Søker):</Undertittel>
                   <Normaltekst>1234546789234</Normaltekst>
                 </section>
               </Column>
             </Row>
             {/* SLUTT ADRESSE */}
+            {/* START KONTAKTINFO */}
+            <Row>
+              <Column xs="6">
+                <section arial-label="Kontaktinfo">
+                  <Undertittel type="undertittel">Kontaktinfo:</Undertittel>
+                  <Normaltekst>Mobil: (+47) 123 123 123</Normaltekst>
+                  <Normaltekst>Telefon: (+47) 222 333 444</Normaltekst>
+                  <Normaltekst>E-post: ola.nordmann@domet.no</Normaltekst>
+                </section>
+              </Column>
+              <Column xs="6">
+                <section arial-label="Arbeidstaker hos">
+                  <Undertittel type="undertittel">Arbeidstaker hos:</Undertittel>
+                  <Normaltekst>Hagemøbler AS</Normaltekst>
+                </section>
+              </Column>
+            </Row>
+            {/* SLUTT KONTAKTINFO */}
+            {/* START EKTEFELLE */}
+            <Row>
+              <Column xs="6">
+                <section arial-label="Ektefelle">
+                  <Undertittel type="undertittel">Ektefelle (nåværende):</Undertittel>
+                  <Normaltekst>Kari Nordmann (40 år)</Normaltekst>
+                </section>
+              </Column>
+            </Row>
+            {/* SLUTT EKTEFELLE */}
+            {/* START BARN */}
+            <Row>
+              <Column xs="6">
+                <section arial-label="Barn">
+                  <Undertittel type="undertittel">Barn:</Undertittel>
+                  <Normaltekst>Mobil: (+47) 123 123 123</Normaltekst>
+                  <Normaltekst>Telefon: (+47) 222 333 444</Normaltekst>
+                  <Normaltekst>E-post: ola.nordmann@domet.no</Normaltekst>
+                </section>
+              </Column>
+              <Column xs="6">
+                <section arial-label="Medfølgende barn?">
+                  <Undertittel type="undertittel">Medfølgende barn?</Undertittel>
+                  <Normaltekst>Skal barn under 18 år oppholde seg i utlandet sammen med søkeren i perioden?</Normaltekst>
+                  <Normaltekst>Nei</Normaltekst>
+                </section>
+              </Column>
+            </Row>
+            {/* SLUTT BARN */}
           </Container>
         </EkspanderbartPanel>
       </div>
