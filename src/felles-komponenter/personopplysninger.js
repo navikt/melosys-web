8d70270a2d87d39e2784190e722b00a319f8a397
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-11 14:24:05 +0200

    Lagt inn dummy-props og hooket opp til DOM-elementer.

--- src/felles-komponenter/personopplysninger.js
+++ src/felles-komponenter/personopplysninger.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PT from 'prop-types';
 import EkspanderbartPanel from 'nav-frontend-ekspanderbartpanel';
 import { Container, Row, Column } from 'nav-frontend-grid';
 import { Undertittel, Normaltekst } from 'nav-frontend-typografi';
@@ -8,15 +9,37 @@ import './personopplysninger.less';
 
 class Personopplysninger extends Component {
   static propTypes = {
-
+    personopplysninger: PT.object,
   };
   static defaultProps = {
-    saksbehandler: {
-      navn: '',
+    personopplysninger: {
+      personnummer: '01017712345',
+      personnummerUtland: '111 222 333',
+      adresse: 'Adresseveien 123, 1234 Adresseby, Norge',
+      bostedsadresse: 'Gateadressen 99, 9999 Gateby, Norge',
+      mobil: '(+47) 123 123 123',
+      telefon: '(+47) 222 333 444',
+      epost: 'ola.nordmann@domenet.no',
+      arbeidsgiver: 'Hagemøbler AS',
+      ektefelle: 'Kari Nordmann (40 år)',
+      barn: ['Lea Nordmann (13 år)', 'Trym Nordmann (7 år)'],
+      medfolgende: false,
     },
   };
 
   render() {
+    const { personnummer,
+      personnummerUtland,
+      adresse,
+      bostedsadresse,
+      mobil,
+      telefon,
+      epost,
+      arbeidsgiver,
+      ektefelle,
+      barn,
+      medfolgende } = this.props.personopplysninger;
+
     return (
       <div className="personopplysninger">
         <EkspanderbartPanel tittel="Ola Nordmann" apen>
@@ -26,13 +49,13 @@ class Personopplysninger extends Component {
               <Column xs="6">
                 <section aria-label="Personnummer">
                   <Undertittel type="undertittel">Personnummer:</Undertittel>
-                  <Normaltekst>1234546789234</Normaltekst>
+                  <Normaltekst>{personnummer}</Normaltekst>
                 </section>
               </Column>
               <Column xs="6">
                 <section aria-label="Personnummer">
                   <Undertittel type="undertittel">Utenlandsk id:</Undertittel>
-                  <Normaltekst>2233 4455 6677</Normaltekst>
+                  <Normaltekst>{personnummerUtland}</Normaltekst>
                 </section>
               </Column>
             </Row>
@@ -42,13 +65,13 @@ class Personopplysninger extends Component {
               <Column xs="6">
                 <section arial-label="Adresse TPS">
                   <Undertittel type="undertittel">Adresse (TPS):</Undertittel>
-                  <Normaltekst>Adresseveien 123, 1234 Adresseby, Norge</Normaltekst>
+                  <Normaltekst>{adresse}</Normaltekst>
                 </section>
               </Column>
               <Column xs="6">
                 <section arial-label="Bostedsadresse søker">
                   <Undertittel type="undertittel">Bostedsadresse (Søker):</Undertittel>
-                  <Normaltekst>1234546789234</Normaltekst>
+                  <Normaltekst>{bostedsadresse}</Normaltekst>
                 </section>
               </Column>
             </Row>
@@ -58,15 +81,15 @@ class Personopplysninger extends Component {
               <Column xs="6">
                 <section arial-label="Kontaktinfo">
                   <Undertittel type="undertittel">Kontaktinfo:</Undertittel>
-                  <Normaltekst>Mobil: (+47) 123 123 123</Normaltekst>
-                  <Normaltekst>Telefon: (+47) 222 333 444</Normaltekst>
-                  <Normaltekst>E-post: ola.nordmann@domet.no</Normaltekst>
+                  <Normaltekst>Mobil: {mobil}</Normaltekst>
+                  <Normaltekst>Telefon: {telefon}</Normaltekst>
+                  <Normaltekst>E-post: {epost}</Normaltekst>
                 </section>
               </Column>
               <Column xs="6">
                 <section arial-label="Arbeidstaker hos">
                   <Undertittel type="undertittel">Arbeidstaker hos:</Undertittel>
-                  <Normaltekst>Hagemøbler AS</Normaltekst>
+                  <Normaltekst>{arbeidsgiver}</Normaltekst>
                 </section>
               </Column>
             </Row>
@@ -76,7 +99,7 @@ class Personopplysninger extends Component {
               <Column xs="6">
                 <section arial-label="Ektefelle">
                   <Undertittel type="undertittel">Ektefelle (nåværende):</Undertittel>
-                  <Normaltekst>Kari Nordmann (40 år)</Normaltekst>
+                  <Normaltekst>{ektefelle}</Normaltekst>
                 </section>
               </Column>
             </Row>
@@ -86,16 +109,14 @@ class Personopplysninger extends Component {
               <Column xs="6">
                 <section arial-label="Barn">
                   <Undertittel type="undertittel">Barn:</Undertittel>
-                  <Normaltekst>Mobil: (+47) 123 123 123</Normaltekst>
-                  <Normaltekst>Telefon: (+47) 222 333 444</Normaltekst>
-                  <Normaltekst>E-post: ola.nordmann@domet.no</Normaltekst>
+                  {barn.map(item => <Normaltekst>{item}</Normaltekst>)}
                 </section>
               </Column>
               <Column xs="6">
                 <section arial-label="Medfølgende barn?">
                   <Undertittel type="undertittel">Medfølgende barn?</Undertittel>
                   <Normaltekst>Skal barn under 18 år oppholde seg i utlandet sammen med søkeren i perioden?</Normaltekst>
-                  <Normaltekst>Nei</Normaltekst>
+                  <Normaltekst>{medfolgende ? 'JA' : 'NEI'}</Normaltekst>
                 </section>
               </Column>
             </Row>
