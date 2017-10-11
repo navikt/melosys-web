c7d14a89f86686ab15b78cf501376ff75e0afb68
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-11 16:12:32 +0200

    Lagt til manglende 'medlemsskap'-prop.

---
+++ src/felles-komponenter/sideOppsummering.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PT from 'prop-types';
+import EkspanderbartPanel from 'nav-frontend-ekspanderbartpanel';
+import { Knapp } from 'nav-frontend-knapper';
+import { Undertittel, Normaltekst } from 'nav-frontend-typografi';
+import { Panel } from 'nav-frontend-paneler';
+import { Container, Row, Column } from 'nav-frontend-grid';
+
+import './sideOppsummering.less';
+
+class SideOppsummering extends Component {
+  static propTypes = {
+    oppsummering: PT.object,
+  };
+  static defaultProps = {
+    oppsummering: {
+      soknadsType: 'A1',
+      summertStatus: '123456 - Under behandling',
+      arbeidsStatus: 'Arbeidsgiver',
+      arbeidsLand: 'Skal til Tyskland',
+      arbeidsPeriode: '20.11.17 - 15.10.18 (11 mnd)',
+      behandlingsType: 'Førstegangsbehandling',
+      opprettetDato: '22.09.2017',
+      medlemskap: '4833 NAV Medlemsskap',
+      status: 'Åpen',
+      statusDetalj: 'Vurderer inngangsvilkår',
+    },
+  };
+
+  render() {
+    const { soknadsType,
+      summertStatus,
+      arbeidsStatus,
+      arbeidsLand,
+      arbeidsPeriode,
+      behandlingsType,
+      opprettetDato,
+      medlemskap,
+      status,
+      statusDetalj } = this.props.oppsummering;
+
+    return (
+      <div className="tilleggsopplysninger">
+        <Panel className="saksbehandling__soknadSammendrag">
+          <Row>
+            <Column xs="12" md="6">
+              <Undertittel className="soknadSammendrag__header">Søknad om {soknadsType}</Undertittel>
+            </Column>
+            <Column xs="12" md="6">
+              <Knapp>Behandlingsmeny</Knapp>
+            </Column>
+          </Row>
+          {/* START SØKNADSSTATUS */}
+          <Row>
+            <Column xs="12">
+              {summertStatus}
+            </Column>
+          </Row>
+          {/* SLUTT SØKNADSSTATUS */}
+          {/* START ARBEIDSFORHOLD */}
+          <Row>
+            <Column xs="12">
+              <section>
+                <Normaltekst>{arbeidsStatus}</Normaltekst>
+                <Normaltekst>{arbeidsLand}</Normaltekst>
+                <Normaltekst>{arbeidsPeriode}</Normaltekst>
+              </section>
+            </Column>
+          </Row>
+          {/* SLUTT ARBEIDSFORHOLD */}
+          {/* START SØKNADSSTATUS */}
+          <Row>
+            <Column xs="12">
+              <section>
+                <Undertittel>{behandlingsType} - {medlemskap}</Undertittel>
+                <Normaltekst>Opprettet: {opprettetDato}</Normaltekst>
+                <Normaltekst>Behandlingsstatus: {status} - {statusDetalj}</Normaltekst>
+              </section>
+            </Column>
+          </Row>
+          {/* SLUTT SØKNADSSTATUS */}
+        </Panel>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = () => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SideOppsummering);
