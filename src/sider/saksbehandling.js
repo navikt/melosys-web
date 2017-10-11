28487c576a0721a322d4ae6463f05e6eadbb480c
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-11 16:02:54 +0200

    Lagt til sidepanel for søknadsoppsummering.

--- src/sider/saksbehandling.js
+++ src/sider/saksbehandling.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Container, Row, Column } from 'nav-frontend-grid';
-import { Knapp } from 'nav-frontend-knapper';
-import { Undertittel } from 'nav-frontend-typografi';
-import { Panel } from 'nav-frontend-paneler';
 import './saksbehandling.css';
 
 import Vilkarsvurdering from '../felles-komponenter/vilkarsvurdering/vilkarsvurdering';
 import Personopplysninger from '../felles-komponenter/personopplysninger';
 import Tilleggsopplysninger from '../felles-komponenter/tilleggsopplysninger';
+import SideOppsummering from '../felles-komponenter/sideOppsummering';
 
 class Saksbehandling extends Component {
   componentDidMount() {
@@ -26,16 +24,7 @@ class Saksbehandling extends Component {
               <Tilleggsopplysninger />
             </Column>
             <Column xs="5">
-              <Panel className="saksbehandling__soknadSammendrag">
-                <Row>
-                  <Column xs="12" md="6">
-                    <Undertittel className="soknadSammendrag__header">Søknad om A1</Undertittel>
-                  </Column>
-                  <Column xs="12" md="6">
-                    <Knapp>Behandlingsmeny</Knapp>
-                  </Column>
-                </Row>
-              </Panel>
+              <SideOppsummering />
             </Column>
           </Row>
         </Container>
