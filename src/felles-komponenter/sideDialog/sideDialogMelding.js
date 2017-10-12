c2d34c534d5e3d5e28d8af1a7c265c9f4c1ce7c3
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-12 14:05:17 +0200

    Grunnleggende rigg for dialog-seksjonen.

---
+++ src/felles-komponenter/sideDialog/sideDialogMelding.js
@@ -0,0 +1,18 @@
+import React  from 'react';
+import { connect } from 'react-redux';
+
+import './sideDialogMelding.css';
+
+const SideDialogMelding = function() {
+  return (
+    <div className="sideDialogMelding">
+      melding
+    </div>
+  );
+};
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = () => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SideDialogMelding);
