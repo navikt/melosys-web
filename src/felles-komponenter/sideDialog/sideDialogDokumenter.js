34884f8ac2683b6ba3bfb798662d2b1ac19066fa
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-12 16:24:44 +0200

    Diverse refactoring og mocking av data fremfor nøsting av like elementer. Diverse rydding.

--- src/felles-komponenter/sideDialog/sideDialogDokumenter.js
+++ src/felles-komponenter/sideDialog/sideDialogDokumenter.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import './sideDialogDokumenter.css';
 
 const SideDialogDokumenter = () => (
+  // Todo: Innholdet i denne komponenten er uferdig fra arkitektur og forretningssiden.
   <div className="sideDialogDokumenter">
     <Link className="lenke" to="http://www.google.com">Attest for lorem ipsum (10.10.2018)</Link>
     <Link className="lenke" to="http://www.google.com">Dokument for dolor sit amet (10.10.2018)</Link>
