41bf63f9e82ab901f0873b597f29a27f0d554aba
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-12 15:24:35 +0200

    Mindre justeringer av arkitektur, samt div linting.

--- src/felles-komponenter/sideDialog/sideDialogDokumenter.js
+++ src/felles-komponenter/sideDialog/sideDialogDokumenter.js
@@ -1,15 +1,16 @@
-import React  from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import './sideDialogDokumenter.css';
 
-const SideDialogDokumenter = function() {
-  return (
-    <div className="sideDialogDokumenter">
-      dokumenter
-    </div>
-  );
-};
+const SideDialogDokumenter = () => (
+  <div className="sideDialogDokumenter">
+    <Link className="lenke" to="http://www.google.com">Attest for lorem ipsum (10.10.2018)</Link>
+    <Link className="lenke" to="http://www.google.com">Dokument for dolor sit amet (10.10.2018)</Link>
+    <Link className="lenke" to="http://www.google.com">Dokument Lorem ipsum (10.10.2018)</Link>
+  </div>
+);
 
 const mapStateToProps = () => ({});
 
--- src/felles-komponenter/sideDialog/sideDialogMelding.js
+++ src/felles-komponenter/sideDialog/sideDialogMelding.js
@@ -1,15 +1,28 @@
-import React  from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { TextareaControlled, SkjemaGruppe } from 'nav-frontend-skjema';
+import { Knapp } from 'nav-frontend-knapper';
 
 import './sideDialogMelding.css';
 
-const SideDialogMelding = function() {
-  return (
-    <div className="sideDialogMelding">
-      melding
-    </div>
-  );
-};
+class SideDialogMelding extends Component {
+  sendMessage = event => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="sideDialogMelding">
+        <form onSubmit={this.sendMessage}>
+          <SkjemaGruppe>
+            <TextareaControlled label="Send ny melding til søker:" />
+            <Knapp type="hoved">Send melding</Knapp>
+          </SkjemaGruppe>
+        </form>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = () => ({});
 
