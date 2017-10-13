dcf29cf16b7adbda3f3ab0739f7f85c45cd9b36f
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-13 09:41:58 +0200

    Innholdsjustering etter prat med Kristoffer.

--- src/felles-komponenter/sideDialog/sideDialogHistorikk.js
+++ src/felles-komponenter/sideDialog/sideDialogHistorikk.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Snakkebolbe from 'nav-frontend-snakkeboble';
+import { Link } from 'react-router-dom';
 
 import './sideDialogHistorikk.css';
 
-const uuid = require('uuid/v4');
-
-const mockDialogObjekt = [
-  { dato: '26.04.2017 kl. 13:56', tekst: 'OK, da kan du sette bostedsadresse til arbeidsstedet til den er avklart.', avsender: 'behandler' },
-  { dato: '26.04.2017 kl. 13:56', tekst: 'Jeg har ikke noen endelig adresse ennå - arbeidsgiver ser etter mulige leiligheter.', avsender: 'soker' },
-  { dato: '26.04.2017 kl. 13:56', tekst: 'Ser at du har oppgitt bostedsadresse i Norge. Her er det ment bostedsadresse i landet du skal oppholde deg i.', avsender: 'behandler' },
-];
-
 const SideDialogHistorikk = () => (
   <div className="sideDialogHistorikk">
-    { mockDialogObjekt.map(item => (
-      <Snakkebolbe
-        ikonClass={`snakkeboble__ikon snakkeboble__ikon--${item.avsender}`}
-        dato={item.dato}
-        pilHoyre={item.avsender === 'behandler'}
-        pilVenstre={item.avsender === 'soker'}
-        key={uuid()}>{item.tekst}</Snakkebolbe>))
-    }
+    <ul>
+      <li><Link className="lenke" to="http://www.google.com">10.10.2018: Søknad oppdatert med ny informasjon</Link></li>
+      <li><Link className="lenke" to="http://www.google.com">12.10.2018: Dialog med søker</Link></li>
+      <li><Link className="lenke" to="http://www.google.com">10.10.2018: Søknad sendt</Link></li>
+    </ul>
   </div>
 );
 
