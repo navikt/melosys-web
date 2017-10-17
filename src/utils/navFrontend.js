8eb621af5a0f91f246fcd793a13e7393b73f90cb
abjolseth <abjolseth@gmail.com>
2017-10-17 20:38:08 +0200

    Nytt forslag til gruppering av nav-frontend-* components. Er ikke helt sikker på Nav. prefix, .....

---
+++ src/utils/navFrontend.js
@@ -0,0 +1,5 @@
+
+import EkspanderbartPanel from 'nav-frontend-ekspanderbartpanel';
+import { Container, Row, Column } from 'nav-frontend-grid';
+
+export { EkspanderbartPanel, Container, Row, Column };
