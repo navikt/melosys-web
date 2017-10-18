9cb0b11c6fd5c301db01d073ac538c29c1ad7c85
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-18 12:18:55 +0200

    Startet implementering av organisasjonerNorge

--- src/sider/saksbehandling.js
+++ src/sider/saksbehandling.js
@@ -8,6 +8,7 @@ import Vilkarsvurdering from '../felles-komponenter/vilkarsvurdering/vilkarsvurd
 import Personopplysninger from '../felles-komponenter/personopplysninger';
 import Tilleggsopplysninger from '../felles-komponenter/tilleggsopplysninger';
 import Medlemskap from '../felles-komponenter/medlemskap';
+import OrganisasjonerNorge from '../felles-komponenter/organisasjonerNorge';
 import SideOppsummering from '../felles-komponenter/sideOppsummering';
 import SideDialog from '../felles-komponenter/sideDialog/sideDialog';
 import SideKommentarer from '../felles-komponenter/sideKommentarer';
@@ -34,6 +35,7 @@ class Saksbehandling extends Component {
               <Personopplysninger />
               <Tilleggsopplysninger />
               <Medlemskap />
+              <OrganisasjonerNorge />
             </Column>
             <Column xs="5">
               <SideOppsummering />
