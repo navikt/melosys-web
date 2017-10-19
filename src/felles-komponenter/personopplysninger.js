af1715f0415b4a4fc42e830da9b8f7d2fc73b5e2
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-19 15:07:53 +0200

    Refactor til stateless-komponent

--- src/felles-komponenter/personopplysninger.js
+++ src/felles-komponenter/personopplysninger.js
@@ -1,72 +1,70 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import * as Nav from '../utils/navFrontend';
 import * as MPT from '../proptypes';
 
 import './personopplysninger.css';
 
-class Personopplysninger extends Component {
-  static propTypes = {
-    person: MPT.PersonPropType.isRequired,
-  };
+const Personopplysninger = props => {
+  if (Object.keys(props.person).length === 0) return (<div />);
 
-  render() {
-    if (Object.keys(this.props.person).length === 0) return (<div />);
+  const { fnr,
+    sivilstand,
+    statsborgerskap,
+    kjoenn,
+    fornavn,
+    etternavn,
+    sammensattNavn,
+    foedselsdato,
+    bostedsadresse } = props.person;
 
-    const { fnr,
-      sivilstand,
-      statsborgerskap,
-      kjoenn,
-      fornavn,
-      etternavn,
-      sammensattNavn,
-      foedselsdato,
-      bostedsadresse } = this.props.person;
+  // Påkrevde felter fra API
+  const { poststed, postnr, land, gateadresse: { gatenavn, husnummer, husbokstav = '' } } = bostedsadresse;
 
-    // Påkrevde felter fra API
-    const { poststed, postnr, land, gateadresse: { gatenavn, husnummer, husbokstav = '' } } = bostedsadresse;
+  return (
+    <div className="personopplysninger panelSeksjon">
+      <Nav.EkspanderbartPanel tittel={sammensattNavn} apen>
+        <Nav.Container fluid>
+          {/* START PERSONINFO */}
+          <Nav.Row className="person__seksjon">
+            <Nav.Column xs="6">
+              <dl className="person__detaljer">
+                <dt>Fornavn:</dt><dd>{fornavn}</dd>
+                <dt>Etternavn:</dt><dd>{etternavn}</dd>
+                <dt>Fødselsnummer:</dt><dd>{fnr}</dd>
+                <dt>Kjønn:</dt><dd>{kjoenn}</dd>
+              </dl>
+            </Nav.Column>
+            <Nav.Column xs="6">
+              <dl className="person__detaljer">
+                <dt>Fødselsdato:</dt><dd>{foedselsdato}</dd>
+                <dt>Statsborgerskap:</dt><dd>{statsborgerskap}</dd>
+                <dt>Sivilstand:</dt><dd>{sivilstand}</dd>
+              </dl>
+            </Nav.Column>
+          </Nav.Row>
+          {/* SLUTT PERSONINFO */}
+          {/* START ADRESSE */}
+          <Nav.Row className="person__seksjon">
+            <Nav.Column xs="6">
+              <dl className="person__detaljer">
+                <dt>Bostedsadresse</dt>
+                <dd>{`${gatenavn} ${husnummer} ${husbokstav}`}</dd>
+                <dd>{`${postnr} ${poststed}`}</dd>
+                <dd>{`${land}`}</dd>
+              </dl>
+            </Nav.Column>
+          </Nav.Row>
+          {/* SLUTT ADRESSE */}
+        </Nav.Container>
+      </Nav.EkspanderbartPanel>
+    </div>
+  );
+};
 
-    return (
-      <div className="personopplysninger panelSeksjon">
-        <Nav.EkspanderbartPanel tittel={sammensattNavn} apen>
-          <Nav.Container fluid>
-            {/* START PERSONINFO */}
-            <Nav.Row className="person__seksjon">
-              <Nav.Column xs="6">
-                <dl className="person__detaljer">
-                  <dt>Fornavn:</dt><dd>{fornavn}</dd>
-                  <dt>Etternavn:</dt><dd>{etternavn}</dd>
-                  <dt>Fødselsnummer:</dt><dd>{fnr}</dd>
-                  <dt>Kjønn:</dt><dd>{kjoenn}</dd>
-                </dl>
-              </Nav.Column>
-              <Nav.Column xs="6">
-                <dl className="person__detaljer">
-                  <dt>Fødselsdato:</dt><dd>{foedselsdato}</dd>
-                  <dt>Statsborgerskap:</dt><dd>{statsborgerskap}</dd>
-                  <dt>Sivilstand:</dt><dd>{sivilstand}</dd>
-                </dl>
-              </Nav.Column>
-            </Nav.Row>
-            {/* SLUTT PERSONINFO */}
-            {/* START ADRESSE */}
-            <Nav.Row className="person__seksjon">
-              <Nav.Column xs="6">
-                <dl className="person__detaljer">
-                  <dt>Bostedsadresse</dt>
-                  <dd>{`${gatenavn} ${husnummer} ${husbokstav}`}</dd>
-                  <dd>{`${postnr} ${poststed}`}</dd>
-                  <dd>{`${land}`}</dd>
-                </dl>
-              </Nav.Column>
-            </Nav.Row>
-            {/* SLUTT ADRESSE */}
-          </Nav.Container>
-        </Nav.EkspanderbartPanel>
-      </div>
-    );
-  }
-}
+Personopplysninger.propTypes = {
+  person: MPT.PersonPropType.isRequired,
+};
 
 const mapStateToProps = () => ({});
 
