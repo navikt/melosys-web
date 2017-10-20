947c5c3b1386d5c4cbe87fc77d301fbbca7e23ba
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-20 13:01:06 +0200

    Lagt til flere felter for personopplysninger.

--- src/felles-komponenter/personopplysninger.js
+++ src/felles-komponenter/personopplysninger.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import * as Nav from '../utils/navFrontend';
 import * as MPT from '../proptypes';
+import moment from 'moment';
 
 import './personopplysninger.css';
+
+const uuid = require('uuid/v4');
 
 function Personopplysninger(props) {
   if (Object.keys(props.person).length === 0) return (<div />);
 
-  const { fnr,
+  const {
+    fnr,
     sivilstand,
     statsborgerskap,
     kjoenn,
@@ -15,22 +19,27 @@ function Personopplysninger(props) {
     etternavn,
     sammensattNavn,
     foedselsdato,
-    bostedsadresse } = props.person;
+    bostedsadresse,
+    mobiltelefon,
+    epost,
+    familerelasjon: { ektefelle, barn } } = props.person;
 
   // Påkrevde felter fra API
   const { poststed, postnr, land, gateadresse: { gatenavn, husnummer, husbokstav = '' } } = bostedsadresse;
+  const aar = moment().diff(foedselsdato, 'years');
+
 
   return (
     <div className="personopplysninger panelSeksjon">
-      <Nav.EkspanderbartPanel tittel={sammensattNavn} apen>
+      <Nav.EkspanderbartPanel tittel={`${sammensattNavn} (${aar})`}>
         <Nav.Container fluid>
           {/* START PERSONINFO */}
           <Nav.Row className="person__seksjon">
             <Nav.Column xs="6">
               <dl className="person__detaljer">
+                <dt>Fødselsnummer:</dt><dd>{fnr}</dd>
                 <dt>Fornavn:</dt><dd>{fornavn}</dd>
                 <dt>Etternavn:</dt><dd>{etternavn}</dd>
-                <dt>Fødselsnummer:</dt><dd>{fnr}</dd>
                 <dt>Kjønn:</dt><dd>{kjoenn}</dd>
               </dl>
             </Nav.Column>
@@ -51,6 +60,16 @@ function Personopplysninger(props) {
                 <dd>{`${gatenavn} ${husnummer} ${husbokstav}`}</dd>
                 <dd>{`${postnr} ${poststed}`}</dd>
                 <dd>{`${land}`}</dd>
+                <dt>Mobil:</dt><dd>{mobiltelefon}</dd>
+                <dt>E-post:</dt><dd>{epost}</dd>
+              </dl>
+            </Nav.Column>
+            <Nav.Column xs="6">
+              <dl className="person__detaljer">
+                <dt>Ektefelle:</dt><dd>{ektefelle.sammensattNavn} ({ektefelle.alder})</dd>
+                <dt>Barn:</dt>
+                { barn.map(item => <dd key={uuid()}>{item.sammensattNavn} ({item.alder})</dd>)}
+
               </dl>
             </Nav.Column>
           </Nav.Row>
