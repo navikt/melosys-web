e413e6a2b43c03a35aada6a446f568a6dee7dc79
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-24 15:47:59 +0200

    Refactor av DataOmrade til egen fil for gjenbruk. Refactor av Arbeidsforholdene og Medlemskap tilsvarende.

---
+++ src/felles-komponenter/datoOmrade/datoOmrade.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import * as Nav from '../../utils/navFrontend';
+import * as MPT from '../../proptypes';
+
+/** Dato-område som viser fra- og til-dato.
+ *
+ * @param tittel
+ * @param dato
+ * @constructor
+ */
+const DatoOmrade = ({ periode }) => (
+  <Nav.Row>
+    <Nav.Column xs="6" className="blokk-xs"><Nav.Element>Fra</Nav.Element>{periode.fom || '-'}</Nav.Column>
+    <Nav.Column xs="6" className="blokk-xs"><Nav.Element>Til</Nav.Element>{periode.tom || '-'}</Nav.Column>
+  </Nav.Row>
+);
+
+DatoOmrade.propTypes = {
+  periode: MPT.Periode.isRequired,
+};
+
+export default DatoOmrade;
