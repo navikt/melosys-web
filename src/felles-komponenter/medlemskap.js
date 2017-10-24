92be639d4a06b62601103ec57ce2ad000cd3726e
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-24 08:29:58 +0200

    Gjort en refactor av DatoOmrade. Kan vurderes å løftes ut som egen komponent-fil hvis det er hensiktsmessig.

--- src/felles-komponenter/medlemskap.js
+++ src/felles-komponenter/medlemskap.js
@@ -7,24 +7,22 @@ import './medlemskap.css';
 
 const uuid = require('uuid/v4');
 
-/** Dato-område
+/** Dato-område med mulighet for Periode og custom Titler,
  *
- * @param tittel
- * @param dato
- * @constructor
+ * @param props.priode Object Periode i formatet {fom:'',tom:''}
+ * @param props.titler Object Tittel i formatet {fom:'',tom:''}
+ * Todo 24/10/17: Vurder å løfte denne komponenten ut som en egen fil for gjenburk andre steder om hensiktsmessig.
  */
-const DatoOmrade = ({ tittel1, tittel2, dato1, dato2 }) => (
+const DatoOmrade = ({ periode, titler }) => (
   <Nav.Row>
-    <Nav.Column xs="6" className="blokk-xs"><Nav.Element>{tittel1}</Nav.Element>{dato1}</Nav.Column>
-    <Nav.Column xs="6" className="blokk-xs"><Nav.Element>{tittel2}</Nav.Element>{dato2}</Nav.Column>
+    <Nav.Column xs="6" className="blokk-xs"><Nav.Element>{titler.fom}</Nav.Element>{periode.fom}</Nav.Column>
+    <Nav.Column xs="6" className="blokk-xs"><Nav.Element>{titler.tom}</Nav.Element>{periode.tom}</Nav.Column>
   </Nav.Row>
 );
 
 DatoOmrade.propTypes = {
-  tittel1: PT.string.isRequired,
-  tittel2: PT.string.isRequired,
-  dato1: PT.string.isRequired,
-  dato2: PT.string.isRequired,
+  periode: MPT.Periode.isRequired,
+  titler: PT.shape({ fom: PT.string, tom: PT.string }).isRequired,
 };
 
 /** MedlemskapsSeksjon inneholdet ett enkelt medlemskap. Hver søker kan ha
@@ -47,13 +45,15 @@ function MedlemskapPeriode({ medlemskapPeriode }) {
     register,
   } = medlemskapPeriode;
 
+  const registreringsDato = { fom: dato.registrert, tom: dato.besluttet };
+
   return (
     <div className="medlemskap__enkelt" aria-label="Enkeltmedlemsskap">
       <Nav.Row>
         {/* START DATO RANGE */}
         <Nav.Column xs="6" lg="5">
-          <DatoOmrade tittel1="Fra" dato1={periode.fom} tittel2="Til" dato2={periode.tom} />
-          <DatoOmrade tittel1="Registrert" dato1={dato.registrert} tittel2="Besluttet" dato2={dato.besluttet} />
+          <DatoOmrade periode={periode} titler={{ fom: 'Fra', tom: 'Til' }} />
+          <DatoOmrade periode={registreringsDato} titler={{ fom: 'Registrert', tom: 'Besluttet' }} />
         </Nav.Column>
         {/* SLUTT DATO RANGE */}
 
