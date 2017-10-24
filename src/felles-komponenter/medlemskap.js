b910e74d69ea6fc098e989bb549657a38ace3191
Terje Karlsen <terjeofnorway@gmail.com>
2017-10-24 09:38:37 +0200

    Refactor av DatoOmrade til en mer spisset funksjonalitet for Periode. Fjernet overflødig prop-types-import og justert kommentarer.

--- src/felles-komponenter/medlemskap.js
+++ src/felles-komponenter/medlemskap.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PT from 'prop-types';
 import * as Nav from '../utils/navFrontend';
 
 import * as MPT from '../proptypes';
@@ -7,22 +6,20 @@ import './medlemskap.css';
 
 const uuid = require('uuid/v4');
 
-/** Dato-område med mulighet for Periode og custom Titler,
+/** Dato-område med mulighet for Periode.
  *
- * @param props.priode Object Periode i formatet {fom:'',tom:''}
- * @param props.titler Object Tittel i formatet {fom:'',tom:''}
+ * @param props.periode Object Periode i formatet {fom:'',tom:''}
  * Todo 24/10/17: Vurder å løfte denne komponenten ut som en egen fil for gjenburk andre steder om hensiktsmessig.
  */
-const DatoOmrade = ({ periode, titler }) => (
+const DatoOmrade = ({ periode }) => (
   <Nav.Row>
-    <Nav.Column xs="6" className="blokk-xs"><Nav.Element>{titler.fom}</Nav.Element>{periode.fom}</Nav.Column>
-    <Nav.Column xs="6" className="blokk-xs"><Nav.Element>{titler.tom}</Nav.Element>{periode.tom}</Nav.Column>
+    <Nav.Column xs="6" className="blokk-xs"><Nav.Element>Fra</Nav.Element>{periode.fom}</Nav.Column>
+    <Nav.Column xs="6" className="blokk-xs"><Nav.Element>Til</Nav.Element>{periode.tom}</Nav.Column>
   </Nav.Row>
 );
 
 DatoOmrade.propTypes = {
   periode: MPT.Periode.isRequired,
-  titler: PT.shape({ fom: PT.string, tom: PT.string }).isRequired,
 };
 
 /** MedlemskapsSeksjon inneholdet ett enkelt medlemskap. Hver søker kan ha
@@ -45,15 +42,16 @@ function MedlemskapPeriode({ medlemskapPeriode }) {
     register,
   } = medlemskapPeriode;
 
-  const registreringsDato = { fom: dato.registrert, tom: dato.besluttet };
-
   return (
     <div className="medlemskap__enkelt" aria-label="Enkeltmedlemsskap">
       <Nav.Row>
         {/* START DATO RANGE */}
         <Nav.Column xs="6" lg="5">
           <DatoOmrade periode={periode} titler={{ fom: 'Fra', tom: 'Til' }} />
-          <DatoOmrade periode={registreringsDato} titler={{ fom: 'Registrert', tom: 'Besluttet' }} />
+          <Nav.Row>
+            <Nav.Column xs="6" className="blokk-xs"><Nav.Element>Registrert</Nav.Element>{dato.registrert}</Nav.Column>
+            <Nav.Column xs="6" className="blokk-xs"><Nav.Element>Besluttet</Nav.Element>{dato.besluttet}</Nav.Column>
+          </Nav.Row>
         </Nav.Column>
         {/* SLUTT DATO RANGE */}
 
