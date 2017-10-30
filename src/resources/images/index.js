0fc690f1d291355df9e573d0aaa02209554c8b3c
abjolseth <abjolseth@gmail.com>
2017-10-30 22:59:43 +0100

    Added namespace, and optimied import exports modules.

--- src/resources/images/index.js
+++ src/resources/images/index.js
@@ -1,13 +1,13 @@
-import * as Ubehandlet from './ikon-steg-ubehandlet.svg';
-import * as Aktivt from './ikon-steg-aktivt.svg';
-import * as Ferdig from './ikon-steg-ferdig.svg';
-import * as Varsel from './ikon-steg-varsel.svg';
-import * as Feil from './ikon-steg-feil.svg';
-import * as VedtakAvslatt from './ikon-vedtak-avslatt.svg';
-import * as VedakUbehandlet from './ikon-vedtak-ubehandlet.svg';
-import * as VedtakGodkjent from './ikon-vedtak-godkjent.svg';
-import * as Mann from './ikon-mann.svg';
-import * as Kvinne from './ikon-kvinne.svg';
+import Ubehandlet from './ikon-steg-ubehandlet.svg';
+import Aktivt from './ikon-steg-aktivt.svg';
+import Ferdig from './ikon-steg-ferdig.svg';
+import Varsel from './ikon-steg-varsel.svg';
+import Feil from './ikon-steg-feil.svg';
+import VedtakAvslatt from './ikon-vedtak-avslatt.svg';
+import VedakUbehandlet from './ikon-vedtak-ubehandlet.svg';
+import VedtakGodkjent from './ikon-vedtak-godkjent.svg';
+import Mann from './ikon-mann.svg';
+import Kvinne from './ikon-kvinne.svg';
 
 export { Ubehandlet };
 export { Aktivt };
