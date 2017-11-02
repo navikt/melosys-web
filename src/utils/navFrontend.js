6a2c148e5b2bb6d553440160cc43225ef276502e
Terje Karlsen <terjeofnorway@gmail.com>
2017-11-02 10:59:17 +0100

    Endret importrekkefølge for enklere commit-sammenlikning.

--- src/utils/navFrontend.js
+++ src/utils/navFrontend.js
@@ -1,14 +1,14 @@
 import EkspanderbartPanel from 'nav-frontend-ekspanderbartpanel';
 import { Panel } from 'nav-frontend-paneler';
 import { Container, Row, Column } from 'nav-frontend-grid';
-import { Input, Checkbox, Radio, Fieldset, Select } from 'nav-frontend-skjema';
+import { Checkbox, Radio, Fieldset, Select, Input } from 'nav-frontend-skjema';
 import { Innholdstittel, Systemtittel, Undertittel, Element, Normaltekst, UndertekstBold } from 'nav-frontend-typografi';
 import { Knapp } from 'nav-frontend-knapper';
 
 export {
   EkspanderbartPanel, Panel,
   Container, Row, Column,
-  Input, Checkbox, Radio, Fieldset, Select,
+  Checkbox, Radio, Fieldset, Select, Input,
   Innholdstittel, Systemtittel, Undertittel, Element, Normaltekst, UndertekstBold,
   Knapp,
 };
