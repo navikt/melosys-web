ed820e9f6672caac498dfd1ff048b51e82c279d6
Terje Karlsen <terjeofnorway@gmail.com>
2017-11-02 10:25:29 +0100

    Rettet feil i VurderingLand. Vurderingsprosess er nå linjær. Endret aktivt-ikon til sirkel med utropstegn.

--- src/felles-komponenter/vilkarsveileder/faneinnhold/vurderingSektor.js
+++ src/felles-komponenter/vilkarsveileder/faneinnhold/vurderingSektor.js
@@ -8,11 +8,12 @@ const VurderingSektor = props => {
   return (
     <div>
       <Nav.Undertittel>Vurdering:</Nav.Undertittel>
-      <Nav.Fieldset legend="Gjelder én eller flere av disse for søkeren?">
-        <Nav.Checkbox id="steg1_ansatt_offentlig" label="Offentlig tjenesteperson (relevant for 11.3 b)" />
-        <Nav.Checkbox id="steg1_ansatt_skip" label="Ansatt på skip (relevant for 11.4" />
-        <Nav.Checkbox id="steg1_ansatt_sokkel" label="Ansatt på sokkel (relevant for 11.3 a)" />
-        <Nav.Checkbox id="steg1_ansatt_flyvende" label="Flyvende personell (relevant for 11.5)" />
+      <Nav.Fieldset legend="Hva gjelder for søkeren?">
+        <Nav.Radio name="ansatt-sektor" id="steg1_ansatt_offentlig" label="Offentlig tjenesteperson (relevant for 11.3 b)" />
+        <Nav.Radio name="ansatt-sektor" id="steg1_ansatt_skip" label="Ansatt på skip (relevant for 11.4" />
+        <Nav.Radio name="ansatt-sektor" id="steg1_ansatt_sokkel" label="Ansatt på sokkel (relevant for 11.3 a)" />
+        <Nav.Radio name="ansatt-sektor" id="steg1_ansatt_flyvende" label="Flyvende personell (relevant for 11.5)" />
+        <Nav.Radio name="ansatt-sektor" id="steg1_ansatt_ingen" label="Ingen av disse" />
       </Nav.Fieldset>
       <div className="fane__knapplinje">
         <Nav.Knapp type="hoved" className="fane__navigasjonsknapp" onClick={bekreftOgFortsett}>Bekreft og fortsett</Nav.Knapp>
