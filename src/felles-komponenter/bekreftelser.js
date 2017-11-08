4ad5dd7a996fc20e7affb8d2a63c2f683b0b5dee
Terje Karlsen <terjeofnorway@gmail.com>
2017-11-08 09:04:13 +0100

    Refactor av arbeidsavtale.

--- src/felles-komponenter/bekreftelser.js
+++ src/felles-komponenter/bekreftelser.js
@@ -3,11 +3,9 @@ import * as Nav from '../utils/navFrontend';
 import * as MPT from '../proptypes/';
 
 import EnkeltDato from '../felles-komponenter/datoOmrade/enkeltDato';
+import { boolTilNorsk } from '../utils/utils';
 import './bekreftelser.css';
 
-function boolTilNorsk (value) {
-  return value ? 'JA' : 'NEI';
-}
 
 function Bekreftelser ({ bekreftelser }) {
   const {
--- src/proptypes/arbeidsavtale.js
+++ src/proptypes/arbeidsavtale.js
@@ -5,9 +5,6 @@ const ArbeidsavtalePropType = PT.shape({
   arbeidstidsordning: PT.string,
   avloenningstype: PT.string,
   yrke: PT.string,
-  avtaltArbeidstimerPerUke: PT.number,
-  stillingsprosent: PT.number,
-  sisteLoennsendringsdato: PT.string,
   beregnetAntallTimerPrUke: PT.number,
   endringsdatoStillingsprosent: PT.string,
 });
