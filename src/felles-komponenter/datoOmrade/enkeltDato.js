c097c4212e5cda9a30629ae07b2caf0264e4c6ed
Terje Karlsen <terjeofnorway@gmail.com>
2017-11-16 16:58:13 +0100

    Komponenten EnkeltDato formatterer nå datoer riktig.

--- src/felles-komponenter/datoOmrade/enkeltDato.js
+++ src/felles-komponenter/datoOmrade/enkeltDato.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import PT from 'prop-types';
+import moment from 'moment';
 
 /** EnkeltDato gjør det lettere å følge UU der datoer skal benyttes i tillegg til at
  * en konsekvent "-" vises der dato er ukjent eller ikke relevant.
- *
- * @param props.dato
  * @constructor
  */
 function EnkeltDato(props) {
-  const { dato } = props;
-  return (dato ? <time dateTime={dato}>{dato}</time> : '-');
+  const { dato, visTidspunkt } = props;
+  const momentFormat = visTidspunkt ? 'DD.MM.YYYY HH:MM' : 'DD.MM.YYYY';
+
+  const lesbarDato = moment(dato).format(momentFormat);
+  return (dato ? <time dateTime={dato}>{lesbarDato}</time> : '-');
 }
 
 EnkeltDato.propTypes = {
   dato: PT.string,
+  visTidspunkt: PT.bool,
 };
 
 EnkeltDato.defaultProps = {
   dato: '',
+  visTidspunkt: false,
 };
 
 export default EnkeltDato;
