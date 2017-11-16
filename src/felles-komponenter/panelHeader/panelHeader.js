78cf07287eb559b3d8764f3fb4e60cb439167e28
Terje Karlsen <terjeofnorway@gmail.com>
2017-11-16 16:55:51 +0100

    Lagt til ikon for kjønn, samt undertittel.

---
+++ src/felles-komponenter/panelHeader/panelHeader.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PT from 'prop-types';
+import * as Nav from '../../utils/navFrontend';
+import './panelHeader.css';
+
+
+const PanelHeader = ({ ikon, tittel, undertittel }) => (
+  <div className="panelheader">
+    <div className="panelheader__ikon" style={{ backgroundImage: `url('${ikon}')` }} />
+    <div className="panelheader__tittel">
+      <Nav.Undertittel className="panelheader__tittel__hoved">{tittel}</Nav.Undertittel>
+      <span className="panelheader__tittel__under">{undertittel}</span>
+    </div>
+  </div>
+);
+
+PanelHeader.propTypes = {
+  tittel: PT.string.isRequired,
+  ikon: PT.string,
+  undertittel: PT.string,
+};
+
+PanelHeader.defaultProps = {
+  ikon: null,
+  undertittel: '',
+};
+
+export default PanelHeader;
