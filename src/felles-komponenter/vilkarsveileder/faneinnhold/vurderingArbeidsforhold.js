2628caed0175ba954ae6cca76b1b30501e159c48
Terje Karlsen <terjeofnorway@gmail.com>
2017-11-20 15:07:05 +0100

    Lister ut alle arbeidsforhold for bort-velging.

--- src/felles-komponenter/vilkarsveileder/faneinnhold/vurderingArbeidsforhold.js
+++ src/felles-komponenter/vilkarsveileder/faneinnhold/vurderingArbeidsforhold.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import PT from 'prop-types';
+
 import * as Nav from '../../../utils/navFrontend';
+import * as MPT from '../../../proptypes';
+
+import EnkeltDato from '../../datoOmrade/enkeltDato';
+
+import './vurderingArbeidsforhold.css';
+
+const uuid = require('uuid/v4');
+
+const ArbeidsforholdLinje = ({ arbeidsforhold }) => (
+  <div key={uuid()} className="arbeidsforhold__enkeltlinje">
+    <div className="arbeidsforhold__enkeltlinje__navn">{ arbeidsforhold.arbeidsgiver.navn }</div>
+    <div className="arbeidsforhold__enkeltlinje__periode">Periode: <EnkeltDato dato={arbeidsforhold.ansettelsesPeriode.fom} /> - <EnkeltDato dato={arbeidsforhold.ansettelsesPeriode.tom} /></div>
+    <button className="arbeidsforhold__enkeltlinje__knapp" onClick={() => {}}>-</button>
+  </div>
+);
+
+ArbeidsforholdLinje.propTypes = {
+  arbeidsforhold: MPT.Arbeidsforhold.isRequired,
+};
 
 const VurderingArbeidsforhold = props => {
-  const { bekreftOgFortsett } = props;
+  const { bekreftOgFortsett, arbeidsforholdene } = props;
 
   return (
-    <div>
+    <div className="vurderingarbeidsforhold">
       <Nav.Undertittel>Velg relevante arbeidsforhold:</Nav.Undertittel>
+      <div>
+        {arbeidsforholdene.map(arbeidsforhold => <ArbeidsforholdLinje key={uuid()} arbeidsforhold={arbeidsforhold} />)}
+      </div>
       <div className="fane__knapplinje">
         <Nav.Knapp type="hoved" className="fane__navigasjonsknapp" onClick={bekreftOgFortsett}>Bekreft og fortsett</Nav.Knapp>
       </div>
@@ -17,6 +40,7 @@ const VurderingArbeidsforhold = props => {
 
 VurderingArbeidsforhold.propTypes = {
   bekreftOgFortsett: PT.func.isRequired,
+  arbeidsforholdene: MPT.Arbeidsforholdene.isRequired,
 };
 
 
