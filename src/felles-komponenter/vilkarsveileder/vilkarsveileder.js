015bebc0fd231e9bbf2613fcf6df2e18416b2b25
Terje Karlsen <terjeofnorway@gmail.com>
2017-11-23 15:01:03 +0100

    Satt aktivt steg til 0

--- src/felles-komponenter/vilkarsveileder/vilkarsveileder.js
+++ src/felles-komponenter/vilkarsveileder/vilkarsveileder.js
@@ -69,7 +69,7 @@ class Vilkarsveileder extends Component {
 
   componentWillMount() {
     this.setState({
-      aktivtSteg: 3,
+      aktivtSteg: 0,
       steg: [
         { status: Vilkarsveileder.status.AKTIV, id: 0, ikoner: Vilkarsveileder.stegIkoner, tilgjengelig: true },
         { status: Vilkarsveileder.status.UBEHANDLET, id: 1, ikoner: Vilkarsveileder.stegIkoner, tilgjengelig: false },
