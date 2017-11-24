b717a347dac378a8eece636d464c9a0b0bc749cb
Terje Karlsen <terjeofnorway@gmail.com>
2017-11-24 15:35:37 +0100

    Fikset proptypes issue i Header (vil skape konflikt ved merge). Lagt inn sjekk på gyldig form.

--- src/felles-komponenter/skjema/validering/index.js
+++ src/felles-komponenter/skjema/validering/index.js
@@ -1,9 +1,12 @@
 import { adresseKreves, norskPostNummer } from './adresse';
 import { minLengde, erPakrevet } from './generelt';
+import { fulltNavn, fnr } from './person';
 
 export {
   adresseKreves,
   norskPostNummer,
   minLengde,
   erPakrevet,
+  fulltNavn,
+  fnr,
 };
