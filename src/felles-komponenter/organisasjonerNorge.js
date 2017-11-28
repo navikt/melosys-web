a74ee66884462ea67f442efa2177add80e45f8cb
Terje Karlsen <terjeofnorway@gmail.com>
2017-11-28 10:25:14 +0100

    Justert arbeidsgiver i Norge. Diverse styling-justeringer.

--- src/felles-komponenter/organisasjonerNorge.js
+++ src/felles-komponenter/organisasjonerNorge.js
@@ -16,18 +16,16 @@ const EnkeltOrganisasjon = ({ organisasjon }) => {
   const { gatenavn } = gateadresse;
 
   return (
-    <Nav.Row className="enkeltorganisasjon__seksjon">
-      <Nav.Column xs="6">
-        <dl className="enkeltorganisasjon__detaljer">
-          <dt>Navn</dt>
-          <dd>{navn || '-'}</dd>
-          <dt>Org. nr / Id. nr</dt>
-          <dd>{orgnr || '-'}</dd>
-          <dt>Besøksadresse</dt>
-          <dd>{gatenavn}<br />{postnr} {land}<br /></dd>
-        </dl>
-      </Nav.Column>
-    </Nav.Row>
+    <Nav.Column xs="6">
+      <dl className="enkeltorganisasjon__detaljer">
+        <dt>Firmanavn</dt>
+        <dd>{navn || '-'}</dd>
+        <dt>Org. nr / Id. nr</dt>
+        <dd>{orgnr || '-'}</dd>
+        <dt>Besøksadresse</dt>
+        <dd>{gatenavn}<br />{postnr} {land}<br /></dd>
+      </dl>
+    </Nav.Column>
   );
 };
 
@@ -42,7 +40,9 @@ function OrganisasjonerNorge ({ organisasjoner }) {
         heading={<PanelHeader ikon={Ikoner.Ferdig} tittel="Arbeidsgiver i Norge" undertittel="" />}
         ariaTittel="Panel for arbeidsgiver i Norge" >
         <Nav.Container fluid>
-          {organisasjoner.map(organisasjon => <EnkeltOrganisasjon key={uuid()} organisasjon={organisasjon} />)}
+          <Nav.Row className="enkeltorganisasjon__seksjon">
+            {organisasjoner.map(organisasjon => <EnkeltOrganisasjon key={uuid()} organisasjon={organisasjon} />)}
+          </Nav.Row>
         </Nav.Container>
       </Nav.EkspanderbartpanelBase>
     </div>
