0327ad278fa1e8e2a49cdcdbef37b9709af694ae
Terje Karlsen <terjeofnorway@gmail.com>
2017-12-06 08:08:48 +0100

    Knyttet input[radio] til redux soknad store.

--- src/felles-komponenter/bekreftelser.js
+++ src/felles-komponenter/bekreftelser.js
@@ -3,6 +3,7 @@ import React from 'react';
 import * as Nav from '../utils/navFrontend';
 import * as MPT from '../proptypes/';
 import * as Ikoner from '../resources/images';
+import * as Skjema from './skjema';
 
 import PanelHeader from '../felles-komponenter/panelHeader/panelHeader';
 import DatoFelt from './skjema/datofelt';
@@ -20,40 +21,40 @@ function Bekreftelser () {
           <Nav.Column xs="12">
             <Nav.Fieldset legend="Arbeidsgiver bekrefter at arbeidstaker er utsendt?">
               <div className="skjema__horisontalefelter">
-                <Nav.Radio name="arbeidsgiverBekrefterUtsendelse" label="Ja" />
-                <Nav.Radio name="arbeidsgiverBekrefterUtsendelse" label="nei" />
+                <Skjema.Radio feltNavn="arbeidsgiverBekrefterUtsendelse" label="ja" value="ja" />
+                <Skjema.Radio feltNavn="arbeidsgiverBekrefterUtsendelse" label="nei" value="nei" />
               </div>
             </Nav.Fieldset>
             <Nav.Fieldset legend="Er arbeidstaker ansatt under utsendelsen?">
               <div className="skjema__horisontalefelter">
-                <Nav.Radio name="arbeidstakerAnsattUnderUtsendelsen" label="Ja" />
-                <Nav.Radio name="arbeidstakerAnsattUnderUtsendelsen" label="nei" />
+                <Skjema.Radio feltNavn="arbeidstakerAnsattUnderUtsendelsen" label="ja" value="ja" />
+                <Skjema.Radio feltNavn="arbeidstakerAnsattUnderUtsendelsen" label="nei" value="nei" />
               </div>
             </Nav.Fieldset>
             <Nav.Fieldset legend="Erstatter arbeidstakeren en eller flere utsendte?">
               <div className="skjema__horisontalefelter">
-                <Nav.Radio name="erstatterArbeidstakerenUtsendte" label="Ja" />
-                <Nav.Radio name="erstatterArbeidstakerenUtsendte" label="nei" />
+                <Skjema.Radio feltNavn="erstatterArbeidstakerenUtsendte" label="ja" value="ja" />
+                <Skjema.Radio feltNavn="erstatterArbeidstakerenUtsendte" label="nei" value="nei" />
               </div>
             </Nav.Fieldset>
             <Nav.Fieldset legend="Er arbeidstaker tidligere utsendt i en periode over 24 mnd?">
               <div className="skjema__horisontalefelter">
-                <Nav.Radio name="arbeidstakerTidligereUtsendt24Mnd" label="Ja" />
-                <Nav.Radio name="arbeidstakerTidligereUtsendt24Mnd" label="nei" />
+                <Skjema.Radio feltNavn="arbeidstakerTidligereUtsendt24Mnd" label="ja" value="ja" />
+                <Skjema.Radio feltNavn="arbeidstakerTidligereUtsendt24Mnd" label="nei" value="nei" />
               </div>
             </Nav.Fieldset>
             <Nav.Fieldset legend="Plikter arbeidsgiver å betale arbeidsgiveravgift?">
               <div className="skjema__horisontalefelter">
-                <Nav.Radio name="arbeidsgiverBetalerArbeidsgiveravgift" label="Ja" />
-                <Nav.Radio name="arbeidsgiverBetalerArbeidsgiveravgift" label="nei" />
+                <Skjema.Radio feltNavn="arbeidsgiverBetalerArbeidsgiveravgift" label="ja" value="ja" />
+                <Skjema.Radio feltNavn="arbeidsgiverBetalerArbeidsgiveravgift" label="nei" value="nei" />
               </div>
             </Nav.Fieldset>
             <Nav.Fieldset legend="Blir trygdeavgift trukket gjennom skatten under utenlandsoppholdet?">
               <div className="skjema__horisontalefelter">
-                <Nav.Radio name="trygdeavgiftTrukketGjennomSkatt" label="Ja" />
-                <Nav.Radio name="trygdeavgiftTrukketGjennomSkatt" label="nei" />
+                <Skjema.Radio feltNavn="trygdeavgiftTrukketGjennomSkatt" label="ja" value="ja" />
+                <Skjema.Radio feltNavn="trygdeavgiftTrukketGjennomSkatt" label="nei" value="nei" />
               </div>
-              <DatoFelt name="trygdeavgiftTrukketGjennomSkattDato" label="Til dato:" />
+              <DatoFelt feltNavn="trygdeavgiftTrukketGjennomSkattDato" label="Til dato:" />
             </Nav.Fieldset>
           </Nav.Column>
         </Nav.Row>
