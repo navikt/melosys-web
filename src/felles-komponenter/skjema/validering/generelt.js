f2be4b6369d34206c0d6f55ffbf99c7169b4a8bc
Terje Karlsen <terjeofnorway@gmail.com>
2017-12-06 13:22:35 +0100

    Lagt på strengere moment-validering.

--- src/felles-komponenter/skjema/validering/generelt.js
+++ src/felles-komponenter/skjema/validering/generelt.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 const minLengde = (value, min) => (value && value.length < min ? `Minimum ${min} tegn kreves` : null);
 const erPakrevet = value => (!value ? 'Dette feltet er påkrevet.' : null);
 const kunTall = value => (isNaN(value) ? 'Skriv inn kun hele tall' : null);
-const erDato = value => (!moment(value, 'DD.MM.YYYY').isValid() ? 'Skriv inn korrekt dato.' : null);
+const erDato = value => (!moment(value, 'DD.MM.YYYY', true).isValid() ? 'Skriv inn korrekt dato.' : null);
 
 export {
   minLengde,
