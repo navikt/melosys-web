c22756c4f7a7ac18922c8f40bd39a3b446b80971
Terje Karlsen <terjeofnorway@gmail.com>
2017-12-07 07:41:15 +0100

    Refactor til RadioGruppe for korrekt validering.

--- src/utils/navFrontend.js
+++ src/utils/navFrontend.js
@@ -1,14 +1,14 @@
 import { EkspanderbartpanelBase } from 'nav-frontend-ekspanderbartpanel';
 import { Panel } from 'nav-frontend-paneler';
 import { Container, Row, Column } from 'nav-frontend-grid';
-import { Checkbox, Radio, Fieldset, Select, Input, Textarea } from 'nav-frontend-skjema';
+import { Checkbox, Radio, RadioGruppe, Fieldset, Select, Input, Textarea } from 'nav-frontend-skjema';
 import { Innholdstittel, Systemtittel, Undertittel, Element, Normaltekst, UndertekstBold } from 'nav-frontend-typografi';
 import { Knapp } from 'nav-frontend-knapper';
 
 export {
   EkspanderbartpanelBase, Panel,
   Container, Row, Column,
-  Checkbox, Radio, Fieldset, Select, Input, Textarea,
+  Checkbox, Radio, RadioGruppe, Fieldset, Select, Input, Textarea,
   Innholdstittel, Systemtittel, Undertittel, Element, Normaltekst, UndertekstBold,
   Knapp,
 };
