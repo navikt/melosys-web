9d47930cfb2c802774bcd8197b94d2b478500d43
Terje Karlsen <terjeofnorway@gmail.com>
2017-12-13 15:24:33 +0100

    Ekstrahert Tabell med paginering for gjenbruk i flere utlistinger.

--- src/felles-komponenter/arbeidsforhold/timertimelonnet.js
+++ src/felles-komponenter/arbeidsforhold/timertimelonnet.js
@@ -1,85 +1,34 @@
-import React, { Component } from 'react';
-import classnames from 'classnames';
+import React from 'react';
 
 import * as MPT from '../../proptypes/index';
-import * as Nav from '../../utils/navFrontend';
+import Tabell from '../tabell/tabell';
 
 import EnkeltDato from '../datoOmrade/enkeltDato';
-
-const uuid = require('uuid/v4');
-
-/** Lister en enkeltlinje for antall timer timelønnet.
- *
- * @param { props.timeLinje } Enkeltlinje for timelønn.
- */
-const TimeLinje = ({ timeLinje }) => {
-  const { timelonnetPeriode, rapporteringsPeriode, antallTimer } = timeLinje;
-  return (
-    <tr className="border-bottom">
-      <td>{<EnkeltDato dato={timelonnetPeriode.fom} />}</td>
-      <td>{<EnkeltDato dato={timelonnetPeriode.tom} />}</td>
-      <td>{rapporteringsPeriode}</td>
-      <td>{antallTimer}</td>
-    </tr>
-  );
-};
-
-TimeLinje.propTypes = {
-  timeLinje: MPT.TimerTimelonnetLinje.isRequired,
-};
 
 /** Lister alle timer timelønnet i form av en table.
  *
  * @param permisjoner Array med timelonnet.
  */
-class TimerTimelonnet extends Component {
-  state = {
-    side: 0,
-    linjePerSide: 5,
-  }
+const TimerTimelonnet = ({ timerTimelonnet }) => {
+  const timelonnetArrayed = timerTimelonnet.map(linje => {
+    const linjeTilArray = [
+      <EnkeltDato dato={linje.timelonnetPeriode.fom} />,
+      <EnkeltDato dato={linje.timelonnetPeriode.tom} />,
+      linje.rapporteringsPeriode,
+      linje.antallTimer,
+    ];
+    return linjeTilArray;
+  });
 
-  visListeSide = side => {
-    this.setState({ side });
-  }
-
-  render() {
-    const { timerTimelonnet } = this.props;
-
-    // Lag en chunk av timerTimelonnet for paginering og visning av korrekt side.
-    const timeLinjeChunk = timerTimelonnet.filter((linje, index) => {
-      const startIndex = this.state.side * this.state.linjePerSide;
-      const sluttIndex = startIndex + this.state.linjePerSide;
-      return index >= startIndex && index < sluttIndex;
-    });
-
-    const sideNav = [];
-    for (let side = 0; side < Math.ceil(timerTimelonnet.length / this.state.linjePerSide); side += 1) {
-      const classname = classnames({ paginering__nav: true, 'paginering__nav--aktiv': side === this.state.side });
-      sideNav.push(<button key={uuid()} className={classname} onClick={() => this.visListeSide(side)}>{side + 1}</button>);
-    }
-
-    return (
-      <div className="permisjoner">
-        <Nav.Undertittel>Timer timelønnet</Nav.Undertittel>
-        <table className="tabellutlisting">
-          <tbody>
-            <tr>
-              <th>Startdato</th>
-              <th>Sluttdato</th>
-              <th>Rapporteringsperiode</th>
-              <th>Antall timer</th>
-            </tr>
-            { timeLinjeChunk.map(timeLinje => <TimeLinje key={uuid()} timeLinje={timeLinje} />) }
-          </tbody>
-        </table>
-        <div className="paginering" aria-label="Naviger blant flere sider i dennne tabellen">
-          {sideNav.map(nav => nav)}
-        </div>
-      </div>
-    );
-  }
-}
-
+  return (
+    <Tabell
+      className="timertimelonnet"
+      kolonneNavn={['Startdato', 'Sluttdato', 'Rapporteringsperiode', 'Antall timer']}
+      tabellData={timelonnetArrayed}
+      linjerPerSide={5}
+    />
+  );
+};
 
 TimerTimelonnet.propTypes = {
   timerTimelonnet: MPT.TimerTimelonnet,
