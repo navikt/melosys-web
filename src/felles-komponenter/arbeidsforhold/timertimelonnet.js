53a4dffe0ca8f8b8fb619d6ac8c7c06881a38944
Terje Karlsen <terjeofnorway@gmail.com>
2017-12-13 17:13:15 +0100

    Diverse opprydding og kommentarer.

--- src/felles-komponenter/arbeidsforhold/timertimelonnet.js
+++ src/felles-komponenter/arbeidsforhold/timertimelonnet.js
@@ -2,24 +2,26 @@ import React from 'react';
 
 import * as MPT from '../../proptypes/index';
 import * as Nav from '../../utils/navFrontend';
-import Tabell from '../tabell/tabell';
 
 import EnkeltDato from '../datoOmrade/enkeltDato';
+import Tabell from '../tabell/tabell';
 
 /** Lister alle timer timelønnet i form av en table.
  *
  * @param permisjoner Array med timelonnet.
  */
 const TimerTimelonnet = ({ timerTimelonnet }) => {
-  const timelonnetArrayed = timerTimelonnet.map(linje => {
-    const linjeTilArray = [
+  const timelonnetArrayed = timerTimelonnet.map(linje => (
+    // Tabell-komponenten er generisk og trenger at hver linje
+    // kommer inn som en ren array og som rendres gjennomsiktig ut i GUI.
+    // All formattering eller komponent-innsett må derfor gjøres her og returnere
+    // en ny ferdigtygget array (timelonnetArrayed).
+    [
       <EnkeltDato dato={linje.timelonnetPeriode.fom} />,
       <EnkeltDato dato={linje.timelonnetPeriode.tom} />,
       linje.rapporteringsPeriode,
       linje.antallTimer,
-    ];
-    return linjeTilArray;
-  });
+    ]));
 
   return (
     <div>
