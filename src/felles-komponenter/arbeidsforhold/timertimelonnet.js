b81521c86cd163ef51b98e49fdaa0716e7e2cea3
Terje Karlsen <terjeofnorway@gmail.com>
2017-12-13 10:42:26 +0100

    Lagt til første iterasjon av paginering for tabell

--- src/felles-komponenter/arbeidsforhold/timertimelonnet.js
+++ src/felles-komponenter/arbeidsforhold/timertimelonnet.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import classnames from 'classnames';
 
 import * as MPT from '../../proptypes/index';
 import * as Nav from '../../utils/navFrontend';
@@ -31,22 +33,54 @@ TimeLinje.propTypes = {
  *
  * @param permisjoner Array med timelonnet.
  */
-const TimerTimelonnet = ({ timerTimelonnet }) => (
-  <div className="permisjoner">
-    <Nav.Undertittel>Timer timelønnet</Nav.Undertittel>
-    <table className="tabellutlisting">
-      <tbody>
-        <tr>
-          <th>Startdato</th>
-          <th>Sluttdato</th>
-          <th>Rapporteringsperiode</th>
-          <th>Antall timer</th>
-        </tr>
-        { timerTimelonnet.map(timeLinje => <TimeLinje key={uuid()} timeLinje={timeLinje} />) }
-      </tbody>
-    </table>
-  </div>
-);
+class TimerTimelonnet extends Component {
+  state = {
+    side: 0,
+    linjePerSide: 5,
+  }
+
+  visListeSide = side => {
+    this.setState({ side });
+  }
+
+  render() {
+    const { timerTimelonnet } = this.props;
+
+    // Lag en chunk av timerTimelonnet for paginering og visning av korrekt side.
+    const timeLinjeChunk = timerTimelonnet.filter((linje, index) => {
+      const startIndex = this.state.side * this.state.linjePerSide;
+      const sluttIndex = startIndex + this.state.linjePerSide;
+      return index >= startIndex && index < sluttIndex;
+    });
+
+    const sideNav = [];
+    for (let side = 0; side < Math.ceil(timerTimelonnet.length / this.state.linjePerSide); side += 1) {
+      const classname = classnames({ paginering__nav: true, 'paginering__nav--aktiv': side === this.state.side });
+      sideNav.push(<Link to={'#'} key={uuid()} className={classname} onClick={() => this.visListeSide(side)}>{side + 1}</Link>);
+    }
+
+    return (
+      <div className="permisjoner">
+        <Nav.Undertittel>Timer timelønnet</Nav.Undertittel>
+        <table className="tabellutlisting">
+          <tbody>
+            <tr>
+              <th>Startdato</th>
+              <th>Sluttdato</th>
+              <th>Rapporteringsperiode</th>
+              <th>Antall timer</th>
+            </tr>
+            { timeLinjeChunk.map(timeLinje => <TimeLinje key={uuid()} timeLinje={timeLinje} />) }
+          </tbody>
+        </table>
+        <div className="paginering" aria-label="Naviger blant flere sider i dennne tabellen">
+          {sideNav.map(nav => nav)}
+        </div>
+      </div>
+    );
+  }
+}
+
 
 TimerTimelonnet.propTypes = {
   timerTimelonnet: MPT.TimerTimelonnet,
