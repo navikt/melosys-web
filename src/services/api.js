02f84b22fd67af61c39925b792a156a11acdc4f5
Bjølseth, Are <are.bjolseth@nav.no>
2018-01-03 15:23:55 +0100

    La til proxy conf i package.json Da er det ikke nødvending og starte egen instans av proxy server

--- src/services/api.js
+++ src/services/api.js
@@ -11,9 +11,7 @@ function erDev() {
 }
 */
 // from .env or .env.local
-const API_SERVER = `${process.env.REACT_APP_API_SERVER}`;
 const API_BASE_URL = `${process.env.REACT_APP_API_BASE_URL}`;
-const API_MELOSYS_URL = `${API_SERVER}${API_BASE_URL}`;
 const API_SAKSBEHANDLER = `${process.env.REACT_APP_API_SAKSBEHANDLER}`;
 const API_SAKSOPPLYSNINGER = `${process.env.REACT_APP_API_SAKSOPPLYSNINGER}`;
 console.log('process.env', process.env);
@@ -24,37 +22,37 @@ export function health() {
 }
 
 export function hentSoknad() {
-  const URI_SOKNAD =  `${API_MELOSYS_URL}soknad/`;
+  const URI_SOKNAD =  `${API_BASE_URL}soknad/`;
   return getAsJson(URI_SOKNAD);
 }
 
 export function sendSoknad(dokument) {
-  const URI_SOKNAD =`${API_MELOSYS_URL}soknad`;
+  const URI_SOKNAD =`${API_BASE_URL}soknad`;
   return postAsJson(URI_SOKNAD, dokument);
 }
 
 export function hentFagsaker(snr) {
-  const URI_FAGSAKER =  `${API_MELOSYS_URL}fagsaker/${snr}`;
+  const URI_FAGSAKER =  `${API_BASE_URL}fagsaker/${snr}`;
   return getAsJson(URI_FAGSAKER);
 }
 
 export function hentNyesaker(fnr) {
-  const URI_NYESAKER = `${API_MELOSYS_URL}sok/fagsaker/?fnr=${fnr}`;
+  const URI_NYESAKER = `${API_BASE_URL}sok/fagsaker/?fnr=${fnr}`;
   return getAsJson(URI_NYESAKER);
 }
 
 export function opprettSak(fnr) {
-  const URI_OPPRETTSAK = `${API_MELOSYS_URL}opprettsak/${fnr}`;
+  const URI_OPPRETTSAK = `${API_BASE_URL}opprettsak/${fnr}`;
   return postAsJson(URI_OPPRETTSAK);
 }
 
 export function hentSakerbehandles(brukernavn) {
-  const URI_SAKERBEHANDLES = `${API_MELOSYS_URL}sakerbehandles/${brukernavn}`;
+  const URI_SAKERBEHANDLES = `${API_BASE_URL}sakerbehandles/${brukernavn}`;
   return getAsJson(URI_SAKERBEHANDLES);
 }
 
 export function hentTidligeresaker(brukernavn) {
-  const URI_TIDLIGERESAKER = `${API_MELOSYS_URL}tidligeresaker/${brukernavn}`;
+  const URI_TIDLIGERESAKER = `${API_BASE_URL}tidligeresaker/${brukernavn}`;
   return getAsJson(URI_TIDLIGERESAKER);
 }
 
@@ -64,11 +62,11 @@ export function hentSaksbehandler() {
 }
 
 export function hentSaksopplysninger(fnr) {
-  const URI_SAMMENSATT_ARBEIDSFORHOLD = `${API_MELOSYS_URL}${API_SAKSOPPLYSNINGER}/${fnr}`;
+  const URI_SAMMENSATT_ARBEIDSFORHOLD = `${API_BASE_URL}${API_SAKSOPPLYSNINGER}/${fnr}`;
   return getAsJson(URI_SAMMENSATT_ARBEIDSFORHOLD);
 }
 
 export function nyHenvendelse(henvendelse) {
-  const URI_HENVENDELSE =`${API_MELOSYS_URL}henvendelse`;
+  const URI_HENVENDELSE =`${API_BASE_URL}henvendelse`;
   return postAsJson(URI_HENVENDELSE, henvendelse);
 }
