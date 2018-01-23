f8e6215487d0f2c318c29ffb2d253b95b031b99a
Terje Karlsen <terjeofnorway@gmail.com>
2018-01-23 15:33:01 +0100

    Lagt inn steg for utsending.

--- src/felles-komponenter/vilkarsveileder/vurderinger/vurderingUtsending.js
+++ src/felles-komponenter/vilkarsveileder/vurderinger/vurderingUtsending.js
@@ -9,9 +9,9 @@ const VurderingUtsending = props => {
   return (
     <div>
       <Nav.Fieldset legend="Hva gjelder for søkeren?">
+        <Skjema.Checkbox feltNavn="faktaavklaringUtsendingMindreEnn24Mnd" value="true" label="Er utsendingsperioden mindre enn 24 mnd?" />
         <Skjema.Checkbox feltNavn="faktaavklaringAnsattINorskSelskap" value="true" label="Skal personen være ansatt i det norske selskapet i hele utsendingsperioden?" />
         <Skjema.Checkbox feltNavn="faktaavklaringErstatterTidligereUtsendt" value="true" label="Skal personen erstatte en annen?" />
-        <Skjema.Checkbox feltNavn="faktaavklaringUtsendingMindreEnn24Mnd" value="true" label="Er utsendingsperioden mindre enn 24 mnd?" />
       </Nav.Fieldset>
       <div className="fane__knapplinje">
         <Nav.Knapp type="hoved" className="fane__navigasjonsknapp" onClick={bekreftOgFortsett}>Bekreft og fortsett</Nav.Knapp>
