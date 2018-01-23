478b9f39f7aae485528c5878efe86a31cf0f58a4
Bjølseth, Are <are.bjolseth@nav.no>
2018-01-23 13:16:28 +0100

    * Integration changes. - homepage: /melosys-web in package.json - proxy config in package.json - melosys-web context in constands.js and history.js - removed saksopplysninger*.js * Docker file - ny melosys.conf for nginx

--- src/history.js
+++ src/history.js
@@ -1,7 +1,19 @@
 import { createBrowserHistory } from 'history';
 import { CONTEXT_PATH } from './constants'; // eslint-disable-line
 
+function isProductionEnv() {
+  return `${process.env.NODE_ENV}` === 'production';
+}
+const contextPath = isProductionEnv() ? CONTEXT_PATH : '';
+/* eslint-disable no-console */
+if (isProductionEnv()) {
+  console.log(`NODE_ENV:${process.env.NODE_ENV}`, `basename:${contextPath}`);
+} else {
+  console.log(`NODE_ENV:${process.env.NODE_ENV}`, 'basename:"" no context path');
+}
+/* eslint-enable no-console */
 const routerHistory = createBrowserHistory({
-  basename: CONTEXT_PATH,
+  basename: contextPath,
 });
 export default routerHistory;
+
