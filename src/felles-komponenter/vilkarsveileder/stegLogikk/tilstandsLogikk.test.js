06e9c3164ebb801e4c577993cb26b6f5d0d1a66f
Terje Karlsen <terjeofnorway@gmail.com>
2018-01-24 16:24:21 +0100

    Lagt inn tilstandsLogikk for stegene. Lagt inn dum logikk i første steg.

---
+++ src/felles-komponenter/vilkarsveileder/stegLogikk/tilstandsLogikk.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+
+import Logikk from './stegLogikk';
+
+import VurderingSysselsetting from '../vurderinger/vurderingSysselsetting';
+
+it('viser korrekt tilstand i SYSSELSETTING', () => {
+
+  expect(true).toBe(true);
+});
