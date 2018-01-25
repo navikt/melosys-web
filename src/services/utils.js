454e7707e09a748d00793f59cacaa566df31129d
Bjølseth, Are <are.bjolseth@nav.no>
2018-01-25 13:38:26 +0100

    Added 501 intercept Changed api path /sokand => /soknader

--- src/services/utils.js
+++ src/services/utils.js
@@ -9,6 +9,9 @@ export const STATUS = {
 export function sjekkStatuskode(response) {
   if (response.status >= 200 && response.status < 300 && response.ok) {
     return response;
+  } else if (response.status === 501) {
+    console.warn('501:', response.statusText, response.url.split('/').slice(3).join('/'));
+    return response;
   }
   const error = new Error(response.statusText);
   error.response = response;
