7bc6bbc5a2416d6142adf02602902c35a4ccf534
Terje Karlsen <terjeofnorway@gmail.com>
2018-01-26 15:11:59 +0100

    Refactor av ternary til egne komponenter for bedre lesbarhet. Løftet ut constant typer til egen eksport for å ungå lint-problemer.

--- src/felles-komponenter/vilkarsveileder/vurderinger/vurderingUtsending.js
+++ src/felles-komponenter/vilkarsveileder/vurderinger/vurderingUtsending.js
@@ -3,33 +3,31 @@ import PT from 'prop-types';
 import * as Nav from '../../../utils/navFrontend';
 import * as Skjema from '../../skjema';
 
+export const VurderingUtsendingTyper = {
+  TRUE: 'true',
+};
+
+const UtsendingMindreEnn24Mnd = () => (
+  <Skjema.Checkbox feltNavn="faktaavklaringUtsendingMindreEnn24Mnd" value={VurderingUtsendingTyper.TRUE} label="Er utsendingsperioden mindre enn 24 mnd?" />
+);
+
+const ErstatterTidligereUtsendt = () => (
+  <Skjema.Checkbox feltNavn="faktaavklaringErstatterTidligereUtsendt" value={VurderingUtsendingTyper.TRUE} label="Skal personen erstatte en annen?" />
+);
+
+const AnsattINorskSelskap = () => (
+  <Skjema.Checkbox feltNavn="faktaavklaringAnsattINorskSelskap" value={VurderingUtsendingTyper.TRUE} label="Skal personen være ansatt i det norske selskapet i hele utsendingsperioden?" />
+);
+
 const VurderingUtsending = props => {
   const { bekreftOgFortsett, tilstand } = props;
-
-  const utsendingMindreEnn24Mnd = tilstand.visUtsendingMindreEnn24Mnd
-    ?
-    <Skjema.Checkbox feltNavn="faktaavklaringUtsendingMindreEnn24Mnd" value={VurderingUtsending.TRUE} label="Er utsendingsperioden mindre enn 24 mnd?" />
-    :
-    null;
-
-  const erstatterTidligereUtsendt = tilstand.visErstatterTidligereUtsendt
-    ?
-    <Skjema.Checkbox feltNavn="faktaavklaringErstatterTidligereUtsendt" value={VurderingUtsending.TRUE} label="Skal personen erstatte en annen?" />
-    :
-    null;
-
-  const ansattINorskSelskap = tilstand.visAnsattINorskSelskap
-    ?
-    <Skjema.Checkbox feltNavn="faktaavklaringAnsattINorskSelskap" value={VurderingUtsending.TRUE} label="Skal personen være ansatt i det norske selskapet i hele utsendingsperioden?" />
-    :
-    null;
 
   return (
     <div>
       <Nav.Fieldset legend="Hva gjelder for søkeren?">
-        {utsendingMindreEnn24Mnd}
-        {ansattINorskSelskap}
-        {erstatterTidligereUtsendt}
+        {tilstand.visUtsendingMindreEnn24Mnd && UtsendingMindreEnn24Mnd}
+        {tilstand.visErstatterTidligereUtsendt && ErstatterTidligereUtsendt}
+        {tilstand.visAnsattINorskSelskap && AnsattINorskSelskap}
       </Nav.Fieldset>
       <div className="fane__knapplinje">
         <Nav.Knapp type="hoved" className="fane__navigasjonsknapp" onClick={bekreftOgFortsett}>Bekreft og fortsett</Nav.Knapp>
@@ -37,9 +35,6 @@ const VurderingUtsending = props => {
     </div>
   );
 };
-
-VurderingUtsending.TRUE = 'true';
-VurderingUtsending.FALSE = 'false';
 
 VurderingUtsending.propTypes = {
   bekreftOgFortsett: PT.func.isRequired,
