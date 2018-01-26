e515e8719b4ba4f1386dcd91d6e568b2b2d0b37b
Terje Karlsen <terjeofnorway@gmail.com>
2018-01-26 08:22:30 +0100

    Lagt til steg for bostedsland.

---
+++ src/felles-komponenter/vilkarsveileder/vurderinger/vurderingBostedsland.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PT from 'prop-types';
+import * as Nav from '../../../utils/navFrontend';
+import * as Skjema from '../../skjema';
+
+import LandVelger from '../../skjema/landvelger/';
+
+const VurderingBostedsland = props => {
+  const { bekreftOgFortsett } = props;
+
+  return (
+    <div>
+      <Nav.Fieldset legend="Jeg bekrefter å ha vurdert:">
+        <Skjema.Checkbox feltNavn="faktaavklaringBekrefterFamiliebosted" value={VurderingBostedsland.TRUE} label="Hvor søkers nærmeste familie bor" />
+        <Skjema.Checkbox feltNavn="faktaavklaringBekrefterFamiliebosted" value={VurderingBostedsland.TRUE} label="Hvor søker disponerer" />
+      </Nav.Fieldset>
+      <Nav.Fieldset legend="Basert på dette vurderes bosted til:">
+        <LandVelger feltNavn="faktaavklaringBostedsland" multiland={false} />
+      </Nav.Fieldset>
+      <div className="fane__knapplinje">
+        <Nav.Knapp type="hoved" className="fane__navigasjonsknapp" onClick={bekreftOgFortsett}>Bekreft og fortsett</Nav.Knapp>
+      </div>
+    </div>
+  );
+};
+
+VurderingBostedsland.propTypes = {
+  bekreftOgFortsett: PT.func.isRequired,
+  tilstand: PT.object,
+};
+
+VurderingBostedsland.defaultProps = {
+  tilstand: {},
+};
+
+VurderingBostedsland.TRUE = 'true';
+
+export default VurderingBostedsland;
