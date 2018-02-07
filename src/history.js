6babc436163621dba4fa8caad681969f89c75336
Bjølseth, Are <are.bjolseth@nav.no>
2018-02-07 10:35:47 +0100

    MELOSYS-881 Justering av byggescript for felles war deployment

--- src/history.js
+++ src/history.js
@@ -1,11 +1,24 @@
 import { createBrowserHistory } from 'history';
 
 const CONTEXT_PATH = '/melosys-web';
+const NO_CONTEXT_PATH = '/';
 
 function isProductionEnv() {
   return `${process.env.NODE_ENV}` === 'production';
 }
-const contextPath = isProductionEnv() ? CONTEXT_PATH : '';
+
+function hasCombinedWarDeployment() {
+  const combinedDeployment = `${process.env.REACT_APP_COMBINED_WAR_DEPLOYMENT}`;
+  return combinedDeployment && combinedDeployment.toLowerCase() === 'true';
+}
+
+let contextPath = NO_CONTEXT_PATH; // default
+
+if (isProductionEnv()) {
+  if (hasCombinedWarDeployment() === false) {
+    contextPath = CONTEXT_PATH; // Need context path to work in NGINX
+  }
+}
 /* eslint-disable no-console */
 if (isProductionEnv()) {
   console.log(`NODE_ENV:${process.env.NODE_ENV}`, `basename:${contextPath}`);
