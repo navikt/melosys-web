9745359f6d9b8b899d23c33265700410882e3d49
Terje Karlsen <terjeofnorway@gmail.com>
2018-02-13 10:44:45 +0100

    Refactor av inntektsgraf slik at 1. inntekt summeres opp pr periode 2. det ikke lenger differensieres på sikker / usikker inntekt 3. inntekten er riktig sortert

--- src/felles-komponenter/arbeidsforhold/inntekt.js
+++ src/felles-komponenter/arbeidsforhold/inntekt.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import ReactHighcharts from 'react-highcharts';
-import moment from 'moment';
 
 import * as MPT from '../../proptypes/index';
 import * as Nav from '../../utils/navFrontend';
@@ -31,33 +30,29 @@ class Inntekt extends Component {
     '2017/03': { sikkertBeloep: 44000, usikkertBeloep: 20000 },
     }
      */
-    const grafInntekt = inntekt.reduce((samling, linje) => {
-      // Sjekk om det finnes en opptjeningsperiode. Hvis ikke er denne inntekten usikker og blir senere
-      // markert deretter i grafen.
-      const harOpptjeningsPeriode = (linje.opptjeningsperiode && linje.opptjeningsperiode.fom);
-      const inntektPeriode = harOpptjeningsPeriode ? moment(linje.opptjeningsperiode.fom, 'YYYYMMDD').format('YYYY-MM') : linje.utbetaltIPeriode;
-      // Lag et kopi av objektet fra samlingen for den aktuelle inntektPeriode. Dette gjør at vi kan legge til beløp (sikkert eller usikkert)
-      // dersom vi har fått flere inntekter innenfor samme periode.
-      const inntektDelObjekt = samling[inntektPeriode] ? { ...samling[inntektPeriode] } : { sikkertBeloep: 0, usikkertBeloep: 0 };
+    const grafInntekt = inntekt
+      .reduce((samling, linje) => {
+        const { utbetaltIPeriode, beloep } = linje;
+        const utbetaltTotaltIPeriode = samling[utbetaltIPeriode] || 0;
 
-      // Sett inn oppdaterte beløp (pluss evt tidligere inn i del-objektet.
-      inntektDelObjekt.sikkertBeloep = harOpptjeningsPeriode ? (inntektDelObjekt.sikkertBeloep + linje.beloep) : inntektDelObjekt.sikkertBeloep;
-      inntektDelObjekt.usikkertBeloep = !harOpptjeningsPeriode ? (inntektDelObjekt.usikkertBeloep + linje.beloep) : inntektDelObjekt.usikkertBeloep;
+        return [...samling, { periode: utbetaltIPeriode, beloep: (utbetaltTotaltIPeriode + beloep) }];
+      }, [])
+      .sort((a, b) => ((a.periode > b.periode) ? 1 : -1));
 
-      return { ...samling, [inntektPeriode]: inntektDelObjekt };
-    }, {});
 
     const grafConfig = {
       rangeSelector: {
         selected: 0,
+      },
+      legend: {
+        enabled: false,
       },
       title: {
         text: '',
       },
       chart: {
         type: 'column',
-        description: 'Grafen viser utvikling i inntekt knyttet til arbeidsforholdet. Inntekt som som har en kjent opptjeningsperiode er ' +
-        'markert som sikker. Inntekt hvor kun utbetalingsperiode er kjent er markett som usikker.',
+        description: 'Grafen viser utvikling i inntekt knyttet til juridisk arbeidsgiver eller virksomhet.',
       },
       yAxis: {
         min: 0,
@@ -66,7 +61,7 @@ class Inntekt extends Component {
         },
       },
       xAxis: {
-        categories: Object.keys(grafInntekt),
+        categories: grafInntekt.map(linje => linje.periode),
         crosshair: true,
         description: 'Perioder med inntekt.',
       },
@@ -77,39 +72,35 @@ class Inntekt extends Component {
       },
       series: [
         {
-          name: 'Usikker periode',
-          data: Object.values(grafInntekt).map(linje => linje.usikkertBeloep),
-          color: '#b7b1a9',
-          description: 'Beskrivelse av usikkerhet',
-        },
-        {
-          name: 'Sikker periode',
-          data: Object.values(grafInntekt).map(linje => linje.sikkertBeloep),
+          name: 'Samlet  i én periode',
+          data: grafInntekt.map(linje => linje.beloep),
           color: '#0067c5',
-          description: 'Beskrivelse av usikkerhet',
+          description: 'Inntekt',
         },
       ],
     };
 
     /** Basert på grafInntekt (som nå har gjort en vurdering av sikker vs usikker periode), lag en array-versjon som
      * kan serveres til Tabell-komponenten. Denne trengs for UU, men vises kun ved klikk.
+     *
+     * Tabell-komponenten er generisk og trenger at hver linje
+     * kommer inn som en ren array og som rendres gjennomsiktig ut i GUI.
+     * All formattering eller komponent-innsett må derfor gjøres her og returnere
+     * en ny ferdigtygget array.
      */
-    const inntektArrayed = Object.keys(grafInntekt).map(periode => (
-      // Tabell-komponenten er generisk og trenger at hver linje
-      // kommer inn som en ren array og som rendres gjennomsiktig ut i GUI.
-      // All formattering eller komponent-innsett må derfor gjøres her og returnere
-      // en ny ferdigtygget array.
-      [
-        periode,
-        grafInntekt[periode].sikkertBeloep,
-        grafInntekt[periode].usikkertBeloep,
-      ]));
+    const inntektArrayed = grafInntekt
+      .sort((a, b) => ((a.periode < b.periode) ? 1 : -1))
+      .map(linje => (
+        [
+          linje.periode,
+          linje.beloep,
+        ]));
 
     const uuTabell = this.state.visInntektTabell ? (
       <div>
         <Nav.Undertittel>Inntekt</Nav.Undertittel>
         <Tabell
-          kolonneNavn={['Periode', 'Sikker inntekt', 'Usikker inntekt']}
+          kolonneNavn={['Periode', 'Samlet inntekt']}
           tabellData={inntektArrayed}
           linjerPerSide={5}
         />
@@ -118,10 +109,10 @@ class Inntekt extends Component {
 
     return inntekt.length > 0 ? (
       <div className="inntekt">
-        <div className="my-pretty-chart-container">
+        <div className="inntekt__graf">
           <ReactHighcharts config={grafConfig} />
+          <Nav.Knapp mini onClick={this.toggleInntektTabellHandler} className="vistabell__knapp">Vis tabell for inntekten</Nav.Knapp>
         </div>
-        <Nav.Knapp mini onClick={this.toggleInntektTabellHandler} className="vistabell__knapp">Vis tabell for inntekten</Nav.Knapp>
         {uuTabell}
       </div>
     ) : null;
