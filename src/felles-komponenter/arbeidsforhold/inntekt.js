a310fdef606b33f20436284b23a9230e4191f2e7
Terje Karlsen <terjeofnorway@gmail.com>
2018-02-15 13:46:51 +0100

    Diverse styling, satt inn knapp for vising av historikk.

--- src/felles-komponenter/arbeidsforhold/inntekt.js
+++ src/felles-komponenter/arbeidsforhold/inntekt.js
@@ -106,6 +106,7 @@ class Inntekt extends Component {
 
     return inntekt.length > 0 ? (
       <div className="inntekt">
+        <Nav.Undertittel>Inntektgraf</Nav.Undertittel>
         <div className="inntekt__graf">
           <ReactHighcharts config={grafConfig} />
           <Nav.Knapp mini onClick={this.toggleInntektTabellHandler} className="vistabell__knapp">Vis tabell for inntekten</Nav.Knapp>
