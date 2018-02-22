8d602d04dd75196dffcd525ac4ad759804b96d9e
Terje Karlsen <terjeofnorway@gmail.com>
2018-02-22 14:35:07 +0100

    Byttet ut CustomField med Field for å unngå at validering resettes.

--- src/felles-komponenter/skjema/input/input.js
+++ src/felles-komponenter/skjema/input/input.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PT from 'prop-types';
-import { CustomField } from 'react-redux-form-validation';
+import { Field } from 'redux-form';
 
 import * as Nav from '../../../utils/navFrontend';
 import { normaliserInputDato } from '../../../utils/dato';
@@ -10,9 +10,9 @@ import '../skjema.css';
  * objekt som NAV-Input-komponenten forventer. Før den settes inn i Nav.Input.
  */
 function InnerInputComponent({
-  input, label, errorMessage, ...rest
+  input, label, ...rest
 }) {
-  const feil = errorMessage ? { feilmelding: errorMessage[0] } : undefined;
+  const feil = rest.meta.error ? { feilmelding: rest.meta.error } : undefined;
   const inputProps = { ...input, ...rest };
   return <Nav.Input label={label} feil={feil} {...inputProps} />;
 }
@@ -20,14 +20,12 @@ function InnerInputComponent({
 InnerInputComponent.propTypes = {
   label: PT.string.isRequired,
   bredde: PT.string,
-  errorMessage: PT.arrayOf(PT.node),
   meta: PT.object, // eslint-disable-line react/forbid-prop-types
   input: PT.object, // eslint-disable-line react/forbid-prop-types
 };
 
 InnerInputComponent.defaultProps = {
   bredde: undefined,
-  errorMessage: undefined,
   meta: undefined,
   input: undefined,
 };
@@ -37,12 +35,12 @@ function Input({
 }) {
   const normaliserDatoFunksjon = datoFelt ? normaliserInputDato : null;
   return (
-    <CustomField
+    <Field
       bredde={bredde}
       name={feltNavn}
-      errorClass="skjemaelement--harFeil"
       normalize={normaliserDatoFunksjon}
-      customComponent={<InnerInputComponent {...rest} />}
+      component={InnerInputComponent}
+      props={rest}
     />
   );
 }
