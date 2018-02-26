f5abae4d68fc552f00ebcabb4794a4d0d8391f62
Terje Karlsen <terjeofnorway@gmail.com>
2018-02-26 09:41:41 +0100

    Lagt inn nye sjekkbokser for utsending.

--- src/felles-komponenter/vilkarsveileder/vurderinger/vurderingUtsending.js
+++ src/felles-komponenter/vilkarsveileder/vurderinger/vurderingUtsending.js
@@ -19,6 +19,18 @@ const AnsattINorskSelskap = () => (
   <Skjema.Checkbox feltNavn="faktaavklaringAnsattINorskSelskap" value={VurderingUtsendingTyper.TRUE} label="Skal personen være ansatt i det norske selskapet i hele utsendingsperioden?" />
 );
 
+const ForetakDriverINorge = () => (
+  <Skjema.Checkbox feltNavn="faktaavklaringForetakDriverINorge" value={VurderingUtsendingTyper.TRUE} label="Er ansatt i et foretak som driver virksomhet i Norge." />
+);
+
+const HarForutgaendeMedlemskap = () => (
+  <Skjema.Checkbox feltNavn="faktaavklaringHarForutgaendeMedlemskap" value={VurderingUtsendingTyper.TRUE} label="Har forutgående medlemskap." />
+);
+
+const ArbeidKnyttetTilVirksomhetUtlandet = () => (
+  <Skjema.Checkbox feltNavn="faktaavklaringArbeidKnyttetTilVirksomhetUtlandet" value={VurderingUtsendingTyper.TRUE} label="Søkers arbeid er knyttet til virksomhet i utlandet." />
+);
+
 const VurderingUtsending = props => {
   const { bekreftOgFortsett, tilstand } = props;
 
@@ -28,6 +40,9 @@ const VurderingUtsending = props => {
         {tilstand.visUtsendingMindreEnn24Mnd && <UtsendingMindreEnn24Mnd />}
         {tilstand.visErstatterTidligereUtsendt && <ErstatterTidligereUtsendt />}
         {tilstand.visAnsattINorskSelskap && <AnsattINorskSelskap />}
+        {tilstand.visForetakDriverINorge && <ForetakDriverINorge />}
+        {tilstand.visHarForutgaendeMedlemskap && <HarForutgaendeMedlemskap />}
+        {tilstand.visArbeidKnyttetTilVirksomhetUtlandet && <ArbeidKnyttetTilVirksomhetUtlandet />}
       </Nav.Fieldset>
       <div className="fane__knapplinje">
         <Nav.Knapp type="hoved" className="fane__navigasjonsknapp" onClick={bekreftOgFortsett}>Bekreft og fortsett</Nav.Knapp>
