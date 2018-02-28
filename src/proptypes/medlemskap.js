8f8426af15932f95fb7b4ff9cd44529142366866
Terje Karlsen <terjeofnorway@gmail.com>
2018-02-28 18:56:01 +0100

    Lagt inn grupperinger og korrigert navnefeil for perioder.

--- src/proptypes/medlemskap.js
+++ src/proptypes/medlemskap.js
@@ -2,7 +2,7 @@ import PT from 'prop-types';
 import { Periode } from './periode';
 import { Kodeverk } from './kodeverk';
 
-const MedlemskapPeriodePropType = PT.shape({
+const MedlemskapEnkeltPeriodePropType = PT.shape({
   periode: Periode,
   type: Kodeverk,
   status: Kodeverk,
@@ -14,14 +14,17 @@ const MedlemskapPeriodePropType = PT.shape({
   kilde: PT.string,
 });
 
+const MedlemskapPerioderPropType = PT.arrayOf(MedlemskapEnkeltPeriodePropType);
+
 const MedlemskapPropType = PT.shape({
-  perioderMed: PT.arrayOf(MedlemskapPeriodePropType),
-  perioderUten: PT.arrayOf(MedlemskapPeriodePropType),
-  perioderUavklart: PT.arrayOf(MedlemskapPeriodePropType),
-  perioderAvvist: PT.arrayOf(MedlemskapPeriodePropType),
+  perioderMed: PT.arrayOf(MedlemskapEnkeltPeriodePropType),
+  perioderUten: PT.arrayOf(MedlemskapEnkeltPeriodePropType),
+  perioderUavklart: PT.arrayOf(MedlemskapEnkeltPeriodePropType),
+  perioderAvvist: PT.arrayOf(MedlemskapEnkeltPeriodePropType),
 });
 
 export {
-  MedlemskapPeriodePropType as MedlemskapPeriode,
+  MedlemskapEnkeltPeriodePropType as MedlemskapEnkeltPeriode,
+  MedlemskapPerioderPropType as MedlemskapPerioder,
   MedlemskapPropType as Medlemskap,
 };
