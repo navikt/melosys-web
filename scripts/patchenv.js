69497bc8672743300c07c1d90d14ba5a2a9d898c
Bjølseth, Are <are.bjolseth@nav.no>
2018-03-06 16:09:16 +0100

    Fine tuning

--- scripts/patchenv.js
+++ scripts/patchenv.js
@@ -3,7 +3,10 @@ const dotenv = require('dotenv-save');
 
 moment.locale('nb');
 
-const jenkinsBuildNumber = process.env.BUILD_NUMBER || 'local';
-const version = `v${process.env.npm_package_version}:${jenkinsBuildNumber}`;
+const buildNumber = process.env.BUILD_NUMBER || 'local';
+const branchName = process.env.BRANCH_NAME || 'unknown';
+const version = `v${process.env.npm_package_version}`;
 dotenv.set('REACT_APP_VERSION', version);
-dotenv.set('REACT_APP_DATETIME', moment().format('LLLL'));
+dotenv.set('REACT_APP_BUILD_DATETIME', moment().format('DDMMMYYYY-HH:mm:ss'));
+dotenv.set('REACT_APP_BUILD_VERSION', buildNumber);
+dotenv.set('REACT_APP_BRANCH_NAME', branchName);
--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App({ children }) {
   return (
     <div className="App">
       <Rammeverk>{children}</Rammeverk>
+      <dl>
+        <dt>Versjon</dt><dd>{process.env.REACT_APP_VERSION}</dd>
+        <dt>Build time</dt><dd>{process.env.REACT_APP_DATETIME}</dd>
+        <dt>Build version</dt><dd>{process.env.REACT_APP_BUILD_VERSION}</dd>
+        <dt>Branch</dt><dd>{process.env.REACT_APP_BRANCH_NAME}</dd>
+      </dl>
     </div>
   );
 }
