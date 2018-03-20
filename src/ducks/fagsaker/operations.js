6516198ca68bba131d6e20550c5cfd1a83b2524c
Terje Karlsen <terjeofnorway@gmail.com>
2018-03-20 09:44:26 +0100

    Skrevet dokumentasjon og kommentarer på operations.

--- src/ducks/fagsaker/operations.js
+++ src/ducks/fagsaker/operations.js
@@ -1,9 +1,24 @@
-// Action Creators
+/**
+ * Operations
+ * ----------------------------------------------------------------------------------
+ * Dette er Thunk-operasjoner som muliggjør asynkrone kall mot Redux
+ * ved å returnere en action-generatoren som en egen funksjon. Denne kjøres deretter
+ * når det asynkrone kallet, feks fra API'et er ferdigkjørt.
+ *
+ */
+
 import { doThenDispatch } from '../../services/utils';
 import * as Api from '../../services/api';
-
 import * as Types from './types';
 
+/**
+ * Henter fagsaker. Dette er registerinformasjon som allerede er hentet backend i forbindelse
+ * med saken. Kallet får altså kun lagret fagsak fra backend og ikke nødvendigvis oppdatert
+ * registerdata fra TPS, Aa-reg etc. Dette er det backend som er ansvarlig for,.
+ *
+ * @param snr String Saksnummeret
+ * @returns {*}
+ */
 export function hentFagsaker(snr) {
   return doThenDispatch(() => Api.hentFagsaker(snr), {
     OK: Types.OK,
@@ -12,6 +27,12 @@ export function hentFagsaker(snr) {
   });
 }
 
+/**
+ * Oppretter en ny fagsak dersom dette ikke er gjort tidligere. Backend henter registerdata fra
+ * TPS, Aa-reg etc og lagrer i Melosys-databasen før frontend mottar JSON med fagsaken.
+ * @param fnr String Fødselsnummeret til vedkommende som det skal opprettes ny fagsak på.
+ * @returns {*}
+ */
 export function opprettNyFagsak(fnr) {
   return doThenDispatch(() => Api.opprettNyFagsak(fnr), {
     OK: Types.OK,
