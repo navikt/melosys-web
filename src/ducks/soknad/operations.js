bbc3d66e30c71c03fc7c75fc2f26598bc80e791a
Terje Karlsen <terjeofnorway@gmail.com>
2018-03-21 16:50:25 +0100

    Refactor av soknad for ducks.

---
+++ src/ducks/soknad/operations.js
@@ -0,0 +1,30 @@
+import { doThenDispatch } from '../../services/utils';
+import * as Api from '../../services/api';
+
+import * as Types from './types';
+
+/**
+ * Operations
+ * ----------------------------------------------------------------------------------
+ * Dette er Thunk-operasjoner som muliggjør asynkrone kall mot Redux
+ * ved å returnere en action-generatoren som en egen funksjon. Denne kjøres deretter
+ * når det asynkrone kallet, feks fra API'et er ferdigkjørt.
+ *
+ */
+
+// Action Creators
+export function hentSoknad(behandlingID) {
+  return doThenDispatch(() => Api.hentSoknader(behandlingID), {
+    OK: Types.OK,
+    FEILET: Types.FEILET,
+    PENDING: Types.PENDING,
+  });
+}
+
+export function sendSoknad(bid, soknad) {
+  return doThenDispatch(() => Api.sendSoknad(bid, soknad), {
+    OK: Types.OK,
+    FEILET: Types.FEILET,
+    PENDING: Types.PENDING,
+  });
+}
