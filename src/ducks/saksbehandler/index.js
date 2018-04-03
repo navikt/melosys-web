9f3d622b529bbfddde53641be74cbba9fa1e28a0
Terje Karlsen <terjeofnorway@gmail.com>
2018-04-03 09:32:29 +0200

    Begynt refactor.

---
+++ src/ducks/saksbehandler/index.js
@@ -0,0 +1,15 @@
+import * as vurderingOperations from './operations';
+import * as vurderingSelectors from './selectors';
+import * as vurderingActions from './actions';
+import * as vurderingTypes from './types';
+
+import vurderingReducers from './reducers';
+
+export {
+  vurderingOperations,
+  vurderingSelectors,
+  vurderingTypes,
+  vurderingActions,
+};
+
+export default vurderingReducers;
---
+++ src/ducks/saksbehandler/operations.js
@@ -0,0 +1,8 @@
+/**
+ * Operations
+ * ----------------------------------------------------------------------------------
+ * Dette er Thunk-operasjoner som muliggjør asynkrone kall mot Redux
+ * ved å returnere en action-generatoren som en egen funksjon. Denne kjøres deretter
+ * når det asynkrone kallet, feks fra API'et er ferdigkjørt.
+ *
+ */
