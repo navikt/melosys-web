4142f774834220bded074bec9ceee91054d8d7b7
Terje Karlsen <terjeofnorway@gmail.com>
2018-04-03 09:32:29 +0200

    Begynt refactor.

--- src/ducks/saksbehandler.js
+++ src/ducks/saksbehandler.js
@@ -3,27 +3,8 @@ import * as Api from '../services/api';
 import { STATUS, doThenDispatch } from '../services/utils';
 
 // Actions
-export const OK = 'saksbehandler/OK';
-export const FEILET = 'saksbehandler/FEILET';
-export const PENDING = 'saksbehandler/PENDING';
 
-const initalState = {
-  status: STATUS.NOT_STARTED,
-  data: {},
-};
-// Reducer
-export default function reducer(state = initalState, action) {
-  switch (action.type) {
-    case PENDING:
-      return { ...state, status: STATUS.PENDING };
-    case FEILET:
-      return { ...state, status: STATUS.ERROR, data: action.data };
-    case OK:
-      return { ...state, status: STATUS.OK, data: action.data };
-    default:
-      return state;
-  }
-}
+
 
 // Action Creators
 export function hentSaksbehandler() {
