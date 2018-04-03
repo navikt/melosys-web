3fd688fe48171d6e270321fcb2a5a240ea6ad7af
Terje Karlsen <terjeofnorway@gmail.com>
2018-04-03 16:04:36 +0200

    Lagt til komponenter for hoyreside av forsiden.

---
+++ src/felles-komponenter/forside/journalforing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Nav from '../../utils/navFrontend';
+
+const Journalforing = props => {
+  return (
+    <Nav.Panel className="enkeltlinje__panel">
+      Innhold
+    </Nav.Panel>
+  );
+};
+
+export default Journalforing;
