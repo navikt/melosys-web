6b0624405ca95aaee94bf68d341f027c9fa49af1
Terje Karlsen <terjeofnorway@gmail.com>
2018-04-03 16:04:36 +0200

    Lagt til komponenter for hoyreside av forsiden.

---
+++ src/felles-komponenter/forside/logg.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Nav from '../../utils/navFrontend';
+
+const Logg = props => {
+  return (
+    <Nav.Panel className="enkeltlinje__panel">
+      Innhold
+    </Nav.Panel>
+  );
+};
+
+export default Logg;
