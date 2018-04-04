a6159c3e27199602f28c3441f01a390fcb0bd0ff
Terje Karlsen <terjeofnorway@gmail.com>
2018-04-04 13:17:09 +0200

    Fikset promise handling på dispatch av hentSaksbehandler.

--- src/startupDataLoader.js
+++ src/startupDataLoader.js
@@ -1,6 +1,11 @@
-import { saksbehandlerOperations } from './ducks/saksbehandler/';
+import { saksbehandlerOperations, saksbehandlerTypes } from './ducks/saksbehandler/';
 import { kodeverkOperations } from './ducks/kodeverk/';
 
 export default function loadInitialData(store) {
-  store.dispatch(saksbehandlerOperations.hentSaksbehandler().then(kodeverkOperations.hentKodeverk()));
+  store.dispatch(saksbehandlerOperations.hentSaksbehandler())
+    .then(response => {
+      if (response.type === saksbehandlerTypes.OK) {
+        store.dispatch(kodeverkOperations.hentKodeverk());
+      }
+    });
 }
