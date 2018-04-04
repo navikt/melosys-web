c53fb92c980943da684eb7c04ce3856c1c4e42db
Bjølseth, Are <are.bjolseth@nav.no>
2018-04-04 18:15:16 +0200

    Tweeking. La til Tittel med antall.

--- src/felles-komponenter/forside/statistikk.js
+++ src/felles-komponenter/forside/statistikk.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import PT from 'prop-types';
+import { connect } from 'react-redux';
 import * as Nav from '../../utils/navFrontend';
+import * as Oppgaver from '../../ducks/oppgaver';
 
 const Statistikk = props => {
+  const { antallSaker } = props;
   return (
     <Nav.Panel className="forside__sidepanel">
       <Nav.Undertittel>Statistikk</Nav.Undertittel>
+      <h3>Mine Saker</h3>
+      <h1>{antallSaker}</h1>
     </Nav.Panel>
   );
 };
+Statistikk.propTypes = {
+  antallSaker: PT.number.isRequired,
+};
+const mapStateToProps = state => ({
+  antallSaker: Oppgaver.oppgaverSelectors.MineSakerAntallSelector(state),
+});
 
-export default Statistikk;
+export default connect(mapStateToProps)(Statistikk);
