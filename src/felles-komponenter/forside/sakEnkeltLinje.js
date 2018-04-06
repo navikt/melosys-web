e510efed3a8e49380f614611b5553aa21aa037f1
Terje Karlsen <terjeofnorway@gmail.com>
2018-04-06 10:49:25 +0200

    Splittet SakEnkeltLinje for gjenbruk av både "mine saker" og "søk"

---
+++ src/felles-komponenter/forside/sakEnkeltLinje.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import * as MPT from '../../proptypes';
+import * as Ikoner from '../../resources/images';
+import * as Nav from '../../utils/navFrontend';
+
+import PanelHeader from '../panelHeader/panelHeader';
+import EnkeltDato from '../datoOmrade/enkeltDato';
+
+import './sakEnkeltLinje.css';
+
+/**
+ * Dette er enkeltlinjen for én sak som inneholder sakstittel og metadata
+ * for å gi saksbehandler en oversikt over sakens innhold før hun klikker
+ * seg inn på den.
+ */
+const SakEnkeltLinje = ({ sak }) => {
+  const {
+    sammensattNavn, sakstype, saksnummer, behandling, aktivTil, soknadsperiode,
+  } = sak;
+
+  const { status } = behandling;
+  const { fom, tom } = soknadsperiode;
+  const tittel = `${sakstype.term} - ${sammensattNavn}`;
+  const link = `/saksbehandling/${saksnummer}`;
+
+  return (
+    <Link to={link} className="sakEnkeltLinje__link">
+      <Nav.Panel className="sakEnkeltLinje">
+        <PanelHeader
+          ikon={Ikoner.IkonSak}
+          tittel={tittel}
+          undertittel={
+            <Nav.Row>
+              <Nav.Column xs="12" md="6">
+                <dl className="sakEnkeltLinje__meta">
+                  <dt className="sakEnkeltLinje__meta__term">Status:</dt>
+                  <dd className="sakEnkeltLinje__meta__detalj">{status.term}</dd>
+                  <dt className="sakEnkeltLinje__meta__term">Frist:</dt>
+                  <dd className="sakEnkeltLinje__meta__detalj">{aktivTil}</dd>
+                </dl>
+              </Nav.Column>
+              <Nav.Column xs="12" md="6">
+                <dl className="sakEnkeltLinje__meta">
+                  <dt className="sakEnkeltLinje__meta__term">Søknadsperiode: </dt>
+                  <dd className="sakEnkeltLinje__meta__detalj"><EnkeltDato dato={fom} /> - <EnkeltDato dato={tom} /></dd>
+                  <dt className="sakEnkeltLinje__meta__term">Land:</dt>
+                  <dd className="sakEnkeltLinje__meta__detalj">TODO fra søknaden</dd>
+                </dl>
+              </Nav.Column>
+            </Nav.Row>
+          }
+        />
+      </Nav.Panel>
+    </Link>
+  );
+};
+
+SakEnkeltLinje.propTypes = {
+  sak: MPT.MinSak,
+};
+
+SakEnkeltLinje.defaultProps = {
+  sak: {},
+};
+
+export default SakEnkeltLinje;
