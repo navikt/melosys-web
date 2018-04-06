087b796f3e585337e9cb4bc9b822bf37bcde6edd
Bjølseth, Are <are.bjolseth@nav.no>
2018-04-06 14:52:36 +0200

    Splittet ut soknadDokument, behandlingsOppgave, journalforingOppgave til separate filer.

---
+++ src/debug/mock/behandlingsoppgave.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export const behandlingsoppgave = {
+  oppgavetype: 'BEH_SAK',
+  sakstyper: [
+    'EU_EOS',
+    'TRYGDAVTALE',
+    'FOLKETRYGD',
+  ],
+  behandlingstyper: [
+    'ae0034',
+    'ae0058',
+  ],
+};
