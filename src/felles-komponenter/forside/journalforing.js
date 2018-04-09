78238a07ae4f1fbb7f4ada3c14eaf892a1d82541
Terje Karlsen <terjeofnorway@gmail.com>
2018-04-09 16:37:52 +0200

    Fikset linting i logg og journalføring.

--- src/felles-komponenter/forside/journalforing.js
+++ src/felles-komponenter/forside/journalforing.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import * as Nav from '../../utils/navFrontend';
 
-const Journalforing = props => {
-  return (
-    <Nav.Panel className="forside__sidepanel">
-      <Nav.Systemtittel>Journalføring</Nav.Systemtittel>
-    </Nav.Panel>
-  );
-};
+const Journalforing = () => (
+  <Nav.Panel className="forside__sidepanel">
+    <Nav.Systemtittel>Journalføring</Nav.Systemtittel>
+  </Nav.Panel>
+);
 
 export default Journalforing;
