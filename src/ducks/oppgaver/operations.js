113f4e336690562d92414bba23ae0148e40724b2
Bjølseth, Are <are.bjolseth@nav.no>
2018-04-11 17:52:57 +0200

    Work in progress

--- src/ducks/oppgaver/operations.js
+++ src/ducks/oppgaver/operations.js
@@ -31,9 +31,9 @@ export function oppgavePlukker(oppgavetype, checkboxliste) {
   };
 
   return Api.sendPlukkOppgave(oppgave).then(response => {
-    const { saksnummer, journalPostID } = response;
+    const { saksnummer, journalpostID } = response;
     const saksbehandling = `/saksbehandling/${saksnummer}`;
-    const journalforing = `/journalforing/${journalPostID}`;
+    const journalforing = `/journalforing/${journalpostID}`;
     return oppgavetype === 'BEH_SAK' ? saksbehandling : journalforing;
   });
 }
