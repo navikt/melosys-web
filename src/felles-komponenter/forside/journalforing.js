c1ca327ebb1ff93b1f7c352a8c10c5c07c43cebb
Bjølseth, Are <are.bjolseth@nav.no>
2018-04-11 17:52:57 +0200

    Work in progress

--- src/felles-komponenter/forside/journalforing.js
+++ src/felles-komponenter/forside/journalforing.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import PT from 'prop-types';
+import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { withRouter } from 'react-router-dom';
+import PT from 'prop-types';
+
 import * as Nav from '../../utils/navFrontend';
 import { oppgaverOperations } from '../../ducks/oppgaver';
 
-const Journalforing = props => {
-  const { handleSubmit } = props;
-  return (
-    <Nav.Panel className="forside__sidepanel">
-      <Nav.Systemtittel>Journalføring</Nav.Systemtittel>
-      <form onSubmit={handleSubmit}>
-        <Nav.Knapp>Journalføring</Nav.Knapp>
-      </form>
-    </Nav.Panel>
-  );
-};
+class Journalforing extends Component {
+  submitOgVideresend = form => {
+    this.props.handleSubmit(form).then(redirectURL => this.props.history.push(redirectURL));
+  };
+  render() {
+    return (
+      <Nav.Panel className="forside__sidepanel">
+        <Nav.Systemtittel>Journalføring</Nav.Systemtittel>
+        <form onSubmit={this.submitOgVideresend}>
+          <Nav.Knapp>Journalføring</Nav.Knapp>
+        </form>
+      </Nav.Panel>
+    );
+  }
+}
+
 Journalforing.propTypes = {
   handleSubmit: PT.func.isRequired,
+  history: PT.object.isRequired,
 };
 
 export default reduxForm({
   form: 'journalforingsform',
   onSubmit: () => oppgaverOperations.oppgavePlukker('JFR', ['SKND', 'UFM', 'KLG', 'REV', 'ML_U', 'PS_U']),
-})(Journalforing);
+})(withRouter((Journalforing)));
