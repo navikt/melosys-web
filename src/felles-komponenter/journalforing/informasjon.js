cc45a27b9c70ab72855f7681e246a73c94cadaa4
Terje Karlsen <terjeofnorway@gmail.com>
2018-04-11 12:02:39 +0200

    Satt opp grunnleggende rammeverk for journalføring og knyttet opp ReduxForm.

---
+++ src/felles-komponenter/journalforing/informasjon.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PT from 'prop-types';
+import './informasjon.css';
+
+/** Denne komponenten inneholder skjemafelter nødvendig for journalføringen
+ * slik som informasjon om bruker, informasjon om dokument etc.
+ * @returns {*}
+ * @constructor
+ */
+const Informasjon = () => {
+  return (
+    <div>Informasjonsskjema her</div>
+  );
+};
+
+Informasjon.propTypes = {};
+
+Informasjon.defaultProps = {};
+
+export default Informasjon;
