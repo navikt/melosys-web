f05122fce40bcd11a48f7feab277cda5e6fc9cc3
Terje Karlsen <terjeofnorway@gmail.com>
2018-04-12 13:23:12 +0200

    Lagt inn skjema-felter for person og dokumentinformasjon.

--- src/felles-komponenter/journalforing/informasjon.js
+++ src/felles-komponenter/journalforing/informasjon.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import * as Skjema from '../skjema/';
+import * as Nav from '../../utils/navFrontend';
+
 import './informasjon.css';
 
 /** Denne komponenten inneholder skjemafelter nødvendig for journalføringen
@@ -7,9 +12,46 @@ import './informasjon.css';
  * @constructor
  */
 const Informasjon = () => {
-  const tekst = 'Informasjonsskjema her. Dette er en placeholdertekst.';
   return (
-    <div>{tekst}</div>
+    <div className="informasjon">
+      <Nav.Fieldset legend="Informasjon om brukeren">
+        <Skjema.Input feltNavn="brukersPersonnummer" label="Brukers personnummer" />
+        <Skjema.Input feltNavn="brukersFornavn" label="Brukers fornavn" />
+        <Skjema.Input feltNavn="brukersEtternavn" label="Brukers etternavn" />
+        <Skjema.Checkbox feltNavn="brukerErAvsender" label="Bruker er avsender" />
+        <Skjema.Input feltNavn="avsenderFnrOrgnr" label="Avsender fødselsnummmer / Organisasjonsnummer" />
+        <Skjema.Input feltNavn="avsenderFornavn" label="Avsender fornavn" />
+        <Skjema.Input feltNavn="avsenderEtternavn" label="Avsender etternavn" />
+      </Nav.Fieldset>
+      <Nav.Fieldset legend="Informasjon om dokument">
+        <Link to="/foo/bar.pdf">26.04.2018: Kort navn på dokumentet</Link>
+        <Skjema.Select feltNavn="dokumentKategori" label="Dokumentkategori">
+          <option value="kategori1">Kategori 1</option>
+          <option value="kategori2">Kategori 2</option>
+          <option value="kategori3">Kategori 3</option>
+        </Skjema.Select>
+        <Skjema.Select feltNavn="saksType" label="Sakstype">
+          <option value="kategori1">Kategori 1</option>
+          <option value="kategori2">Kategori 2</option>
+          <option value="kategori3">Kategori 3</option>
+        </Skjema.Select>
+        <Skjema.Select feltNavn="tittel" label="Tittel">
+          <option value="kategori1">Kategori 1</option>
+          <option value="kategori2">Kategori 2</option>
+          <option value="kategori3">Kategori 3</option>
+        </Skjema.Select>
+        <Skjema.Select feltNavn="vedleggsTittel" label="Vedleggstittel / beskrivelse">
+          <option value="kategori1">Kategori 1</option>
+          <option value="kategori2">Kategori 2</option>
+          <option value="kategori3">Kategori 3</option>
+        </Skjema.Select>
+      </Nav.Fieldset>
+      <Skjema.Checkbox feltNavn="inneholderSensitivInfo" label="Inneholder sensitiv info" />
+      <div className="informasjon__knapper">
+        <Nav.Knapp>Åpne i Gosys</Nav.Knapp>
+        <Nav.Knapp>Avbryt</Nav.Knapp>
+      </div>
+    </div>
   );
 };
 
