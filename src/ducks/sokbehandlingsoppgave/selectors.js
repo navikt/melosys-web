4b3d8fed5bd84ae0b31b397d4f7a19219d8d27e7
Terje Karlsen <terjeofnorway@gmail.com>
2018-04-26 12:04:13 +0200

    Filtrerer oppgavesøk slik at kun behandlingsoppgaver vises.

--- src/ducks/sokbehandlingsoppgave/selectors.js
+++ src/ducks/sokbehandlingsoppgave/selectors.js
@@ -10,5 +10,8 @@ import { createSelector } from 'reselect';
 /* eslint-disable import/prefer-default-export */
 export const OppgaveSelector = createSelector(
   state => state.sokbehandlingsoppgave.data,
-  oppgaver => oppgaver
+  // TODO: Nedenfor er en midlertidig filter fix for å unngå at søkeresultatet
+  // viser journalføringsoppgaver. Denne skal egentlig filtreres backend, så denne vil bli
+  // overflødig. Vurderes fjernet. (Notat skrevet 26. april)
+  oppgaver => oppgaver.filter(oppgave => oppgave.oppgavetype.kode !== 'JFR')
 );
