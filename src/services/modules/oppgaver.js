6dae70b4bb8081aec6c1fffb75817af6efd40c99
Bjølseth, Are <are.bjolseth@nav.no>
2018-05-08 10:40:56 +0200

    Oppgaver: Oppdaterte navnekonvensjon for endepunkt

--- src/services/modules/oppgaver.js
+++ src/services/modules/oppgaver.js
@@ -1,26 +1,26 @@
 import { getAsJson, postAsJson } from '../utils';
 import { API_BASE_URL } from '../api-constants';
 
-function hentOppgaveOversikt() {
+function oversikt() {
   const URI_OPPGAVER_MINESAKER = `${API_BASE_URL}oppgaver/oversikt`;
   return getAsJson(URI_OPPGAVER_MINESAKER);
 }
-function sendPlukkOppgave(oppgave) {
+function send(oppgave) {
   const URI_OPPGAVER_PLUKK = `${API_BASE_URL}oppgaver/plukk`;
   return postAsJson(URI_OPPGAVER_PLUKK, oppgave);
 }
-function hentBehandlingsOppgaver(fnr) {
+function hent(fnr) {
   const URI_BEHANDLINGS_OPPGAVER = `${API_BASE_URL}oppgaver/sok/?fnr=${fnr}`;
   return getAsJson(URI_BEHANDLINGS_OPPGAVER);
 }
 
-function sparkResetOppgaver() {
+function sparkReset() {
   const URI_RESET = `${API_BASE_URL}oppgaver/reset`;
   return getAsJson(URI_RESET);
 }
 export {
-  hentOppgaveOversikt,
-  sendPlukkOppgave,
-  hentBehandlingsOppgaver,
-  sparkResetOppgaver,
+  oversikt,
+  send,
+  hent,
+  sparkReset,
 };
