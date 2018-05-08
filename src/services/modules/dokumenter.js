dd52c690f0b06ead2e686b893822275c21f92ed3
Bjølseth, Are <are.bjolseth@nav.no>
2018-05-08 10:31:26 +0200

    Dokumenter: Oppdaterte navnekonvensjon for endepunkt

--- src/services/modules/dokumenter.js
+++ src/services/modules/dokumenter.js
@@ -1,7 +1,7 @@
 import { API_BASE_URL } from '../api-constants';
 
 // eslint-disable-next-line import/prefer-default-export
-export function PDFDokumentURI(journalforingID, dokumentID) {
+export function hentPdfURI(journalforingID, dokumentID) {
   const URI_DOKUMENT_PDF = `${API_BASE_URL}dokumenter/pdf/${journalforingID}/${dokumentID}`;
   return URI_DOKUMENT_PDF;
 }
