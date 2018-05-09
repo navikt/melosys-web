36239cd61c272cafd0d8ff754ee2825cb1fc0358
Terje Karlsen <terjeofnorway@gmail.com>
2018-05-09 08:35:20 +0200

    Refactor av logikk for bedre lesbarhet i kode. Fjernet dobbel negativ i navning og return value.

--- src/felles-komponenter/skjema/validering/journalforing.js
+++ src/felles-komponenter/skjema/validering/journalforing.js
@@ -4,31 +4,47 @@ import * as Organisasjon from './generisk/organisasjon';
 /** Mikrovalidering pr hendelse. Dette gjør at vi kan både kan spisse tekstlig tilbakemelding
  * og validere på tvers av verdier.
  */
-const erIdOppgitt = verdier => ((verdier.brukerID === '') ? 'Tast inn fnr eller dnr.' : false);
-const erIdNummer = verdier => (Number.isNaN(verdier.brukerID) ? 'Tast inn kun nummer.' : false);
-const erIdFnr = verdier => (!Person.erFnr(verdier.brukerID) ? 'Tast inn gyldig fødselsnummer' : false);
-const eksistererId = ({ avsenderNavn, avsenderID }) => {
-  if (avsenderNavn === '' && Organisasjon.erOrgnr(avsenderID)) {
+const idErBlank = verdi => ((verdi === '') && 'Tast inn fnr eller dnr.');
+const idErIkkeNummer = verdi => (!(new RegExp(/^\d+$/).test(verdi)) && 'Tast inn kun nummer.');
+const erIdErIkkeFnrEllerDnr = verdi => ((!Person.erFnr(verdi) && !Person.erDnr(verdi)) && 'Tast inn gyldig fnr eller dnr.');
+const idFinnesIkke = (navn, id) => {
+  if (navn === '' && Organisasjon.erOrgnr(id)) {
     return 'Fant ingen navn på dette organisasjonsnummeret.';
   }
-  if (avsenderNavn === '' && (Person.erFnr(avsenderID) || Person.erDnr(avsenderID))) {
+  if (navn === '' && (Person.erFnr(id) || Person.erDnr(id))) {
     return 'Fant ingen navn på dette fnr eller dnr.';
   }
-  return false;
+  return null;
 };
-const dokumentTittelErOppgitt = verdier => (verdier.dokumentTittel.length === 0 ? 'Velg dokumenttittel fra listen eller skriv din egen.' : false);
-const minstEnVedleggTittelErOppgitt = ({ vedleggsTitler = [] }) => (vedleggsTitler.length === 0 ? 'Velg minst én vedleggstittel eller skriv inn din egen' : false);
-const eksisterendeSakErValgt = verdier => (!verdier.saksnummer ? 'velg sak' : false);
+
+const dokumentTittelErBlank = verdier => (verdier.dokumentTittel.length === 0 ? 'Velg dokumenttittel fra listen eller skriv din egen.' : false);
+const vedleggTittelErBlank = ({ vedleggsTitler = [] }) => (vedleggsTitler.length === 0 ? 'Velg minst én vedleggstittel eller skriv inn din egen' : false);
+const eksisterendeSakIkkeValgt = verdier => (!verdier.saksnummer ? 'velg sak' : false);
 
 /** Ved å short circuite igjennom alle forutsetninger helt til den siste som returnerer false,
- * kan vi bygge opp sjekk per felt-navn. Rekkefølgen har betydning med tanke på hvilken feilmelding
+ * kan vi bygge opp sjekk per felt-navn. Rekkefølgen har betydning med hensyn til hvilken feilmelding
  * som er relevant. Feks: feilmelding om at et felt er tomt skal vises før feilmelding om at fødselsnummer ikke er gyldig.
  */
-const JournalforingValidering = verdier => {
-  const brukerID = erIdOppgitt(verdier) || erIdNummer(verdier) || erIdFnr(verdier) || eksistererId(verdier) || false;
-  const avsenderID = erIdOppgitt(verdier) || erIdNummer(verdier) || erIdFnr(verdier) || eksistererId(verdier) || false;
-  const dokumentTittel = dokumentTittelErOppgitt(verdier) || false;
-  const vedleggsTitler = minstEnVedleggTittelErOppgitt(verdier) ? { _error: minstEnVedleggTittelErOppgitt(verdier) } : false;
+const JournalforingGenerellValidering = verdier => {
+  const brukerID = (
+    idErBlank(verdier.brukerID) ||
+    idErIkkeNummer(verdier.brukerID) ||
+    erIdErIkkeFnrEllerDnr(verdier.brukerID) ||
+    idFinnesIkke(verdier.brukerNavn, verdier.brukerID) ||
+    false
+  );
+
+  const avsenderID = (
+    idErBlank(verdier.avsenderID) ||
+    idErIkkeNummer(verdier.avsenderID) ||
+    erIdErIkkeFnrEllerDnr(verdier.avsenderID) ||
+    idFinnesIkke(verdier.avsenderNavn, verdier.avsenderID) ||
+    false
+  );
+
+  const dokumentTittel = dokumentTittelErBlank(verdier) || false;
+
+  const vedleggsTitler = vedleggTittelErBlank(verdier) ? { _error: vedleggTittelErBlank(verdier) } : false;
 
   const valideringsObjekt = {
     brukerID,
@@ -41,7 +57,7 @@ const JournalforingValidering = verdier => {
 };
 
 export {
-  eksisterendeSakErValgt,
+  eksisterendeSakIkkeValgt,
 };
 
-export default JournalforingValidering;
+export default JournalforingGenerellValidering;
