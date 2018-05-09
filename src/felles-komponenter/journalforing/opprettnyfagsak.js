96848ee1ce0bd6bcd828e05a337baccbf2e14f34
Terje Karlsen <terjeofnorway@gmail.com>
2018-05-09 07:19:11 +0200

    Fjernet sakstype siden dette kan utledes backend.

--- src/felles-komponenter/journalforing/opprettnyfagsak.js
+++ src/felles-komponenter/journalforing/opprettnyfagsak.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import PT from 'prop-types';
 
@@ -9,21 +8,12 @@ import * as Nav from '../../utils/navFrontend';
 import './opprettnyfagsak.css';
 import LandVelger from '../skjema/landvelger';
 
-import { KodeverkSelectors } from '../../ducks/kodeverk';
 
 const OpprettNyFagSak = props => {
-  const { opprettNyFagsakSubmit, valgbareSakstyper } = props;
+  const { opprettNyFagsakSubmit } = props;
   return (
     <div className="opprettnysak">
       <Nav.Systemtittel>Opprett ny sak</Nav.Systemtittel>
-      <Nav.Column xs="12">
-        <Skjema.ListeVelger
-          feltNavn="journalforingSakstype"
-          label="Sakstype:"
-          placeholder="(velg eller skriv inn egen tittel)"
-          muligeValg={valgbareSakstyper}
-        />
-      </Nav.Column>
       <Nav.Fieldset legend="Soknadperiode">
         <Nav.Column xs="4">
           <Skjema.Input datoFelt label="Fra" feltNavn="journalforingPeriodeFraOgMed" />
@@ -46,15 +36,6 @@ const OpprettNyFagSak = props => {
 
 OpprettNyFagSak.propTypes = {
   opprettNyFagsakSubmit: PT.func.isRequired,
-  valgbareSakstyper: PT.array,
 };
 
-OpprettNyFagSak.defaultProps = {
-  valgbareSakstyper: [],
-};
-
-const mapStateToProps = state => ({
-  valgbareSakstyper: KodeverkSelectors.sakstyperSelector(state),
-});
-
-export default connect(mapStateToProps)(OpprettNyFagSak);
+export default OpprettNyFagSak;
