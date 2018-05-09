f52beb0b644a463f394f71c263d1bd67a4a47267
Terje Karlsen <terjeofnorway@gmail.com>
2018-05-09 12:01:49 +0200

    Rettet feil i valideringstekst. Rettet mergekonflikt.

--- src/felles-komponenter/skjema/validering/person.js
+++ src/felles-komponenter/skjema/validering/person.js
@@ -4,11 +4,23 @@ const fulltNavn = value => {
 };
 
 const fnr = value => {
-  const regex = /^\d{9,9}$/;
-  return (value.search(regex) ? 'Fødselsnummer må bestå av 9 siffer' : null);
+  const regex = /^\d{11,11}$/;
+  return (value.search(regex) ? 'Fødselsnummer må bestå av 11 siffer' : null);
+};
+
+const erFnr = verdi => {
+  const regex = RegExp(/^\d{11,11}$/);
+  return regex.test(verdi);
+};
+
+const erDnr = verdi => {
+  const regex = RegExp(/^\d{11,11}$/);
+  return regex.test(verdi);
 };
 
 export {
   fulltNavn,
   fnr,
+  erFnr,
+  erDnr,
 };
