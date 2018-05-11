3b9ec2e4c9561eeb5e750f41c6bd6f1f8f74de16
Terje Karlsen <terjeofnorway@gmail.com>
2018-05-11 14:13:01 +0200

    Lagt inn manuell validering for å overkomme props update race cond.

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,8 @@
 export const ANTALL_TALL_I_FNR = 11;
 export const ANTALL_TALL_I_DNR = 11;
 export const ANTALL_TALL_I_ORGNR = 9;
+
+export const JOURNALFORING_HENSIKT = {
+  KNYTT: 'KNYTT',
+  OPPRETT: 'OPPRETT',
+};
