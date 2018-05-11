7278f18f5dac2008bb59acbe1a9a87ce7ae9e6e3
abjolseth <abjolseth@gmail.com>
2018-05-11 11:08:14 +0200

    La til cachedGetAsJson wrapper for fetch, med bruk av localStorage

--- src/services/modules/kodeverk.js
+++ src/services/modules/kodeverk.js
@@ -1,8 +1,8 @@
-import { getAsJson } from '../utils';
+import {getAsJson, cachedGetAsJson } from '../utils';
 import { API_BASE_URL } from '../api-constants';
 
 // eslint-disable-next-line import/prefer-default-export
 export function hent() {
   const URI_KODEVERK = `${API_BASE_URL}kodeverk`;
-  return getAsJson(URI_KODEVERK);
+  return cachedGetAsJson(URI_KODEVERK);
 }
