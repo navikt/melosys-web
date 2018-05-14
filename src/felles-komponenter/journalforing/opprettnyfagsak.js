f0ef53f540d614d3633aa60012b9600166e4a23a
Terje Karlsen <terjeofnorway@gmail.com>
2018-05-14 08:53:01 +0200

    Diverse korrigeringer i styling og layout.

--- src/felles-komponenter/journalforing/opprettnyfagsak.js
+++ src/felles-komponenter/journalforing/opprettnyfagsak.js
@@ -13,18 +13,22 @@ const OpprettNyFagSak = props => {
   return (
     <div className="opprettnysak">
       <Nav.Systemtittel>Opprett ny sak</Nav.Systemtittel>
-      <Nav.Fieldset legend="Soknadperiode">
-        <Nav.Column xs="4">
-          <Skjema.Input datoFelt label="Fra" feltNavn="journalforingPeriodeFraOgMed" />
-        </Nav.Column>
-        <Nav.Column xs="4">
-          <Skjema.Input datoFelt label="Til" feltNavn="journalforingPeriodeTilOgMed" />
-        </Nav.Column>
+      <Nav.Fieldset legend="Soknadperiode:" className="opprettnysak__soknadsperiode">
+        <Nav.Row>
+          <Nav.Column xs="6">
+            <Skjema.Input datoFelt label="Fra" feltNavn="journalforingPeriodeFraOgMed" />
+          </Nav.Column>
+          <Nav.Column xs="6">
+            <Skjema.Input datoFelt label="Til" feltNavn="journalforingPeriodeTilOgMed" />
+          </Nav.Column>
+        </Nav.Row>
       </Nav.Fieldset>
       <Nav.Fieldset legend="Land:">
-        <Nav.Column xs="12">
-          <LandVelger feltNavn="journalforingOppholdsLand" multiLand />
-        </Nav.Column>
+        <Nav.Row>
+          <Nav.Column xs="12">
+            <LandVelger feltNavn="journalforingOppholdsLand" multiLand />
+          </Nav.Column>
+        </Nav.Row>
       </Nav.Fieldset>
       <div className="opprettnysak__knapper">
         <Nav.Knapp className="knapp" onClick={opprettFagsak}>Opprett ny sak</Nav.Knapp>
