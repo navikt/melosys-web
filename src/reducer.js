ada16dc435a6389c125a120fb7434166bb770c28
Bjølseth, Are <are.bjolseth@nav.no>
2018-05-15 14:06:22 +0200

    Oppdatert Kodeverk med 6 timers cacheDuration. La til nytt endepunkt /api/inngang/:snr

--- src/reducer.js
+++ src/reducer.js
@@ -2,26 +2,28 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'react-redux-form-validation';
 
 import fagsakerReducer from './ducks/fagsaker/';
-import soknadReducer from './ducks/soknad/';
 import faktaavklaringReducer from './ducks/faktaavklaring/';
-import vurderingReducer from './ducks/vurdering/';
-import saksbehandlerReducer from './ducks/saksbehandler/';
-import kodeverkReducer from './ducks/kodeverk';
-import oppgaverReducer from './ducks/oppgaver';
+import inngangReducer from './ducks/inngang';
 import journalforingReducer from './ducks/journalforing';
+import kodeverkReducer from './ducks/kodeverk';
+import saksbehandlerReducer from './ducks/saksbehandler/';
+import oppgaverReducer from './ducks/oppgaver';
 import organisasjonReducer from './ducks/organisasjon';
 import personReducer from './ducks/person';
+import soknadReducer from './ducks/soknad/';
+import vurderingReducer from './ducks/vurdering/';
 
 export default combineReducers({
   form: formReducer,
   fagsaker: fagsakerReducer,
-  soknad: soknadReducer,
   faktaavklaring: faktaavklaringReducer,
-  vurdering: vurderingReducer,
-  saksbehandler: saksbehandlerReducer,
-  kodeverk: kodeverkReducer,
-  oppgaver: oppgaverReducer,
+  inngang: inngangReducer,
   journalforing: journalforingReducer,
+  kodeverk: kodeverkReducer,
+  saksbehandler: saksbehandlerReducer,
+  soknad: soknadReducer,
+  oppgaver: oppgaverReducer,
   organisasjon: organisasjonReducer,
   person: personReducer,
+  vurdering: vurderingReducer,
 });
