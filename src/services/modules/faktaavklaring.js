ca4713a13b410c08b18c8b902f8ff653dcbfe138
Bjølseth, Are <are.bjolseth@nav.no>
2018-05-16 15:59:01 +0200

    MELOSYS-1077 Manuell bostedsvurdering yrkesaktive

--- src/services/modules/faktaavklaring.js
+++ src/services/modules/faktaavklaring.js
@@ -1,17 +1,24 @@
 import { getAsJson, postAsJson } from '../utils';
 import { API_BASE_URL } from '../api-constants';
 
+const endepunkt = 'faktaavklaring';
+
 function hent(bid) {
-  const URI_FAKTAAVKLARING = `${API_BASE_URL}faktaavklaring/${bid}`;
+  const URI_FAKTAAVKLARING = `${API_BASE_URL}${endepunkt}/${bid}`;
   return getAsJson(URI_FAKTAAVKLARING);
 }
 
 function send(bid, dokument) {
-  const URI_FAKTAAVKLARING = `${API_BASE_URL}faktaavklaring/${bid}`;
+  const URI_FAKTAAVKLARING = `${API_BASE_URL}${endepunkt}/${bid}`;
   return postAsJson(URI_FAKTAAVKLARING, dokument);
 }
 
+function bosted(bid, data) {
+  const URI_FAKTAAVKLARING_BOSTED = `${API_BASE_URL}${endepunkt}/bosted/${bid}`;
+  return postAsJson(URI_FAKTAAVKLARING_BOSTED, data);
+}
 export {
   hent,
   send,
+  bosted,
 };
