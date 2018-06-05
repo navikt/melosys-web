9d297cc2d5f99c74ce89e80d0a9a49207e60ebd7
Terje Karlsen <terjeofnorway@gmail.com>
2018-06-05 09:12:02 +0200

    Skrevet unit test for person.

--- src/felles-komponenter/skjema/validering/generisk/person.js
+++ src/felles-komponenter/skjema/validering/generisk/person.js
@@ -3,21 +3,51 @@ const fulltNavn = value => {
   return value && value.search(regex) ? 'Du må skrive inn både fornavn og etternavn.' : null;
 };
 
-const erFnr = verdi => {
+const erFnrLengde = verdi => {
   const regex = RegExp(/^\d{11,11}$/);
   return regex.test(verdi);
 };
 
-const fnr = value => (!erFnr(value) ? 'Fødselsnummer må bestå av 9 siffer.' : null);
-
-const erDnr = verdi => {
+const erDnrLengde = verdi => {
   const regex = RegExp(/^\d{11,11}$/);
   return regex.test(verdi);
 };
+
+const erGyldigFnr = verdi => {
+  const fodselsnr = verdi.toString();
+  if (!fodselsnr || fodselsnr.length !== 11) return false;
+
+  const vekt1 = [3, 7, 6, 1, 8, 9, 4, 5, 2];
+  const vekt2 = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2];
+
+  const Q1 = vekt1
+    .reduce((samling, tall, indeks) => (samling + (parseInt(fodselsnr.charAt(indeks), 10) * tall)), 0);
+
+  const Q2 = vekt2
+    .reduce((samling, tall, indeks) => (samling + (parseInt(fodselsnr.charAt(indeks), 10) * tall)), 0);
+
+  let k1 = 11 - (Q1 % 11);
+  if (k1 === 11) k1 = 0;
+
+  let k2 = 11 - (Q2 % 11);
+  if (k2 === 11) k2 = 0;
+
+  return k1 === parseInt(fodselsnr.charAt(9), 10)
+    && k2 === parseInt(fodselsnr.charAt(10), 10);
+};
+
+/** Validering av d-nummer kjører også mod 11 med samme vekter som fnr.
+ * Gjenbruk derfor erGyldigFnr.
+ *
+ * @param verdi {string} Fødselsnummer
+ * @returns {boolean|*}
+ */
+const erGyldigDnr = verdi => erGyldigFnr(verdi);
 
 export {
   fulltNavn,
-  fnr,
-  erFnr,
-  erDnr,
+  erFnrLengde,
+  erDnrLengde,
+  erGyldigFnr,
+  erGyldigDnr,
 };
--- src/felles-komponenter/skjema/validering/generisk/person.test.js
+++ src/felles-komponenter/skjema/validering/generisk/person.test.js
@@ -18,24 +18,92 @@ describe('Tester person.js:', () => {
     })
   })
 
-  describe('fnr', () => {
-    test('feiler dersom verdi er mindre enn 11', () => {
-      const mockData = '0123456783';
-      expect(Person.fnr(mockData)).toBe('Fødselsnummer må bestå av 9 siffer.');
+  describe('erGyldigFnr', () => {
+    test('returnerer false ved feil fødselsnummer', () => {
+      const mockData1 = '22222222222';
+      const mockData2 = '31025043514';
+      const mockData3 = '01010533445';
+      const forventetFeil = false;
+      expect(Person.erGyldigFnr(mockData1)).toEqual(forventetFeil);
+      expect(Person.erGyldigFnr(mockData2)).toEqual(forventetFeil);
+      expect(Person.erGyldigFnr(mockData3)).toEqual(forventetFeil);
+    });
+
+    test('returnerer true ved riktig fødselsnummer', () => {
+      const mockData1 = '19117220349'; //Glitrende Hatt
+      const mockData2 = '21027500134'; //Blå Konsoll
+      const mockData3 = '22127501762'; //Rask Kaffi
+      const mockData4 = '02025618273'; //Artig Saks
+      const mockData5 = '31057406782'; //Naturlig Maskin
+
+      const forventetFeil = true;
+      expect(Person.erGyldigFnr(mockData1)).toEqual(forventetFeil);
+      expect(Person.erGyldigFnr(mockData2)).toEqual(forventetFeil);
+      expect(Person.erGyldigFnr(mockData3)).toEqual(forventetFeil);
+      expect(Person.erGyldigFnr(mockData4)).toEqual(forventetFeil);
+      expect(Person.erGyldigFnr(mockData5)).toEqual(forventetFeil);
+    });
+  })
+
+  describe('erFnrLengde', () => {
+    test('returnerer false når fnr ikke er 11 siffer.', () => {
+      const mockData1 = '1111111111';
+      const mockData2 = '111111111111';
+      const forventet = false;
+
+      expect(Person.erFnrLengde(mockData1)).toBe(forventet);
+      expect(Person.erFnrLengde(mockData2)).toBe(forventet);
     })
 
-    test('feiler dersom verdi er større enn 11', () => {
-      const mockData = '0123456783';
-      expect(Person.fnr(mockData)).toBe('Fødselsnummer må bestå av 9 siffer.');
+    test('returnerer true når fnr er 11 siffer.', () => {
+      const mockData = '11111111111';
+      const forventet = true;
+      expect(Person.erFnrLengde(mockData)).toEqual(forventet)
+    })
+  })
+
+  describe('erDnrLengde', () => {
+    test('returnerer false når fnr ikke er 11 siffer.', () => {
+      const mockData1 = '1111111111';
+      const mockData2 = '111111111111';
+      const forventet = false;
+
+      expect(Person.erDnrLengde(mockData1)).toBe(forventet);
+      expect(Person.erDnrLengde(mockData2)).toBe(forventet);
     })
 
-    test('', () => {
-      const mockData1 = 'Ola Nordmann';
-      const mockData2 = 'Ola Jensenius Nordmann';
-      const mockData3 = 'Ola Jensenius Hansen Nordmann';
-      expect(Person.fulltNavn(mockData1)).toBe(null);
-      expect(Person.fulltNavn(mockData2)).toBe(null);
-      expect(Person.fulltNavn(mockData3)).toBe(null);
+    test('returnerer true når fnr er 11 siffer.', () => {
+      const mockData = '11111111111';
+      const forventet = true;
+      expect(Person.erDnrLengde(mockData)).toEqual(forventet)
     })
+  })
+
+  describe('erGyldigDnr', () => {
+    test('returnerer false ved feil dnr', () => {
+      const mockData1 = '22222222222';
+      const mockData2 = '31025043514';
+      const mockData3 = '01010533445';
+      const forventetFeil = false;
+      expect(Person.erGyldigDnr(mockData1)).toEqual(forventetFeil);
+      expect(Person.erGyldigDnr(mockData2)).toEqual(forventetFeil);
+      expect(Person.erGyldigDnr(mockData3)).toEqual(forventetFeil);
+    });
+
+    test('returnerer true ved riktig dnr', () => {
+      const mockData1 = '59117220332'; //Glitrende Hatt + 4
+      const mockData2 = '61027500128'; //Blå Konsoll + 4
+      const mockData3 = '62127501756'; //Rask Kaffi + 4
+      const mockData4 = '42025618267'; //Artig Saks + 4
+      const mockData5 = '71057406776'; //Naturlig Maskin + 4
+
+      const forventetFeil = true;
+      expect(Person.erGyldigDnr(mockData1)).toEqual(forventetFeil);
+      expect(Person.erGyldigDnr(mockData2)).toEqual(forventetFeil);
+      expect(Person.erGyldigDnr(mockData3)).toEqual(forventetFeil);
+      expect(Person.erGyldigDnr(mockData4)).toEqual(forventetFeil);
+      expect(Person.erGyldigDnr(mockData5)).toEqual(forventetFeil);
+      //expect(Person.erGyldigDnr(mockData6)).toEqual(forventetFeil);
+    });
   })
 });
