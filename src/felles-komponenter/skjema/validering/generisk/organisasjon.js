19f4ece6a5034587f7bf5b06b15cbbc06d33380e
Terje Karlsen <terjeofnorway@gmail.com>
2018-06-05 09:23:49 +0200

    Begynt unit test for organisasjon.

--- src/felles-komponenter/skjema/validering/generisk/organisasjon.js
+++ src/felles-komponenter/skjema/validering/generisk/organisasjon.js
@@ -1,4 +1,4 @@
-const erOrgnr = verdi => RegExp(/^\d{9,9}$/).test(verdi);
+const erOrgnrLengde = verdi => RegExp(/^\d{9,9}$/).test(verdi);
 
 /* eslint import/prefer-default-export:off */
-export { erOrgnr };
+export { erOrgnrLengde };
