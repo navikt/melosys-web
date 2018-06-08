5633b27ef5aaf2ee97c2a5d330c42178625f80f1
Terje Karlsen <terjeofnorway@gmail.com>
2018-06-08 15:37:10 +0200

    Endelig fikset spyOn-issue.

--- src/felles-komponenter/skjema/validering/felles.js
+++ src/felles-komponenter/skjema/validering/felles.js
@@ -6,128 +6,120 @@ import { feltGrupper } from '../../../utils/panelFelter';
  * valideringer som vi må forholde oss til.
  * --------------------------------------------------------------
  */
-export const inneholderFeilmeldinger = (data = {}) => {
-  const { form = { } } = data;
-  const { feilmeldinger = [] } = form;
-  return feilmeldinger.length > 0;
-};
 
-export const byggValideringsObjekt = data => {
-  const { form: { feilmeldinger } = {} } = data;
-  const feltIDListe = feilmeldinger.reduce((samling, feilmelding) => (
-    { ...samling, [feilmelding.skjemaFeltID]: feilmelding.melding }
-  ), {});
-  return feltIDListe;
-};
+class Felles {
+  static inneholderFeilmeldinger = (data = {}) => {
+    const { form = {} } = data;
+    const { feilmeldinger = [] } = form;
+    return feilmeldinger.length > 0;
+  };
 
-export const forsokValidering = (dispatch, data) => {
-  if (inneholderFeilmeldinger(data)) {
-    const valideringsObjekt = byggValideringsObjekt(data);
-    formOperations.oppdaterAlleSkjemaValideringer(dispatch)(valideringsObjekt);
-  }
-};
+  static byggValideringsObjekt = data => {
+    const { form: { feilmeldinger } = {} } = data;
+    const feltIDListe = feilmeldinger.reduce((samling, feilmelding) => (
+      { ...samling, [feilmelding.skjemaFeltID]: feilmelding.melding }
+    ), {});
+    return feltIDListe;
+  };
 
-/** -------------------------------------------------------------
- * 3 neste funksjoner brukes av utils for å sjekke om motatte data inneholder
- * valideringer som vi må forholde oss til.
- * --------------------------------------------------------------
- */
-
-/** Slår feltGrupper (nøstede) sammen til ett-nivås-objekt.
- */
-const flatUtFeltGrupper = grupper => (Object.keys(grupper).reduce(
-  (samling, gruppe) =>
-    ({ ...samling, ...grupper[gruppe] })
-  , {}
-));
-
-/** Valideringer ligger som arrays av funksjoner i panelGrupper. Disse må kjøres individuelt
- * og parses inn til string for å være gyldige Redux Form-valideringer.
- */
-const kjorAlleValideringerSomHarFunksjoner = (valideringer, values, props) => (
-  Object.keys(valideringer).reduce((samling, feltNavn) => {
-    const faktiskVerdi = values[feltNavn];
-
-    // Hvis Array (av funksjoner)
-    if (Array.isArray(valideringer[feltNavn])) {
-      const ferdigValideringString = valideringer[feltNavn]
-        .map(f => f(faktiskVerdi, props))
-        .join(' ');
-      return { ...samling, [feltNavn]: ferdigValideringString };
+  static forsokValidering = (dispatch, data) => {
+    if (Felles.inneholderFeilmeldinger(data)) {
+      const valideringsObjekt = Felles.byggValideringsObjekt(data);
+      formOperations.oppdaterAlleSkjemaValideringer(dispatch)(valideringsObjekt);
     }
+  };
 
-    // Hvis kun én funksjon (dvs ikke i form av en array)
-    if (typeof valideringer[feltNavn] === 'function') {
-      return { ...samling, [feltNavn]: valideringer[feltNavn](faktiskVerdi) };
-    }
+  /** -------------------------------------------------------------
+   * 3 neste funksjoner brukes av utils for å sjekke om motatte data inneholder
+   * valideringer som vi må forholde oss til.
+   * --------------------------------------------------------------
+   */
 
-    // Hvis faktisk string (som er feil, siden den alltid vil validere negativt)
-    if (typeof valideringer[feltNavn] === 'string') {
-      return { ...samling, [feltNavn]: valideringer[feltNavn] };
-    }
+  /** Slår feltGrupper (nøstede) sammen til ett-nivås-objekt.
+   */
+  static flatUtFeltGrupper = grupper => (Object.keys(grupper).reduce(
+    (samling, gruppe) =>
+      ({ ...samling, ...grupper[gruppe] })
+    , {}
+  ));
 
-    return { ...samling };
-  }, {})
-);
+  /** Valideringer ligger som arrays av funksjoner i panelGrupper. Disse må kjøres individuelt
+   * og parses inn til string for å være gyldige Redux Form-valideringer.
+   */
+  static kjorAlleValideringerSomHarFunksjoner = (valideringer, values, props) => (
+    Object.keys(valideringer).reduce((samling, feltNavn) => {
+      const faktiskVerdi = values[feltNavn];
 
-/** Fletter to objekter og kombinerer verdiene i tilfeller hvor begge
- * objekter har samme nøkkel. I tillegg fjern nøkler som viser seg å ikke utløse noen
- * spesiell validering.
- */
-const flettOgFilterValidering = (generiskValidering, forretningsValidering = {}) => {
-  const samletValidering = { ...generiskValidering };
-  const filtrertValidering = Object.keys(forretningsValidering).reduce((samling, feltNavn) => {
-    const valideringsTekst = samletValidering[feltNavn]
-      ?
-      `${samletValidering[feltNavn]} ${forretningsValidering[feltNavn]}`
-      :
-      `${forretningsValidering[feltNavn]}`;
+      // Hvis Array (av funksjoner)
+      if (Array.isArray(valideringer[feltNavn])) {
+        const ferdigValideringString = valideringer[feltNavn]
+          .map(f => f(faktiskVerdi, props))
+          .join(' ');
+        return { ...samling, [feltNavn]: ferdigValideringString };
+      }
 
-    return valideringsTekst !== '' ? { ...samling, [feltNavn]: valideringsTekst } : { ...samling };
-  }, {});
+      // Hvis kun én funksjon (dvs ikke i form av en array)
+      if (typeof valideringer[feltNavn] === 'function') {
+        return { ...samling, [feltNavn]: valideringer[feltNavn](faktiskVerdi) };
+      }
 
-  return filtrertValidering;
-};
+      // Hvis faktisk string (som er feil, siden den alltid vil validere negativt)
+      if (typeof valideringer[feltNavn] === 'string') {
+        return { ...samling, [feltNavn]: valideringer[feltNavn] };
+      }
 
-/**
- * Kombinerer generisk og forretningsvalidering til én som kan brukes av redux form.
- * Merk at kun generisk validering kan ha funksjoner. Forretningsvalidering som kommer
- * fra server er kun string eller array.
- */
-export const byggValidering = (values, props) => {
-  const { forretningsValidering } = props;
-  const generiskValideringFunksjoner = flatUtFeltGrupper(feltGrupper);
-  const generiskValidering = kjorAlleValideringerSomHarFunksjoner(generiskValideringFunksjoner, values, props);
+      return { ...samling };
+    }, {})
+  );
 
-  return flettOgFilterValidering(generiskValidering, forretningsValidering);
-};
+  /** Fletter to objekter og kombinerer verdiene i tilfeller hvor begge
+   * objekter har samme nøkkel. I tillegg fjern nøkler som viser seg å ikke utløse noen
+   * spesiell validering.
+   */
+  static flettOgFilterValidering = (generiskValidering, forretningsValidering = {}) => {
+    const samletValidering = { ...generiskValidering };
+    const filtrertValidering = Object.keys(forretningsValidering).reduce((samling, feltNavn) => {
+      const valideringsTekst = samletValidering[feltNavn]
+        ?
+        `${samletValidering[feltNavn]} ${forretningsValidering[feltNavn]}`
+        :
+        `${forretningsValidering[feltNavn]}`;
 
-/** Traverser alle feil som finnes i skjemaet (på grunnlag av validering) og mål opp mot kjente
- * felter i grupper (paneler) slik at det er mulig å avgjøre om et panel i sin helhet validerer korrekt.
- * @param felterMedFeil Object med alle felter som ikke validerer
- */
-export const gyldigePaneler = felterMedFeil => {
-  const felterMedFeilArray = felterMedFeil ? Object.keys(felterMedFeil) : [];
+      return valideringsTekst !== '' ? { ...samling, [feltNavn]: valideringsTekst } : { ...samling };
+    }, {});
 
-  return Object.keys(feltGrupper).reduce((collection, gruppeNavn) => {
-    const felterIGruppen = feltGrupper[gruppeNavn];
-    // Match felter i den aktuelle gruppen med listen over felter som faktisk inneholder feil. Dersom ingen treff,
-    // returneres true, som må reverseres siden Saksbehandling forventer state gyldigPanel: true | false.
-    const paneletInneholderIkkeFeil = !Object.keys(felterIGruppen).some(felt => felterMedFeilArray.includes(felt));
-    return { ...collection, [gruppeNavn]: paneletInneholderIkkeFeil };
-  }, {});
-};
+    return filtrertValidering;
+  };
 
-// eslint-disable-next-line
-export let __private__ = {};
+  /**
+   * Kombinerer generisk og forretningsvalidering til én som kan brukes av redux form.
+   * Merk at kun generisk validering kan ha funksjoner. Forretningsvalidering som kommer
+   * fra server er kun string eller array.
+   */
+  static byggValidering = (values, props) => {
+    const { forretningsValidering } = props;
+    const generiskValideringFunksjoner = Felles.flatUtFeltGrupper(feltGrupper);
+    const generiskValidering = Felles.kjorAlleValideringerSomHarFunksjoner(generiskValideringFunksjoner, values, props);
 
-if (process.env.NODE_ENV === 'test') {
-  __private__ = {
-    inneholderFeilmeldinger,
-    byggValideringsObjekt,
-    flatUtFeltGrupper,
-    gyldigePaneler,
-    flettOgFilterValidering,
-    kjorAlleValideringerSomHarFunksjoner,
+    return Felles.flettOgFilterValidering(generiskValidering, forretningsValidering);
+  };
+
+  /** Traverser alle feil som finnes i skjemaet (på grunnlag av validering) og mål opp mot kjente
+   * felter i grupper (paneler) slik at det er mulig å avgjøre om et panel i sin helhet validerer korrekt.
+   * @param felterMedFeil Object med alle felter som ikke validerer
+   */
+  static gyldigePaneler = felterMedFeil => {
+    const felterMedFeilArray = felterMedFeil ? Object.keys(felterMedFeil) : [];
+
+    return Object.keys(feltGrupper).reduce((collection, gruppeNavn) => {
+      const felterIGruppen = feltGrupper[gruppeNavn];
+      // Match felter i den aktuelle gruppen med listen over felter som faktisk inneholder feil. Dersom ingen treff,
+      // returneres true, som må reverseres siden Saksbehandling forventer state gyldigPanel: true | false.
+      const paneletInneholderIkkeFeil = !Object.keys(felterIGruppen).some(felt => felterMedFeilArray.includes(felt));
+      return { ...collection, [gruppeNavn]: paneletInneholderIkkeFeil };
+    }, {});
   };
 }
+
+
+export default Felles;
