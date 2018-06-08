b49003212d3eb149aaa41d57d75ca4bac736cfaa
Terje Karlsen <terjeofnorway@gmail.com>
2018-06-08 07:11:35 +0200

    Added scaffolding test

---
+++ src/ducks/kodeverk/operations.test.js
@@ -0,0 +1,10 @@
+import { STATUS } from '../../services/utils';
+import Reducer from './reducers';
+import * as Types from './types';
+import * as Operations from './operations';
+
+describe('Testing kodeverk/operations.js', () => {
+  test('foo', () => {
+    expect(true).toBe(true);
+  });
+});
