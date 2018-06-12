25dfd10b515b88d598163384ecae06ac6e787dd4
Terje Karlsen <terjeofnorway@gmail.com>
2018-06-12 16:18:21 +0200

    Unit-tester for selector.

--- src/ducks/kodeverk/operations.test.js
+++ src/ducks/kodeverk/operations.test.js
@@ -1,10 +1,7 @@
-import { STATUS } from '../../services/utils';
-import Reducer from './reducers';
-import * as Types from './types';
 import * as Operations from './operations';
 
 describe('Testing kodeverk/operations.js', () => {
   test('foo', () => {
-    expect(true).toBe(true);
+    expect(typeof Operations.hent()).toBe('function');
   });
 });
