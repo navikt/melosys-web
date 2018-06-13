2f33d06e861f75882d6e520effd395e3cc6f72d7
Terje Karlsen <terjeofnorway@gmail.com>
2018-06-13 14:23:31 +0200

    Satt opp TODO for testing av doThenDispatch.

--- src/ducks/kodeverk/operations.test.js
+++ src/ducks/kodeverk/operations.test.js
@@ -2,6 +2,7 @@ import * as Operations from './operations';
 
 describe('Testing kodeverk/operations.js', () => {
   test('foo', () => {
+    // Todo: Hvordan kan vi tesre at funksjonen heter "doThenDispatch"?
     expect(typeof Operations.hent()).toBe('function');
   });
 });
