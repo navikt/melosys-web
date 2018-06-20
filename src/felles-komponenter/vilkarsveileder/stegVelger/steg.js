84aabc5a962e3a8d8618f14f083bbea52e058514
Terje Karlsen <terjeofnorway@gmail.com>
2018-06-20 12:21:55 +0200

    Lagt inn beregning av tilstand.

--- src/felles-komponenter/vilkarsveileder/stegVelger/steg.js
+++ src/felles-komponenter/vilkarsveileder/stegVelger/steg.js
@@ -30,9 +30,15 @@ class Steg {
   };
 
   nesteSteg = () => {
+    const { faktaAvklaring = {} } = this._propsLight;
+
+    const flatFaktaAvklaring = Object
+      .keys(this._propsLight.faktaAvklaring)
+      .reduce((collection, key) => ({ ...collection, ...this._propsLight.faktaAvklaring[key] }), {});
+
     const kriterieMatch = this._kriterier.find(kriterie => {
       const { exec } = kriterie;
-      return this.assertRegel(exec, this._propsLight.faktaavklaring);
+      return this.assertRegel(exec, flatFaktaAvklaring);
     });
 
     return kriterieMatch.nesteSteg;
