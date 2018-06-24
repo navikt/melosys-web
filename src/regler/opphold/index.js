d5df57e9858336a2ab042aa17b63987b292912f4
Terje Karlsen <terjeofnorway@gmail.com>
2018-06-24 11:56:54 +0200

    Refactor finansiering => studieFinansiering. Flyttet regler ett nivå opp.

--- src/regler/opphold/index.js
+++ src/regler/opphold/index.js
@@ -1,6 +1,6 @@
 import moment from 'moment';
 import DomeneRegel from '../domeneRegel';
-import { strengTilBool } from '../../../utils/streng';
+import { strengTilBool } from '../../utils/streng';
 
 class Opphold extends DomeneRegel {
   inntilTolvManeder = () => {
