97263d2268b4f74f8465e2dfb052ae0403216cef
Terje Karlsen <terjeofnorway@gmail.com>
2018-06-24 12:03:20 +0200

    Brukt datoDiff i utregning av oppholdsperiode.

--- src/regler/opphold/index.js
+++ src/regler/opphold/index.js
@@ -1,15 +1,14 @@
-import moment from 'moment';
 import DomeneRegel from '../domeneRegel';
 import { strengTilBool } from '../../utils/streng';
+
+import { datoDiff } from '../../utils/dato';
 
 class Opphold extends DomeneRegel {
   inntilTolvManeder = () => {
     const { skjema } = this;
-    const datoFom = moment(skjema.oppholdUtlandFom, 'DD.MM.YYYY');
-    const datoTom = moment(skjema.oppholdUtlandTom, 'DD.MM.YYYY');
-    const oppholdsDiff = datoTom.diff(datoFom, 'months');
-    if (Number.isNaN(oppholdsDiff)) { return undefined; }
-    return oppholdsDiff < 12;
+    const { oppholdUtlandFom, oppholdUtlandTom } = skjema;
+
+    return datoDiff(oppholdUtlandFom, oppholdUtlandTom, 'months') < 12;
   }
 
   intensjonOmReturTilNorge = () => {
