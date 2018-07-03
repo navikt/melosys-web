9618d4ce5b826df1bf83ab7d1b58bd93a7c21e3c
Terje Karlsen <terjeofnorway@gmail.com>
2018-07-03 10:09:18 +0200

    Lagt inn varsel dersom en periode ikke har noen arbeidsgivere.

--- src/utils/navFrontend.js
+++ src/utils/navFrontend.js
@@ -1,6 +1,6 @@
 import { EkspanderbartpanelBase } from 'nav-frontend-ekspanderbartpanel';
 import { Panel } from 'nav-frontend-paneler';
-import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
+import AlertStripe, { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
 import { Container, Row, Column } from 'nav-frontend-grid';
 import { Checkbox, Radio, SkjemaGruppe, Fieldset, Select, Input, Textarea } from 'nav-frontend-skjema';
 import { Innholdstittel, Systemtittel, Undertittel, Element, Normaltekst, UndertekstBold } from 'nav-frontend-typografi';
@@ -9,7 +9,7 @@ import Stegindikator from 'nav-frontend-stegindikator';
 import NavFrontendSpinner from 'nav-frontend-spinner';
 
 export {
-  AlertStripeAdvarsel,
+  AlertStripeAdvarsel, AlertStripe,
   Container, Row, Column,
   Checkbox, Radio, SkjemaGruppe, Fieldset, Select, Input, Textarea,
   EkspanderbartpanelBase, Panel,
