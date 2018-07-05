f91f0e984a32e0ef97488fb918c90ccc1d7ba0fc
Terje Karlsen <terjeofnorway@gmail.com>
2018-07-05 16:53:56 +0200

    Lagt til panel for SelvstendigArbeid. Global refactor av bool-håndtering - Redux Form håndterer by default ikke bools i verdier og gjør disse om til strings. Med normalize er dette løst, men det krevde refactor av alle steder hvor value i radio buttons var stringified bool.

--- src/felles-komponenter/bosted.js
+++ src/felles-komponenter/bosted.js
@@ -13,6 +13,7 @@ import Landvelger from './skjema/landvelger';
 import './arbeidsgiverUtland.css';
 import { KodeverkSelectors } from '../ducks/kodeverk';
 import { formSelectors } from '../ducks/form';
+import { BOOLSK } from '../constants';
 
 const uuid = require('uuid/v4');
 
@@ -29,24 +30,24 @@ const Bosted = props => {
           <Nav.Row>
             <Nav.Column xs="12">
               <Skjema.RadioGruppe feltNavn="forutgaendeBostedINorge" legend="Har forutgående bosted i Norge.">
-                <Skjema.Radio feltNavn="forutgaendeBostedINorge" value="true" label="Ja" />
-                <Skjema.Radio feltNavn="forutgaendeBostedINorge" value="false" label="Nei" />
+                <Skjema.Radio feltNavn="forutgaendeBostedINorge" value={BOOLSK.SANN} label="Ja" />
+                <Skjema.Radio feltNavn="forutgaendeBostedINorge" value={BOOLSK.USANN} label="Nei" />
               </Skjema.RadioGruppe>
               <Nav.Fieldset legend="Nærmeste families bosted">
                 <Landvelger feltNavn="familiesBosted" />
               </Nav.Fieldset>
               <Nav.Undertittel>Yrkesaktiv</Nav.Undertittel>
               <Skjema.RadioGruppe feltNavn="adresseIUtlandet" legend="Har søker utenlandsadresse?">
-                <Skjema.Radio feltNavn="adresseIUtlandet" value="true" label="Ja" />
-                <Skjema.Radio feltNavn="adresseIUtlandet" value="false" label="Nei" />
+                <Skjema.Radio feltNavn="adresseIUtlandet" value={BOOLSK.SANN} label="Ja" />
+                <Skjema.Radio feltNavn="adresseIUtlandet" value={BOOLSK.USANN} label="Nei" />
               </Skjema.RadioGruppe>
               <Skjema.RadioGruppe feltNavn="sammeAdresseSomArbeidsgiver" legend="Er norsk adresse samme som arbeidsgivers?">
-                <Skjema.Radio feltNavn="sammeAdresseSomArbeidsgiver" value="true" label="Ja" />
-                <Skjema.Radio feltNavn="sammeAdresseSomArbeidsgiver" value="false" label="Nei" />
+                <Skjema.Radio feltNavn="sammeAdresseSomArbeidsgiver" value={BOOLSK.SANN} label="Ja" />
+                <Skjema.Radio feltNavn="sammeAdresseSomArbeidsgiver" value={BOOLSK.USANN} label="Nei" />
               </Skjema.RadioGruppe>
               <Skjema.RadioGruppe feltNavn="EOSBarnetrygdFraNAV" legend="Mottar EU/EØS barnetrygd fra NAV?">
-                <Skjema.Radio feltNavn="EOSBarnetrygdFraNAV" value="true" label="Ja" />
-                <Skjema.Radio feltNavn="EOSBarnetrygdFraNAV" value="false" label="Nei" />
+                <Skjema.Radio feltNavn="EOSBarnetrygdFraNAV" value={BOOLSK.SANN} label="Ja" />
+                <Skjema.Radio feltNavn="EOSBarnetrygdFraNAV" value={BOOLSK.USANN} label="Nei" />
               </Skjema.RadioGruppe>
               <Nav.Undertittel>Pensjonist</Nav.Undertittel>
               <Nav.Fieldset legend="Opphold i Norge (måneder pr kalenderår)">
@@ -55,8 +56,8 @@ const Bosted = props => {
                 </Skjema.Select>
               </Nav.Fieldset>
               <Skjema.RadioGruppe feltNavn="ektefelleEllerBarnINorge" legend="Har ektefelle / mindreårige barn i Norge">
-                <Skjema.Radio feltNavn="ektefelleEllerBarnINorge" value="true" label="Ja" />
-                <Skjema.Radio feltNavn="ektefelleEllerBarnINorge" value="false" label="Nei" />
+                <Skjema.Radio feltNavn="ektefelleEllerBarnINorge" value={BOOLSK.SANN} label="Ja" />
+                <Skjema.Radio feltNavn="ektefelleEllerBarnINorge" value={BOOLSK.USANN} label="Nei" />
               </Skjema.RadioGruppe>
               <Nav.Undertittel>Student i EØS-land</Nav.Undertittel>
               <Landvelger label="Velg studieland:" feltNavn="studieLand" />
@@ -68,8 +69,8 @@ const Bosted = props => {
                 {studieFinansiering.map(valg => <option key={uuid()} value={valg.kode}>{valg.term}</option>)}
               </Skjema.Select>
               <Skjema.RadioGruppe feltNavn="intensjonOmRetur" legend="Har intensjon om å returnere til Norge">
-                <Skjema.Radio feltNavn="intensjonOmRetur" value="true" label="Ja" />
-                <Skjema.Radio feltNavn="intensjonOmRetur" value="false" label="Nei" />
+                <Skjema.Radio feltNavn="intensjonOmRetur" value={BOOLSK.SANN} label="Ja" />
+                <Skjema.Radio feltNavn="intensjonOmRetur" value={BOOLSK.USANN} label="Nei" />
               </Skjema.RadioGruppe>
             </Nav.Column>
           </Nav.Row>
