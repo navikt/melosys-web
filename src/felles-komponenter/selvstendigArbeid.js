e09f7a2a23b3556b5abc3c58b2412ab35553426a
Terje Karlsen <terjeofnorway@gmail.com>
2018-07-06 08:32:46 +0200

    Lagt til funksjon for søkbarhet i organisasjonsnummer.

--- src/felles-komponenter/selvstendigArbeid.js
+++ src/felles-komponenter/selvstendigArbeid.js
@@ -1,67 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PT from 'prop-types';
 import { FieldArray } from 'redux-form';
 
 import * as Nav from '../utils/navFrontend';
 import * as Ikoner from '../resources/images';
 import * as Skjema from './skjema';
-
+import * as API from '../services/api';
+import { erOrgnrGyldig } from './skjema/validering/generisk/organisasjon';
 import { BOOLSK } from '../constants';
 
-import PanelHeader from '../felles-komponenter/panelHeader/panelHeader';
+import PanelHeader from './panelHeader/panelHeader';
+import OrganisasjonsAdresse from './adresser/organisasjonsadresse';
 
 import './selvstendigArbeid.css';
 
-const EnkeltForetak = props => {
-  const { nummer, foretaket, slettForetak } = props;
+class EnkeltForetak extends Component {
+  state = { organisasjon: {} }
 
-  return (
-    <div className="enkeltForetak">
-      <Nav.Fieldset legend={`Foretak #${nummer}`}>
-        <Nav.Row>
-          <Nav.Column xs="4">
-            <Skjema.Input feltNavn={`${foretaket}.orgnr`} bredde="S" label="Organisasjonsnummer" />
-            <div className="enkeltforetak__adresse">Multisoft AS, Adresseveien 123, 2343 Rørvik</div>
-          </Nav.Column>
-          <Nav.Column xs="5">
-            <label>Oppgir at virksomheten fortsetter etter arbeid i utlandet:
-              <div>
-                <Skjema.Radio feltNavn={`${foretaket}.fortsetterEtterArbeidIUtlandet`} value={BOOLSK.SANN} label="Ja" />
-                <Skjema.Radio feltNavn={`${foretaket}.fortsetterEtterArbeidIUtlandet`} value={BOOLSK.USANN} label="Nei" />
-              </div>
-            </label>
-          </Nav.Column>
-          <Nav.Column xs="3">
-            <Nav.Knapp mini onClick={slettForetak}>Fjern</Nav.Knapp>
-          </Nav.Column>
-        </Nav.Row>
-      </Nav.Fieldset>
-    </div>
-  );
-};
+  componentDidMount() {
+    const { orgnr } = this.props;
+
+    this.hentOrganisasjon(orgnr);
+  }
+
+  hentOrganisasjon = orgnr => {
+    if (erOrgnrGyldig(orgnr)) {
+      API.Organisasjoner.hentOrganisasjon(orgnr).then(response => {
+        if (Object.keys(response).length === 0) {
+          this.setState({ organisasjon: false });
+        } else {
+          this.setState({ organisasjon: response });
+        }
+      });
+    }
+  }
+
+  inputKeyHandler = event => this.hentOrganisasjon(event.target.value);
+
+  render () {
+    const { nummer, foretaket, slettForetak } = this.props;
+    const { inputKeyHandler } = this;
+    const feilmelding = !this.state.organisasjon ? { feilmelding: 'Fant ikke virksomheten' } : null;
+
+    return (
+      <div className="enkeltForetak">
+        <Nav.Fieldset legend={`Foretak #${nummer}`}>
+          <Nav.Row>
+            <Nav.Column xs="4">
+              <Skjema.Input feltNavn={`${foretaket}.orgnr`} feil={feilmelding} bredde="S" label="Organisasjonsnummer" onKeyUp={inputKeyHandler} />
+              { this.state.organisasjon && <OrganisasjonsAdresse className="enkeltforetak__adresse" organisasjon={this.state.organisasjon} /> }
+            </Nav.Column>
+            <Nav.Column xs="5">
+              <label>Oppgir at virksomheten fortsetter etter arbeid i utlandet:
+                <div>
+                  <Skjema.Radio feltNavn={`${foretaket}.fortsetterEtterArbeidIUtlandet`} value={BOOLSK.SANN} label="Ja" />
+                  <Skjema.Radio feltNavn={`${foretaket}.fortsetterEtterArbeidIUtlandet`} value={BOOLSK.USANN} label="Nei" />
+                </div>
+              </label>
+            </Nav.Column>
+            <Nav.Column xs="3">
+              <Nav.Knapp mini onClick={slettForetak}>Fjern</Nav.Knapp>
+            </Nav.Column>
+          </Nav.Row>
+        </Nav.Fieldset>
+      </div>
+    );
+  }
+}
 
 EnkeltForetak.propTypes = {
   nummer: PT.number.isRequired,
   foretaket: PT.string.isRequired,
   slettForetak: PT.func.isRequired,
+  orgnr: PT.string.isRequired,
 };
 
-const SelvstendigeForetak = props => {
-  const { fields } = props;
-
-  return (
-    <div>
-      {fields.map((foretaket, index) => <EnkeltForetak key={foretaket} foretaket={foretaket} nummer={index + 1} slettForetak={() => fields.remove(index)} />)}
-      <div className="leggTilForetak">
-        <Nav.Row>
-          <Nav.Column xs="12">
-            <Nav.Knapp mini onClick={() => fields.push({})}>Legg til nytt foretak</Nav.Knapp>
-          </Nav.Column>
-        </Nav.Row>
-      </div>
+const SelvstendigeForetak = ({ fields }) => (
+  <div>
+    {fields.map((foretaket, index) => <EnkeltForetak key={foretaket} orgnr={fields.get(index).orgnr} foretaket={foretaket} nummer={index + 1} slettForetak={() => fields.remove(index)} />)}
+    <div className="leggTilForetak">
+      <Nav.Row>
+        <Nav.Column xs="12">
+          <Nav.Knapp mini onClick={() => fields.push({})}>Legg til nytt foretak</Nav.Knapp>
+        </Nav.Column>
+      </Nav.Row>
     </div>
-  );
-};
+  </div>
+);
 
 SelvstendigeForetak.propTypes = {
   fields: PT.object.isRequired,
