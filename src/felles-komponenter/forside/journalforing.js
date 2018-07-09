30cc75dfc4977420f43f2b851e3c84499a82069f
Bjølseth, Are <are.bjolseth@nav.no>
2018-07-09 03:23:48 +0200

    MELOSYS-1204 Splitte journalføringsknapp i 2 (medlemskap og unntak)

--- src/felles-komponenter/forside/journalforing.js
+++ src/felles-komponenter/forside/journalforing.js
@@ -5,6 +5,7 @@ import PT from 'prop-types';
 
 import * as Nav from '../../utils/navFrontend';
 import { oppgaverOperations } from '../../ducks/oppgaver';
+import * as Skjema from '../skjema';
 
 class Journalforing extends Component {
   submitOgVideresend = form => {
@@ -14,8 +15,15 @@ class Journalforing extends Component {
     return (
       <Nav.Panel className="forside__sidepanel">
         <Nav.Systemtittel>Journalføring</Nav.Systemtittel>
-        <p>Klikk &quot;journalfør sak&quot; for å starte en journalføringsoppgave.</p>
+        <p>Velg type, og klikk &quot;journalfør sak&quot; for å starte en journalføringsoppgave.</p>
         <form onSubmit={this.submitOgVideresend}>
+
+          <Skjema.RadioGruppe feltNavn="Behandlingstype" label="Velg journalføringstype">
+            <div className="skjema__horisontalefelter">
+              <Skjema.Radio feltNavn="journalforingstype" label="Medlemsskap" value="MDL" forhandsvalgt />
+              <Skjema.Radio feltNavn="journalforingstype" label="Unntak" value="UFM" />
+            </div>
+          </Skjema.RadioGruppe>
           <Nav.Knapp>Journalfør sak</Nav.Knapp>
         </form>
       </Nav.Panel>
@@ -27,8 +35,22 @@ Journalforing.propTypes = {
   handleSubmit: PT.func.isRequired,
   history: PT.object.isRequired,
 };
+const jfrTypePicker = ({ journalforingstype }) => {
+  const jfrTyperMedlemsskap = {
+    SKND: '',
+    KLG: '',
+    REV: '',
+    ML_U: '',
+    PS_U: '',
+  };
+  const jfrTyperUnntak = {
+    UFM: '',
+  };
+  const jfrtyper = (journalforingstype === 'UFM') ? jfrTyperUnntak : jfrTyperMedlemsskap;
+  return oppgaverOperations.send('JFR', jfrtyper);
+};
 
 export default reduxForm({
   form: 'journalforingsform',
-  onSubmit: () => oppgaverOperations.send('JFR', ['SKND', 'UFM', 'KLG', 'REV', 'ML_U', 'PS_U']),
+  onSubmit: journalforingstype => jfrTypePicker(journalforingstype),
 })(withRouter((Journalforing)));
