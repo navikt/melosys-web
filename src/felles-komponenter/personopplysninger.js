bc2f30e5befad66a506f695a85c652b77b8ced3d
Terje Karlsen <terjeofnorway@gmail.com>
2018-08-01 13:48:08 +0200

    Implementert diverse adresser - Lagt inn bostedsadresse, postadresse og midlertidig postadresse. - Justert felles adressekomponenter.

--- src/felles-komponenter/personopplysninger.js
+++ src/felles-komponenter/personopplysninger.js
@@ -2,11 +2,15 @@ import React from 'react';
 import moment from 'moment';
 
 import * as Nav from '../utils/navFrontend';
-import * as MPT from '../proptypes/';
+import * as MPT from '../proptypes';
+import * as Skjema from '../felles-komponenter/skjema';
 import * as Ikoner from '../resources/images';
 
 import EnkeltDato from '../felles-komponenter/datoOmrade/enkeltDato';
 import PanelHeader from '../felles-komponenter/panelHeader/panelHeader';
+
+import Bostedsadresse from './adresser/bostedsadresse';
+import Postadresse from './adresser/postadresse';
 
 import './personopplysninger.css';
 
@@ -16,23 +20,6 @@ const ikonFraKjonn = kjoenn => {
     case 'M': { return Ikoner.Mann; }
     default: { return Ikoner.Ukjentkjoenn; }
   }
-};
-const visBostedsAdresse = bostedsadresse => {
-  if (bostedsadresse) {
-    const {
-      poststed, postnr, land, gateadresse: { gatenavn, husnummer, husbokstav = '' },
-    } = bostedsadresse;
-
-    return (
-      <div>
-        <dd>{`${gatenavn} ${husnummer} ${husbokstav}`}</dd>
-        <dd>{`${postnr} ${poststed}`}</dd>
-        <dd>{`${land}`}</dd>
-      </div>
-    );
-  }
-
-  return null;
 };
 
 function Personopplysninger(props) {
@@ -46,6 +33,8 @@ function Personopplysninger(props) {
     sammensattNavn,
     foedselsdato,
     bostedsadresse,
+    postadresse,
+    postadresseMidlertidig,
   } = props.person;
 
   const kjoennKode = kjoenn.kode || kjoenn;
@@ -57,23 +46,48 @@ function Personopplysninger(props) {
     <div className="personopplysninger panelSeksjon">
       <Nav.EkspanderbartpanelBase
         heading={<PanelHeader ikon={ikonFraKjonn(kjoennKode)} tittel={`${sammensattNavn} (${aar})`} undertittel={`Fødselsnummer: ${fnr}`} />}
-        ariaTittel="Panel for personinformasjon" >
+        ariaTittel="Panel for personinformasjon"
+        apen >
         <Nav.Container fluid>
           {/* START PERSONINFO */}
           <Nav.Row className="person__seksjon">
             <Nav.Column xs="6">
               <dl className="person__detaljer">
                 <dt>Fødselsnummer:</dt><dd>{fnr}</dd>
+                <dt>Statsborgerskap:</dt><dd>{statsborgerskap.term || statsborgerskap}</dd>
+                <dt>Fødselsdato:</dt><dd><EnkeltDato dato={foedselsdato} /></dd>
                 <dt>Kjønn:</dt><dd>{kjoenn.term || kjoenn}</dd>
-                <dt>Bostedsadresse</dt>
-                {visBostedsAdresse(bostedsadresse)}
+                <dt>Sivilstand:</dt><dd>{sivilstand}</dd>
               </dl>
             </Nav.Column>
             <Nav.Column xs="6">
+              Utenlandsk ID
+            </Nav.Column>
+          </Nav.Row>
+          <Nav.Row className="person__seksjon">
+            <Nav.Column xs="4">
               <dl className="person__detaljer">
-                <dt>Fødselsdato:</dt><dd><EnkeltDato dato={foedselsdato} /></dd>
-                <dt>Statsborgerskap:</dt><dd>{statsborgerskap.term || statsborgerskap}</dd>
-                <dt>Sivilstand:</dt><dd>{sivilstand}</dd>
+                <dt>Bostedsadresse (TPS):</dt>
+                <Bostedsadresse bostedsadresse={bostedsadresse} />
+              </dl>
+            </Nav.Column>
+            <Nav.Column xs="4">
+              <dl className="person__detaljer">
+                <dt>Postadresse (TPS):</dt>
+                <Postadresse postadresse={postadresse} />
+              </dl>
+            </Nav.Column>
+            <Nav.Column xs="4">
+              <dl className="person__detaljer">
+                <dt>Midl. postadresse (TPS):</dt>
+                <Postadresse postadresse={postadresseMidlertidig} />
+              </dl>
+            </Nav.Column>
+          </Nav.Row>
+          <Nav.Row className="person__seksjon">
+            <Nav.Column xs="6">
+              <dl className="person__detaljer">
+                <Skjema.Textarea feltNavn="personAresse" label="Adresse oppgitt av søker:" />
               </dl>
             </Nav.Column>
           </Nav.Row>
