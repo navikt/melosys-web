aea718b8f6a55cd6dc60797cf9da5e2375b520dd
Terje Karlsen <terjeofnorway@gmail.com>
2018-08-02 11:27:15 +0200

    Lagt til Barn som komponent.

---
+++ src/felles-komponenter/personopplysninger/barn.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PT from 'prop-types';
+import { FieldArray } from 'redux-form';
+
+import * as Skjema from '../skjema';
+import Landvelger from '../skjema/landvelger';
+import * as Nav from '../../utils/navFrontend';
+
+const Barn = () => {
+  return (
+    <div>barn</div>
+  )
+}
+
+export default Barn;
