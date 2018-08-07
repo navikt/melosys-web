256456cdca024be409ec184fbdfd993ce7977c3d
Bjølseth, Are <are.bjolseth@nav.no>
2018-08-07 18:27:22 +0200

    MELOSYS-1411 Oppdatere Spark, for Oppgaver.

--- src/debug/mock/journalforingoppgave.js
+++ src/debug/mock/journalforingoppgave.js
@@ -1,17 +1,4 @@
 /* eslint-disable */
 export const journalforingOppgave = {
-  oppgavetype: 'JFR',
-  sakstyper: [
-    'EU_EOS',
-    'TRYGDAVTALE',
-    'FOLKETRYGD',
-  ],
-  behandlingstyper: [
-    'SKND',
-    'UFM',
-    'KLG',
-    'REV',
-    'ML_U',
-    'PS_U',
-  ],
+  fagomrade: 'MED',
 };
