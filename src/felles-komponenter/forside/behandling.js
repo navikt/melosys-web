afac73872ef076c8f8d33e0d6f97517c70fee924
Bjølseth, Are <are.bjolseth@nav.no>
2018-08-07 18:27:22 +0200

    MELOSYS-1411 Oppdatere Spark, for Oppgaver.

--- src/felles-komponenter/forside/behandling.js
+++ src/felles-komponenter/forside/behandling.js
@@ -72,7 +72,7 @@ const mapStateToProps = state => ({
 
 const BehandlngForm = reduxForm({
   form: 'behandlingsform',
-  onSubmit: checkboxliste => oppgaverOperations.send('BEH_SAK', checkboxliste),
+  onSubmit: checkboxliste => oppgaverOperations.sendBehandlingsOppgave(checkboxliste),
 })(Behandling);
 
 export default withRouter(connect(mapStateToProps, null)(BehandlngForm));
