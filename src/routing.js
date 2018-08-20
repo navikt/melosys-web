48e85b9308504a99e6cf6a68973023eb64c3768c
Terje Karlsen <terjeofnorway@gmail.com>
2018-08-20 07:55:32 +0200

    Implementert AlertStripe, samt lagt inn nav-frontend-lenker.

--- src/routing.js
+++ src/routing.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PT from 'prop-types';
-import { Link, Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
+
+import UkjentSide from './sider/ukjentSide';
 
 import Forside from './sider/forside';
 import Sok from './sider/sok';
@@ -8,17 +10,6 @@ import Saksbehandling from './sider/saksbehandling';
 import Journalforing from './sider/journalforing';
 import Spark from './sider/spark';
 
-const NoMatch = ({ location }) => (
-  <div>
-    <h3>Ukjent side <code>{location.pathname}</code></h3>
-    <Link to="/" alt="NAV, lenke hovedsiden">
-      <h3>Tilbake til start</h3>
-    </Link>
-  </div>
-);
-NoMatch.propTypes = {
-  location: PT.object.isRequired,
-};
 
 const Routing = ({ location }) => (
   <Switch location={location}>
@@ -27,7 +18,7 @@ const Routing = ({ location }) => (
     <Route exact path="/spark" component={Spark} />
     <Route path="/saksbehandling/:snr" component={Saksbehandling} />
     <Route path="/journalforing/:oppgaveID/:journalpostID" component={Journalforing} />
-    <Route component={NoMatch} />
+    <Route component={UkjentSide} />
   </Switch>
 );
 
