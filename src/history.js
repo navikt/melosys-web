0ba1910446662fb4293cd00a824afbab43e0381d
Bjølseth, Are <are.bjolseth@nav.no>
2018-08-21 14:38:59 +0200

    Hack fix.

--- src/history.js
+++ src/history.js
@@ -1,15 +1,16 @@
 import { createBrowserHistory } from 'history';
 
 const CONTEXT_PATH = '/melosys';
+/*
 const NO_CONTEXT_PATH = '/';
 
 const isLocalJavaDevEnv = () => `${process.env.REACT_APP_BUILD_VERSION}` === 'java_local';
 
 const contextPath = isLocalJavaDevEnv() ? CONTEXT_PATH : NO_CONTEXT_PATH;
-
+*/
 /* eslint-enable no-console */
 const routerHistory = createBrowserHistory({
-  basename: contextPath,
+  basename: CONTEXT_PATH,
 });
 export default routerHistory;
 
