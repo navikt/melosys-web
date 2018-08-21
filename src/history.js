d81be9aed923a8aa7aae0f70ee2a2bf7b1710bc9
Bjølseth, Are <are.bjolseth@nav.no>
2018-08-21 12:02:23 +0200

    Oppdatere context path logikk og valg av custom context /melosys

--- src/history.js
+++ src/history.js
@@ -1,30 +1,12 @@
 import { createBrowserHistory } from 'history';
 
-const CONTEXT_PATH = '/melosys-web';
+const CONTEXT_PATH = '/melosys';
 const NO_CONTEXT_PATH = '/';
 
-function isProductionEnv() {
-  return `${process.env.NODE_ENV}` === 'production';
-}
+const isLocalAllixENv = () => `${process.env.REACT_APP_BUILD_VERSION}` === 'allix';
 
-function hasCombinedWarDeployment() {
-  const combinedDeployment = `${process.env.REACT_APP_COMBINED_WAR_DEPLOYMENT}`;
-  return combinedDeployment && combinedDeployment.toLowerCase() === 'true';
-}
+const contextPath = isLocalAllixENv() ? CONTEXT_PATH : NO_CONTEXT_PATH;
 
-let contextPath = NO_CONTEXT_PATH; // default
-
-if (isProductionEnv()) {
-  if (hasCombinedWarDeployment() === false) {
-    contextPath = CONTEXT_PATH; // Need context path to work in NGINX
-  }
-}
-/* eslint-disable no-console */
-if (isProductionEnv()) {
-  console.log(`NODE_ENV:${process.env.NODE_ENV}`, `basename:${contextPath}`);
-} else {
-  console.log(`NODE_ENV:${process.env.NODE_ENV}`, 'basename:"" no context path');
-}
 /* eslint-enable no-console */
 const routerHistory = createBrowserHistory({
   basename: contextPath,
