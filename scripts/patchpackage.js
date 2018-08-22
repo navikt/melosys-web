c5ec171ec6d65e0e4ea8b63746e01d3db38f8c12
Bjølseth, Are <are.bjolseth@nav.no>
2018-08-22 12:07:38 +0200

    Hack fix.

--- scripts/patchpackage.js
+++ scripts/patchpackage.js
@@ -6,12 +6,15 @@ const readPkg = require('read-pkg');
 const pkg = readPkg.sync();
 // get value of 'disabled-hompage'
 const homepage = pkg['disabled-homepage'];
-// apply value to new key 'homepage'
-pkg.homepage = homepage;
-// remove key 'disabled-homepage'
-delete pkg['disabled-homepage'];
-// write new content to package.json
-writePkg.sync(pkg);
+if (homepage) {
+  // apply value to new key 'homepage'
+  pkg.homepage = homepage;
+  // remove key 'disabled-homepage'
+  delete pkg['disabled-homepage'];
+  // write new content to package.json
+  writePkg.sync(pkg);
+}
+
 
 // update .env with custom version
 dotenv.set('REACT_APP_BUILD_VERSION', 'java_local');
--- src/history.js
+++ src/history.js
@@ -1,16 +1,8 @@
 import { createBrowserHistory } from 'history';
 
-const CONTEXT_PATH = '/melosys';
-/*
-const NO_CONTEXT_PATH = '/';
-
-const isLocalJavaDevEnv = () => `${process.env.REACT_APP_BUILD_VERSION}` === 'java_local';
-
-const contextPath = isLocalJavaDevEnv() ? CONTEXT_PATH : NO_CONTEXT_PATH;
-*/
 /* eslint-enable no-console */
 const routerHistory = createBrowserHistory({
-  basename: CONTEXT_PATH,
+  basename: '/melosys/',
 });
 export default routerHistory;
 
