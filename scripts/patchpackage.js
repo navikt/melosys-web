60d8682f95f8c625da8295bacafebd9f39997c3b
Bjølseth, Are <are.bjolseth@nav.no>
2018-08-22 13:34:17 +0200

    Updates wrt building java local with context path.

--- scripts/patchpackage.js
+++ scripts/patchpackage.js
@@ -10,11 +10,10 @@ if (homepage) {
   // apply value to new key 'homepage'
   pkg.homepage = homepage;
   // remove key 'disabled-homepage'
-  delete pkg['disabled-homepage'];
+  // delete pkg['disabled-homepage'];
   // write new content to package.json
   writePkg.sync(pkg);
 }
-
 
 // update .env with custom version
 dotenv.set('REACT_APP_BUILD_VERSION', 'java_local');
