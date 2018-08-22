fa1d08a8ce86c12facb7014cc28c8b5ba00b17bf
abjolseth <abjolseth@gmail.com>
2018-08-22 22:21:54 +0200

    static serve virker med '/' root contekst

--- scripts/serve.js
+++ scripts/serve.js
@@ -7,16 +7,13 @@ const app = express();
 const SCRIPTS_DIR = `${process.cwd()}`;
 const PROJECT_ROOT = path.resolve(`${SCRIPTS_DIR}/..`);
 const STATIC_BUILD_DIR = path.join(PROJECT_ROOT, 'build');
-/*
 const envfile = `${PROJECT_ROOT}/.env`;
 require('dotenv').config({ path: envfile });
 
 const isLocalJavaDevEnv = () => `${process.env.REACT_APP_BUILD_VERSION}` === 'java_local';
 const context = isLocalJavaDevEnv() ? '/melosys' : '/';
 app.use(context, express.static(STATIC_BUILD_DIR));
-*/
 
-app.use(express.static(STATIC_BUILD_DIR));
 const serverProxy = proxy({ target: 'http://localhost:3002', changeOrigin: true });
 const apiContext = '/api'; // `${context}api`;
 app.use(apiContext, serverProxy);
