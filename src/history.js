ce32d3d0c9c1ac21032fbeed5d34a582fae44f69
Bjølseth, Are <are.bjolseth@nav.no>
2018-08-22 14:13:29 +0200

    Fikset dynamisk contextpath.

--- src/history.js
+++ src/history.js
@@ -1,8 +1,15 @@
 import { createBrowserHistory } from 'history';
 
+const CONTEXT_PATH = '/melosys/'; // Must end with '/', since /melosys/static, /melosys/api, etc. Webpack compile.
+const NO_CONTEXT_PATH = '/';
+
+const isLocalJavaDevEnv = () => `${process.env.REACT_APP_BUILD_VERSION}` === 'java_local';
+
+const contextPath = isLocalJavaDevEnv() ? CONTEXT_PATH : NO_CONTEXT_PATH;
+
 /* eslint-enable no-console */
 const routerHistory = createBrowserHistory({
-  basename: '/melosys/',
+  basename: contextPath,
 });
 export default routerHistory;
 
