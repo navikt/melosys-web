8866b7dda15887fc33dccb99c12d089c81464ad6
Terje Karlsen <terjeofnorway@gmail.com>
2018-08-23 08:00:24 +0200

    Diverse justering i styling.

--- src/felles-komponenter/skjema/landvelger/multiLand.js
+++ src/felles-komponenter/skjema/landvelger/multiLand.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PT from 'prop-types';
 
 import * as Nav from '../../../utils/navFrontend';
@@ -12,7 +12,12 @@ const MultiLandEnkelt = ({ landObjekt, slettLandHandler }) => (
     <div className="landliste__linje__navn">
       {landTekstFormat(landObjekt)}
     </div>
-    <Nav.Knapp mini className="landliste__linje__knapp" onClick={e => slettLandHandler(e, landObjekt.kode)}>Fjern</Nav.Knapp>
+    <Nav.Knapp
+      mini
+      className="landliste__linje__knapp"
+      onClick={e => slettLandHandler(e, landObjekt.kode)}
+    >Fjern
+    </Nav.Knapp>
   </div>
 );
 
@@ -21,41 +26,47 @@ MultiLandEnkelt.propTypes = {
   slettLandHandler: PT.func.isRequired,
 };
 
-const MultiLand = props => {
-  const {
-    alleLand,
-    feil,
-    fokusUtHandler,
-    inputVerdi,
-    inputEndringHandler,
-    inputTastNedHandler,
-    label,
-    valgteLand,
-    slettLandHandler,
-  } = props;
+class MultiLand extends Component {
+  dynamiskTittel = () => (this.props.valgteLand.length > 0 ? 'Legg til evt flere land:' : 'Legg til første land:');
 
-  return (
-    <div className="landliste">
-      {
-        valgteLand.map(land => <MultiLandEnkelt key={land} slettLandHandler={slettLandHandler} landObjekt={kodeTilObjekt(land, alleLand)} />)
-      }
-      <div className="landliste__linje">
-        <Nav.Input
-          list="alleLand"
-          label={label}
-          bredde="XL"
-          feil={feil}
-          className="landliste__linje__input"
-          value={inputVerdi}
-          onBlur={fokusUtHandler}
-          onChange={inputEndringHandler}
-          onKeyDown={inputTastNedHandler}
-        />
-        <Nav.Knapp mini className="landliste__linje__knapp--leggtil">Legg til</Nav.Knapp>
+  render() {
+    const {
+      alleLand,
+      feil,
+      fokusUtHandler,
+      inputVerdi,
+      inputEndringHandler,
+      inputTastNedHandler,
+      label,
+      valgteLand,
+      slettLandHandler,
+    } = this.props;
+
+    const dynamiskLabel = label || this.dynamiskTittel();
+
+    return (
+      <div className="landliste">
+        {
+          valgteLand.map(land => <MultiLandEnkelt key={land} slettLandHandler={slettLandHandler} landObjekt={kodeTilObjekt(land, alleLand)} />)
+        }
+        <div className="landliste__leggtil">
+          <Nav.Input
+            list="alleLand"
+            label={dynamiskLabel}
+            bredde="XL"
+            feil={feil}
+            className="landliste__linje__input"
+            value={inputVerdi}
+            onBlur={fokusUtHandler}
+            onChange={inputEndringHandler}
+            onKeyDown={inputTastNedHandler}
+          />
+          <Nav.Knapp mini className="landliste__linje__knapp">Legg til</Nav.Knapp>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 MultiLand.propTypes = {
   fokusUtHandler: PT.func.isRequired,
