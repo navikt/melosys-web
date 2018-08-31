50c086a246d0acf6bc6794152977bd0c8c57d60a
Terje Karlsen <terjeofnorway@gmail.com>
2018-08-31 15:13:27 +0200

    Mindre kodelinting.

--- src/felles-komponenter/personopplysninger.js
+++ src/felles-komponenter/personopplysninger.js
@@ -79,8 +79,7 @@ function Personopplysninger(props) {
             <PanelHeader ikon={ikonFraKjonn(kodeverkObjektTilKode(kjoenn))} tittel={`${sammensattNavn} (${beregnAlder(foedselsdato)})`} undertittel={`Fødselsnummer: ${fnr}`} />
             <PersonMerkelapper personStatus={personStatus} erEgenAnsatt={erEgenAnsatt} />
           </div>}
-        ariaTittel="Panel for personinformasjon"
-      >
+        ariaTittel="Panel for personinformasjon">
         <Nav.Container fluid>
           {/* START PERSONINFO */}
           <Nav.Row className="person__seksjon">
@@ -88,12 +87,8 @@ function Personopplysninger(props) {
               <dl className="person__detaljer">
                 <dt>Fødselsnummer:</dt><dd>{fnr}</dd>
                 <dt>Statsborgerskap:</dt>
-                <dd>
-                  Gjeldende: {kodeverkObjektTilTerm(statsborgerskap)}
-                </dd>
-                <dd>
-                  Ved start av søknadsperioden: {kodeverkObjektTilTerm(historiskStatsborgerskap)}
-                </dd>
+                <dd>Gjeldende: {kodeverkObjektTilTerm(statsborgerskap)}</dd>
+                <dd>Ved start av søknadsperioden: {kodeverkObjektTilTerm(historiskStatsborgerskap)}</dd>
                 <dt>Fødselsdato:</dt><dd><EnkeltDato dato={foedselsdato} /></dd>
                 <dt>Kjønn:</dt><dd>{kodeverkObjektTilTerm(kjoenn)}</dd>
                 <dt>Sivilstand:</dt><dd>{sivilstand}</dd>
