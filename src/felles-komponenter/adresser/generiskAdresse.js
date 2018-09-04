943876fc33afa0acd926fb3ef6d6acf7aac60a35
Terje Karlsen <terjeofnorway@gmail.com>
2018-09-04 21:41:07 +0200

    Lagt til adressekomponenter.

---
+++ src/felles-komponenter/adresser/generiskAdresse.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import * as MPT from '../../proptypes';
+
+import { kodeverkObjektTilTerm } from '../../utils/kodeverk';
+
+/** Forretningsadresse formatterer adressen korrekt med sjekk på
+ * varierende keys i objektet.
+ *
+ */
+const GeneriskAdresse = ({ adresse }) => {
+  const INGEN_TILGJENGELIG_TEKST = '(ingen tilgjengelig)';
+  if (!adresse) { return INGEN_TILGJENGELIG_TEKST; }
+
+  const {
+    gateadresse, land, postnr, poststed,
+  } = adresse;
+
+  const {
+    gatenavn, gatenummer, husnummer, husbokstav,
+  } = gateadresse;
+
+  return (gatenavn || gatenummer || husnummer || husbokstav || land || postnr || poststed) ? (
+    <address className="bostedsadresse">
+      {gatenavn} {gatenummer} {husnummer} {husbokstav}<br />
+      {postnr} {poststed}<br />
+      {kodeverkObjektTilTerm(land)}
+    </address>
+  ) : INGEN_TILGJENGELIG_TEKST;
+};
+
+GeneriskAdresse.propTypes = {
+  adresse: MPT.GeneriskAdresse,
+};
+
+GeneriskAdresse.defaultProps = {
+  adresse: {},
+};
+
+export default GeneriskAdresse;
