035088ab827579791e17cda9774fa7daa968b451
Terje Karlsen <terjeofnorway@gmail.com>
2018-09-05 08:46:52 +0200

    Mindre justeringer fra demo.

--- src/felles-komponenter/vilkarsveileder/stegVelger/kontrollere/arbeidsgivere.js
+++ src/felles-komponenter/vilkarsveileder/stegVelger/kontrollere/arbeidsgivere.js
@@ -9,7 +9,7 @@ class Sysselsetting extends Steg {
       {
         beskrivelse: 'alle andre valg',
         exec: () => true,
-        nesteSteg: STEG.SEKTOR,
+        nesteSteg: STEG.VESENTLIG_VIRKSOMHET,
       },
     ];
     this._id = STEG.ARBEIDSGIVERE;
