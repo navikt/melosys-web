7348aa04177f7dc0b90f33ab1afefc2d611893d8
Terje Karlsen <terjeofnorway@gmail.com>
2018-09-06 07:29:03 +0200

    Satt opp punkt for sjekk av land i inngangsvilkår.

--- src/felles-komponenter/vilkarsveileder/vurderinger/vurderingInngang.js
+++ src/felles-komponenter/vilkarsveileder/vurderinger/vurderingInngang.js
@@ -8,18 +8,23 @@ import { kodeverkObjektTilTerm } from '../../../utils/kodeverk';
 import './vurderingVedtak.css';
 
 const VurderingInngang = props => {
-  const { bekreftOgFortsett, inngangsvilkar } = props;
+  const { bekreftOgFortsett, inngangsvilkar, faktaavklaring } = props;
   const { vurdering } = inngangsvilkar;
+  const { opphold } = faktaavklaring;
+
+  console.log(opphold);
 
   return (
     <div className="inngang vedtak">
-      <Nav.Fieldset legend="Inngangsvilkår">
+      <Nav.Undertittel>Kontroller inngangsvilkår</Nav.Undertittel>
         <ul className="betingelser__liste">
           <li className="liste__element liste__element--oppfylt">
             { kodeverkObjektTilTerm(vurdering) }
           </li>
+          <li className="liste__element liste__element--varsel">
+            Sjekk at land er innenfor et territorium / område som dekkes av forordningen.
+          </li>
         </ul>
-      </Nav.Fieldset>
       <div className="fane__knapplinje">
         <Nav.Knapp type="hoved" className="fane__navigasjonsknapp" onClick={bekreftOgFortsett}>Start behandling</Nav.Knapp>
       </div>
@@ -29,6 +34,7 @@ const VurderingInngang = props => {
 
 VurderingInngang.propTypes = {
   bekreftOgFortsett: PT.func.isRequired,
+  faktaavklaring: PT.object.isRequired,
   inngangsvilkar: PT.shape({
     vurdering: MPT.Kodeverk,
   }).isRequired,
