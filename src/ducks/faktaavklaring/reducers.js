163a14225dde4fa74fa09995a980bd2ff9b3419b
Terje Karlsen <terjeofnorway@gmail.com>
2018-09-17 16:57:22 +0200

    Lagt opp kobling mot redux form og store.

--- src/ducks/faktaavklaring/reducers.js
+++ src/ducks/faktaavklaring/reducers.js
@@ -59,13 +59,16 @@ export default function reducer(state = initialState, action) {
           erstatterTidligereUtsendt: dokument.faktaavklaringErstatterTidligereUtsendt,
           utsendingMindreEnn24Mnd: dokument.faktaavklaringUtsendingMindreEnn24Mnd,
           foretakDriverINorge: dokument.faktaavklaringForetakDriverINorge,
-          harForutgaendeMedlemskap: dokument.faktaavklaringHarForutgaendeMedlemskap,
           arbeidKnyttetTilVirksomhetUtlandet: dokument.faktaavklaringArbeidKnyttetTilVirksomhetUtlandet,
           sammeTypeVirksomhet: dokument.faktaavklaringSammeTypeVirksomhet,
         },
         valgteArbeidsgivere: [...dokument.faktaavklaringValgteArbeidsgivere],
         sektor: {
           ansattISektor: dokument.faktaavklaringAnsattISektor,
+        },
+        forutgaendeMedlemskap: {
+          harForutgaendeMedlemskap: dokument.faktaavklaringHarForutgaendeMedlemskap,
+          forutgaendeMedlemskapBegrunnelser: dokument.faktaavklaringForutgaendeMedlemskapBegrunnelser,
         },
         bosted: {
           vurdering: { ...state.data.avklaring.bosted.vurdering },
