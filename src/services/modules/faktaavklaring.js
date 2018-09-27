1b5ece6fa8b44125210b57a0ae47cb06f4877015
abjolseth <abjolseth@gmail.com>
2018-09-27 02:27:46 +0200

    Refactor journalforing med async/await

--- src/services/modules/faktaavklaring.js
+++ src/services/modules/faktaavklaring.js
@@ -3,12 +3,12 @@ import { API_BASE_URL } from '../api-constants';
 
 const endepunkt = 'faktaavklaring';
 
-function hent(bid) {
+async function hent(bid) {
   const URI_FAKTAAVKLARING = `${API_BASE_URL}${endepunkt}/${bid}`;
   return getAsJson(URI_FAKTAAVKLARING);
 }
 
-function send(bid, dokument) {
+async function send(bid, dokument) {
   const URI_FAKTAAVKLARING = `${API_BASE_URL}${endepunkt}/${bid}`;
   return postAsJson(URI_FAKTAAVKLARING, dokument);
 }
--- src/services/modules/kodeverk.js
+++ src/services/modules/kodeverk.js
@@ -2,7 +2,7 @@ import { cachedGetAsJson } from '../utils';
 import { API_BASE_URL } from '../api-constants';
 
 // eslint-disable-next-line import/prefer-default-export
-export function hent() {
+export async function hent() {
   const URI_KODEVERK = `${API_BASE_URL}kodeverk`;
   const hrs6 = 60 * 60 * 6;
   return cachedGetAsJson(URI_KODEVERK, hrs6);
