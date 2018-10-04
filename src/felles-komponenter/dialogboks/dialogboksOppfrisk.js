080140b2cf6c6f6505ac6962aa23f64027eec896
Bjørnar Askevold <bjornar.askevold@nav.no>
2018-10-04 08:42:51 +0200

    Flyttet dialogene til en felles dialogboksmappe. Lagt til kall i api mot nytt endpoint "behandlinger"

--- src/felles-komponenter/dialogboks/dialogboksOppfrisk.js
+++ src/felles-komponenter/dialogboks/dialogboksOppfrisk.js
@@ -1,51 +1,49 @@
 import React, { Component } from 'react';
 import PT from 'prop-types';
 
-import Dialogboks from '../dialogboks';
-import DialogboksVenter from '../dialogboksVenter';
-import * as Nav from "../../utils/navFrontend";
+import DialogboksBekreft from './dialogboksBekreft';
+import DialogboksVenter from './dialogboksVenter';
 
 class OppfriskSakDialogboks extends Component {
+  oppdateringintervall = 1000;
+  timeoutTid = 30000;
+
   state = {
     venterResultat: false,
     tid: 0,
   };
 
+  componentWillUnmount() {
+    clearInterval(this.timer)
+  }
+
   start = () => {
+    this.timer = setInterval(this.sjekkForTimeout, this.oppdateringintervall);
     this.setState({ venterResultat: true });
     this.props.bekreft();
-
-    this.timer = setInterval(() => this.sjekkForTimeout, 10000);
   }
 
   sjekkForTimeout = () => {
-    this.setState({ tid: this.state.tid + 10000 });
+    console.log("Sjekk timeout: "+ this.state.tid)
+    this.setState({ tid: this.state.tid + this.oppdateringintervall });
 
-    if (this.state.tid >= 30000) {
-      this.skjul();
+    if (this.state.tid >= this.timeoutTid) {
+      this.props.skjulDialog();
     }
-  }
-
-  skjul = () => {
-    this.reset();
-    this.props.skjulDialog();
-  }
-
-  reset = () => {
-    clearInterval(this.timer)
-    this.setState({ tid: 0 });
-    this.setState({ venterResultat: false });
+    else {
+      this.props.oppdater();
+    }
   }
 
   render () {
     const {
-      avbryt,
+      avbryt, skjulDialog
     } = this.props;
 
     return (
       <div>
         {!this.state.venterResultat &&
-        <Dialogboks
+        <DialogboksBekreft
           tittel="Vil du oppdatere registeropplysninger?"
           tekst="Oppdatering av registeropplysning kan ta noe tid. Du vil derfor bli sendt tilbake til oppgavelisten hvor du kan journalføre eller behandle en annen sak i mellomtiden."
           bekreft={this.start}
@@ -58,7 +56,7 @@ class OppfriskSakDialogboks extends Component {
           tittel="Oppdaterer registeropplysninger"
           tekst="Oppdatering av registeropplysning kan ta noen minutter, venligst vent."
           synlig
-          skjul={this.skjul}
+          skjul={skjulDialog}
         />}
       </div>
     );
@@ -69,6 +67,7 @@ OppfriskSakDialogboks.propTypes = {
   bekreft: PT.func.isRequired,
   avbryt: PT.func.isRequired,
   skjulDialog: PT.func.isRequired,
+  oppdater: PT.func.isRequired,
 };
 
 export default OppfriskSakDialogboks;
