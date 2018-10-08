84847476de17fd1236d5257e138fc1c161a103b7
abjolseth <abjolseth@gmail.com>
2018-10-08 11:47:55 +0200

    Refactored Api.saksflyt into doThenDispatch(Api.saksflyt...)

--- src/services/modules/saksflyt.js
+++ src/services/modules/saksflyt.js
@@ -1,9 +1,7 @@
 import { getAsJson } from '../utils';
 import { API_BASE_URL } from '../api-constants';
 
-function status(behandlingID) {
-  const URI_HENT_SAKSFLYTSTATUS = `${API_BASE_URL}saksflyt/${behandlingID}/status`;
+export function sjekkStatus(behandlingID) {
+  const URI_HENT_SAKSFLYTSTATUS = `${API_BASE_URL}saksflyt/status/${behandlingID}`;
   return getAsJson(URI_HENT_SAKSFLYTSTATUS);
 }
-
-export { status };
