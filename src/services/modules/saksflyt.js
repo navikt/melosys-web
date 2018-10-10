fa47f1571236f463e814b45535a59287a550f933
Terje Karlsen <terjeofnorway@gmail.com>
2018-10-10 08:13:58 +0200

    Fikset korrekt endpoint for saksflyt.

--- src/services/modules/saksflyt.js
+++ src/services/modules/saksflyt.js
@@ -2,7 +2,7 @@ import { getAsJson } from '../utils';
 import { API_BASE_URL } from '../api-constants';
 
 function status(behandlingID) {
-  const URI_HENT_SAKSFLYTSTATUS = `${API_BASE_URL}saksflyt/${behandlingID}/status`;
+  const URI_HENT_SAKSFLYTSTATUS = `${API_BASE_URL}saksflyt/status/${behandlingID}`;
   return getAsJson(URI_HENT_SAKSFLYTSTATUS);
 }
 
