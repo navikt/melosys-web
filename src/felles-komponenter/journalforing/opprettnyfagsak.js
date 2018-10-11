6744d27e126711e0c5e80ef05a764399f9fcf696
Bjølseth, Are <are.bjolseth@nav.no>
2018-10-11 16:58:46 +0200

    Flyttet søk på arbeidsgiverID, representantID fra informasjon.js => opprettnyfagsak.js

--- src/felles-komponenter/journalforing/opprettnyfagsak.js
+++ src/felles-komponenter/journalforing/opprettnyfagsak.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import PT from 'prop-types';
 
@@ -7,35 +7,85 @@ import * as Nav from '../../utils/navFrontend';
 
 import './opprettnyfagsak.css';
 import LandVelger from '../skjema/landvelger';
+import * as Utils from '../../utils/utils';
+import * as Konstanter from '../../constants';
 
-const OpprettNyFagSak = props => {
-  const { opprettFagsak } = props;
-  return (
-    <div className="opprettnysak">
-      <Nav.Systemtittel>Opprett ny sak</Nav.Systemtittel>
-      <Nav.Fieldset legend="Soknadperiode:" className="opprettnysak__soknadsperiode">
+class OpprettNyFagSak extends Component {
+  state = { spinner: {} };
+  /*
+  async componentDidMount() {
+    await this.oppdaterFelter(this.props, true);
+  }
+
+  async componentDidUpdate(prevProps) {
+    await this.oppdaterFelter(prevProps);
+  }
+  oppdaterFelter = async (props, tvingOppdatering) => {
+    const { arbeidsgiverID, representantID } = props.journalforingSkjemaVerdier;
+  };
+  */
+  toggleSpinn = (navn, spin) => ({ spinner: { ...this.state.spinner, [navn]: spin } });
+  spinner = async (navn, ms = 1000) => {
+    this.setState(this.toggleSpinn(navn, true));
+    await Utils.delay(ms);
+    this.setState(this.toggleSpinn(navn, false));
+  };
+
+  erGyldigOrgnummer = verdi => verdi.length === Konstanter.ANTALL_TALL_I_ORGNR;
+  sjekkArbeidsgiver = async verdi => {
+    const { erGyldigOrgnummer } = this;
+    const { settFeltInnhold, hentOgVisArbeidsgiver } = this.props;
+    if (erGyldigOrgnummer(verdi)) {
+      await this.spinner('arbeidsgiverNavn');
+      await hentOgVisArbeidsgiver(verdi);
+    } else {
+      await settFeltInnhold('arbeidsgiverNavn');
+    }
+  };
+
+  IDFeltTastOppHandler = async event => {
+    const { id: opprinneligFeltID, value } = event.target;
+    if (opprinneligFeltID === 'arbeidsgiverID') { await this.sjekkArbeidsgiver(value); }
+  };
+
+  render() {
+    const { spinner: { arbeidsgiverNavn: visArbeidsgiverSpinner } } = this.state;
+
+    const { opprettFagsak } = this.props;
+    return (
+      <div className="opprettnysak">
+        <Nav.Systemtittel>Opprett ny sak</Nav.Systemtittel>
         <Nav.Row>
           <Nav.Column xs="6">
-            <Skjema.Input datoFelt label="Fra" feltNavn="journalforingPeriodeFraOgMed" />
-          </Nav.Column>
-          <Nav.Column xs="6">
-            <Skjema.Input datoFelt label="Til" feltNavn="journalforingPeriodeTilOgMed" />
-          </Nav.Column>
-        </Nav.Row>
-      </Nav.Fieldset>
-      <Nav.Fieldset legend="Land:">
-        <Nav.Row>
-          <Nav.Column xs="12">
-            <LandVelger feltNavn="journalforingOppholdsLand" multiLand />
+            <Skjema.Input feltNavn="arbeidsgiverID" label="Representantens organisasjonsnummer" onKeyUp={this.IDFeltTastOppHandler} />
+            <Skjema.Input feltNavn="arbeidsgiverNavn" label="Representantens navn" />
+            { visArbeidsgiverSpinner && <Nav.NavFrontendSpinner className="informasjon__spinner" /> }
           </Nav.Column>
         </Nav.Row>
-      </Nav.Fieldset>
-      <div className="opprettnysak__knapper">
-        <Nav.Knapp className="knapp" onClick={opprettFagsak}>Opprett ny sak</Nav.Knapp>
+        <Nav.Fieldset legend="Soknadperiode:" className="opprettnysak__soknadsperiode">
+          <Nav.Row>
+            <Nav.Column xs="6">
+              <Skjema.Input datoFelt label="Fra" feltNavn="journalforingPeriodeFraOgMed" />
+            </Nav.Column>
+            <Nav.Column xs="6">
+              <Skjema.Input datoFelt label="Til" feltNavn="journalforingPeriodeTilOgMed" />
+            </Nav.Column>
+          </Nav.Row>
+        </Nav.Fieldset>
+        <Nav.Fieldset legend="Land:">
+          <Nav.Row>
+            <Nav.Column xs="12">
+              <LandVelger feltNavn="journalforingOppholdsLand" multiLand />
+            </Nav.Column>
+          </Nav.Row>
+        </Nav.Fieldset>
+        <div className="opprettnysak__knapper">
+          <Nav.Knapp className="knapp" onClick={opprettFagsak}>Opprett ny sak</Nav.Knapp>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 OpprettNyFagSak.propTypes = {
   opprettFagsak: PT.func.isRequired,
