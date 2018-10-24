06fb9ad525a224115d1de2c177d1fec353daf9c2
Terje Karlsen <terjeofnorway@gmail.com>
2018-10-24 18:54:24 +0200

    Satt opp steg for Artikkel 16.1 - lagt til steg som manglet.

---
+++ src/felles-komponenter/stegvelger/stegMotor/kontrollere/artikkel16.js
@@ -0,0 +1,27 @@
+import Steg from '../steg';
+import { FANE_STATUS, STEG } from '../typer';
+import VurderingArtikkel16 from '../../stegKomponenter/vurderingArtikkel16';
+
+class Artikkel16 extends Steg {
+  constructor(propsLight, stegPosisjon) {
+    super(propsLight, stegPosisjon);
+    this._kriterier = [
+      {
+        beskrivelse: 'alle valg',
+        exec: () => true,
+        nesteSteg: STEG.VEDTAK,
+      },
+    ];
+    this._id = STEG.ARTIKKEL_16;
+    this._tittel = 'Artikkel 16.1';
+    this._komponent = VurderingArtikkel16;
+    this._samleRelevanteData = () => ({});
+    this._beregnRelevantUI = () => ({});
+    this._handlers = {
+      sendAnmodningHandler: this._propsLight.tilgjengeligeHandlers.sendAnmodningHandler,
+    };
+    this._status = FANE_STATUS.OK;
+  }
+}
+
+export default Artikkel16;
