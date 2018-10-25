f2b67220f12cd0c63b4ba89790d963d0c7396420
Bjølseth, Are <are.bjolseth@nav.no>
2018-10-25 13:55:04 +0200

    Added validering, problem med TextArea komponenten.

--- src/felles-komponenter/sideDialog/BrevBestilling.js
+++ src/felles-komponenter/sideDialog/BrevBestilling.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { reduxForm, reset } from 'redux-form';
+import { reduxForm, reset, setSubmitFailed } from 'redux-form';
 import PT from 'prop-types';
 
 import * as Nav from '../../utils/navFrontend';
@@ -11,7 +11,7 @@ import { formSelectors } from '../../ducks/form/';
 import PanelHeader from '../../felles-komponenter/panelHeader/panelHeader';
 import * as Ikoner from '../../resources/images';
 // import * as Validering from '../skjema/validering';
-import { brevbestillingValidering } from '../skjema/validering/brevbestilling';
+import { brevbestillingValidering, erSkjemaGyldig } from '../skjema/validering/brevbestilling';
 import { dokumenterOperations, dokumenterSelectors } from '../../ducks/dokumenter';
 import * as fagsakSelectors from '../../ducks/fagsaker/selectors';
 
@@ -35,6 +35,9 @@ const InfoPanel = () => {
 };
 
 class BrevBestilling extends Component {
+  overstyrSubmit = event => {
+    event.preventDefault();
+  };
   erMangelBrevMedFritekst = () => {
     const { brevbestillingSkjemaVerdier } = this.props;
     if (!brevbestillingSkjemaVerdier) return false;
@@ -50,11 +53,17 @@ class BrevBestilling extends Component {
   };
 */
   utkastBrev = async () => {
-    const { brevbestillingSkjemaVerdier, lagPdfUtkast, oppsummering } = this.props;
+    const {
+      brevbestillingSkjemaVerdier, lagPdfUtkast, oppsummering, settFeilFelt,
+    } = this.props;
     const { behandlingID } = oppsummering;
     const { fritekst, mottaker, dokumenttypeKode } = brevbestillingSkjemaVerdier;
     const dokument = this.erMangelBrevMedFritekst() ? Object.assign({ fritekst, mottaker }) : {};
 
+    if (!erSkjemaGyldig(brevbestillingSkjemaVerdier)) {
+      settFeilFelt('mottaker', 'dokumenttypeKode', 'fritekst');
+      return false;
+    }
     const extededResponse = await lagPdfUtkast(behandlingID, dokumenttypeKode, dokument);
     if (extededResponse.data.ok) {
       const { data: response } = extededResponse;
@@ -68,6 +77,7 @@ class BrevBestilling extends Component {
       this.setState({ feil: { error, message, path, timestamp } });
       */
     }
+    return true;
   };
 
   forkastBrev = () => {
@@ -81,36 +91,40 @@ class BrevBestilling extends Component {
     const placeholder = 'Feks: "Opplysning om antall utsendet i perioden, "Opplysninger om den ansatt erstatter en annen utsendt ansatt""';
     // const feilmelding = {feilmelding: 'Her er det noe feil.'};
     return (
-      <Nav.Panel>
-        <Nav.Fieldset legend="Nytt brev">
-          <Nav.Row>
-            <Skjema.Select feltNavn="mottaker" bredde="fullbredde" label="Mottaker">
-              {aktoerroller && aktoerroller.map(elem => <option key={elem.kode} value={elem.kode}>{elem.term}</option>)}
-            </Skjema.Select>
-            <Skjema.Select feltNavn="dokumenttypeKode" bredde="fullbredde" label="Type brev" disabled>
-              {dokumenttyper && dokumenttyper.map(elem => <option key={elem.kode} value={elem.kode}>{elem.term}</option>)}
-            </Skjema.Select>
-          </Nav.Row>
-          <Nav.Row>
-            {this.erMangelBrevMedFritekst() && <InfoPanel /> }
-          </Nav.Row>
-          <Nav.Row>
-            {this.erMangelBrevMedFritekst() && <Skjema.Textarea feltNavn="fritekst" label="Hva skal søker sende inn?" maxLength={200} placeholder={placeholder} visTellerFra={100} feil={undefined} />}
-          </Nav.Row>
-          {/* {this.state.feil && <Nav.Row><p>{this.state.feil.message}</p></Nav.Row>} */}
-          <Nav.Row>
-            <Nav.Column xs="12" md="6">
-              <Nav.Knapp htmlType="reset" type="standard" onClick={this.forkastBrev}>Forkast Brev</Nav.Knapp>
-            </Nav.Column>
-            <Nav.Column xs="12" md="6">
-              <Nav.Knapp htmlType="submit" type="hoved" onClick={this.utkastBrev}>Send Utkast</Nav.Knapp>
-            </Nav.Column>
-          </Nav.Row>
-        </Nav.Fieldset>
-      </Nav.Panel>
+      <form onSubmit={this.overstyrSubmit}>
+        <Nav.Panel>
+          <Nav.Fieldset legend="Nytt brev">
+            <Nav.Row>
+              <Skjema.Select feltNavn="mottaker" bredde="fullbredde" label="Mottaker">
+                {aktoerroller && aktoerroller.map(elem => <option key={elem.kode} value={elem.kode}>{elem.term}</option>)}
+              </Skjema.Select>
+              <Skjema.Select feltNavn="dokumenttypeKode" bredde="fullbredde" label="Type brev" disabled>
+                {dokumenttyper && dokumenttyper.map(elem => <option key={elem.kode} value={elem.kode}>{elem.term}</option>)}
+              </Skjema.Select>
+            </Nav.Row>
+            <Nav.Row>
+              {this.erMangelBrevMedFritekst() && <InfoPanel />}
+            </Nav.Row>
+            <Nav.Row>
+              {this.erMangelBrevMedFritekst() &&
+              <Skjema.Textarea feltNavn="fritekst" label="Hva skal søker sende inn?" maxLength={200} placeholder={placeholder} visTellerFra={100} feil={undefined} />}
+            </Nav.Row>
+            {/* {this.state.feil && <Nav.Row><p>{this.state.feil.message}</p></Nav.Row>} */}
+            <Nav.Row>
+              <Nav.Column xs="12" md="6">
+                <Nav.Knapp htmlType="reset" type="standard" onClick={this.forkastBrev}>Forkast Brev</Nav.Knapp>
+              </Nav.Column>
+              <Nav.Column xs="12" md="6">
+                <Nav.Knapp htmlType="submit" type="hoved" onClick={this.utkastBrev}>Send Utkast</Nav.Knapp>
+              </Nav.Column>
+            </Nav.Row>
+          </Nav.Fieldset>
+        </Nav.Panel>
+      </form>
     );
   }
 }
+
 BrevBestilling.propTypes = {
   resetBrevBestillingForm: PT.func.isRequired,
   opprettDokument: PT.func.isRequired,
@@ -130,14 +144,14 @@ BrevBestilling.defaultProps = {
   oppsummering: {},
 };
 
-const BrevBestillingForm = reduxForm({
+const form = {
   form: 'brevbestilling',
   enableReinitialize: true,
-  destroyOnUnmount: false,
-  keepDirtyOnReinitialize: true,
+  destroyOnUnmount: true,
   updateUnregisteredFields: true,
   validate: brevbestillingValidering,
-})(BrevBestilling);
+  onSubmit: () => {},
+};
 
 const mapStateToProps = state => ({
   brevbestillingSkjemaVerdier: formSelectors.BrevBestillingFormSelector(state).values,
@@ -148,14 +162,16 @@ const mapStateToProps = state => ({
   initialValues: {
     dokumenttypeKode: 'MELDING_MANGLENDE_OPPLYSNINGER',
     mottaker: 'BRUKER',
+    fritekst: undefined,
   },
 });
 
 const mapDispatchToProps = dispatch => ({
+  settFeilFelt: (...feltNavn) => (setSubmitFailed('brevbestilling', ...feltNavn)),
   resetBrevBestillingForm: () => dispatch(reset('brevbestilling')),
   resetDokument: () => dispatch(dokumenterOperations.resetDokument()),
   opprettDokument: (behandlingID, dokumenttypeKode, dokument) => dispatch(dokumenterOperations.opprettDokument(behandlingID, dokumenttypeKode, dokument)),
   lagPdfUtkast: (behandlingID, dokumenttypeKode, dokument) => dispatch(dokumenterOperations.lagPdfUtkast(behandlingID, dokumenttypeKode, dokument)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(BrevBestillingForm);
+export default connect(mapStateToProps, mapDispatchToProps)(reduxForm(form)(BrevBestilling));
