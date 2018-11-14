e6aff6bf125735dd432469bf9fe7928d391c31a7
Bjølseth, Are <are.bjolseth@nav.no>
2018-11-14 15:05:58 +0100

    Hardkoded dokument liste fra UX design

--- src/felles-komponenter/sideDialog/sideDialogDokumenter.js
+++ src/felles-komponenter/sideDialog/sideDialogDokumenter.js
@@ -1,16 +1,78 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import * as Ikoner from '../../resources/images/index';
 import './sideDialogDokumenter.css';
 
 const SideDialogDokumenter = () => (
-  // Todo: Innholdet i denne komponenten er uferdig fra arkitektur og forretningssiden.
   <div className="sideDialogDokumenter">
-    <ul>
-      <li><Link className="lenke" to="http://www.google.com">Attest for lorem ipsum (10.10.2018)</Link></li>
-      <li><Link className="lenke" to="http://www.google.com">Dokument for dolor sit amet (10.10.2018)</Link></li>
-      <li><Link className="lenke" to="http://www.google.com">Dokument Lorem ipsum (10.10.2018)</Link></li>
-    </ul>
+    <table width="100%">
+      <thead>
+        <tr>
+          <th>inn/ut</th>
+          <th>Dokument</th>
+          <th>Avsender/mottaker</th>
+          <th>Dato</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><img src={Ikoner.Svar} alt="SVAR" /></td>
+          <td>
+            <span>
+              <a href="#">Vedtaksbrev</a><br />
+              <img src={Ikoner.Binders} alt="Vedlegg" /><a href="#">A1</a>
+            </span>
+          </td>
+          <td>Peder Christen Asbjørnsen</td>
+          <td>03.06.2017</td>
+        </tr>
+        <tr>
+          <td><img src={Ikoner.InnBrev} alt="INN" /></td>
+          <td>
+            <span><a href="#">Inntektsmelding</a></span>
+          </td>
+          <td>Svensk trygdemyndighet</td>
+          <td>30.05.2017</td>
+        </tr>
+        <tr>
+          <td><img src={Ikoner.Svar} alt="SVAR" /></td>
+          <td>
+            <span><a href="#">Manglende dokumentasjon</a></span>
+          </td>
+          <td>Clemens Bonifacious</td>
+          <td>03.06.2017</td>
+        </tr>
+        <tr>
+          <td><img src={Ikoner.InnBrev} alt="Innbrev" /></td>
+          <td>
+            <span>
+              <a href="#">Endring av søknad</a><br />
+              <img src={Ikoner.Binders} alt="Vedlegg" /><a href="#">Fullmakt</a>
+            </span>
+          </td>
+          <td>Thorine Elisabeth Bruun</td>
+          <td>24.05.2017</td>
+        </tr>
+        <tr>
+          <td><img src={Ikoner.Svar} alt="SVAR" /></td>
+          <td>
+            <span><a href="#">Forvaltningsmelding</a></span>
+          </td>
+          <td>Peder Christen Asbjørnsen</td>
+          <td>12.05.2017</td>
+        </tr>
+        <tr>
+          <td><img src={Ikoner.InnBrev} alt="BrevMottat" /></td>
+          <td>
+            <span>
+              <a href="#">Søknad</a><br />
+              <img src={Ikoner.Binders} alt="Vedlegg" /><a href="#">Fremtidig arbeidskontrakt</a>
+            </span>
+          </td>
+          <td>Peder Christen Asbjørnsen</td>
+          <td>22.04.2017</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 );
 
