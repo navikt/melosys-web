779a0c081f1add592d0f771c238d5574dc2403d3
Bjølseth, Are <are.bjolseth@nav.no>
2018-11-15 12:26:49 +0100

    Work In Progress

--- src/felles-komponenter/sideDialog/sideDialogDokumenter.js
+++ src/felles-komponenter/sideDialog/sideDialogDokumenter.js
@@ -1,79 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PT from 'prop-types';
 import * as Ikoner from '../../resources/images/index';
 import './sideDialogDokumenter.css';
 
-const SideDialogDokumenter = () => (
-  <div className="sideDialogDokumenter">
-    <table width="100%">
-      <thead>
-        <tr>
-          <th>inn/ut</th>
-          <th>Dokument</th>
-          <th>Avsender/mottaker</th>
-          <th>Dato</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td><img src={Ikoner.Svar} alt="SVAR" /></td>
-          <td>
-            <span>
-              <a href="#">Vedtaksbrev</a><br />
-              <img src={Ikoner.Binders} alt="Vedlegg" /><a href="#">A1</a>
-            </span>
-          </td>
-          <td>Peder Christen Asbjørnsen</td>
-          <td>03.06.2017</td>
-        </tr>
-        <tr>
-          <td><img src={Ikoner.InnBrev} alt="INN" /></td>
-          <td>
-            <span><a href="#">Inntektsmelding</a></span>
-          </td>
-          <td>Svensk trygdemyndighet</td>
-          <td>30.05.2017</td>
-        </tr>
-        <tr>
-          <td><img src={Ikoner.Svar} alt="SVAR" /></td>
-          <td>
-            <span><a href="#">Manglende dokumentasjon</a></span>
-          </td>
-          <td>Clemens Bonifacious</td>
-          <td>03.06.2017</td>
-        </tr>
-        <tr>
-          <td><img src={Ikoner.InnBrev} alt="Innbrev" /></td>
-          <td>
-            <span>
-              <a href="#">Endring av søknad</a><br />
-              <img src={Ikoner.Binders} alt="Vedlegg" /><a href="#">Fullmakt</a>
-            </span>
-          </td>
-          <td>Thorine Elisabeth Bruun</td>
-          <td>24.05.2017</td>
-        </tr>
-        <tr>
-          <td><img src={Ikoner.Svar} alt="SVAR" /></td>
-          <td>
-            <span><a href="#">Forvaltningsmelding</a></span>
-          </td>
-          <td>Peder Christen Asbjørnsen</td>
-          <td>12.05.2017</td>
-        </tr>
-        <tr>
-          <td><img src={Ikoner.InnBrev} alt="BrevMottat" /></td>
-          <td>
-            <span>
-              <a href="#">Søknad</a><br />
-              <img src={Ikoner.Binders} alt="Vedlegg" /><a href="#">Fremtidig arbeidskontrakt</a>
-            </span>
-          </td>
-          <td>Peder Christen Asbjørnsen</td>
-          <td>22.04.2017</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-);
+class SideDialogDokumenter extends Component {
+  render() {
+    return (
+      <div className="sideDialogDokumenter">
+        <table width="100%">
+          <thead>
+            <tr>
+              <th>inn/ut</th>
+              <th>Dokument</th>
+              <th>Avsender/mottaker</th>
+              <th>Dato</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><img src={Ikoner.Svar} alt="SVAR" /></td>
+              <td>
+                <span>
+                  <a href="#">Vedtaksbrev</a><br />
+                  <img src={Ikoner.Binders} alt="Vedlegg" /><a href="#">A1</a>
+                </span>
+              </td>
+              <td>Peder Christen Asbjørnsen</td>
+              <td>03.06.2017</td>
+            </tr>
+            <tr>
+              <td><img src={Ikoner.InnBrev} alt="INN" /></td>
+              <td>
+                <span><a href="#">Inntektsmelding</a></span>
+              </td>
+              <td>Svensk trygdemyndighet</td>
+              <td>30.05.2017</td>
+            </tr>
+            <tr>
+              <td><img src={Ikoner.Svar} alt="SVAR" /></td>
+              <td>
+                <span><a href="#">Manglende dokumentasjon</a></span>
+              </td>
+              <td>Clemens Bonifacious</td>
+              <td>03.06.2017</td>
+            </tr>
+            <tr>
+              <td><img src={Ikoner.InnBrev} alt="Innbrev" /></td>
+              <td>
+                <span>
+                <a href="#">Endring av søknad</a><br />
+                <img src={Ikoner.Binders} alt="Vedlegg" /><a href="#">Fullmakt</a>
+              </span>
+              </td>
+              <td>Thorine Elisabeth Bruun</td>
+              <td>24.05.2017</td>
+            </tr>
+            <tr>
+              <td><img src={Ikoner.Svar} alt="SVAR" /></td>
+              <td>
+                <span><a href="#">Forvaltningsmelding</a></span>
+              </td>
+              <td>Peder Christen Asbjørnsen</td>
+              <td>12.05.2017</td>
+            </tr>
+            <tr>
+              <td><img src={Ikoner.InnBrev} alt="BrevMottat"/></td>
+              <td>
+                <span>
+                  <a href="#">Søknad</a><br />
+                  <img src={Ikoner.Binders} alt="Vedlegg" /><a href="#">Fremtidig arbeidskontrakt</a>
+                </span>
+              </td>
+              <td>Peder Christen Asbjørnsen</td>
+              <td>22.04.2017</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+};
 
 export default SideDialogDokumenter;
