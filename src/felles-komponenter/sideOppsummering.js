4cb31abf103fdc097bf9f3167ee39e40ae14657b
Bjølseth, Are <are.bjolseth@nav.no>
2018-11-23 15:33:18 +0100

    La til dropdown list og oppdater knapp

--- src/felles-komponenter/sideOppsummering.js
+++ src/felles-komponenter/sideOppsummering.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PT from 'prop-types';
 
@@ -13,85 +13,116 @@ import { soknadSelectors } from '../ducks/soknad';
 
 import './sideOppsummering.css';
 
-function SideOppsummering(props) {
-  const {
-    oppsummering,
-    person,
-    oppholdUtlandFom,
-    oppholdUtlandTom,
-    oppholdsland,
-  } = props;
+class SideOppsummering extends Component {
+  state = { status: 'VELG' };
+  onChange = event => {
+    const { value } = event.currentTarget;
+    this.setState({ status: value });
+  };
 
-  if (!oppsummering) return <div />;
-  const {
-    saksnummer,
-    sakstype,
-    status,
-    registrertDato,
-    sisteOpplysningerHentetDato,
-  } = oppsummering;
-  const {
-    fnr,
-    sammensattNavn,
-  } = person;
-  const {
-    lagreOgLukkHandle,
-    oppfriskSaksopplysningerHandle,
-    tilbakeleggeHandle,
-  } = props;
+  overstyrSubmit = event => {
+    event.preventDefault();
+  };
+  sendOppdatering = () => {
+    return true;
+  };
 
-  return (
-    <section aria-label="oppsummeringer" className="sideOppsummering panelSeksjon">
-      <Nav.Panel className="saksbehandling__soknadSammendrag">
-        {/* START BEHANDLINGSMENY */}
-        <Nav.Row>
-          <Nav.Column xs="12" md="12">
-            <div className="oppsummering__menylinje">
-              <Nav.EkspanderbartpanelBase ariaTittel="Behandlingsmeny" className="oppsummering__meny" heading={<div className="behandlingsmeny_title">Behandlingsmeny</div>}>
-                <div className="meny__innhold">
-                  <Nav.Knapp type="hoved" mini className="innhold__element" onClick={lagreOgLukkHandle}>Lagre og lukk</Nav.Knapp>
-                  <Nav.Knapp type="hoved" mini className="innhold__element" onClick={tilbakeleggeHandle}>Legg tilbake i kø</Nav.Knapp>
-                  <Nav.Knapp type="hoved" mini className="innhold__element" onClick={oppfriskSaksopplysningerHandle}>Oppfrisk saksopplysninger</Nav.Knapp>
-                </div>
-              </Nav.EkspanderbartpanelBase>
-            </div>
-          </Nav.Column>
-        </Nav.Row>
-        {/* END BEHANDLINGSMENY */}
-        <Nav.Row>
-          <Nav.Column xs="12" md="6">
-            <Nav.Undertittel className="soknadSammendrag__header">Søknad</Nav.Undertittel>
-          </Nav.Column>
-        </Nav.Row>
-        {/* START BEHANDLINGSSTATUS */}
-        <Nav.Row>
-          <Nav.Column xs="12">
-            <dl aria-label="behandlingsinformasjon" className="oppsummering__detaljer--rad">
-              <dt>Søknadstype</dt>
-              <dd>{kodeverkObjektTilTerm(sakstype)}</dd>
-              <dt>Fullt navn</dt>
-              <dd>{sammensattNavn}</dd>
-              <dt>Fnr / dnr</dt>
-              <dd>{fnr}</dd>
-              <dt>Saksnummer:</dt>
-              <dd>{saksnummer || '-'}</dd>
-              <dt>Behandlingsstatus:</dt>
-              <dd>{kodeverkObjektTilTerm(status)}</dd>
-              <dt>Oppholdsland:</dt>
-              <dd>{oppholdsland ? oppholdsland[0] : '-'}</dd>
-              <dt>Periode</dt>
-              <dd>{oppholdUtlandFom} - {oppholdUtlandTom}</dd>
-              <dt>Sist oppdatert:</dt>
-              <dd><EnkeltDato dato={sisteOpplysningerHentetDato} visTidspunkt /></dd>
-              <dt>Registrert dato:</dt>
-              <dd><EnkeltDato dato={registrertDato} /></dd>
-            </dl>
-          </Nav.Column>
-        </Nav.Row>
-        {/* SLUTT BEHANDLINGSSTATUS */}
-      </Nav.Panel>
-    </section>
-  );
+  render() {
+    const {
+      oppsummering,
+      person,
+      oppholdUtlandFom,
+      oppholdUtlandTom,
+      oppholdsland,
+    } = this.props;
+
+    if (!oppsummering) return <div />;
+    const {
+      saksnummer,
+      sakstype,
+      status,
+      registrertDato,
+      sisteOpplysningerHentetDato,
+    } = oppsummering;
+    const {
+      fnr,
+      sammensattNavn,
+    } = person;
+    const {
+      lagreOgLukkHandle,
+      oppfriskSaksopplysningerHandle,
+      tilbakeleggeHandle,
+    } = this.props;
+
+    return (
+      <section aria-label="oppsummeringer" className="sideOppsummering panelSeksjon">
+        <Nav.Panel className="saksbehandling__soknadSammendrag">
+          {/* START BEHANDLINGSMENY */}
+          <Nav.Row>
+            <Nav.Column xs="12" md="12">
+              <div className="oppsummering__menylinje">
+                <Nav.EkspanderbartpanelBase ariaTittel="Behandlingsmeny" className="oppsummering__meny" heading={<div className="behandlingsmeny_title">Behandlingsmeny</div>}>
+                  <div className="meny__innhold">
+                    <Nav.Knapp type="hoved" mini className="innhold__element" onClick={lagreOgLukkHandle}>Lagre og lukk</Nav.Knapp>
+                    <Nav.Knapp type="hoved" mini className="innhold__element" onClick={tilbakeleggeHandle}>Legg tilbake i kø</Nav.Knapp>
+                    <Nav.Knapp type="hoved" mini className="innhold__element" onClick={oppfriskSaksopplysningerHandle}>Oppfrisk saksopplysninger</Nav.Knapp>
+                  </div>
+                </Nav.EkspanderbartpanelBase>
+              </div>
+            </Nav.Column>
+          </Nav.Row>
+          {/* END BEHANDLINGSMENY */}
+          <Nav.Row>
+            <Nav.Column xs="12" md="6">
+              <Nav.Undertittel className="soknadSammendrag__header">Søknad</Nav.Undertittel>
+            </Nav.Column>
+          </Nav.Row>
+          {/* START BEHANDLINGSSTATUS */}
+          <Nav.Row>
+            <Nav.Column xs="12">
+              <dl aria-label="behandlingsinformasjon" className="oppsummering__detaljer--rad">
+                <dt>Søknadstype</dt>
+                <dd>{kodeverkObjektTilTerm(sakstype)}</dd>
+                <dt>Fullt navn</dt>
+                <dd>{sammensattNavn}</dd>
+                <dt>Fnr / dnr</dt>
+                <dd>{fnr}</dd>
+                <dt>Saksnummer:</dt>
+                <dd>{saksnummer || '-'}</dd>
+                <dt>Behandlingsstatus:</dt>
+                <dd>{kodeverkObjektTilTerm(status)}</dd>
+                <dt>Oppholdsland:</dt>
+                <dd>{oppholdsland ? oppholdsland[0] : '-'}</dd>
+                <dt>Periode</dt>
+                <dd>{oppholdUtlandFom} - {oppholdUtlandTom}</dd>
+                <dt>Sist oppdatert:</dt>
+                <dd><EnkeltDato dato={sisteOpplysningerHentetDato} visTidspunkt /></dd>
+                <dt>Registrert dato:</dt>
+                <dd><EnkeltDato dato={registrertDato} /></dd>
+              </dl>
+            </Nav.Column>
+          </Nav.Row>
+          <Nav.Row>
+            <Nav.Column xs="12">
+              <div>
+                <form onSubmit={this.overstyrSubmit}>
+                  <Nav.Fieldset legend="Endre status på behandling til">
+                    <Nav.Select value={this.state.status}>
+                      <option selected key="VELG" value="VELG">Velg...</option>
+                      <option key="AVVENT_DOK_UTL" value="AVVENT_DOK_UTL">AVVENT_DOK_UTL</option>
+                      <option key="AVVENT_DOK_PART" value="AVVENT_DOK_PART">AVVENT_DOK_PART</option>
+                    </Nav.Select>
+                  </Nav.Fieldset>
+                  <Nav.Hovedknapp htmlType="submit" onClick={this.sendOppdatering}>Oppdater</Nav.Hovedknapp>
+                </form>
+              </div>
+            </Nav.Column>
+          </Nav.Row>
+          {/* SLUTT BEHANDLINGSSTATUS */}
+        </Nav.Panel>
+      </section>
+    );
+  }
 }
 
 SideOppsummering.propTypes = {
