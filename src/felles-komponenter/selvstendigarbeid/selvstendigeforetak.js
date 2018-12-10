66786bd1fdf6412e45c56cdd1dd2928f9730cc6c
Bjølseth, Are <are.bjolseth@nav.no>
2018-12-10 14:01:06 +0100

    Refactored selvstendigArbeid til selvstendingarbeid(index, selvstendigArbeid, foretak, enkeltforetak)

---
+++ src/felles-komponenter/selvstendigarbeid/selvstendigeforetak.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import PT from 'prop-types';
+
+import * as Nav from '../../utils/navFrontend';
+import EnkeltForetak from './enkeltforetak';
+
+import './selvstendigArbeid.css';
+
+class SelvstendigeForetak extends Component {
+  componentDidUpdate(prevProps) {
+    const { fields: oldFields } = prevProps;
+    const { oppdaterSoknadState, skjema, fields } = this.props;
+
+    // Sjekk om listen over selvstendig næringsdrivende
+    // har oppdatert seg siden sist. I såfall, oppdaterSoknadState.
+    if (fields.length !== oldFields.length) {
+      oppdaterSoknadState(skjema);
+    }
+  }
+
+  render() {
+    const {
+      fields, organisasjoner, hentOrganisasjon, oppdaterSoknadState, skjema,
+    } = this.props;
+
+    return (
+      <div>
+        {fields.map((foretaket, index) => (
+          <EnkeltForetak
+            key={foretaket}
+            orgnr={fields.get(index).orgnr}
+            organisasjon={organisasjoner.find(organisasjon => organisasjon.orgnr === fields.get(index).orgnr) || null}
+            foretaket={foretaket}
+            posisjon={index + 1}
+            hentOrganisasjon={hentOrganisasjon}
+            slettForetak={() => fields.remove(index)}
+            oppdaterSoknadState={oppdaterSoknadState}
+            skjema={skjema}
+          />))
+        }
+        <div className="leggTilForetak">
+          <Nav.Row>
+            <Nav.Column xs="12">
+              <Nav.Knapp mini onClick={() => fields.push({})}>Legg til nytt foretak</Nav.Knapp>
+            </Nav.Column>
+          </Nav.Row>
+        </div>
+      </div>
+    );
+  }
+}
+
+SelvstendigeForetak.propTypes = {
+  fields: PT.object.isRequired,
+  hentOrganisasjon: PT.func.isRequired,
+  organisasjoner: PT.array.isRequired,
+  oppdaterSoknadState: PT.func.isRequired,
+  skjema: PT.object.isRequired,
+};
+export default SelvstendigeForetak;
