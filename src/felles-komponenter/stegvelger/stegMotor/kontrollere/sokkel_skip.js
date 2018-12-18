bc4303b70068cff3d8f83ba5a887c0390aa4cf5b
Terje Karlsen <terjeofnorway@gmail.com>
2018-12-18 12:30:53 +0100

    Fikset manglende sletting av tidligere valg ved endring i sti.

--- src/felles-komponenter/stegvelger/stegMotor/kontrollere/sokkel_skip.js
+++ src/felles-komponenter/stegvelger/stegMotor/kontrollere/sokkel_skip.js
@@ -54,6 +54,7 @@ class SokkelSkip extends Steg {
   // Hvis SKIP er valgt som vurdering, så skal det ikke legges inn
   // en begrunnelse.
   static alleErAvklart = (sokkelEllerSkip, sokkelSkipKonklusjon) => {
+    console.log(sokkelEllerSkip);
     const avklartSokkelEllerSkip = sokkelEllerSkip.length > 0 && sokkelEllerSkip
       .map(enkelt => {
         if (enkelt.installasjonsType === Koder.SOKKEL && enkelt.installasjonsTypeBegrunnelse && enkelt.arbeidsland) { return true; }
