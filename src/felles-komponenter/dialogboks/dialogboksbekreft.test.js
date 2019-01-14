18475bca29cdcb5d7119b2a55d2a7102eb503549
JohanSakshaug <johan.sakshauger@nav.no>
2019-01-14 14:56:08 +0100

    stash

---
+++ src/felles-komponenter/dialogboks/dialogboksbekreft.test.js
@@ -0,0 +1,3 @@
+import DialogboksBekreft from './dialogboksBekreft';
+
+describe('Dialogboksb')
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,10 @@
 /* eslint-disable */
 
-import Enzyme from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 global.fetch = require('jest-fetch-mock');
 
 Enzyme.configure({ adapter: new Adapter() });
+
+global.shallow = shallow;
+global.mount = mount;
