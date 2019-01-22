e21c5abc27bf913f55cc4d526fe82a9c4fc8f858
JohanSakshaug <johan.sakshaug@nav.no>
2019-01-22 13:44:32 +0100

    Fiks bug hvor stegvelger ikke gikk videre

--- src/felles-komponenter/stegvelger/stegMotor/kontrollere/vesentlig_virksomhet.js
+++ src/felles-komponenter/stegvelger/stegMotor/kontrollere/vesentlig_virksomhet.js
@@ -3,7 +3,7 @@ import { FANE_STATUS, STEG } from '../typer';
 import VurderingVesentligVirksomhet from '../../stegKomponenter/vurderingVesentligVirksomhet';
 import { erVilkarOppfylt } from '../../../../regler/vilkar';
 
-import { lovvalgsbestemmelser } from '../../../../koder';
+import { vilkar } from '../../../../koder';
 
 class VesentligVirksomhet extends Steg {
   constructor(propsLight, stegPosisjon) {
@@ -11,7 +11,7 @@ class VesentligVirksomhet extends Steg {
     this._kriterier = [
       {
         beskrivelse: 'ORDINAER_ARBEIDSTAKER og VESENTLIG_VIRKSOMHET',
-        exec: (avklartefakta, alleVilkar) => erVilkarOppfylt(lovvalgsbestemmelser.ART12_1_VESENTLIG_VIRKSOMHET, alleVilkar) !== undefined,
+        exec: (avklartefakta, alleVilkar) => erVilkarOppfylt(vilkar.ART12_1_VESENTLIG_VIRKSOMHET, alleVilkar) !== undefined,
         nesteSteg: STEG.ARTIKKEL_12_1,
       },
       {
