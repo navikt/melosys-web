ccb1c231534d2358d4f21fbe68a10f4c39191680
JohanSakshaug <johan.sakshaug@nav.no>
2019-01-23 12:37:14 +0100

    Sett opp enzyme

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,9 @@
 /* eslint-disable */
 
 import Enzyme, { shallow, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-16'
+
+
 global.fetch = require('jest-fetch-mock');
 
 Enzyme.configure({ adapter: new Adapter() });
