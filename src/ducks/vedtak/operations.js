1c3554b2472febe8397b4712ee6fc4fe4b9ba451
JohanSakshaug <johan.sakshaug@nav.no>
2019-01-23 09:33:01 +0100

    Bruk behandlingID i stedet for saksnummer i vedtakoperation

--- src/ducks/vedtak/operations.js
+++ src/ducks/vedtak/operations.js
@@ -12,8 +12,8 @@ import * as Api from '../../services/api';
 import * as Types from './types';
 
 /* eslint-disable import/prefer-default-export */
-export function fatte(snr, body) {
-  return doThenDispatch(() => Api.Vedtak.fatte(snr, body), {
+export function fatte(behandlingID, body) {
+  return doThenDispatch(() => Api.Vedtak.fatte(behandlingID, body), {
     OK: Types.OK,
     FEILET: Types.FEILET,
     PENDING: Types.PENDING,
