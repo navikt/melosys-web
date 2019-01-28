fb696d205147b466f6001c32050021e010bb5087
JohanSakshaug <46343153+JohanSakshaug@users.noreply.github.com>
2019-01-28 13:43:34 +0100

    Melosys 2067 innføre testing av react komponenter med enzyme (#435)
    * Sett opp enzyme
    
    * Test kodetermselect
    
    * Test modal for henleggelse

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,12 @@
 /* eslint-disable */
 
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
+
+
 global.fetch = require('jest-fetch-mock');
 
 Enzyme.configure({ adapter: new Adapter() });
+
+global.shallow = shallow;
+global.mount = mount;
