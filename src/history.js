6efdb57e4cc0611892fed2bd4025fe3e5ccda50f
Bjølseth, Are <are.bjolseth@nav.no>
2019-02-19 15:55:11 +0100

    Import lodash functions individually to reduce bundle size. Cleanup.

--- src/history.js
+++ src/history.js
@@ -1,16 +1,9 @@
 import { createBrowserHistory } from 'history';
 
-const CONTEXT_PATH = '/melosys/'; // Must end with '/', since /melosys/static, /melosys/api, etc. Webpack compile.
 const NO_CONTEXT_PATH = '/';
 
-const isLocalJavaDevEnv = () => `${process.env.REACT_APP_BUILD_VERSION}` === 'java_local';
-
-const contextPath = isLocalJavaDevEnv() ? CONTEXT_PATH : NO_CONTEXT_PATH;
-/* eslint-disable no-console */
-console.log('history: contextPath=', contextPath);
-/* eslint-enable no-console */
 const routerHistory = createBrowserHistory({
-  basename: contextPath,
+  basename: NO_CONTEXT_PATH,
 });
 export default routerHistory;
 
