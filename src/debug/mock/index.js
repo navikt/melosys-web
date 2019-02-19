aaf6c40de0fa7a382e072ebba3a111b4cd7b0b76
Bjølseth, Are <are.bjolseth@nav.no>
2019-02-19 15:00:55 +0100

    DEVOPS removed stale spark kode.

--- src/debug/mock/index.js
+++
@@ -1,9 +0,0 @@
-import { soeknadDokument } from './soknadtemplate';
-import { behandlingsOppgave } from './behandlingsoppgave';
-import { journalforingOppgave } from './journalforingoppgave';
-
-export {
-  soeknadDokument,
-  behandlingsOppgave,
-  journalforingOppgave,
-};
