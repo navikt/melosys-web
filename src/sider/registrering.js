465b3c432d9190e4b4907b8037d3f2a12caf9dbc
Bjølseth, Are <are.bjolseth@nav.no>
2019-02-20 13:08:05 +0100

    MELOSYS-2087 Regisistrering av Unntaksperioder.

---
+++ src/sider/registrering.js
@@ -0,0 +1,26 @@
+/* eslint no-alert:off, consistent-return:off */
+import React, { Component } from 'react';
+import PT from 'prop-types';
+import * as Nav from '../utils/navFrontend';
+import UnntaksPerioder from './registrering/unntaksperioder';
+
+import './registrering.css';
+
+class Registrering extends Component {
+  render() {
+    return (
+      <div className="registrering">
+        <Nav.Container fluid>
+          <Nav.Row>
+            <Nav.Column xs="7">
+              <UnntaksPerioder />
+            </Nav.Column>
+            <Nav.Column xs="5">
+            </Nav.Column>
+          </Nav.Row>
+        </Nav.Container>
+      </div>
+    );
+  }
+}
+export default Registrering;
