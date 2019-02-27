7c61b02cef3f8a864bdfd615b825e6839525eea6
JohanSakshaug <johan.sakshaug@nav.no>
2019-02-27 15:49:57 +0100

    Legg til utils for logging, legg til try catch i saksbehandling

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,11 +2,13 @@ import { isUndefined, cloneDeep, assign } from 'lodash';
 import throttle from 'lodash.throttle';
 import * as dato from './dato';
 import * as streng from './streng';
+import * as logger from './logger';
 import { buildinfo, delay, fn, isJSON, verdiSomNullable, queryParamsTilObjekt } from './utils';
 
 export {
   buildinfo, delay, fn, isJSON, verdiSomNullable, queryParamsTilObjekt,
   dato, streng,
+  logger,
   isUndefined as _isUndefined,
   cloneDeep as _cloneDeep,
   assign as _assign,
