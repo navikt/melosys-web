ad2caf9e2ec1f39878be08052a6adeb93bc63011
Bjølseth, Are <are.bjolseth@nav.no>
2019-03-05 16:26:36 +0100

    Refactored personopplysninger og skilte ut PersonInfo og oppgittAdresse fra soknad.

--- src/registrering-komponenter/saksopplysninger.js
+++ src/registrering-komponenter/saksopplysninger.js
@@ -4,12 +4,15 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { reduxForm } from 'redux-form';
 
+import * as Utils from '../utils';
 import * as MPT from '../proptypes';
+import * as Nav from '../utils/navFrontend';
+
 import Personopplysninger from '../soknad-komponenter/personopplysninger';
 import Medlemskap from '../komponenter/medlemskap';
-
 import { fagsakOperations, fagsakSelectors } from '../ducks/fagsaker';
-import ArbeidsgivereNorge from "../soknad-komponenter/arbeidsgivereNorge";
+import { lovvalgsperioderOperations, lovvalgsperioderSelectors } from '../ducks/lovvalgsperioder';
+import Landvelger from '../soknad-komponenter/skjema/landvelger';
 
 class Saksopplysninger extends Component {
   async componentDidMount() {
@@ -17,9 +20,9 @@ class Saksopplysninger extends Component {
   }
 
   lastInnSaksopplysninger= async () => {
-    const { hentFagsaker } = this.props;
-    const response = await hentFagsaker(4);
-    console.log(response);
+    const { hentFagsaker, hentLovvalgsperioder } = this.props;
+    await hentFagsaker(4);
+    await hentLovvalgsperioder(4);
   };
 
   overstyrSubmit = event => {
@@ -27,21 +30,67 @@ class Saksopplysninger extends Component {
   };
 
   render() {
-    const { medlemskap } = this.props;
-
+    const { medlemskap, lovvalgsPeriode: { fomDato, tomDato } } = this.props;
+    const formattertFomDato = Utils.dato.formatterDatoTilNorsk(fomDato);
+    const formattertTomDato = Utils.dato.formatterDatoTilNorsk(tomDato);
     return (
-      <form name="registrering" id="registrering" onSubmit={this.overstyrSubmit} >
-        <Personopplysninger />
+      <div>
+        <form name="registrering" id="registrering" onSubmit={this.overstyrSubmit} >
+          <div className="stegvelger panelSeksjon">
+            <div className="panel stegFane steg0 stegFane--aktiv">
+              <Nav.Systemtittel>Redigering av unntaksperioder</Nav.Systemtittel>
+              <br />
+              <div className="vurderingEndrePeriode">
+                <Nav.Undertittel>Lovvalgsperiode fra SED</Nav.Undertittel>
+                <Nav.Row>
+                  <Nav.Column xs="3">
+                    <Nav.Input
+                      bredde="fullbredde"
+                      label="Startdato"
+                      value={formattertFomDato}
+                      onChange={() => {}}
+                    />
+                  </Nav.Column>
+                  <Nav.Column xs="3">
+                    <Nav.Input
+                      bredde="fullbredde"
+                      label="Sluttdato"
+                      value={formattertTomDato}
+                      onChange={() => {}}
+                    />
+                  </Nav.Column>
+                </Nav.Row>
+                <Nav.Row>
+                  <Nav.Column xs="3">
+                    <Landvelger label="Land" feltNavn="landKode" />
+                  </Nav.Column>
+                  <Nav.Column xs="3">
+                    <Nav.Input
+                      bredde="fullbredde"
+                      label="Hjemmel"
+                      value=""
+                      onChange={() => {}}
+                    />
+                  </Nav.Column>
+                </Nav.Row>
+              </div>
+            </div>
+          </div>
+        </form>
+        <Personopplysninger registrering />
         {medlemskap && <Medlemskap medlemskap={medlemskap} />}
-      </form>
+      </div>
     );
   }
 }
 
 Saksopplysninger.propTypes = {
   hentFagsaker: PT.func.isRequired,
+  hentLovvalgsperioder: PT.func.isRequired,
   inntekt: MPT.Inntekt,
   medlemskap: MPT.Medlemskap,
+  oppsummering: MPT.Oppsummering.isRequired,
+  lovvalgsPeriode: PT.object.isRequired,
   skjema: PT.any,
 };
 
@@ -54,9 +103,12 @@ Saksopplysninger.defaultProps = {
 const mapStateToProps = state => ({
   inntekt: fagsakSelectors.InntektSoknadenSelector(state),
   medlemskap: fagsakSelectors.MedlemskapSelector(state),
+  oppsummering: fagsakSelectors.OppsummeringSelector(state),
+  lovvalgsPeriode: lovvalgsperioderSelectors.LovvalgsperiodeSelector(state),
 });
 const mapDispatchToProps = dispatch => ({
   hentFagsaker: saksnummer => dispatch(fagsakOperations.hent(saksnummer)),
+  hentLovvalgsperioder: behandlingID => dispatch(lovvalgsperioderOperations.hent(behandlingID)),
 });
 
 const SaksopplysningerForm = reduxForm({
