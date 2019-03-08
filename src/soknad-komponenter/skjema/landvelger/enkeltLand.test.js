0c6de11c3627e62cf88d37c8a6f7eb8f41eaf4ae
JohanSakshaug <johan.sakshaug@nav.no>
2019-03-08 16:59:36 +0100

    Øk testdekning i enkeltLand.js

--- src/soknad-komponenter/skjema/landvelger/enkeltLand.test.js
+++ src/soknad-komponenter/skjema/landvelger/enkeltLand.test.js
@@ -7,27 +7,105 @@ describe('EnkeltLand', () => {
   beforeEach(() => {
     props = {
       dataListID: '1',
-      landkoder: [{ kode: 'test', term: 'test' }],
+      landkoder: [{ kode: 'NO', term: 'Norge' }, { kode: 'SE', term: 'Sverige' }],
       meta: {},
       label: '',
       feil: '',
-      input: {},
+      input: { onChange: jest.fn() },
       disabled: false,
     };
   });
 
   it('viser en NAV Input', () => {
-    const Enkeltland = shallow(<EnkeltLand {...props} />);
-    expect(Enkeltland.find('Input')).toHaveLength(1);
+    const enkeltLand = shallow(<EnkeltLand {...props} />);
+    expect(enkeltLand.find('Input')).toHaveLength(1);
   });
 
   it('sender value prop til NAV Input korrekt', () => {
-    const Enkeltland = shallow(<EnkeltLand {...props} />);
+    const enkeltLand = shallow(<EnkeltLand {...props} />);
     const event = { target: { value: 'test' } };
 
-    Enkeltland.instance().inputEndringHandler(event);
+    enkeltLand.instance().inputEndringHandler(event);
 
-    const Input = Enkeltland.find('Input');
+    const Input = enkeltLand.find('Input');
     expect(Input.props().value).toBe('test');
   });
+
+  it('inputEndringHandler', () => {
+    const enkeltLandInstance = shallow(<EnkeltLand {...props} />).instance();
+    enkeltLandInstance.reduxFjernLand = jest.fn();
+    enkeltLandInstance.tomFeilmelding = jest.fn();
+    enkeltLandInstance.finnEttLand = jest.fn(() => ({ kode: 'test' }));
+    enkeltLandInstance.reduxOppdaterLand = jest.fn();
+
+    enkeltLandInstance.fokusUtHandler();
+
+    expect(enkeltLandInstance.reduxFjernLand).toHaveBeenCalledTimes(1);
+    expect(enkeltLandInstance.tomFeilmelding).toHaveBeenCalledTimes(1);
+
+    const event = { target: { value: 'test' } };
+    enkeltLandInstance.inputEndringHandler(event);
+
+    enkeltLandInstance.fokusUtHandler();
+
+    expect(enkeltLandInstance.finnEttLand).toHaveBeenCalledTimes(1);
+    expect(enkeltLandInstance.finnEttLand).toHaveBeenLastCalledWith(event.target.value);
+    expect(enkeltLandInstance.reduxOppdaterLand).toHaveBeenCalledTimes(1);
+    expect(enkeltLandInstance.reduxOppdaterLand).toHaveBeenLastCalledWith('test');
+  });
+
+  it('inputTestNedHandler', () => {
+    const enkeltLandInstance = shallow(<EnkeltLand {...props} />).instance();
+    enkeltLandInstance.fokusUtHandler = jest.fn();
+    const event = { keyCode: 0, preventDefault: jest.fn() };
+
+    enkeltLandInstance.inputTastNedHandler(event);
+
+    expect(enkeltLandInstance.fokusUtHandler).toHaveBeenCalledTimes(0);
+    expect(event.preventDefault).toHaveBeenCalledTimes(0);
+
+    event.keyCode = 13;
+
+    enkeltLandInstance.inputTastNedHandler(event);
+
+    expect(enkeltLandInstance.fokusUtHandler).toHaveBeenCalledTimes(1);
+    expect(event.preventDefault).toHaveBeenCalledTimes(1);
+  });
+
+  it('reduxOppdaterLand', () => {
+    const enkeltLandInstance = shallow(<EnkeltLand {...props} />).instance();
+
+    expect(() => enkeltLandInstance.reduxOppdaterLand(null)).toThrow();
+
+    enkeltLandInstance.reduxOppdaterLand('Norge');
+
+    expect(props.input.onChange).toHaveBeenCalledTimes(1);
+    expect(props.input.onChange).toHaveBeenCalledWith('Norge');
+  });
+
+  it('reduxFjernLand', () => {
+    const enkeltLandInstance = shallow(<EnkeltLand {...props} />).instance();
+
+    enkeltLandInstance.reduxFjernLand();
+
+    expect(props.input.onChange).toHaveBeenCalledTimes(1);
+    expect(props.input.onChange).toHaveBeenCalledWith('');
+  });
+
+  it('fokusInnHandler', () => {
+    const enkeltLandInstance = shallow(<EnkeltLand {...props} />).instance();
+    const event = { target: { select: jest.fn() } };
+
+    enkeltLandInstance.fokusInnHandler(event);
+
+    expect(event.target.select).toHaveBeenCalledTimes(1);
+  });
+
+  // it('finnFlereLand', () => {
+  //   const enkeltLandInstance = shallow(<EnkeltLand {...props} />).instance();
+  //
+  //   expect(enkeltLandInstance.finnFlereLand(null)).toEqual([]);
+  //
+  //   expect(enkeltLandInstance.finnFlereLand())
+  // });
 });
--- src/soknad-komponenter/skjema/landvelger/index.test.js
+++ src/soknad-komponenter/skjema/landvelger/index.test.js
@@ -17,23 +17,23 @@ describe(('Landvelger'), () => {
   describe('Dersom multiland prop er false', () => {
     it('viser enkeltland og ikke multiland', () => {
       props.multiLand = false;
-      const LandVelger = shallow(<Landvelger {...props} />);
-      expect(LandVelger.find('EnkeltLandWrapper')).toHaveLength(1);
-      expect(LandVelger.find('MultiLandWrapper')).toHaveLength(0);
+      const landVelger = shallow(<Landvelger {...props} />);
+      expect(landVelger.find('EnkeltLandWrapper')).toHaveLength(1);
+      expect(landVelger.find('MultiLandWrapper')).toHaveLength(0);
     });
   });
 
   describe('Dersom multiland prop er true', () => {
     it('viser multiland og ikke enkeltland', () => {
       props.multiLand = true;
-      const LandVelger = shallow(<Landvelger {...props} />);
-      expect(LandVelger.find('EnkeltLandWrapper')).toHaveLength(0);
-      expect(LandVelger.find('MultiLandWrapper')).toHaveLength(1);
+      const landVelger = shallow(<Landvelger {...props} />);
+      expect(landVelger.find('EnkeltLandWrapper')).toHaveLength(0);
+      expect(landVelger.find('MultiLandWrapper')).toHaveLength(1);
     });
   });
 
   it('viser en datalist', () => {
-    const LandVelger = shallow(<Landvelger {...props} />);
-    expect(LandVelger.find('datalist')).toHaveLength(1);
+    const landVelger = shallow(<Landvelger {...props} />);
+    expect(landVelger.find('datalist')).toHaveLength(1);
   });
 });
