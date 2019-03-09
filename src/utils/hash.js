dc449e4939aed605693c0cf12e56e39312e60144
Are Bjolseth <abjolseth@gmail.com>
2019-03-09 20:20:06 +0100

    Fjernet ubenyttet hash komponent

--- src/utils/hash.js
+++
@@ -1,3 +0,0 @@
-const fnv1a = require('@sindresorhus/fnv1a');
-
-export const hash = str => fnv1a(str);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,5 @@
 import { isUndefined, cloneDeep, assign } from 'lodash';
 import throttle from 'lodash.throttle';
-import { hash } from './hash';
 import * as dato from './dato';
 import * as streng from './streng';
 import * as logger from './logger';
@@ -9,7 +8,6 @@ import { buildinfo, delay, fn, isJSON, verdiSomNullable, queryParamsTilObjekt } 
 export {
   buildinfo, delay, fn, isJSON, verdiSomNullable, queryParamsTilObjekt,
   dato, streng,
-  hash,
   logger,
   isUndefined as _isUndefined,
   cloneDeep as _cloneDeep,
