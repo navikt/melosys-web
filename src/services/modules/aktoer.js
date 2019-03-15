c1c0b12484c89a68da9adbfed1e2a764f44c2e97
Are Bjolseth <abjolseth@gmail.com>
2019-03-15 15:40:38 +0100

    QueryParams parsing.

--- src/services/modules/aktoer.js
+++ src/services/modules/aktoer.js
@@ -1,13 +1,12 @@
+import * as QS from 'qs';
 import { getAsJson, postAsJson } from '../utils';
 import { API_BASE_URL } from '../api-constants';
 
 export async function hent(saksnr, rolleKode, representererKode) {
-  const sporsmaltegn = rolleKode || representererKode ? '?' : '';
-  const rolleKodeParam = rolleKode ? `rolle=${rolleKode}` : '';
-  const and = rolleKode && representererKode ? '&' : '';
-  const representererKodeParam = representererKode ? `representerer=${representererKode}` : '';
+  const URI_PATH = `${API_BASE_URL}fagsaker/${saksnr}/aktoerer`;
+  const qs = QS.stringify({ rolleKode, representererKode });
 
-  const URI_AKTOER = `${API_BASE_URL}fagsaker/${saksnr}/aktoerer${sporsmaltegn}${rolleKodeParam}${and}${representererKodeParam}`;
+  const URI_AKTOER = qs ? `${URI_PATH}/?${qs}` : URI_PATH;
   return getAsJson(URI_AKTOER);
 }
 
