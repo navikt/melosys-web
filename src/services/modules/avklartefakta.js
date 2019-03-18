47f58d3d18552c5b4aeab4e1827b490ab09aa702
Bjølseth, Are <are.bjolseth@nav.no>
2019-03-18 14:43:28 +0100

    Fjernet unnecessary async keywords. getAsJson and postAsJson returns a promise.

--- src/services/modules/avklartefakta.js
+++ src/services/modules/avklartefakta.js
@@ -1,12 +1,12 @@
 import { getAsJson, postAsJson } from '../utils';
 import { API_BASE_URL, AVKLARTEFAKTA } from '../api-constants';
 
-export const hent = async bid => {
+export const hent = bid => {
   const URI_AVKLARTEFAKTA = `${API_BASE_URL}${AVKLARTEFAKTA}/${bid}`;
   return getAsJson(URI_AVKLARTEFAKTA);
 };
 
-export const send = async (bid, dokument) => {
+export const send = (bid, dokument) => {
   const URI_AVKLARTEFAKTA = `${API_BASE_URL}${AVKLARTEFAKTA}/${bid}`;
   return postAsJson(URI_AVKLARTEFAKTA, dokument);
 };
