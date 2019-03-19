18fe7f3fd4d03b586653912ffb3ed1ea2a2110ce
Are Bjolseth <abjolseth@gmail.com>
2019-03-19 22:21:28 +0100

    Refactored service/utils.js core with fat arrow function. Optimized use of async/await.

--- src/hoc/withErrorHandling.js
+++ src/hoc/withErrorHandling.js
@@ -26,33 +26,52 @@ FeilKomponent.propTypes = {
   }).isRequired,
 };
 
+const parseMockErrorObject = (kontekst, currentReduxState) => {
+  const { melding } = kontekst;
+  // Siden fetch-APIet resulterer i en modell med strukturen 'data.data', lager vi aliaset 'payload'.
+  const { data: payload } = currentReduxState;
+
+  // Deretter kan vi bruke 'data' slik vi mener å bruke den.
+  const { data } = payload;
+  const fetchdata = Utils.isJSON(data) ? JSON.parse(data) : data;
+
+  const { status, message: statusText } = fetchdata;
+  return {
+    status, statusText, melding, fetchdata,
+  };
+};
+const parseErrorObject = (kontekst, currentReduxState) => {
+  debugger;
+  return {
+    status: '403', statusText: 'statusText', melding: 'melding', fetchdata: 'fetchdata',
+  };
+};
+
 /** Dette er en HOC som benyttes til å wrappe rundt alle komponenter som
  * skal støttes av en feilhåndtering.
  *
  * @param kontekster Array av reducer-modeller ('fagsak', 'saksbehandler' etc) som instansen skal ta hensyn til og sjekke for feil.
  * @param WrappedComponent React-komponenten som wrappes.
  */
+
 const withErrorHandling = (kontekster, WrappedComponent) => props => {
+  const NODE_ENV = `${process.env.NODE_ENV}`; // default =>  'development' : 'production'
+
   const ErrorComponent = errorProps => {
     const { reduxRootState } = errorProps;
     const feilSamling = [];
 
     kontekster.forEach(kontekst => {
-      const { navn, melding } = kontekst;
-      const { status: feilStatus } = reduxRootState[navn];
-
+      const currentReduxState = reduxRootState[kontekst.navn];
+      const { status: feilStatus } = currentReduxState;
       if (feilStatus === 'ERROR') {
-        // Siden fetch-APIet resulterer i en modell med strukturen 'data.data', lager vi aliaset 'payload'.
-        const { data: payload } = reduxRootState[navn];
-
-        // Deretter kan vi bruke 'data' slik vi mener å bruke den.
-        const { data } = payload;
-        const fetchdata = Utils.isJSON(data) ? JSON.parse(data) : data;
-
-        const { status, message: statusText } = fetchdata;
-        feilSamling.push({
-          status, statusText, melding, fetchdata,
-        });
+        if (NODE_ENV === 'development') {
+          const emo = parseMockErrorObject(kontekst, currentReduxState);
+          feilSamling.push(emo);
+        } else {
+          const eobj = parseErrorObject(kontekst, currentReduxState);
+          feilSamling.push(eobj);
+        }
       }
     });
 
