aec82fc541c6720e88d19a7a2158a323938230e6
JohanSakshaug <46343153+JohanSakshaug@users.noreply.github.com>
2019-04-01 15:05:39 +0200

    MELOSYS-2419 - Legge til og slette fullmektig (#519)

--- src/soknad-komponenter/fullmektig/fullmektig.js
+++ src/soknad-komponenter/fullmektig/fullmektig.js
@@ -1,179 +1,128 @@
-import React, { Component, Fragment, useState, useEffect } from 'react';
-import { connect } from 'react-redux';
+import React, { Fragment, useState, useEffect } from 'react';
 import PT from 'prop-types';
 import * as MKV from 'melosys-kodeverk';
-import uuid from 'uuid/v4';
 
 import * as Nav from '../../utils/navFrontend';
-import * as Ikoner from '../../resources/images';
-import * as Api from '../../services/api';
 import * as Utils from '../../utils';
 
-import PanelHeader from '../../komponenter/panelHeader/panelHeader';
 import Kontaktopplysninger from '../kontaktopplysninger';
 import PostAdresse from '../../komponenter/adresser/postAdresse';
-
-import { fagsakSelectors } from '../../ducks/fagsaker';
+import SokFullmektigOrg from './sokFullmektigOrg';
 
 import './fullmektig.css';
 
-const Fullmektig = ({ fullmektig, lagreFullmektig, redigerbart }) => {
+function Fullmektig(props) {
+  const {
+    fullmektig,
+    lagreFullmektig,
+    redigerbart,
+    databaseID,
+    slettAktoer,
+    slettFullmektigLokalt,
+    lagreNyFullmektigOgOppdaterLokalt,
+    hentOrg,
+  } = props;
   const [representererKode, settRepresentererKode] = useState(null);
+  const [org, settOrg] = useState(null);
 
-  const vedRolleEndring = event => {
-    lagreFullmektig(event.target.value, fullmektig.org.orgnr);
-    settRepresentererKode(event.target.value);
-  };
-
-  useEffect(() => {
-    settRepresentererKode(fullmektig.representererKode);
-  }, []);
-
-  const uniktNavn = uuid();
-
-  return (
-    <Nav.Row className="fullmektig">
-      <Nav.Column xs="6">
-        {
-          fullmektig.org && fullmektig.org.navn
-        }
-        {
-          fullmektig.org &&
-          <Fragment>
-            <div className="postadresse_tittel">Postadresse</div>
-            <PostAdresse postadresse={fullmektig.org.postadresse} />
-          </Fragment>
-        }
-        <Nav.Fieldset disabled={!redigerbart} legend="Hvem er dette fullmektig for?" className="radioknapper">
-          <Nav.Radio
-            onChange={vedRolleEndring}
-            checked={representererKode === MKV.Koder.representerer.ARBEIDSGIVER}
-            label="Arbeidsgiver"
-            value={MKV.Koder.representerer.ARBEIDSGIVER}
-            name={uniktNavn}
-          />
-          <Nav.Radio
-            onChange={vedRolleEndring}
-            checked={representererKode === MKV.Koder.representerer.BRUKER}
-            label="Arbeidstaker"
-            value={MKV.Koder.representerer.BRUKER}
-            name={uniktNavn}
-          />
-          <Nav.Radio
-            onChange={vedRolleEndring}
-            checked={representererKode === MKV.Koder.representerer.BEGGE}
-            label="Både arbeidstaker og arbeidsgiver"
-            value={MKV.Koder.representerer.BEGGE}
-            name={uniktNavn}
-          />
-        </Nav.Fieldset>
-        <Nav.Knapp disabled type="mini">&times; FJERN FULLMEKTIG</Nav.Knapp>
-      </Nav.Column>
-      <Nav.Column xs="6">
-        <Kontaktopplysninger juridiskOrg={fullmektig.org} />
-      </Nav.Column>
-    </Nav.Row>
-  );
-};
-
-Fullmektig.propTypes = {
-  fullmektig: PT.object,
-  lagreFullmektig: PT.func.isRequired,
-  redigerbart: PT.bool.isRequired,
-};
-
-Fullmektig.defaultProps = {
-  fullmektig: {},
-};
-
-export class FullmektigPanel extends Component {
-  state = {
-    fullmektige: [],
-  };
-
-  componentDidMount() {
-    this.hentFullmektige();
-  }
-
-  lagreFullmektig = (representererKode, orgnr) => {
-    const { lagreAktoer, oppsummering: { saksnummer } } = this.props;
-    lagreAktoer(saksnummer, {
-      aktoerID: null,
-      orgnr,
-      utenlandskPersonID: null,
-      institusjonsID: null,
-      rolleKode: MKV.Koder.aktoersroller.REPRESENTANT,
-      representererKode,
-    });
-  };
-
-  hentFullmektige = async () => {
-    const { saksnummer } = this.props.oppsummering;
-    const { hentAktoer, hentOrg } = this.props;
-
+  const vedRolleEndring = async event => {
+    event.persist();
     try {
-      /* eslint-disable no-param-reassign */
-      const fullmektige = await hentAktoer(saksnummer, MKV.Koder.aktoersroller.REPRESENTANT);
-      fullmektige.forEach(async fullmektig => {
-        fullmektig.org = await hentOrg(fullmektig.orgnr);
-      });
-      /* eslint-enable no-param-reassign */
-      this.setState({ fullmektige });
+      if (org) await lagreFullmektig(event.target.value, org.orgnr, databaseID);
+      settRepresentererKode(event.target.value);
     } catch (e) {
       Utils.logger.error(e);
     }
   };
 
-  render() {
-    const panelIkon = Ikoner.Ferdig;
-    const { redigerbart } = this.props;
+  const hentOrgFraApi = async () => {
+    if (fullmektig.orgnr) settOrg(await hentOrg(fullmektig.orgnr));
+  };
 
-    return (
-      <div>
-        <Nav.EkspanderbartpanelBase
-          heading={<PanelHeader ikon={panelIkon} tittel="Fullmektig" />}
-          ariaTittel="Opplysninger om fullmektig">
-          <Nav.Container fluid>
-            {this.state.fullmektige.map(fullmektig => (
-              <Fullmektig
-                key={uuid()}
-                redigerbart={redigerbart}
-                fullmektig={fullmektig}
-                lagreFullmektig={this.lagreFullmektig}
-              />
-            ))}
-          </Nav.Container>
-        </Nav.EkspanderbartpanelBase>
-      </div>
-    );
-  }
+  useEffect(() => {
+    if (fullmektig.representererKode) settRepresentererKode(fullmektig.representererKode);
+    hentOrgFraApi();
+  }, [fullmektig.representererKode, fullmektig.orgnr]);
+
+  const slettFullmektig = async () => {
+    try {
+      await slettAktoer(databaseID);
+      slettFullmektigLokalt(databaseID);
+    } catch (e) {
+      Utils.logger.error(e);
+    }
+  };
+
+  const databaseIDString = databaseID.toString();
+
+  return (
+    <Nav.Row className="fullmektig">
+      <Nav.Column xs="6">
+        {
+          org &&
+          <Fragment>
+            {org.navn}
+            <div className="postadresse_tittel">Postadresse</div>
+            <PostAdresse postadresse={org.postadresse} />
+          </Fragment>
+        }
+        {
+          !org &&
+          <Fragment>
+            <SokFullmektigOrg lagreNyFullmektigOgOppdaterLokalt={orgnr => lagreNyFullmektigOgOppdaterLokalt(representererKode, orgnr)} />
+          </Fragment>
+        }
+        {
+          org &&
+          <Nav.Fieldset disabled={!redigerbart} legend="Hvem er dette fullmektig for?" className="radioknapper">
+            <Nav.Radio
+              onChange={vedRolleEndring}
+              checked={representererKode === MKV.Koder.representerer.ARBEIDSGIVER}
+              label="Arbeidsgiver"
+              value={MKV.Koder.representerer.ARBEIDSGIVER}
+              name={databaseIDString}
+            />
+            <Nav.Radio
+              onChange={vedRolleEndring}
+              checked={representererKode === MKV.Koder.representerer.BRUKER}
+              label="Arbeidstaker"
+              value={MKV.Koder.representerer.BRUKER}
+              name={databaseIDString}
+            />
+            <Nav.Radio
+              onChange={vedRolleEndring}
+              checked={representererKode === MKV.Koder.representerer.BEGGE}
+              label="Både arbeidstaker og arbeidsgiver"
+              value={MKV.Koder.representerer.BEGGE}
+              name={databaseIDString}
+            />
+          </Nav.Fieldset>
+        }
+        <Nav.Knapp disabled={!redigerbart} onClick={slettFullmektig} type="mini">&times; FJERN FULLMEKTIG</Nav.Knapp>
+      </Nav.Column>
+      <Nav.Column xs="6">
+        {
+          org && <Kontaktopplysninger juridiskOrg={org} />
+        }
+      </Nav.Column>
+    </Nav.Row>
+  );
 }
 
-FullmektigPanel.propTypes = {
-  hentOrg: PT.func.isRequired,
-  hentAktoer: PT.func.isRequired,
-  oppsummering: PT.object.isRequired,
-  lagreAktoer: PT.func.isRequired,
+Fullmektig.propTypes = {
+  fullmektig: PT.object,
+  lagreFullmektig: PT.func.isRequired,
   redigerbart: PT.bool.isRequired,
+  databaseID: PT.number,
+  slettAktoer: PT.func.isRequired,
+  slettFullmektigLokalt: PT.func.isRequired,
+  lagreNyFullmektigOgOppdaterLokalt: PT.func.isRequired,
+  hentOrg: PT.func.isRequired,
 };
 
-const mapStateToProps = state => ({
-  arbeidsgivereNorge: fagsakSelectors.ArbeidsgivereNorgeSelector(state),
-  oppsummering: fagsakSelectors.OppsummeringSelector(state),
-  redigerbart: fagsakSelectors.RedigerbartSelector(state),
-});
+Fullmektig.defaultProps = {
+  fullmektig: {},
+  databaseID: 0,
+};
 
-const hentOrg = orgNr => Api.Organisasjoner.hentOrganisasjon(orgNr);
-const hentAktoer = (saksnr, rolleKode, representererKode) => Api.Fagsaker.aktoer.hent(saksnr, rolleKode, representererKode);
-const lagreAktoer = (saksnr, data) => Api.Fagsaker.aktoer.send(saksnr, data);
-
-const SokersFullmektigWrapper = props => (
-  <FullmektigPanel
-    {...props}
-    lagreAktoer={lagreAktoer}
-    hentAktoer={hentAktoer}
-    hentOrg={hentOrg}
-  />
-);
-
-export default connect(mapStateToProps)(SokersFullmektigWrapper);
+export default Fullmektig;
