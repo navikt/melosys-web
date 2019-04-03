e4e3eda3639d13a1a308b4c05496c5b1ea91c59b
JohanSakshaug <johan.sakshaug@nav.no>
2019-04-03 11:51:04 +0200

    Lås arbeidslandsteg i readonly

--- src/soknad-komponenter/stegvelger/stegKomponenter/vurderingYrkesaktivitetAntallLand.js
+++ src/soknad-komponenter/stegvelger/stegKomponenter/vurderingYrkesaktivitetAntallLand.js
@@ -6,13 +6,13 @@ import * as Skjema from '../../skjema';
 import * as KV from '../../../kodeverk';
 
 const VurderingYrkesaktivitetAntallLand = props => {
-  const { bekreftOgFortsett, tilstand } = props;
+  const { bekreftOgFortsett, tilstand, redigerbart } = props;
   const { harAvklaring } = tilstand;
 
   return (
     <div>
       <Nav.Undertittel>Vurdering av antall land</Nav.Undertittel>
-      <Nav.Fieldset legend="Hvor mange land skal søker ha yrkesaktivitet i?">
+      <Nav.Fieldset disabled={!redigerbart} legend="Hvor mange land skal søker ha yrkesaktivitet i?">
         <Skjema.Radio feltNavn="avklartefakta.yrkesaktivitetAntallLand" value={KV.Koder.VurderingYrkesaktivitetAntallLandTyper.KUN_NORGE} label="Kun Norge" />
         <Skjema.Radio feltNavn="avklartefakta.yrkesaktivitetAntallLand" value={KV.Koder.VurderingYrkesaktivitetAntallLandTyper.ETT_LAND_IKKE_NORGE} label="Ett land, ikke Norge" />
         <Skjema.Radio feltNavn="avklartefakta.yrkesaktivitetAntallLand" value={KV.Koder.VurderingYrkesaktivitetAntallLandTyper.TO_ELLER_FLERE_LAND} label="To eller flere land" />
@@ -28,6 +28,7 @@ VurderingYrkesaktivitetAntallLand.ID = 'YRKESAKTIVITET_ANTALL_LAND';
 
 VurderingYrkesaktivitetAntallLand.propTypes = {
   bekreftOgFortsett: PT.func.isRequired,
+  redigerbart: PT.bool.isRequired,
   tilstand: PT.object,
 };
 
