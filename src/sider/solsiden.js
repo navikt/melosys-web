a376fd6a082d465f08949c997b49cdf92d3e5eca
Are Bjolseth <abjolseth@gmail.com>
2019-04-12 01:34:10 +0200

    Cleanup, and kompliance.

--- src/sider/solsiden.js
+++ src/sider/solsiden.js
@@ -6,13 +6,12 @@ import { reduxForm } from 'redux-form';
 import PT from 'prop-types';
 import * as MKV from 'melosys-kodeverk';
 import * as Nav from '../utils/navFrontend';
-import Sticky from '../hjelpekomponenter/sticky';
 import * as Skjema from '../soknad-komponenter/skjema/';
 import { formSelectors } from '../ducks/form/';
 import { solsidenOperations } from '../ducks/solsiden/';
 
 import * as KV from '../kodeverk';
-import './journalforing.css';
+import './solsiden.css';
 
 class Solsiden extends Component {
   overstyrSubmit = event => {
@@ -20,8 +19,9 @@ class Solsiden extends Component {
   };
 
   render() {
+    const { sendInnData, solsidenSkjemaVerdier } = this.props;
     return (
-      <div className="journalforing">
+      <div className="solsiden">
         <Nav.Container fluid>
           <Nav.Row>
             <Nav.Column xs="4">
@@ -31,24 +31,16 @@ class Solsiden extends Component {
           <form onSubmit={this.overstyrSubmit}>
             <Nav.Row>
               <Nav.Column xs="4">
-                <Sticky>
-                  <Nav.Panel className="journalforing__skjema">
-                    <Skjema.Select
-                      feltNavn="landkode"
-                      bredde="fullbredde"
-                      label="Velg land:">
-                      {
-                        MKV.KTObjects.landkoder && MKV.KTObjects.landkoder
-                          .map(elem => (<option key={elem.kode} value={elem.kode}>{elem.term}</option>))
-                      }
-                    </Skjema.Select>
-                    <Skjema.Input feltNavn="tekstFelt" label="Tekst skrives her:" />
-                    <Nav.Knapp className="knapp" onClick={() => this.props.sendInnData(this.props.solsidenSkjemaVerdier)}>Send Inn</Nav.Knapp>
-                    <div className="journalforing__skjema__scroll">
-                      <div className="journalforing__fotknapper" />
-                    </div>
-                  </Nav.Panel>
-                </Sticky>
+                <Nav.Panel className="solsiden__skjema">
+                  <Skjema.Select
+                    feltNavn="landkode"
+                    bredde="fullbredde"
+                    label="Velg land:">
+                    {MKV.KTObjects.landkoder.map(elem => <option key={elem.kode} value={elem.kode}>{elem.term}</option>)}
+                  </Skjema.Select>
+                  <Skjema.Input feltNavn="tekstFelt" label="Tekst skrives her:" />
+                  <Nav.Knapp className="knapp" onClick={() => sendInnData(solsidenSkjemaVerdier)}>Send Inn</Nav.Knapp>
+                </Nav.Panel>
               </Nav.Column>
             </Nav.Row>
           </form>
