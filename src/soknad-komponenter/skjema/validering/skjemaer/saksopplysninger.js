a361edc4dc0dbaa4d4c6335a5e9f9acc57f0b65e
JohanSakshaug <johan.sakshaug@nav.no>
2019-04-30 10:14:33 +0200

    Legg til validering av poststed

--- src/soknad-komponenter/skjema/validering/skjemaer/saksopplysninger.js
+++ src/soknad-komponenter/skjema/validering/skjemaer/saksopplysninger.js
@@ -5,12 +5,14 @@ const saksopplysninger = Yup.object().shape({
     adresse: Yup.object().shape({
       postnummer: Yup.string().required('Postnummer kreves for Arbeidsgiver i Utlandet'),
       landkode: Yup.string().required('Land kreves for Arbeidsgiver i Utlandet'),
+      poststed: Yup.string().required('Poststed kreves for Arbeidsgiver i Utlandet'),
     }),
   })),
   arbeidUtland: Yup.array().of(Yup.object().shape({
     adresse: Yup.object().shape({
       postnummer: Yup.string().required('Postnummer kreves for Arbeidssted i Utlandet'),
       landkode: Yup.string().required('Land kreves for Arbeidssted i Utlandet'),
+      poststed: Yup.string().required('Poststed kreves for Arbeidssted i Utlandet'),
     }),
   })),
 });
