dd5e3d11db8efbcfacf7781e6b73bd7a273346d0
Bjørnar Askevold <bjornar.askevold@nav.no>
2019-05-08 14:04:11 +0200

    Lagt til forretningssted og vilkår for art13

--- src/soknad-komponenter/stegvelger/stegMotor/kontrollere/forretningssted.js
+++ src/soknad-komponenter/stegvelger/stegMotor/kontrollere/forretningssted.js
@@ -1,8 +1,11 @@
+import * as MKV from 'melosys-kodeverk';
+
 import Steg from '../steg';
 import { FANE_STATUS, STEG } from '../typer';
 import VurderingForretningssted from '../../stegKomponenter/vurderingForretningssted';
-import { hentFaktaListe } from '../../../../regler/avklartefakta';
+import { hentFakta, hentFaktaListe } from '../../../../regler/avklartefakta';
 import * as KV from '../../../../kodeverk';
+import { hentVilkar } from '../../../../regler/vilkar';
 
 class Forretningssted extends Steg {
   constructor(avklartefakta) {
@@ -19,13 +22,24 @@ class Forretningssted extends Steg {
     this.komponent = VurderingForretningssted;
     this.samleRelevanteData = _propsLight => ({
       valgteVirksomheter: _propsLight.valgteVirksomheter,
+      redigerbart: _propsLight.redigerbart,
+
+      art13_1_b1: hentVilkar(MKV.Koder.vilkaar.FO_883_2004_ART13_1_B1, _propsLight.vilkar),
+      art13_1_b2: hentVilkar(MKV.Koder.vilkaar.FO_883_2004_ART13_1_B2, _propsLight.vilkar),
+      art13_1_b3: hentVilkar(MKV.Koder.vilkaar.FO_883_2004_ART13_1_B3, _propsLight.vilkar),
+      art13_1_b4: hentVilkar(MKV.Koder.vilkaar.FO_883_2004_ART13_1_B4, _propsLight.vilkar),
+      art14_11: hentVilkar(MKV.Koder.vilkaar.FO_883_2004_ART14_11, _propsLight.vilkar),
+
       avklarteForretningsland: hentFaktaListe(KV.Koder.avklartefaktaKoder.ARBEIDSGIVERS_FORRETNINGSSTED, _propsLight.avklartefakta),
+      omfattetINorge: hentFakta(KV.Koder.avklartefaktaKoder.OMFATTES_I_NORGE, _propsLight.avklartefakta),
+      omfattetILand: hentFakta(KV.Koder.avklartefaktaKoder.OMFATTES_I_LAND, _propsLight.avklartefakta),
     });
 
     this.beregnRelevantUI = () => {};
     this.handlers = {
       bekreftOgFortsett: this._propsLight.tilgjengeligeHandlers.bekreftOgFortsett,
       oppdaterData: (felt, verdi) => this._propsLight.tilgjengeligeHandlers.oppdaterStegData(this.id, felt, verdi),
+      slettData: (type, felt) => this._propsLight.tilgjengeligeHandlers.slettStegData(this.id, type, felt),
       slettAllDataForSteg: () => this._propsLight.tilgjengeligeHandlers.slettAllDataForSteg(this.id),
     };
     this.status = FANE_STATUS.OK;
