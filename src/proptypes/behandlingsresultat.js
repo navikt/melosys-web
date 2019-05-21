5ce2d0096764c83fdc8b6e494dbc434cfd12bce8
Andreas Strand <andreas.strand@nav.no>
2019-05-21 09:39:35 +0200

    Endrer navn i behandlingsresultat proptype

--- src/proptypes/behandlingsresultat.js
+++ src/proptypes/behandlingsresultat.js
@@ -2,7 +2,7 @@
 import PT from 'prop-types';
 
 const BehandlingsresultatPropType = PT.shape({
-  begrunnelseKoder: PT.arrayOf(PT.string),
+  begrunnelser: PT.arrayOf(PT.string),
   begrunnelseFritekst: PT.string,
 });
 
