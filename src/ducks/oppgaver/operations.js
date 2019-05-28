04fc7c5b8932206055db27226777443d3230a5df
JohanSakshaug <Johan.Sakshaug@nav.no>
2019-05-28 13:53:32 +0200

    Send behandlingstyper til plukk-post

--- src/ducks/oppgaver/operations.js
+++ src/ducks/oppgaver/operations.js
@@ -35,14 +35,16 @@ export const tilbakelegge = (behandlingID, venterPaaDokumentasjon) => {
 };
 
 export const sendBehandlingsOppgave = async checkboxliste => {
-  const { sakstyper: sakstyperListe = [] } = checkboxliste;
+  const { sakstyper: sakstyperListe = [], behandlingstyper: behandlingstyperListe = [] } = checkboxliste;
   if (sakstyperListe.length === 0) { return false; }
 
-  const sakstyper = Object.keys(sakstyperListe);
+  const sakstyper = Object.keys(sakstyperListe).filter(sakstype => sakstyperListe[sakstype]);
+  const behandlingstyper = Object.keys(behandlingstyperListe).filter(behandlingstype => behandlingstyperListe[behandlingstype]);
 
   const oppgave = {
     oppgavetype: MKV.Koder.oppgavetyper.BEH_SAK_MK,
     sakstyper,
+    behandlingstyper,
   };
 
   const response = await Api.Oppgaver.send(oppgave);
