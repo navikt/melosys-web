4de25aa711b5589878818e52e97ad1d6ab26bfc7
JohanSakshaug <Johan.Sakshaug@nav.no>
2019-06-12 15:50:47 +0200

    Lagre avklartfakta for art16 svar begrunnelse

--- src/soknad-komponenter/stegvelger/stegMotor/kontrollere/artikkel16_motta_svar.js
+++ src/soknad-komponenter/stegvelger/stegMotor/kontrollere/artikkel16_motta_svar.js
@@ -1,6 +1,10 @@
+import * as MKV from 'melosys-kodeverk';
+
 import Steg from '../steg';
 import { FANE_STATUS, STEG } from '../typer';
 import VurderingArtikkel16MottaSvar from '../../stegKomponenter/vurderingArtikkel16MottaSvar';
+import { hentFakta } from '../../../../regler/avklartefakta';
+
 
 class Artikkel16MottaSvar extends Steg {
   constructor(propsLight, stegPosisjon) {
@@ -19,9 +23,12 @@ class Artikkel16MottaSvar extends Steg {
       redigerbart: _propsLight.redigerbart,
     });
     this.beregnRelevantUI = _propsLight => ({
+      svarAnmodningUnntakAvklartfakta: hentFakta(MKV.Koder.avklartefakta.SVAR_ANMODNING_UNNTAK, _propsLight.avklartefakta),
     });
     this.handlers = {
       bekreftOgFortsett: this._propsLight.tilgjengeligeHandlers.bekreftOgFortsett,
+      oppdaterData: (felt, verdi) => this._propsLight.tilgjengeligeHandlers.oppdaterStegData(this.id, felt, verdi),
+      slettData: data => this._propsLight.tilgjengeligeHandlers.slettStegData(this.id, data),
     };
     this._status = FANE_STATUS.OK;
   }
