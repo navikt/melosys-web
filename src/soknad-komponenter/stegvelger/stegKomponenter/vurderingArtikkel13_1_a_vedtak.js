3628efc984e878387e4d1ad9db98c19204501b3e
JohanSakshaug <Johan.Sakshaug@nav.no>
2019-06-20 13:59:28 +0200

    Legg til visning av lovvalgsperiode i art13.1.a vedtak-steg

--- src/soknad-komponenter/stegvelger/stegKomponenter/vurderingArtikkel13_1_a_vedtak.js
+++ src/soknad-komponenter/stegvelger/stegKomponenter/vurderingArtikkel13_1_a_vedtak.js
@@ -4,12 +4,16 @@ import PT from 'prop-types';
 import * as MKV from 'melosys-kodeverk';
 
 import * as Nav from '../../../utils/navFrontend';
+import * as Utils from '../../../utils';
+import * as MPT from '../../../proptypes';
 
-import { behandlingerSelectors } from '../../../ducks/behandlinger';
 import PdfLenkeListe from '../../../soknad-komponenter/pdfLenkeListe';
 
+import { behandlingerSelectors } from '../../../ducks/behandlinger';
+import { lovvalgsperioderSelectors } from '../../../ducks/lovvalgsperioder';
+
 const VurderingArtikkel13_1_a_vedtak = props => {
-  const { redigerbart, behandlingID } = props;
+  const { redigerbart, behandlingID, lovvalgsperiode } = props;
 
   const dokumenter = [
     {
@@ -35,25 +39,35 @@ const VurderingArtikkel13_1_a_vedtak = props => {
     },
   ];
 
+  //TODO redigerbart
+
+  const fom = Utils.dato.formatterDatoTilNorsk(lovvalgsperiode.fomDato);
+  const tom = Utils.dato.formatterDatoTilNorsk(lovvalgsperiode.tomDato);
+
   return (
     <Fragment>
       <Nav.Undertittel>Omfattet av norsk lovgivning, etter artikkel 13, nr 1, a</Nav.Undertittel>
       <Nav.Element>Lovvalgsperiode</Nav.Element>
       <Nav.Row>
+        <Nav.Column xs="6">
+          {fom} - {tom}
+        </Nav.Column>
+      </Nav.Row>
+      {/* <Nav.Row>
         <Nav.Column xs="3">
           <Nav.Input disabled={!redigerbart} bredde="fullbredde" label="Startdato" />
         </Nav.Column>
         <Nav.Column xs="3">
           <Nav.Input disabled={!redigerbart} bredde="fullbredde" label="Sluttdato" />
         </Nav.Column>
-      </Nav.Row>
+      </Nav.Row> */}
       <Nav.Row>
         <Nav.Column xs="6">
           <Nav.Checkbox label="Lovvalgsperioden er avkortet." />
         </Nav.Column>
       </Nav.Row>
       <Nav.Row>
-        <Nav.Column xs ="6">
+        <Nav.Column xs="6">
           <PdfLenkeListe behandlingID={behandlingID} dokumenter={dokumenter} />
         </Nav.Column>
       </Nav.Row>
@@ -65,11 +79,17 @@ const VurderingArtikkel13_1_a_vedtak = props => {
 VurderingArtikkel13_1_a_vedtak.propTypes = {
   redigerbart: PT.bool.isRequired,
   behandlingID: PT.number.isRequired,
+  lovvalgsperiode: PT.object,
+};
+
+VurderingArtikkel13_1_a_vedtak.defaultProps = {
+  lovvalgsperiode: {},
 };
 
 const mapStateToProps = state => ({
   redigerbart: behandlingerSelectors.RedigerbartSelector(state),
   behandlingID: behandlingerSelectors.BehandlingIDSelector(state),
+  lovvalgsperiode: lovvalgsperioderSelectors.LovvalgsperiodeSelector(state),
 });
 
 export default connect(mapStateToProps)(VurderingArtikkel13_1_a_vedtak);
