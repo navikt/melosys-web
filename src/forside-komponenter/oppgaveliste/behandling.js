f80da686b16635c355077084fd1f7c11179a4bdc
Andreas Strand <andreas.strand@nav.no>
2019-06-30 20:15:54 +0200

    Nye behandlingstyper. Ny behandlingstype UTL_MYND_UTPEKT_SEG_SELV skal også til /registrering skjermbilde

--- src/forside-komponenter/oppgaveliste/behandling.js
+++ src/forside-komponenter/oppgaveliste/behandling.js
@@ -48,7 +48,7 @@ BehandlingPanel.propTypes = {
 };
 
 // Er ikke mulig å vise behandlinger med type UNNTAK_FRA_MEDLEMSKAP
-const behandlingKanVises = type => KV.objektTilKode(type) !== MKV.Koder.behandlinger.typer.UNNTAK_FRA_MEDLEMSKAP;
+const behandlingKanVises = type => KV.objektTilKode(type) !== MKV.Koder.behandlinger.typer.REGISTRERING_UNNTAK_NORSK_TRYGD;
 
 const Behandling = ({ behandling, link }) => (
   behandlingKanVises(behandling.behandlingstype) ?
