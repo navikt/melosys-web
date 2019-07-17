810199ace502cc1be171ba6686ef5ccf3fee1915
JohanSakshaug <Johan.Sakshaug@nav.no>
2019-07-17 16:31:15 +0200

    Fiks oppdatering av data mellom form og redux-state

--- src/ducks/anmodningsperiodesvar/selectors.js
+++ src/ducks/anmodningsperiodesvar/selectors.js
@@ -4,3 +4,28 @@ export const AnmodningsperiodesvarSelector = createSelector(
   state => state.anmodningsperiodesvar.data || {},
   anmodningsperiodesvar => anmodningsperiodesvar
 );
+
+export const EndretPeriodeSelector = createSelector(
+  state => AnmodningsperiodesvarSelector(state) || {},
+  anmodningsperiodesvar => anmodningsperiodesvar.endretPeriode
+);
+
+export const EndretPeriodeFomSelector = createSelector(
+  state => EndretPeriodeSelector(state) || {},
+  periode => periode.fom
+);
+
+export const EndretPeriodeTomSelector = createSelector(
+  state => EndretPeriodeSelector(state) || {},
+  periode => periode.tom
+);
+
+export const BegrunnelseFritekstSelector = createSelector(
+  AnmodningsperiodesvarSelector,
+  anmodningsperiodesvar => anmodningsperiodesvar.begrunnelseFritekst
+);
+
+export const AnmodningsperiodeSvarTypeSelector = createSelector(
+  AnmodningsperiodesvarSelector,
+  anmodningsperiodesvar => anmodningsperiodesvar.anmodningsperiodeSvarType
+);
