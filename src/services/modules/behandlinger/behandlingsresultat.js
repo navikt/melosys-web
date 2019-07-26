9ab91b72f2465c6fe310dc8245df40c73df063c6
JohanSakshaug <Johan.Sakshaug@nav.no>
2019-07-26 13:54:02 +0200

    behandlingsresultat -> behandlingsresultater

--- src/services/modules/behandlinger/behandlingsresultat.js
+++ src/services/modules/behandlinger/behandlingsresultat.js
@@ -1,8 +1,8 @@
 import { getAsJson } from '../../utils';
-import { API_BASE_URL, BEHANDLINGSRESULTAT } from '../../api-constants';
+import { API_BASE_URL, BEHANDLINGSRESULTATER } from '../../api-constants';
 
 export const hent = behandlingID => {
-  const URI_BEHANDLINGSRESULTAT = `${API_BASE_URL}${BEHANDLINGSRESULTAT}/${behandlingID}`;
+  const URI_BEHANDLINGSRESULTAT = `${API_BASE_URL}${BEHANDLINGSRESULTATER}/${behandlingID}`;
   return getAsJson(URI_BEHANDLINGSRESULTAT);
 };
 
