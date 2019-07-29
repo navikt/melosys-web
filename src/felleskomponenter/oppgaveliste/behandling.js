784ec5e5e5ecf5d9fe09ae659997bfe7a52489ae
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2019-07-29 14:21:24 +0200

    Link kan alltid vises

--- src/felleskomponenter/oppgaveliste/behandling.js
+++ src/felleskomponenter/oppgaveliste/behandling.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PT from 'prop-types';
 import { Link } from 'react-router-dom';
-import * as MKV from 'melosys-kodeverk';
 
 import * as Nav from '../../utils/navFrontend';
 import * as KV from '../../kodeverk';
@@ -47,21 +46,15 @@ BehandlingPanel.propTypes = {
   kanVises: PT.bool.isRequired,
 };
 
-const behandlingKanVises = type => true; // TODO
-
 const Behandling = ({ behandling, link }) => (
-  behandlingKanVises(behandling.behandlingstype) ?
-    <Link to={link} className="behandling__link">
-      <BehandlingPanel behandling={behandling} kanVises />
-    </Link>
-    :
-    <BehandlingPanel behandling={behandling} kanVises={false} />
+  <Link to={link} className="behandling__link">
+    <BehandlingPanel behandling={behandling} kanVises />
+  </Link>
 );
 
 Behandling.propTypes = {
   behandling: PT.object.isRequired,
   link: PT.string.isRequired,
 };
-
 
 export default Behandling;
