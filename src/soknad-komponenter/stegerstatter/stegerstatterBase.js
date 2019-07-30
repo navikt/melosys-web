ac67cb56d2115a111647d99cd4722e19cd4bc0e6
JohanSakshaug <Johan.Sakshaug@nav.no>
2019-07-30 13:56:16 +0200

    Opprydning

--- src/soknad-komponenter/stegerstatter/stegerstatterBase.js
+++ src/soknad-komponenter/stegerstatter/stegerstatterBase.js
@@ -5,18 +5,16 @@ import * as Nav from '../../utils/navFrontend';
 
 import './stegerstatterBase.css';
 
-const StegerstatterBase = ({ tittel, beskrivelse }) => {
-  return (
-    <section aria-label="henlagtStatus" className="henlagtStatus panelSeksjon stegerstatter">
-      <Nav.Panel>
-        <Nav.Row>
-          <Nav.Systemtittel>{tittel}</Nav.Systemtittel>
-        </Nav.Row>
-        <p>{ beskrivelse }</p>
-      </Nav.Panel>
-    </section>
-  );
-};
+const StegerstatterBase = ({ tittel, beskrivelse }) => (
+  <section className="panelSeksjon stegerstatter">
+    <Nav.Panel>
+      <Nav.Row>
+        <Nav.Systemtittel>{tittel}</Nav.Systemtittel>
+      </Nav.Row>
+      <p>{ beskrivelse }</p>
+    </Nav.Panel>
+  </section>
+);
 
 StegerstatterBase.propTypes = {
   tittel: PT.string.isRequired,
