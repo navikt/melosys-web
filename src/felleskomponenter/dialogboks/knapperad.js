8fba4579e66aafce2483cbe033e9aa86080d6dce
JohanSakshaug <Johan.Sakshaug@nav.no>
2019-07-31 11:29:39 +0200

    Lag komponent for knapperad i dialogbokser

---
+++ src/felleskomponenter/dialogboks/knapperad.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PT from 'prop-types';
+
+import * as Nav from '../../utils/navFrontend';
+
+import './knapperad.css';
+
+const Knapperad = ({
+  bekreft, bekreftTekst, avbryt, avbrytTekst, redigerbart,
+}) => (
+  <div className="container__knapperad">
+    <Nav.Hovedknapp onClick={bekreft} disabled={!redigerbart}>{ bekreftTekst }</Nav.Hovedknapp>
+    <Nav.Knapp onClick={avbryt} disabled={!redigerbart}>{avbrytTekst}</Nav.Knapp>
+  </div>
+);
+
+Knapperad.propTypes = {
+  bekreft: PT.func.isRequired,
+  bekreftTekst: PT.string.isRequired,
+  avbryt: PT.func.isRequired,
+  avbrytTekst: PT.string,
+  redigerbart: PT.bool.isRequired,
+};
+
+Knapperad.defaultProps = {
+  avbrytTekst: 'AVBRYT',
+};
+
+export default Knapperad;
