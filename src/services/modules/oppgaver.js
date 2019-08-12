649fcc0092522b9afe894262f549573b1c43e04e
Bjølseth, Are <are.bjolseth@nav.no>
2019-08-12 13:22:47 +0200

    Refactored Oppgaver

--- src/services/modules/oppgaver.js
+++ src/services/modules/oppgaver.js
@@ -2,9 +2,8 @@ import { getAsJson, postAsJson } from '../utils';
 import { API_BASE_URL, OPPGAVER } from '../api-constants';
 
 export const oversikt = () => getAsJson(`${API_BASE_URL}${OPPGAVER}/oversikt`);
+export const sok = () => getAsJson(`${API_BASE_URL}${OPPGAVER}/sok`);
 
-export const send = oppgave => postAsJson(`${API_BASE_URL}${OPPGAVER}/plukk`, oppgave);
-
-export const opprett = oppgave => postAsJson(`${API_BASE_URL}${OPPGAVER}/opprett`, oppgave);
+export const sendPlukk = oppgave => postAsJson(`${API_BASE_URL}${OPPGAVER}/plukk`, oppgave);
 
 export const tilbakelegge = oppgave => postAsJson(`${API_BASE_URL}${OPPGAVER}/tilbakelegge`, oppgave);
