0663bace37a9d92a65095d942f2078a7b21d2760
Bjølseth, Are <are.bjolseth@nav.no>
2019-08-12 12:52:49 +0200

    NB! import with suffix '/' when importing from a directory with index.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import * as Avklartefakta from './modules/avklartefakta';
 import * as Behandlinger from './modules/behandlinger';
 // TODO import * as Behandlingsperioder from './modules/behandlinger/behandlingsperioder';
 import * as Dokumenter from './modules/dokumenter/';
-import * as Eessi from './modules/eessi';
+import * as Eessi from './modules/eessi/';
 import * as Fagsaker from './modules/fagsaker/';
 import * as Inngang from './modules/inngang';
 import * as Journalforing from './modules/journalforing';
