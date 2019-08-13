704517a5a7bfb22a499098758035e5fab36fd1f2
JohanSakshaug <Johan.Sakshaug@nav.no>
2019-08-13 10:38:45 +0200

    Mer deklarativ oppbygning av art16 vedtaksteg, de ulike stegene har ikke like mye til felles lenger.

--- src/sider/saksbehandling/komponenter/stegvelger/stegKomponenter/vurderingArtikkel16Vedtak.js
+++ src/sider/saksbehandling/komponenter/stegvelger/stegKomponenter/vurderingArtikkel16Vedtak.js
@@ -4,32 +4,27 @@ import PT from 'prop-types';
 import * as MKV from 'melosys-kodeverk';
 
 import * as Nav from '../../../../../utils/navFrontend';
-import * as KV from '../../../../../kodeverk';
 import * as MPT from '../../../../../proptypes';
 
 import PdfLenkeListe from '../../../../../felleskomponenter/pdfLenkeListe';
-import { DatoOmradeMedVarighet } from '../../../../../felleskomponenter/datoOmrade/datoOmrade';
+import DatoOmrade from '../../../../../felleskomponenter/datoOmrade/datoOmrade';
 
 import { behandlingerSelectors } from '../../../../../ducks/behandlinger';
 import { anmodningsperioderSelectors } from '../../../../../ducks/anmodningsperioder';
 import { anmodningsperiodesvarSelectors } from '../../../../../ducks/anmodningsperiodesvar';
 
-export const VurderingArtikkel16Vedtak = props => {
+export const VurderingArtikkel16VedtakInnvilgelse = props => {
   const {
-    lagreOgFatteVedtak, redigerbart, behandlingID, anmodningsperiodesvar, anmodningsperiode,
+    redigerbart,
+    behandlingID,
+    gjeldendePeriode,
+    begrunnelseFritekst,
   } = props;
-
-  const { anmodningsperiodeSvarType, endretPeriode, begrunnelseFritekst } = anmodningsperiodesvar;
-
-  const innvilget = [MKV.Koder.anmodningsperiodesvartyper.INNVILGELSE, MKV.Koder.anmodningsperiodesvartyper.DELVIS_INNVILGELSE].includes(anmodningsperiodeSvarType);
-  const innvilgetYrkesaktivType = innvilget ? MKV.Koder.brev.produserbaredokumenter.INNVILGELSE_YRKESAKTIV : MKV.Koder.brev.produserbaredokumenter.AVSLAG_YRKESAKTIV;
-  const innvilgetArbeidsgiverType = innvilget ? MKV.Koder.brev.produserbaredokumenter.INNVILGELSE_ARBEIDSGIVER : MKV.Koder.brev.produserbaredokumenter.AVSLAG_ARBEIDSGIVER;
-  const resultatTekst = `Svar på anmodning om unntak: ${KV.kodeTilTerm(anmodningsperiodeSvarType, MKV.KTObjects.anmodningsperiodesvartyper)}.`;
 
   const dokumenter = [
     {
-      navn: 'Forhåndsvis vedtaksbrev',
-      type: innvilgetYrkesaktivType,
+      navn: 'Forhåndsvis vedtaksbrev og A1',
+      type: MKV.Koder.brev.produserbaredokumenter.INNVILGELSE_YRKESAKTIV,
       data: {
         begrunnelseFritekst,
         mottaker: MKV.Koder.aktoersroller.BRUKER,
@@ -37,7 +32,7 @@ export const VurderingArtikkel16Vedtak = props => {
     },
     {
       navn: 'Brev til arbeidsgiver',
-      type: innvilgetArbeidsgiverType,
+      type: MKV.Koder.brev.produserbaredokumenter.INNVILGELSE_ARBEIDSGIVER,
       data: {
         begrunnelseFritekst,
         mottaker: MKV.Koder.aktoersroller.ARBEIDSGIVER,
@@ -45,42 +40,182 @@ export const VurderingArtikkel16Vedtak = props => {
     },
   ];
 
-  const visFritekstFelt = anmodningsperiodeSvarType === MKV.Koder.anmodningsperiodesvartyper.DELVIS_INNVILGELSE || anmodningsperiodeSvarType === MKV.Koder.anmodningsperiodesvartyper.AVSLAG;
-
-  const visLovvalgsperiode = anmodningsperiodeSvarType === MKV.Koder.anmodningsperiodesvartyper.DELVIS_INNVILGELSE || anmodningsperiodeSvarType === MKV.Koder.anmodningsperiodesvartyper.INNVILGELSE;
-
-  const periodeSomVises = anmodningsperiodeSvarType === MKV.Koder.anmodningsperiodesvartyper.DELVIS_INNVILGELSE ? endretPeriode : { fom: anmodningsperiode.fomDato, tom: anmodningsperiode.tomDato };
-
-  const vedKlikk = () => {
-    lagreOgFatteVedtak(MKV.Koder.behandlinger.resultattyper.FASTSATT_LOVVALGSLAND);
-  };
-
   return (
     <Fragment>
-      <Nav.Undertittel>Svar fra myndigheten</Nav.Undertittel>
+      <Nav.Undertittel>Omfattet av norsk trygdelovgivning etter Fo 883/2004 Artikkel 16 nr. 1.</Nav.Undertittel>
       <Nav.Row>
         <Nav.Column xs="7">
-          {resultatTekst}
+          <DatoOmrade periode={gjeldendePeriode} tekst="Lovvalgsperiode" />
         </Nav.Column>
       </Nav.Row>
-      <Nav.Row>
-        <Nav.Column xs="7">
-          { visFritekstFelt && <Nav.Textarea label="Begrunnelse" onChange={() => {}} disabled value={begrunnelseFritekst || ''} tellerTekst={() => {}} />}
-        </Nav.Column>
-      </Nav.Row>
-      {
-        visLovvalgsperiode &&
-        <Nav.Row>
-          <Nav.Column xs="7">
-            <DatoOmradeMedVarighet periode={periodeSomVises} tekst="Lovvalgsperiode" />
-          </Nav.Column>
-        </Nav.Row>
-      }
       <Nav.Row>
         <Nav.Column xs="7">
           {redigerbart && <PdfLenkeListe behandlingID={behandlingID} dokumenter={dokumenter} />}
         </Nav.Column>
       </Nav.Row>
+    </Fragment>
+  );
+};
+
+VurderingArtikkel16VedtakInnvilgelse.propTypes = {
+  redigerbart: PT.bool.isRequired,
+  behandlingID: PT.number.isRequired,
+  gjeldendePeriode: MPT.Periode.isRequired,
+  begrunnelseFritekst: PT.string,
+};
+
+VurderingArtikkel16VedtakInnvilgelse.defaultProps = {
+  begrunnelseFritekst: '',
+};
+
+export const VurderingArtikkel16VedtakDelvisInnvilgelse = props => {
+  const {
+    redigerbart,
+    behandlingID,
+    gjeldendePeriode,
+    begrunnelseFritekst,
+  } = props;
+
+  const dokumenter = [
+    {
+      navn: 'Forhåndsvis vedtaksbrev og A1',
+      type: MKV.Koder.brev.produserbaredokumenter.INNVILGELSE_YRKESAKTIV,
+      data: {
+        begrunnelseFritekst,
+        mottaker: MKV.Koder.aktoersroller.BRUKER,
+      },
+    },
+    {
+      navn: 'Brev til arbeidsgiver',
+      type: MKV.Koder.brev.produserbaredokumenter.INNVILGELSE_ARBEIDSGIVER,
+      data: {
+        begrunnelseFritekst,
+        mottaker: MKV.Koder.aktoersroller.ARBEIDSGIVER,
+      },
+    },
+  ];
+
+  return (
+    <Fragment>
+      <Nav.Undertittel>Delvis innvilgelse - omfattet av norsk trygdelovgivning etter Fo 883/2004 Artikkel 16 nr. 1.</Nav.Undertittel>
+      <Nav.Row>
+        <Nav.Column xs="7">
+          <DatoOmrade periode={gjeldendePeriode} tekst="Lovvalgsperiode" />
+        </Nav.Column>
+      </Nav.Row>
+      { /* TODO: begrunnelser komponent fra avslag 12.1 */}
+      <Nav.Row>
+        <Nav.Column xs="7">
+          {redigerbart && <PdfLenkeListe behandlingID={behandlingID} dokumenter={dokumenter} />}
+        </Nav.Column>
+      </Nav.Row>
+    </Fragment>
+  );
+};
+
+VurderingArtikkel16VedtakDelvisInnvilgelse.propTypes = {
+  redigerbart: PT.bool.isRequired,
+  behandlingID: PT.number.isRequired,
+  gjeldendePeriode: MPT.Periode.isRequired,
+  begrunnelseFritekst: PT.string,
+};
+
+VurderingArtikkel16VedtakDelvisInnvilgelse.defaultProps = {
+  begrunnelseFritekst: '',
+};
+
+export const VurderingArtikkel16VedtakAvslag = props => {
+  const {
+    redigerbart,
+    behandlingID,
+    begrunnelseFritekst,
+  } = props;
+
+  const dokumenter = [
+    {
+      navn: 'Forhåndsvis vedtaksbrev',
+      type: MKV.Koder.brev.produserbaredokumenter.AVSLAG_YRKESAKTIV,
+      data: {
+        begrunnelseFritekst,
+        mottaker: MKV.Koder.aktoersroller.BRUKER,
+      },
+    },
+    {
+      navn: 'Brev til arbeidsgiver',
+      type: MKV.Koder.brev.produserbaredokumenter.AVSLAG_ARBEIDSGIVER,
+      data: {
+        begrunnelseFritekst,
+        mottaker: MKV.Koder.aktoersroller.ARBEIDSGIVER,
+      },
+    },
+  ];
+
+  return (
+    <Fragment>
+      <Nav.Undertittel>Avslag</Nav.Undertittel>
+      { /* TODO: begrunnelser komponent fra avslag 12.1 */}
+      <Nav.Row>
+        <Nav.Column xs="7">
+          {redigerbart && <PdfLenkeListe behandlingID={behandlingID} dokumenter={dokumenter} />}
+        </Nav.Column>
+      </Nav.Row>
+    </Fragment>
+  );
+};
+
+VurderingArtikkel16VedtakAvslag.propTypes = {
+  redigerbart: PT.bool.isRequired,
+  behandlingID: PT.number.isRequired,
+  begrunnelseFritekst: PT.string,
+};
+
+VurderingArtikkel16VedtakAvslag.defaultProps = {
+  begrunnelseFritekst: '',
+};
+
+export const VurderingArtikkel16Vedtak = props => {
+  const {
+    lagreOgFatteVedtak, redigerbart, behandlingID, anmodningsperiodesvar, anmodningsperiode,
+  } = props;
+
+  const vedKlikk = () => {
+    lagreOgFatteVedtak(MKV.Koder.behandlinger.resultattyper.FASTSATT_LOVVALGSLAND);
+  };
+
+  const { anmodningsperiodeSvarType, endretPeriode, begrunnelseFritekst } = anmodningsperiodesvar;
+
+  const finnVedtakInnhold = svarType => {
+    switch (svarType) {
+      case MKV.Koder.anmodningsperiodesvartyper.INNVILGELSE:
+        return <VurderingArtikkel16VedtakInnvilgelse
+          redigerbart={redigerbart}
+          behandlingID={behandlingID}
+          begrunnelseFritekst={begrunnelseFritekst}
+          gjeldendePeriode={{ fom: anmodningsperiode.fomDato, tom: anmodningsperiode.tomDato }}
+        />;
+      case MKV.Koder.anmodningsperiodesvartyper.DELVIS_INNVILGELSE:
+        return <VurderingArtikkel16VedtakDelvisInnvilgelse
+          redigerbart={redigerbart}
+          behandlingID={behandlingID}
+          begrunnelseFritekst={begrunnelseFritekst}
+          gjeldendePeriode={endretPeriode}
+        />;
+      case MKV.Koder.anmodningsperiodesvartyper.AVSLAG:
+        return <VurderingArtikkel16VedtakAvslag
+          redigerbart={redigerbart}
+          behandlingID={behandlingID}
+          begrunnelseFritekst={begrunnelseFritekst}
+        />;
+      default:
+        throw new Error('AnmodningsperiodeSvarType må være satt');
+    }
+  };
+
+  const vedtakInnhold = finnVedtakInnhold(anmodningsperiodeSvarType);
+
+  return (
+    <Fragment>
+      { vedtakInnhold }
       <Nav.Row>
         <Nav.Column xs="7">
           <Nav.Hovedknapp disabled={!redigerbart} type="hoved" onClick={vedKlikk}>FATT VEDTAK</Nav.Hovedknapp>
