c3579c52b2734964ad95802a44b45b3c0a4e4d8b
Bjølseth, Are <are.bjolseth@nav.no>
2019-08-13 13:48:38 +0200

    Removed obsolete endpoint /sasflyt/unntaksperioder/:behandlingID/anmodning Changed from queryParam to restparm for organisasjoner og personer

--- src/services/modules/organisasjoner.js
+++ src/services/modules/organisasjoner.js
@@ -2,5 +2,5 @@ import { cachedGetAsJson } from '../utils';
 import { API_BASE_URL, ORGANISASJONER } from '../api-constants';
 
 // eslint-disable-next-line import/prefer-default-export
-export const hentOrganisasjon = orgnr => cachedGetAsJson(`${API_BASE_URL}${ORGANISASJONER}/?orgnr=${orgnr}`);
+export const hentOrganisasjon = orgnr => cachedGetAsJson(`${API_BASE_URL}${ORGANISASJONER}/${orgnr}`);
 
