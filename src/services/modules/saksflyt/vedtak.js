ff0e8274f9cd4d753529fd54e7d7e096095a6fc6
Bjølseth, Are <are.bjolseth@nav.no>
2019-08-13 13:26:41 +0200

    fatte => fatt fatteVedtak => fattVedtak

--- src/services/modules/saksflyt/vedtak.js
+++ src/services/modules/saksflyt/vedtak.js
@@ -2,6 +2,6 @@ import { postAsJson } from '../../utils';
 import { API_BASE_URL, SAKSFLYT, VEDTAK } from '../../api-constants';
 
 // eslint-disable-next-line import/prefer-default-export
-export const fatte = (behandlingID, data) => postAsJson(`${API_BASE_URL}${SAKSFLYT}/${VEDTAK}/${behandlingID}`, data);
+export const fatt = (behandlingID, data) => postAsJson(`${API_BASE_URL}${SAKSFLYT}/${VEDTAK}/${behandlingID}/fatt`, data);
 
 export const endrePeriode = (behandlingID, data) => postAsJson(`${API_BASE_URL}${SAKSFLYT}/${VEDTAK}/${behandlingID}/endreperiode`, data);
