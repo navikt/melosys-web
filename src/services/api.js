4ad8d3e5ce9e77a4127929e6b30aa36786374beb
Bjølseth, Are <are.bjolseth@nav.no>
2019-08-13 13:16:04 +0200

    Cleanup

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import * as Anmodningsperioder from './modules/anmodningsperioder/';
 import * as Avklartefakta from './modules/avklartefakta';
 import * as Behandlinger from './modules/behandlinger';
-// TODO import * as Behandlingsperioder from './modules/behandlinger/behandlingsperioder';
 import * as Dokumenter from './modules/dokumenter/';
 import * as Eessi from './modules/eessi/';
 import * as Fagsaker from './modules/fagsaker/';
@@ -19,25 +18,10 @@ import * as ServerInfo from './modules/serverinfo';
 import * as Soknader from './modules/soknader';
 import * as Vilkar from './modules/vilkar';
 
-/*
-function erDev() {
-  const url = window.location.href;
-  return url.includes('debug=true') ||  url.includes('localhost:');
-  return false;
-  //from .env or .env.local
-  return process.env.NODE_ENV !== 'production';
-}
-*/
-// from .env or .env.local
-// const API_BASE_URL = `${process.env.REACT_APP_API_BASE_URL}`;
-// console.log('process.env', process.env);
-
-
 export {
   Anmodningsperioder,
   Avklartefakta,
   Behandlinger,
-  // Behandlingsperioder,
   Dokumenter,
   Eessi,
   Fagsaker,
