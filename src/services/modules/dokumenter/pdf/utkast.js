009d8644aac8d9d7c581639ff52e4c539c1ae138
Are Bjolseth <abjolseth@gmail.com>
2019-08-14 10:19:29 +0200

    Fixed path. Added missing "/"

--- src/services/modules/dokumenter/pdf/utkast.js
+++ src/services/modules/dokumenter/pdf/utkast.js
@@ -9,5 +9,5 @@ import { API_BASE_URL, DOKUMENTER } from '../../../api-constants';
  * @param data
  * @returns {Promise<*>} PDF dokument
  */
-export const forhandsvisPDF = (behandlingID, dokumenttypeKode, data) => postAsJsonReceiveAsPDF(`${API_BASE_URL}${DOKUMENTER}/pdf/utkast${behandlingID}/${dokumenttypeKode}`, data, true);
+export const forhandsvisPDF = (behandlingID, dokumenttypeKode, data) => postAsJsonReceiveAsPDF(`${API_BASE_URL}${DOKUMENTER}/pdf/utkast/${behandlingID}/${dokumenttypeKode}`, data, true);
 
