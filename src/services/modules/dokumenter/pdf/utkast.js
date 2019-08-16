1e97c433b62dce74014587b804230d5bef78af32
Bjølseth, Are <are.bjolseth@nav.no>
2019-08-16 13:17:17 +0200

    Renamed dokumenttypeKode => produserbartDokument

--- src/services/modules/dokumenter/pdf/utkast.js
+++ src/services/modules/dokumenter/pdf/utkast.js
@@ -5,9 +5,9 @@ import { API_BASE_URL, DOKUMENTER } from '../../../api-constants';
 /**
  * Lag pdfUtkast henter pdf dokument basert på :behandligID og :dokumentytpekode
  * @param behandlingID
- * @param dokumenttypeKode
+ * @param produserbartDokument
  * @param data
  * @returns {Promise<*>} PDF dokument
  */
-export const forhandsvisPDF = (behandlingID, dokumenttypeKode, data) => postAsJsonReceiveAsPDF(`${API_BASE_URL}${DOKUMENTER}/pdf/utkast/${behandlingID}/${dokumenttypeKode}`, data, true);
+export const forhandsvisPDF = (behandlingID, produserbartDokument, data) => postAsJsonReceiveAsPDF(`${API_BASE_URL}${DOKUMENTER}/pdf/utkast/${behandlingID}/${produserbartDokument}`, data, true);
 
