15ed6a133c401b3238a467a5adab3f668873696a
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2019-08-20 12:22:57 +0200

    Bytter ut eessikodeverk.json med modulen eessi-kodeverk

---
+++ src/kodeverk/eessiKodeverkHelper.js
@@ -0,0 +1,19 @@
+import * as EKV from 'eessi-kodeverk';
+import { kodeTilObjekt } from './index';
+
+export const hentBucTyperForFagomrade = fagomrade => {
+  if (!fagomrade) {
+    return [];
+  }
+  return EKV.KTObjects.buctyper[EKV.kodemaps.SEKTOR2BUC[fagomrade]];
+};
+
+export const alleBucer = Object.assign({}, ...Object.values(EKV.Terms.buctyper));
+
+const alleBuc2Seds = Object.assign({}, ...Object.values(EKV.kodemaps.BUC2SEDS));
+export const hentSedTyperForBuc = buc => {
+  if (!buc) {
+    return [];
+  }
+  return [kodeTilObjekt(EKV.Koder.sedtyper[alleBuc2Seds[buc]], EKV.KTObjects.sedtyper)];
+};
