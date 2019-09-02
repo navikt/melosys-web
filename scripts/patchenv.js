55ac4a57fdc9212999ba7cd2185aca8c13fd430b
Are Bjolseth <abjolseth@gmail.com>
2019-09-02 21:35:44 +0200

    Add melosys-kodeverk to versjon log, and update to latest deps, with react-nnn: '16.9.0'

--- scripts/patchenv.js
+++ scripts/patchenv.js
@@ -1,6 +1,9 @@
 const moment = require('moment');
 const dotenv = require('dotenv-save');
 const branch = require('git-branch');
+const readPkg = require('read-pkg');
+// Read package.json into js map.
+const pkg = readPkg.sync();
 
 moment.locale('nb');
 
@@ -10,8 +13,14 @@ let branchName = process.env.BRANCH_NAME || 'unknown';
 if (branchName === 'unknown') {
   branchName = branch.sync(process.cwd());
 }
+const {
+  dependencies: {
+    'melosys-kodeverk': kodeverk_versjon,
+  },
+} = pkg;
 dotenv.set('REACT_APP_VERSION', version);
 dotenv.set('REACT_APP_BUILD_DATETIME', moment().format('DD/MM/YYYY HH:mm'));
 dotenv.set('REACT_APP_BUILD_VERSION', buildNumber);
 dotenv.set('REACT_APP_BRANCH_NAME', branchName);
+dotenv.set('REACT_APP_MELOSYS_KODEVERK', kodeverk_versjon);
 
