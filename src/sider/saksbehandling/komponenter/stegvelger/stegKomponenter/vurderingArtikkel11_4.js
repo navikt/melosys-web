ee47ebd147b3adec82ee2af9ac9d6756eb7b4cde
Bjølseth, Are <are.bjolseth@nav.no>
2019-09-03 15:01:52 +0200

    Oppgraderte nav-frontend-knapper. Fjernet type="hoved" fra alle knapper.

--- src/sider/saksbehandling/komponenter/stegvelger/stegKomponenter/vurderingArtikkel11_4.js
+++ src/sider/saksbehandling/komponenter/stegvelger/stegKomponenter/vurderingArtikkel11_4.js
@@ -140,7 +140,7 @@ class VurderingArtikkel11_4 extends Component {
           </Nav.Row>
         </div>
         <div className="fane__knapplinje">
-          <Nav.Knapp disabled={!(redigerbart && harAvklaring)} type="hoved" className="fane__navigasjonsknapp" onClick={bekreftOgFortsett}>Bekreft og fortsett</Nav.Knapp>
+          <Nav.Knapp disabled={!(redigerbart && harAvklaring)} className="fane__navigasjonsknapp" onClick={bekreftOgFortsett}>Bekreft og fortsett</Nav.Knapp>
         </div>
       </div>
     );
