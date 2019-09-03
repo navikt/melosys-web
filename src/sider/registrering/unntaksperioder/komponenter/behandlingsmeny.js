ee6b94d9ed31d518411049c025386f86224e2b3b
Bjølseth, Are <are.bjolseth@nav.no>
2019-09-03 15:15:28 +0200

    Fjernet type="hoved" fra <Nav.Knappp .../>

--- src/sider/registrering/unntaksperioder/komponenter/behandlingsmeny.js
+++ src/sider/registrering/unntaksperioder/komponenter/behandlingsmeny.js
@@ -11,9 +11,9 @@ const Behandlingsmeny = props => {
   return (
     <Nav.EkspanderbartpanelBase ariaTittel="Behandlingsmeny" className="oppsummering__meny" heading={<div className="behandlingsmeny_title">Behandlingsmeny</div>}>
       <div className="meny__innhold">
-        { redigerbart && <Nav.Knapp type="hoved" mini className="innhold__element" onClick={lagreOgLukkHandle}>Lukk</Nav.Knapp> }
-        <Nav.Knapp disabled={!redigerbart} type="hoved" mini className="innhold__element" onClick={tilbakeleggeHandle}>Legg tilbake i kø</Nav.Knapp>
-        { <Nav.Knapp type="hoved" mini className="innhold__element" onClick={apneTidligereBehandlinger}>Vis tidligere behandlinger</Nav.Knapp> }
+        { redigerbart && <Nav.Knapp mini className="innhold__element" onClick={lagreOgLukkHandle}>Lukk</Nav.Knapp> }
+        <Nav.Knapp disabled={!redigerbart} mini className="innhold__element" onClick={tilbakeleggeHandle}>Legg tilbake i kø</Nav.Knapp>
+        { <Nav.Knapp mini className="innhold__element" onClick={apneTidligereBehandlinger}>Vis tidligere behandlinger</Nav.Knapp> }
       </div>
     </Nav.EkspanderbartpanelBase>
   );
