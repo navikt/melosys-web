6341f9f7b0263fb590e507a8e6a2ce1f11bba55d
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2019-09-10 15:02:39 +0200

    unntaksperioder/saksopplysninger som functional component

--- src/sider/registrering/unntaksperioder/komponenter/saksopplysninger.js
+++ src/sider/registrering/unntaksperioder/komponenter/saksopplysninger.js
@@ -38,43 +38,42 @@ RegisterkontrollTreff.propTypes = {
   begrunnelseKode: PT.string.isRequired,
 };
 
-class Saksopplysninger extends Component {
-  state = {
-    unntaksperiodeVurdering: KV.Koder.Unntaksperiode.GODKJENT,
-    begrunnelseFritekst: '',
-    ikkeGodkjentBegrunnelseKoder: [],
-    endrePeriodeFeilmeldinger: { fom: undefined, tom: undefined, fritekst: undefined },
-  };
+const Saksopplysninger = props => {
+  const [unntaksperiodeVurdering, setUnntaksperiodeVurdering] = React.useState(KV.Koder.Unntaksperiode.GODKJENT);
+  const [begrunnelseFritekst, setBegrunnelseFritekst] = React.useState('');
+  const [ikkeGodkjentBegrunnelseKoder, setIkkeGodkjentBegrunnelseKoder] = React.useState([]);
+  const [endrePeriodeFeilmeldinger, setEndrePeriodeFeilmeldinger] = React.useState({ fom: undefined, tom: undefined, fritekst: undefined });
 
-  overstyrSubmit = event => {
+  const overstyrSubmit = event => {
     event.preventDefault();
   };
-  textAreaOnChange = event => {
-    const begrunnelseFritekst = event.target.value;
-    this.setState({ begrunnelseFritekst });
+
+  const textAreaOnChange = event => {
+    setBegrunnelseFritekst(event.target.value);
   };
-  submitRegistrering = () => {
-    if (!this.validerFelt()) {
+
+  const submitRegistrering = () => {
+    if (!validerFelt()) {
       return false;
     }
 
-    const { behandlingID, history } = this.props;
+    const { behandlingID, history } = props;
     const tilForsiden = () => history.push('/');
-    switch (this.state.unntaksperiodeVurdering) {
+    switch (unntaksperiodeVurdering) {
       case KV.Koder.Unntaksperiode.GODKJENT:
-        this.godkjenn(behandlingID)
+        godkjenn(behandlingID)
           .then(tilForsiden)
           .catch(Utils.logger.error);
         return true;
       case KV.Koder.Unntaksperiode.INNHENT:
-        this.innhentInfo(behandlingID)
+        innhentInfo(behandlingID)
           .then(tilForsiden)
           .catch(Utils.logger.error);
         return true;
       case KV.Koder.Unntaksperiode.AVSLAG: {
         const ikkegodkjenn = {
-          ikkeGodkjentBegrunnelseKoder: [...this.state.ikkeGodkjentBegrunnelseKoder],
-          begrunnelseFritekst: this.state.begrunnelseFritekst,
+          ikkeGodkjentBegrunnelseKoder: [...ikkeGodkjentBegrunnelseKoder],
+          begrunnelseFritekst: begrunnelseFritekst,
         };
         Api.Saksflyt.Unntaksperioder.ikkegodkjenn(behandlingID, { ...ikkegodkjenn })
           .then(tilForsiden)
@@ -86,156 +85,154 @@ class Saksopplysninger extends Component {
     }
   };
 
-  godkjenn = behandlingID => this.endrePeriodeOgLagre(() => Api.Saksflyt.Unntaksperioder.godkjenn(behandlingID));
+  const godkjenn = behandlingID => endrePeriodeOgLagre(() => Api.Saksflyt.Unntaksperioder.godkjenn(behandlingID));
 
-  innhentInfo = behandlingID => this.endrePeriodeOgLagre(() => Api.Saksflyt.Unntaksperioder.innhentinfo(behandlingID));
+  const innhentInfo = behandlingID => endrePeriodeOgLagre(() => Api.Saksflyt.Unntaksperioder.innhentinfo(behandlingID));
 
-  endrePeriodeOgLagre = dispatchSaksflyt => (
-    this.props.endrePeriode.skalEndres
-      ? this.oppdaterAvklartefakta()
-        .then(() => this.oppdaterLovvalgsperioder()
+  const endrePeriodeOgLagre = dispatchSaksflyt => (
+    props.endrePeriode.skalEndres
+      ? oppdaterAvklartefakta()
+        .then(() => oppdaterLovvalgsperioder()
           .then(() => dispatchSaksflyt()))
       : dispatchSaksflyt());
 
-  lagAvklartfakta = () => ({
+  const lagAvklartfakta = () => ({
     referanse: MKV.Koder.avklartefakta.AARSAK_ENDRING_PERIODE,
     avklartefaktaKode: MKV.Koder.avklartefakta.AARSAK_ENDRING_PERIODE,
-    fakta: [this.props.endrePeriode.begrunnelse],
+    fakta: [props.endrePeriode.begrunnelse],
     subjektID: null,
     begrunnelseKoder: [],
-    begrunnelseFritekst: this.props.endrePeriode.fritekst || null,
+    begrunnelseFritekst: props.endrePeriode.fritekst || null,
   });
 
-  oppdaterAvklartefakta = () =>
-    this.props.oppdaterAvklartefakta(this.props.behandlingID, [...this.props.avklartefakta, this.lagAvklartfakta()]);
+  const oppdaterAvklartefakta = () =>
+    props.oppdaterAvklartefakta(props.behandlingID, [...props.avklartefakta, lagAvklartfakta()]);
 
-  lagLovvalgsperioder = () => ([{
-    fomDato: `${Utils.dato.formatterDatoTilISO(this.props.endrePeriode.fom)}`,
-    tomDato: `${Utils.dato.formatterDatoTilISO(this.props.endrePeriode.tom)}`,
-    lovvalgsbestemmelse: this.props.sed.lovvalgsbestemmelse,
+  const lagLovvalgsperioder = () => ([{
+    fomDato: `${Utils.dato.formatterDatoTilISO(props.endrePeriode.fom)}`,
+    tomDato: `${Utils.dato.formatterDatoTilISO(props.endrePeriode.tom)}`,
+    lovvalgsbestemmelse: props.sed.lovvalgsbestemmelse,
     tilleggBestemmelse: null,
     unntakFraBestemmelse: null,
     innvilgelsesResultat: KV.Koder.INNVILGET,
-    lovvalgsland: this.props.sed.lovvalgslandKode,
+    lovvalgsland: props.sed.lovvalgslandKode,
     unntakFraLovvalgsland: null,
     trygdeDekning: MKV.Koder.trygdedekninger.UTEN_DEKNING,
     medlemskapstype: MKV.Koder.medlemskapstyper.UNNTATT,
     medlemskapsperiodeID: null,
   }]);
 
-  oppdaterLovvalgsperioder = () =>
-    this.props.oppdaterLovvalgsperioder(this.props.behandlingID, this.lagLovvalgsperioder());
+  const oppdaterLovvalgsperioder = () =>
+    props.oppdaterLovvalgsperioder(props.behandlingID, lagLovvalgsperioder());
 
-  kanEndrePeriode = () => this.props.redigerbart
-    && (this.state.unntaksperiodeVurdering === KV.Koder.Unntaksperiode.GODKJENT
-    || this.state.unntaksperiodeVurdering === KV.Koder.Unntaksperiode.INNHENT);
+  const kanEndrePeriode = () => props.redigerbart
+    && (unntaksperiodeVurdering === KV.Koder.Unntaksperiode.GODKJENT
+    || unntaksperiodeVurdering === KV.Koder.Unntaksperiode.INNHENT);
 
-  validerFelt = () => this.validerEndrePeriode();
+  const validerFelt = () => validerEndrePeriode();
 
-  validerEndrePeriode = () => {
-    const { endrePeriode } = this.props;
-    if (!this.kanEndrePeriode() || !endrePeriode.skalEndres) {
+  const validerEndrePeriode = () => {
+    const { endrePeriode } = props;
+    if (!kanEndrePeriode() || !endrePeriode.skalEndres) {
       return true;
     }
 
     const fritekstPakrevd = endrePeriode.begrunnelse === MKV.Koder.begrunnelser.folketrygdloven.endret_unntaksperiode.ANNET;
     const settings = { context: { fritekstPakrevd } };
     const stateObject = { fom: endrePeriode.fom, tom: endrePeriode.tom, fritekst: endrePeriode.fritekst };
-    const endrePeriodeFeilmeldinger = createValidator(endrePeriodeSkjema, settings)(stateObject);
-    const validert = Utils._isEmpty(endrePeriodeFeilmeldinger);
+    const feilmeldinger = createValidator(endrePeriodeSkjema, settings)(stateObject);
+    const validert = Utils._isEmpty(feilmeldinger);
 
     if (!validert) {
-      this.setState({ endrePeriodeFeilmeldinger });
+      setEndrePeriodeFeilmeldinger(feilmeldinger);
     }
     return validert;
   };
 
-  render() {
-    const {
-      medlemskap, sed, vurderingBegrunnelser, redigerbart,
-    } = this.props;
-    if (!sed.lovvalgsperiode) {
-      return null;
-    }
+  const {
+    medlemskap, sed, vurderingBegrunnelser, redigerbart,
+  } = props;
+  if (!sed.lovvalgsperiode) {
+    return null;
+  }
 
-    const listevalgEndringHandler = event => {
-      const ikkeGodkjentBegrunnelseKoder = [...event.value];
-      this.setState({ ikkeGodkjentBegrunnelseKoder });
-    };
-    const unikRadioButtonGruppeID = uuid();
-    return (
-      <div>
-        <form name="registrering" id="registrering" onSubmit={this.overstyrSubmit}>
-          <div className="stegvelger panelSeksjon">
-            <div className="panel stegFane steg0 stegFane--aktiv">
-              <Nav.Systemtittel>Registrering av unntaksperioder</Nav.Systemtittel>
-              <br />
-              <div className="vurderingEndrePeriode">
-                <Nav.Row className="seksjon">
-                  <Nav.Column xs="12">
-                    <Nav.Element>Treff ved automatisk kontroll</Nav.Element>
-                    {vurderingBegrunnelser.begrunnelseKoder && vurderingBegrunnelser.begrunnelseKoder.map(begrunnelseKode =>
-                      <RegisterkontrollTreff key={uuid()} begrunnelseKode={begrunnelseKode} />)}
-                  </Nav.Column>
-                </Nav.Row>
-                <Nav.Row className="seksjon">
-                  <Nav.Column xs="12">
-                    <Nav.Fieldset legend="Vurder unntaksperiode" onChange={e => this.setState({ unntaksperiodeVurdering: e.target.value })} disabled={!redigerbart}>
-                      <Nav.Radio name={unikRadioButtonGruppeID} value={KV.Koder.Unntaksperiode.GODKJENT} label="Godkjenn" defaultChecked />
-                      <Nav.Radio name={unikRadioButtonGruppeID} value={KV.Koder.Unntaksperiode.INNHENT} label="Innhent informasjon" />
-                      <Nav.Radio name={unikRadioButtonGruppeID} value={KV.Koder.Unntaksperiode.AVSLAG} label="Ikke godkjenn" />
-                    </Nav.Fieldset>
-                  </Nav.Column>
-                </Nav.Row>
-                {this.state.unntaksperiodeVurdering === KV.Koder.Unntaksperiode.AVSLAG && (
-                  <Fragment>
-                    <Nav.Row>
-                      <Nav.Column xs="6">
-                        <Nav.Fieldset legend="Begrunnelse for ikke godkjent unntaksperiode">
-                          <ListevelgerFlervalg
-                            disabled={false}
-                            muligeValg={MKV.KTObjects.begrunnelser.ikke_godkjent_begrunnelser}
-                            label="Legg til begrunnelse for ikke oppfylt:"
-                            tillatFritekst={false}
-                            onChange={listevalgEndringHandler}
-                          />
-                        </Nav.Fieldset>
-                      </Nav.Column>
-                    </Nav.Row>
-                    <Nav.Row>
-                      <Nav.Column xs="6">
-                        {this.state.ikkeGodkjentBegrunnelseKoder.includes('ANNET') &&
+  const listevalgEndringHandler = event => {
+    setIkkeGodkjentBegrunnelseKoder([...event.value]);
+  };
+
+  const unikRadioButtonGruppeID = uuid();
+  return (
+    <div>
+      <form name="registrering" id="registrering" onSubmit={overstyrSubmit}>
+        <div className="stegvelger panelSeksjon">
+          <div className="panel stegFane steg0 stegFane--aktiv">
+            <Nav.Systemtittel>Registrering av unntaksperioder</Nav.Systemtittel>
+            <br />
+            <div className="vurderingEndrePeriode">
+              <Nav.Row className="seksjon">
+                <Nav.Column xs="12">
+                  <Nav.Element>Treff ved automatisk kontroll</Nav.Element>
+                  {vurderingBegrunnelser.begrunnelseKoder && vurderingBegrunnelser.begrunnelseKoder.map(begrunnelseKode =>
+                    <RegisterkontrollTreff key={uuid()} begrunnelseKode={begrunnelseKode} />)}
+                </Nav.Column>
+              </Nav.Row>
+              <Nav.Row className="seksjon">
+                <Nav.Column xs="12">
+                  <Nav.Fieldset legend="Vurder unntaksperiode" onChange={e => setUnntaksperiodeVurdering(e.target.value)} disabled={!redigerbart}>
+                    <Nav.Radio name={unikRadioButtonGruppeID} value={KV.Koder.Unntaksperiode.GODKJENT} label="Godkjenn" defaultChecked />
+                    <Nav.Radio name={unikRadioButtonGruppeID} value={KV.Koder.Unntaksperiode.INNHENT} label="Innhent informasjon" />
+                    <Nav.Radio name={unikRadioButtonGruppeID} value={KV.Koder.Unntaksperiode.AVSLAG} label="Ikke godkjenn" />
+                  </Nav.Fieldset>
+                </Nav.Column>
+              </Nav.Row>
+              {unntaksperiodeVurdering === KV.Koder.Unntaksperiode.AVSLAG && (
+                <Fragment>
+                  <Nav.Row>
+                    <Nav.Column xs="6">
+                      <Nav.Fieldset legend="Begrunnelse for ikke godkjent unntaksperiode">
+                        <ListevelgerFlervalg
+                          disabled={false}
+                          muligeValg={MKV.KTObjects.begrunnelser.ikke_godkjent_begrunnelser}
+                          label="Legg til begrunnelse for ikke oppfylt:"
+                          tillatFritekst={false}
+                          onChange={listevalgEndringHandler}
+                        />
+                      </Nav.Fieldset>
+                    </Nav.Column>
+                  </Nav.Row>
+                  <Nav.Row>
+                    <Nav.Column xs="6">
+                      {ikkeGodkjentBegrunnelseKoder.includes('ANNET') &&
                         <Nav.Textarea
                           label="Skriv inn begrunnelse for avslaget..."
-                          onChange={this.textAreaOnChange}
-                          value={this.state.begrunnelseFritekst}
+                          onChange={textAreaOnChange}
+                          value={begrunnelseFritekst}
                           maxLength={255}
                           bredde="fullbredde" />
-                        }
-                      </Nav.Column>
-                    </Nav.Row>
-                  </Fragment>
-                )}
-                <Nav.Row className="seksjon">
-                  <EndrePeriode
-                    feilmeldinger={this.state.endrePeriodeFeilmeldinger}
-                    redigerbart={this.kanEndrePeriode()} />
-                </Nav.Row>
-                <Nav.Row className="seksjon">
-                  <Nav.Column xs="3">
-                    <Nav.Hovedknapp onClick={() => this.submitRegistrering()} disabled={!redigerbart}>LAGRE</Nav.Hovedknapp>
-                  </Nav.Column>
-                </Nav.Row>
-              </div>
+                      }
+                    </Nav.Column>
+                  </Nav.Row>
+                </Fragment>
+              )}
+              <Nav.Row className="seksjon">
+                <EndrePeriode
+                  feilmeldinger={endrePeriodeFeilmeldinger}
+                  redigerbart={kanEndrePeriode()} />
+              </Nav.Row>
+              <Nav.Row className="seksjon">
+                <Nav.Column xs="3">
+                  <Nav.Hovedknapp onClick={() => submitRegistrering()} disabled={!redigerbart}>LAGRE</Nav.Hovedknapp>
+                </Nav.Column>
+              </Nav.Row>
             </div>
           </div>
-        </form>
-        {/* <Personopplysninger redigerbart /> TODO: Må hentes fra context (SPRINT-34) */}
-        {medlemskap && <Medlemskap medlemskap={medlemskap} />}
-      </div>
-    );
-  }
-}
+        </div>
+      </form>
+      {/* <Personopplysninger redigerbart /> TODO: Må hentes fra context (SPRINT-34) */}
+      {medlemskap && <Medlemskap medlemskap={medlemskap} />}
+    </div>
+  );
+};
 
 
 Saksopplysninger.propTypes = {
