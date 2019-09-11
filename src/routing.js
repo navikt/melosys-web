0770e18142b68b2c01959fa37712bfaa292b2ec9
Are Bjolseth <abjolseth@gmail.com>
2019-09-11 18:28:53 +0200

    Fixed problem with merge conflict and ErrorBoundary.

--- src/routing.js
+++ src/routing.js
@@ -13,19 +13,21 @@ const ForsideLoadable = loadable(() => import('./sider/forside'), { fallback: Si
 const SokLoadable = loadable(() => import('./sider/sok'), { fallback: SideLoadingStatus });
 const SaksbehandlingLoadable = loadable(() => import('./sider/saksbehandling'), { fallback: SideLoadingStatus });
 const JournalforingLoadable = loadable(() => import('./sider/journalforing'), { fallback: SideLoadingStatus });
-const RegistreringLoadable = loadable(() => import('./sider/registrering'), { fallback: SideLoadingStatus });
+const RegistreringUnntaksperioderLoadable = loadable(() => import('./sider/registrering/unntaksperioder'), { fallback: SideLoadingStatus });
+const RegistreringAnmodningunntakLoadable = loadable(() => import('./sider/registrering/anmodningunntak'), { fallback: SideLoadingStatus });
 
 const Routing = ({ location }) => (
-  <Switch location={location}>
-    <ErrorBoundary message={SideLoadingFailMessage}>
+  <ErrorBoundary message={SideLoadingFailMessage}>
+    <Switch location={location}>
       <Route exact path="/" component={ForsideLoadable} />
       <Route exact path="/sok/:fnr" component={SokLoadable} />
-      {Utils.feature.featureToggle('REL1.1') && <Route exact path="/registrering/:snr" component={RegistreringLoadable} />}
+      {Utils.feature.featureToggle('REL1.1') && <Route exact path="/registrering/:snr/unntaksperioder" component={RegistreringUnntaksperioderLoadable} />}
+      {Utils.feature.featureToggle('REL1.1') && <Route exact path="/registrering/:snr/anmodningunntak" component={RegistreringAnmodningunntakLoadable} />}
       <Route path="/saksbehandling/:snr" component={SaksbehandlingLoadable} />
       <Route path="/journalforing/:journalpostID/:oppgaveID" component={JournalforingLoadable} />
       <Route component={UkjentSideLoadable} />
-    </ErrorBoundary>
-  </Switch>
+    </Switch>
+  </ErrorBoundary>
 );
 
 Routing.propTypes = {
