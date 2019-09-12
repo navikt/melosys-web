dbacbfe986613fde3393f92b19c550bf3576e2e7
Are Bjolseth <abjolseth@gmail.com>
2019-09-12 22:27:57 +0200

    Added SKIP_PREFLIGHT_CHECK=true => .env

--- scripts/patchenv.js
+++ scripts/patchenv.js
@@ -25,4 +25,5 @@ dotenv.set('REACT_APP_BUILD_VERSION', buildNumber);
 dotenv.set('REACT_APP_BRANCH_NAME', branchName);
 dotenv.set('REACT_APP_MELOSYS_KODEVERK', kodeverk_versjon);
 dotenv.set('REACT_APP_REACT_LIB', react_lib_versjon.slice(1));
+dotenv.set('SKIP_PREFLIGHT_CHECK', true);
 
