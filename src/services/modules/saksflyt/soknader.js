e165e8c4c2c6d62df81484af8860124744b33925
JohanSakshaug <Johan.Sakshaug@nav.no>
2019-09-19 12:37:11 +0200

    Endring videresende endepunkt

--- src/services/modules/saksflyt/soknader.js
+++
@@ -1,4 +0,0 @@
-import { putAsText } from '../../utils';
-import { API_BASE_URL, SAKSFLYT, SOKNADER } from '../../api-constants';
-
-export const videresend = behandlingID => putAsText(`${API_BASE_URL}${SAKSFLYT}/${SOKNADER}/${behandlingID}/videresend`);
