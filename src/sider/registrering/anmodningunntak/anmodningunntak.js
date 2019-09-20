4f04367dd63a44408eaf3c89c0977e580a3b936c
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2019-09-20 13:09:32 +0200

    Rydder opp i anmodningunntak

--- src/sider/registrering/anmodningunntak/anmodningunntak.js
+++ src/sider/registrering/anmodningunntak/anmodningunntak.js
@@ -5,11 +5,7 @@ import PT from 'prop-types';
 import * as Utils from '../../../utils/';
 import * as Nav from '../../../utils/navFrontend';
 import * as MPT from '../../../proptypes';
-import * as Api from '../../../services/api';
 
-import DialogboksOppfriskSak from '../../../felleskomponenter/dialogboks/dialogboksOppfrisk';
-import DialogboksVenter from '../../../felleskomponenter/dialogboks/dialogboksVenter';
-import DialogboksHenlegg from '../../../felleskomponenter/dialogboks/dialogboksHenlegg';
 import Saksopplysninger from './saksopplysninger';
 import SideDialog from '../../../felleskomponenter/sideDialog/sideDialog';
 import SideOppsummering from '../komponenter/sideOppsummering';
@@ -18,30 +14,18 @@ import { behandlingerOperations, behandlingerSelectors } from '../../../ducks/be
 import { avklartefaktaOperations, avklartefaktaSelectors } from '../../../ducks/avklartefakta';
 import { lovvalgsperioderOperations, lovvalgsperioderSelectors } from '../../../ducks/lovvalgsperioder';
 import { oppgaverOperations } from '../../../ducks/oppgaver';
+import { redigerbartSelectors } from '../../../ducks/redigerbart';
+import { RegistreringStateProviderWrapper } from '../state/registreringStateProvider';
 
 import '../registrering.css';
 
-import { saksopplysningerOperations } from '../../../ducks/saksopplysninger';
-import { RegistreringStateProviderWrapper } from '../state/registreringStateProvider';
-
 const Anmodningunntak = props => {
-  const [saksnummer, setSaksnummer] = React.useState(null);
+  const { match: { params: { snr } } } = props;
+  const [saksnummer] = React.useState(snr);
   const [behandlingID, setBehandlingID] = React.useState(-1);
-  const [oppfriskDialog, visOppfriskDialog] = React.useState(false);
-  const [henleggDialog, visHenleggDialog] = React.useState(false);
-  const [oppfriskningBlokkererInnhold, visOppfriskningBlokkererInnhold] = React.useState(false);
 
-  const tilForsiden = () => {
-    this.props.history.push('/');
-  };
-  const skjulOppfriskBekreftelse = () => {
-    visOppfriskDialog(false);
-    visOppfriskningBlokkererInnhold(false);
-  };
   const lastInnSaksopplysninger = async () => {
-    const { match, location } = props;
-    const { snr } = match.params;
-    setSaksnummer(snr);
+    const { location } = props;
     const _behandlingID = Utils.queryString.getParam(location, 'behandlingID');
     setBehandlingID(Utils._toInteger(_behandlingID));
 
@@ -51,7 +35,7 @@ const Anmodningunntak = props => {
     try {
       await Promise.all([
         hentBehandling(_behandlingID),
-        hentFagsaker(snr),
+        hentFagsaker(saksnummer),
         hentAvklartefakta(_behandlingID),
         hentLovvalgsperioder(_behandlingID),
       ]);
@@ -60,30 +44,6 @@ const Anmodningunntak = props => {
     }
   };
 
-  const henleggSak = async data => {
-    const { fagsak: { saksnummer: snr } } = props;
-    Api.Fagsaker.fagsak.henlegg(snr, data);
-  };
-  const hentBehandlingStatus = async () => {
-    const oppfriskning = await Api.Saksopplysninger.sjekkStatus(behandlingID);
-
-    if (oppfriskning && oppfriskning.response) {
-      skjulOppfriskBekreftelse();
-    } else if (oppfriskning === 'DONE') {
-      skjulOppfriskBekreftelse();
-      lastInnSaksopplysninger();
-    }
-  };
-
-  const henleggHandle = async data => {
-    try {
-      await henleggSak(data);
-    } catch (e) {
-      Utils.logger.error(e);
-    } finally {
-      tilForsiden();
-    }
-  };
   const lagreOgLukk = async () => {
     const { history, hentOppgaveOversikt } = props;
     await hentOppgaveOversikt();
@@ -96,45 +56,19 @@ const Anmodningunntak = props => {
     await tilbakeleggOppgave(behandlingID, venterPaaDokumentasjon);
     lagreOgLukk();
   };
-  const skjulHenleggDialog = () => {
-    visHenleggDialog(false);
-  };
 
-  const blokkerInnholdMedOppfriskSpinner = () => {
-    visOppfriskningBlokkererInnhold(true);
-  };
-  const lagreSoknadOgOppfriskSaksopplysninger = async () => {
-    const { oppfriskSaksopplysninger } = props;
-    await oppfriskSaksopplysninger(behandlingID);
-    blokkerInnholdMedOppfriskSpinner();
-  };
   const navigerTilOversiktSide = () => {
-    visOppfriskDialog(false);
     props.history.push('/');
   };
-  // ---------------------------------------
+
   React.useEffect(() => {
     lastInnSaksopplysninger();
     return () => props.resetFagsakState();
   }, []);
 
-  // TODO add unmounts
-  // ---------------------------------------
   const {
     vurderingBegrunnelser, medlemskap, sed, redigerbart,
   } = props;
-
-  const oppfriskVenterDialog = oppfriskningBlokkererInnhold && (
-    <div>
-      <DialogboksVenter
-        tittel="Oppdaterer registeropplysninger"
-        tekst="Vent mens registeropplysningene hentes på nytt fra TPS, Aa-register, Medl etc."
-        synlig
-        tilForsiden={navigerTilOversiktSide}
-        oppdater={hentBehandlingStatus}
-      />
-    </div>
-  );
 
   if (Utils._isNil(redigerbart)) return null;
   return (
@@ -157,27 +91,16 @@ const Anmodningunntak = props => {
               tilbakeleggeHandle={tilbakeleggHandle}
               tilForsidenHandle={navigerTilOversiktSide}
             />
-            <SideDialog saksnummer={saksnummer} behandlingID={behandlingID} redigerbart={redigerbart} />
+            <SideDialog
+              saksnummer={saksnummer}
+              behandlingID={behandlingID}
+              brevBestillingRedigerbart={redigerbart}
+              sedBestillingRedigerbart={redigerbart}
+              brevBestillingRedigerbartIArtikkel13={redigerbart}
+            />
           </Nav.Column>
         </Nav.Row>
       </Nav.Container>
-      { oppfriskVenterDialog }
-      {
-        oppfriskDialog &&
-        <DialogboksOppfriskSak
-          bekreft={lagreSoknadOgOppfriskSaksopplysninger}
-          avbryt={skjulOppfriskBekreftelse}
-          tilForsiden={navigerTilOversiktSide}
-          oppdater={hentBehandlingStatus}
-        />
-      }
-      {
-        henleggDialog &&
-        <DialogboksHenlegg
-          avbryt={skjulHenleggDialog}
-          henleggHandle={henleggHandle}
-        />
-      }
     </div>
   );
 };
@@ -189,7 +112,6 @@ Anmodningunntak.propTypes = {
   hentOppgaveOversikt: PT.func.isRequired,
   resetFagsakState: PT.func.isRequired,
   tilbakeleggOppgave: PT.func.isRequired,
-  oppfriskSaksopplysninger: PT.func.isRequired,
   redigerbart: PT.bool,
   avklartefakta: MPT.AvklartefaktaListe,
   vurderingBegrunnelser: PT.object,
@@ -213,7 +135,7 @@ Anmodningunntak.defaultProps = {
   vurderingBegrunnelser: {},
 };
 const mapStateToProps = state => ({
-  redigerbart: behandlingerSelectors.RedigerbartSelector(state),
+  redigerbart: redigerbartSelectors.RedigerbartSelector(state),
   avklartefakta: avklartefaktaSelectors.AvklartefaktaSelector(state),
   vurderingBegrunnelser: avklartefaktaSelectors.VurderingUnntakPeriode(state),
   fagsak: fagsakSelectors.FagsakSelector(state),
@@ -231,7 +153,6 @@ const mapDispatchToProps = dispatch => ({
   resetFagsakState: () => dispatch(fagsakOperations.resetFagsakState()),
   hentLovvalgsperioder: behandlingID => dispatch(lovvalgsperioderOperations.hent(behandlingID)),
   hentOppgaveOversikt: () => dispatch(oppgaverOperations.oversikt()),
-  oppfriskSaksopplysninger: saksnummer => saksopplysningerOperations.oppfrisk(saksnummer),
   tilbakeleggOppgave: (oppgaveID, venterPaaDokumentasjon) => oppgaverOperations.tilbakelegg(oppgaveID, venterPaaDokumentasjon),
 });
 
