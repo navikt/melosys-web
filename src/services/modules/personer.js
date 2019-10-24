fa0ccb4b14d7cde28df94ab9c7da63262712302a
JohanSakshaug <Johan.Sakshaug@nav.no>
2019-10-24 15:32:56 +0200

    cachedGetAsJson thrower ikke error ved http-errorkode returnert fra backend.

--- src/services/modules/personer.js
+++ src/services/modules/personer.js
@@ -1,7 +1,7 @@
 import { API_BASE_URL, PERSONER } from '../api-constants';
 
-import { getAsJson } from '../utils';
+import { getAsJson, cachedGetAsJson } from '../utils';
 
 // eslint-disable-next-line import/prefer-default-export
 export const hentPerson = fnrdnr => getAsJson(`${API_BASE_URL}${PERSONER}/${fnrdnr}`);
-
+export const hentPersonCached = fnrdnr => cachedGetAsJson(`${API_BASE_URL}${PERSONER}/${fnrdnr}`);
