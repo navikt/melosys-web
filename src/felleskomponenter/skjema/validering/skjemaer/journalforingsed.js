392bf8bb34c292dbb6a85140c20f77cf3165e234
JohanSakshaug <Johan.Sakshaug@nav.no>
2019-10-24 17:23:47 +0200

    Fjern sjekk på orgnrlengde for brukerID på journalforing SED

--- src/felleskomponenter/skjema/validering/skjemaer/journalforingsed.js
+++ src/felleskomponenter/skjema/validering/skjemaer/journalforingsed.js
@@ -7,7 +7,6 @@ const {
 const SKRIV_INN_FNR_ELLER_DNR = { melding: 'Skriv inn fnr eller dnr.' };
 const SKRIV_INN_KUN_NUMMER = { melding: 'Skriv inn kun nummer.' };
 const SKRIV_INN_GYLDIG_FNR_ELLER_DNR = { melding: 'Skriv inn gyldig fnr eller dnr.' };
-const FANT_INGEN_NAVN_PA_ORGNR = { melding: 'Fant ingen navn på dette organisasjonsnummeret.' };
 const FANT_INGEN_NAVN_PA_FNR_ELLER_DNR = { melding: 'Fant ingen navn på dette fnr eller dnr.' };
 
 const journalforingSED = object().shape({
@@ -19,7 +18,6 @@ const journalforingSED = object().shape({
     .when('bruker', {
       is: undefined,
       then: string()
-        .harIkkeOrgnrLengde(FANT_INGEN_NAVN_PA_ORGNR)
         .harIkkeFnrEllerDnrLengde(FANT_INGEN_NAVN_PA_FNR_ELLER_DNR),
     }),
   bruker: mixed(),
