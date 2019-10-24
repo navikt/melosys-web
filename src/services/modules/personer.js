d6c8d84cc4d34dd7d0d35c489108a890f3f29491
JohanSakshaug <Johan.Sakshaug@nav.no>
2019-10-24 15:50:27 +0200

    cachedGetAsJson thrower nå error ved http-errorkode returnert fra backend

--- src/services/modules/personer.js
+++ src/services/modules/personer.js
@@ -1,7 +1,6 @@
 import { API_BASE_URL, PERSONER } from '../api-constants';
 
-import { getAsJson, cachedGetAsJson } from '../utils';
+import { cachedGetAsJson } from '../utils';
 
 // eslint-disable-next-line import/prefer-default-export
-export const hentPerson = fnrdnr => getAsJson(`${API_BASE_URL}${PERSONER}/${fnrdnr}`);
-export const hentPersonCached = fnrdnr => cachedGetAsJson(`${API_BASE_URL}${PERSONER}/${fnrdnr}`);
+export const hentPerson = fnrdnr => cachedGetAsJson(`${API_BASE_URL}${PERSONER}/${fnrdnr}`);
