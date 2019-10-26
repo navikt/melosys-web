aa89ececff9c802a226fc40bc63b9c825744f8a1
JohanSakshaug <Johan.Sakshaug@nav.no>
2019-10-26 15:50:09 +0200

    Vis uredigerbare felter ved preutfylt avsenderID og avsenderNavn

--- src/sider/journalforing/komponenter/hvemeravsender.js
+++ src/sider/journalforing/komponenter/hvemeravsender.js
@@ -8,6 +8,29 @@ import * as Skjema from '../../../felleskomponenter/skjema/';
 import * as Nav from '../../../utils/navFrontend';
 import * as KV from '../../../kodeverk';
 
+import { journalforingSelectors } from '../../../ducks/journalforing';
+
+import './hvemeravsender.css';
+
+const PreutfyltAvsender = ({
+  className,
+  avsenderID,
+  avsenderNavn,
+}) => (
+  <div className={className}>
+    <Nav.Element className="linje">Avsender ID</Nav.Element>
+    <Nav.Normaltekst className="linje">{avsenderID}</Nav.Normaltekst>
+    <Nav.Element className="linje">Avsenders navn</Nav.Element>
+    <Nav.Normaltekst className="linje">{avsenderNavn}</Nav.Normaltekst>
+  </div>
+);
+
+PreutfyltAvsender.propTypes = {
+  className: PT.string.isRequired,
+  avsenderID: PT.string.isRequired,
+  avsenderNavn: PT.string.isRequired,
+};
+
 const HvemErAvsender = ({
   className,
   kopierBrukerTilAvsender,
@@ -15,6 +38,8 @@ const HvemErAvsender = ({
   formValues,
   settFeltInnhold,
   visAvsenderSpinner,
+  journalforingAvsenderID,
+  journalforingAvsenderNavn,
 }) => {
   const avsenderTypeEndret = avsenderType => {
     switch (avsenderType) {
@@ -42,6 +67,16 @@ const HvemErAvsender = ({
     settFeltInnhold('avsenderID', landkode);
     settFeltInnhold('avsenderNavn', avsenderNavn);
   };
+
+  if (journalforingAvsenderID && journalforingAvsenderNavn) {
+    return (
+      <PreutfyltAvsender
+        className={className}
+        avsenderID={journalforingAvsenderID}
+        avsenderNavn={journalforingAvsenderNavn}
+      />
+    );
+  }
 
   return (
     <div className={className}>
@@ -103,15 +138,21 @@ HvemErAvsender.propTypes = {
   formValues: PT.object,
   settFeltInnhold: PT.func.isRequired,
   visAvsenderSpinner: PT.bool.isRequired,
+  journalforingAvsenderID: PT.string,
+  journalforingAvsenderNavn: PT.string,
 };
 
 HvemErAvsender.defaultProps = {
   className: undefined,
   formValues: {},
+  journalforingAvsenderID: undefined,
+  journalforingAvsenderNavn: undefined,
 };
 
 const mapStateToProps = state => ({
   formValues: getFormValues(KV.Form.JOURNALFORING)(state),
+  journalforingAvsenderID: journalforingSelectors.AvsenderIDSelector(state),
+  journalforingAvsenderNavn: journalforingSelectors.AvsenderNavnSelector(state),
 });
 
 export default connect(mapStateToProps)(HvemErAvsender);
