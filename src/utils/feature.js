d7e29a3906b76ae46102bd166e7e51bf91672e94
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2019-10-29 17:20:48 +0100

    try-catch rundt henting av data for feature-toggle

--- src/utils/feature.js
+++ src/utils/feature.js
@@ -1,8 +1,14 @@
 import * as Api from '../services/api';
+import * as Utils from '../utils';
 
 export const toggle = async allowedEnvs => {
-  const { namespace: serverNamespace = '', cluster: serverCluster = '' } = await Api.ServerInfo.hentServerInfo();
-  return allowedEnvs.some(({ namespace, cluster }) =>
-    namespace.toUpperCase() === serverNamespace.toUpperCase()
-    && cluster.toUpperCase() === serverCluster.toUpperCase());
+  try {
+    const { namespace: serverNamespace = '', cluster: serverCluster = '' } = await Api.ServerInfo.hentServerInfo();
+    return allowedEnvs.some(({ namespace, cluster }) =>
+      namespace.toUpperCase() === serverNamespace.toUpperCase()
+      && cluster.toUpperCase() === serverCluster.toUpperCase());
+  } catch (e) {
+    Utils.logger.error(e);
+    return false;
+  }
 };
