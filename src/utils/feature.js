8ccd3d86d82ac546b3b7510d8c0da7d71eb729dd
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2019-10-29 16:56:29 +0100

    Endrer system for feature-toggle og legger til toggle for prod

--- src/utils/feature.js
+++ src/utils/feature.js
@@ -1,28 +1,8 @@
 import * as Api from '../services/api';
-import * as Utils from '../utils';
 
-const caseInsensitiveIncludes = (collection, searchElement) =>
-  collection.map(el => el.toUpperCase()).includes(searchElement.toUpperCase());
-
-export const featureToggle = (...features) => caseInsensitiveIncludes(features, `${process.env.REACT_APP_FEATURE_TOGGLE}`);
-
-export const namespaceToggle = async (...namespaces) => {
-  try {
-    const { namespace } = await Api.ServerInfo.hentServerInfo();
-    return caseInsensitiveIncludes(namespaces, namespace || '');
-  } catch (e) {
-    Utils.logger.error(e);
-    return false;
-  }
+export const toggle = async allowedEnvs => {
+  const { namespace: serverNamespace = '', cluster: serverCluster = '' } = await Api.ServerInfo.hentServerInfo();
+  return allowedEnvs.some(({ namespace, cluster }) =>
+    namespace.toUpperCase() === serverNamespace.toUpperCase()
+    && cluster.toUpperCase() === serverCluster.toUpperCase());
 };
-
-export const clusterToggle = async (...clusters) => {
-  try {
-    const { cluster } = await Api.ServerInfo.hentServerInfo();
-    return caseInsensitiveIncludes(clusters, cluster || '');
-  } catch (e) {
-    Utils.logger.error(e);
-    return false;
-  }
-};
-
