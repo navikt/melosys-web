534b59baaaf58922f964036f3593cfc765e35671
JohanSakshaug <johan.sakshauger@nav.no>
2019-11-08 16:35:20 +0100

    Feil tester hvor det kastes en proptype-error

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,6 +4,8 @@ import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
 import each from 'jest-each';
 
+// Feiler tester som kaster en proptype-error
+import 'jest-prop-type-error';
 
 global.fetch = require('jest-fetch-mock');
 
