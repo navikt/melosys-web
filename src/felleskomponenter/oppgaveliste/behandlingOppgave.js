16a9f1759809a8eafe4951ea796cd9661d136b3f
JohanSakshaug <johan.sakshauger@nav.no>
2019-11-12 18:05:36 +0100

    Test artikkel 16.1 anmodning

--- src/felleskomponenter/oppgaveliste/behandlingOppgave.js
+++ src/felleskomponenter/oppgaveliste/behandlingOppgave.js
@@ -71,7 +71,7 @@ const BehandlingOppgave = ({ sak }) => {
 
   return (
     <BehandlingOppgavesLinjeWrapper link={link} stengt={erUnderOppdatering}>
-      <Nav.Panel className={cl}>
+      <Nav.Panel data-cy-behandlingstype={behandlingstype.kode} className={cl}>
         <PanelHeader
           ikon={Ikoner.IkonSak}
           tittel={tittel}
