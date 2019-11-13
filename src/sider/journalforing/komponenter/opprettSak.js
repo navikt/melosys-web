d06dff016d848736ffb2faa142fd857044e7d54f
Are Bjølseth <are.bjolseth@nav.no>
2019-11-13 15:40:11 +0100

    Work in Progress. customRadioPanelGruppe.js la til nytt optional panel "footer". La til nytt ikon paperplane.svg Added opprettSak.js footer panel. Added sendForvaltningsMelding.js

---
+++ src/sider/journalforing/komponenter/opprettSak.js
@@ -0,0 +1,134 @@
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import { change } from 'redux-form';
+import PT from 'prop-types';
+import * as MKV from 'melosys-kodeverk';
+
+import * as Skjema from '../../../felleskomponenter/skjema/';
+import * as Nav from '../../../utils/navFrontend';
+import * as MPT from '../../../proptypes/';
+import * as Utils from '../../../utils';
+import * as KV from '../../../kodeverk';
+import { formSelectors } from '../../../ducks/form';
+
+import LandVelger from '../../../felleskomponenter/skjema/landvelger';
+
+export const OpprettSakTittel = () => (
+  <div className="enkeltSak__meta">
+    <Nav.Element>Opprett ny sak</Nav.Element>
+  </div>
+);
+const OpprettFagsak = props => {
+  const { sakstyper, behandlingstyper } = props;
+  const clsBehandlingsPanel = {
+    background: 'lightgray',
+    border: '1px solid #b7b1a9',
+    borderRadius: '3px',
+    margin: '0.5em 0',
+    padding: '0.5em 0.5em 0 1.25em',
+  };
+  const { journalforingSkjemaVerdier } = props;
+  const { opprettnysak_behandlingstype: valgtBehandlingstype } = journalforingSkjemaVerdier;
+  const alleLovvalg = [
+    ...MKV.KTObjects.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004,
+    ...MKV.KTObjects.lovvalgsbestemmelser.lovvalgbestemmelser_987_2009,
+    ...MKV.KTObjects.lovvalgsbestemmelser.tilleggsbestemmelser_883_2004,
+  ];
+  const art16 = [
+    KV.kodeTilObjekt(
+      MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART16_1,
+      MKV.KTObjects.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004
+    ),
+    KV.kodeTilObjekt(
+      MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART16_2,
+      MKV.KTObjects.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004
+    ),
+  ];
+  const skalViseSoknadsperiodeOgLand = behandlingstype => ![
+    MKV.Koder.behandlinger.behandlingstyper.ØVRIGE_SED,
+    MKV.Koder.behandlinger.behandlingstyper.VURDER_TRYGDETID,
+  ].includes(behandlingstype);
+
+  return (
+    <div style={clsBehandlingsPanel}>
+      <Skjema.Select feltNavn="sakstype" bredde="fullbredde" label="Sakstype">
+        { sakstyper.map(elem => (<option key={elem.kode} value={elem.kode}>{elem.term}</option>)) }
+      </Skjema.Select>
+      <Skjema.Select feltNavn="opprettnysak_behandlingstype" bredde="fullbredde" label="Behandlingstype">
+        {
+          behandlingstyper &&
+          behandlingstyper
+            .filter(elem => (elem.kode !== MKV.Koder.behandlinger.behandlingstyper.ENDRET_PERIODE))
+            .map(elem => (<option key={elem.kode} value={elem.kode}>{elem.term}</option>))
+        }
+      </Skjema.Select>
+      {skalViseSoknadsperiodeOgLand(valgtBehandlingstype) &&
+        <Fragment>
+          { valgtBehandlingstype === MKV.Koder.behandlinger.behandlingstyper.ANMODNING_OM_UNNTAK_HOVEDREGEL &&
+          <Fragment>
+            <Nav.Fieldset legend="Unntak fra lovvalgsland:">
+              <Nav.Row className="">
+                <Nav.Column xs="12">
+                  <LandVelger label="Velg ett land:" feltNavn="journalforingUnntakFraLovvalgsland" multiLand={false} />
+                </Nav.Column>
+              </Nav.Row>
+            </Nav.Fieldset>
+            <Nav.Fieldset legend="Lovvalgsbestemmelse">
+              <Nav.Row className="">
+                <Nav.Column xs="12">
+                  <Skjema.Select label="Artikkelen det gjelder:" feltNavn="journalforingLovvalgsbestemmelse">
+                    { art16.map(kodeObjekt => <option key={Utils._uuid()} value={kodeObjekt.kode}>{kodeObjekt.term}</option>)}
+                  </Skjema.Select>
+                </Nav.Column>
+              </Nav.Row>
+            </Nav.Fieldset>
+            <Nav.Fieldset legend="Unntak fra lovvalgsbestemmelse">
+              <Nav.Row className="">
+                <Nav.Column xs="12">
+                  <Skjema.Select label="Artikkelen det søkes unntak fra:" feltNavn="journalforingUnntakFraLovvalgsbestemmelse">
+                    { alleLovvalg.map(kodeObjekt => <option key={Utils._uuid()} value={kodeObjekt.kode}>{kodeObjekt.term}</option>)}
+                  </Skjema.Select>
+                </Nav.Column>
+              </Nav.Row>
+            </Nav.Fieldset>
+          </Fragment>
+          }
+          <Nav.Fieldset legend="Soknadperiode:" className="opprettnysak__soknadsperiode">
+            <Nav.Row className="">
+              <Nav.Column xs="6">
+                <Skjema.Input datoFelt label="Fra" feltNavn="journalforingPeriodeFraOgMed" />
+              </Nav.Column>
+              <Nav.Column xs="6">
+                <Skjema.Input datoFelt label="Til" feltNavn="journalforingPeriodeTilOgMed" />
+              </Nav.Column>
+            </Nav.Row>
+          </Nav.Fieldset>
+          <Nav.Fieldset legend="Land:">
+            <Nav.Row className="">
+              <Nav.Column xs="12">
+                <LandVelger feltNavn="journalforingSoknadsland" multiLand />
+              </Nav.Column>
+            </Nav.Row>
+          </Nav.Fieldset>
+        </Fragment>
+      }
+    </div>
+  );
+};
+OpprettFagsak.propTypes = {
+  behandlingstyper: PT.arrayOf(MPT.Kodeverk).isRequired,
+  sakstyper: PT.arrayOf(MPT.Kodeverk).isRequired,
+  journalforingSkjemaVerdier: PT.object,
+};
+
+OpprettFagsak.defaultProps = {
+  journalforingSkjemaVerdier: {},
+};
+const mapStateToProps = state => ({
+  journalforingSkjemaVerdier: formSelectors.JournalforingFormSelector(state).values,
+});
+
+const mapDispatchToProps = dispatch => ({
+  settFeltInnhold: (feltNavn, verdi) => dispatch(change('journalforing', feltNavn, verdi)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(OpprettFagsak);
---
+++ src/sider/journalforing/komponenter/sendForvaltningsMelding.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as Nav from '../../../utils/navFrontend';
+
+const SendForvaltningsMelding = () => (
+  <Nav.Element>Skal melding om saksbehandlingtid sendes automatisk?</Nav.Element>
+);
+export default SendForvaltningsMelding;
