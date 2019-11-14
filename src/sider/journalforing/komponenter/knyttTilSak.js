8910a979030691f3aaeb73ec64ad7b01b42a0d8b
Are Bjølseth <are.bjolseth@nav.no>
2019-11-14 17:07:01 +0100

    La til nytt infomationcircle.svg ikon Utvidet Behandlingsstatus med footer panel og radiogruppe og behandlingstype.

--- src/sider/journalforing/komponenter/knyttTilSak.js
+++ src/sider/journalforing/komponenter/knyttTilSak.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { formValueSelector } from 'redux-form';
+import PT from 'prop-types';
+import * as MKV from 'melosys-kodeverk';
 
-const KnyttTilSak = () => {
+import { BOOLSK } from '../../../constants';
+import * as MPT from '../../../proptypes';
+import * as Ikoner from '../../../resources/images';
+import * as Skjema from '../../../felleskomponenter/skjema';
+import { Elementskrift } from './overskrift';
+
+const KnyttTilSak = props => {
+  const { sak, behandlingstyper, opprettBehandling } = props;
+  const { saksstatus } = sak;
   const clsBehandlingsPanel = {
     background: 'lightgray',
     border: '1px solid #b7b1a9',
@@ -8,10 +20,46 @@ const KnyttTilSak = () => {
     margin: '0.5em 0',
     padding: '0.5em',
   };
-
+  const clsElementskrift = {
+    'border-bottom': 'none',
+  };
+  if (saksstatus.kode === MKV.Koder.behandlinger.behandlingsstatus.AVSLUTTET) {
+    return (
+      <div style={clsBehandlingsPanel}>
+        <Elementskrift tekst="Melding om saksbehandlingstid" ikon={Ikoner.InformationCircle} className="elementTittel oversteUndertittel" style={clsElementskrift} />
+        <Skjema.RadioGruppe feltNavn="opprettBehandling" label="Knytt til sak">
+          <Skjema.Radio feltNavn="opprettBehandling" value={BOOLSK.SANN} label="Opprett ny behandling" />
+          <Skjema.Radio feltNavn="opprettBehandling" value={BOOLSK.USANN} label="Uten å opprette behandling" />
+        </Skjema.RadioGruppe>
+        {
+          opprettBehandling() &&
+          <Skjema.Select feltNavn="behandlingstype" bredde="fullbredde" label="Velg behandlingstype" emptyFieldDisabled={false}>
+            {
+              behandlingstyper &&
+              behandlingstyper
+                .filter(elem => elem.kode !== MKV.Koder.behandlinger.behandlingstyper.SOEKNAD
+                  && elem.kode !== MKV.Koder.behandlinger.behandlingstyper.ANMODNING_OM_UNNTAK_HOVEDREGEL
+                  && elem.kode !== MKV.Koder.behandlinger.behandlingstyper.ØVRIGE_SED)
+                .map(elem => <option key={elem.kode} value={elem.kode}>{elem.term}</option>)
+            }
+          </Skjema.Select>
+        }
+      </div>
+    );
+  }
   return (
-    <div style={clsBehandlingsPanel}>Knytt til sak panel</div>
+    <div />
   );
 };
-
-export default KnyttTilSak;
+KnyttTilSak.propTypes = {
+  sak: MPT.Fagsak.isRequired,
+  behandlingstyper: PT.arrayOf(MPT.Kodeverk).isRequired,
+  opprettBehandling: PT.func.isRequired,
+};
+KnyttTilSak.defaultProps = {
+};
+const selector = formValueSelector('journalforing');
+const mapStateToProps = state => ({
+  opprettBehandling: () => selector(state, 'opprettBehandling'),
+});
+export default connect(mapStateToProps)(KnyttTilSak);
