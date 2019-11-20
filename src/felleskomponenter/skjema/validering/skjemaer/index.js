5bae338938471040bf9b4d9ab80fae2726fd6710
JohanSakshaug <johan.sakshauger@nav.no>
2019-11-20 15:01:47 +0100

    Benytt redux form for art 12 vedtaksteg(slipper da en egen komponent for valg av vedtakstype uten reduxform)

--- src/felleskomponenter/skjema/validering/skjemaer/index.js
+++ src/felleskomponenter/skjema/validering/skjemaer/index.js
@@ -1,5 +1,6 @@
 export { lagYupToReduxformErrorMapper } from './lagYupToReduxformErrorMapper';
 export { saksopplysninger } from './saksopplysninger';
+export { artikkel12_vedtak } from './artikkel12vedtak';
 export { avslag_artikkel_12_og_16 } from './avslagartikkel12og16';
 export { artikkel16_motta_svar } from './artikkel16mottasvar';
 export { artikkel16_vedtak } from './artikkel16vedtak';
