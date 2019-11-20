f48eb01c5bcae5203db4bcaf171ea765ffd8e51e
JohanSakshaug <johan.sakshauger@nav.no>
2019-11-20 16:23:59 +0100

    Test vedtaktypeskjema og vedtaktypebegrunnelseskjema

---
+++ src/sider/saksbehandling/komponenter/vedtaktypebegrunnelseskjema.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import * as MKV from 'melosys-kodeverk';
+
+import * as Skjema from '../../../felleskomponenter/skjema';
+
+import VedtaktypebegrunnelseSkjema from './vedtaktypebegrunnelseskjema';
+
+describe('VedtaketypebegrunnelseSkjema', () => {
+  describe('select', () => {
+    let props = null;
+    let select = null;
+
+    beforeEach(() => {
+      props = {
+        className: 'artikkel',
+        redigerbart: true,
+      };
+      const vedtaktypebegrunnelseSkjema = shallow(<VedtaktypebegrunnelseSkjema {...props} />);
+      select = vedtaktypebegrunnelseSkjema.find(Skjema.Select);
+    });
+
+    it('vises', () => {
+      expect(select).toHaveLength(1);
+    });
+
+    it('setter className', () => {
+      expect(select.props().className).toBe(props.className);
+    });
+
+    it('setter disabled', () => {
+      expect(select.props().disabled).toBe(false);
+
+      props.redigerbart = false;
+      select = shallow(<VedtaktypebegrunnelseSkjema {...props} />).find(Skjema.Select);
+
+      expect(select.props().disabled).toBe(true);
+    });
+  })
+});
