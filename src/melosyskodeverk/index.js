b94b3ada4806cf8daee3a7217ac730325619526a
JohanSakshaug <johan.sakshauger@nav.no>
2019-11-21 16:40:20 +0100

    Kan ikke reassigne importert variabel

--- src/melosyskodeverk/index.js
+++ src/melosyskodeverk/index.js
@@ -2,8 +2,12 @@ import * as MKV from 'melosys-kodeverk';
 
 /* Filtrerer bort koder som vi ikke ønsker at bruker skal kunne oppgi, men som backend har behov for. */
 
-MKV.KTObjects.begrunnelser.art12_1_vesentlig_virksomhet = MKV.KTObjects.begrunnelser.art12_1_vesentlig_virksomhet.filter(({ kode }) => (
+const filtrertMKV = { ...MKV };
+
+filtrertMKV.KTObjects.begrunnelser.art12_1_vesentlig_virksomhet = MKV.KTObjects.begrunnelser.art12_1_vesentlig_virksomhet.filter(({ kode }) => (
   kode !== MKV.Koder.begrunnelser.art12_1_vesentlig_virksomhet.KONTRAKTER_IKKE_NORSK_LOV
 ));
+delete filtrertMKV.Koder.begrunnelser.art12_1_vesentlig_virksomhet.KONTRAKTER_IKKE_NORSK_LOV;
+delete filtrertMKV.Terms.begrunnelser.art12_1_vesentlig_virksomhet.KONTRAKTER_IKKE_NORSK_LOV;
 
-export default MKV;
+export default filtrertMKV;
