8be4b47ad9778e08158bd4ab985886667d9338d7
Are Bjølseth <are.bjolseth@nav.no>
2019-12-02 15:25:42 +0100

    Ny oppdatering av css. Fjernet local styles.

--- src/felleskomponenter/skjema/validering/skjemaer/journalforing.js
+++ src/felleskomponenter/skjema/validering/skjemaer/journalforing.js
@@ -24,7 +24,6 @@ const VELG_EN_BESTEMMELSE = 'Velg en bestemmelse.';
 const DATO_MA_VAERE_ETTER_FOM = { melding: 'Dato må være lik eller senere enn fra.' };
 
 const kreverPeriodeOgLand = (journalforingHensikt, behandlingstype) => {
-  console.log(journalforingHensikt, behandlingstype);
   return journalforingHensikt === Konstanter.JOURNALFORING_HENSIKT.OPPRETT && ![
     MKV.Koder.behandlinger.behandlingstyper.ØVRIGE_SED,
     MKV.Koder.behandlinger.behandlingstyper.VURDER_TRYGDETID,
