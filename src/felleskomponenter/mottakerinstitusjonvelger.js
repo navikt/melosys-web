bf2233161aa9921689e406633ba425c100b2f16e
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2019-12-11 14:06:46 +0100

    Skal ikke hente mottakerinstitusjoner dersom norge er søknadsland

--- src/felleskomponenter/mottakerinstitusjonvelger.js
+++ src/felleskomponenter/mottakerinstitusjonvelger.js
@@ -19,8 +19,9 @@ export const MottakerinstitusjonvelgerSchema = ({
   oppdaterKreverMottakerinstitusjon,
   ...rest
 }) => {
-  const hentMottakerinstitusjoner = async () => Api.Eessi.mottakerinstitusjoner.hent(bucType, landkode);
-  const [mottakerinstitusjoner] = useAsyncCallbackState(hentMottakerinstitusjoner, [], Utils.logger.error, [landkode, bucType]);
+  const landkodeIkkeNorge = landkode === 'NO' ? '' : landkode;
+  const hentMottakerinstitusjoner = async () => Api.Eessi.mottakerinstitusjoner.hent(bucType, landkodeIkkeNorge);
+  const [mottakerinstitusjoner] = useAsyncCallbackState(hentMottakerinstitusjoner, [], Utils.logger.error, [landkodeIkkeNorge, bucType]);
 
   useEffect(() => {
     oppdaterKreverMottakerinstitusjon(!Utils._isEmpty(mottakerinstitusjoner));
