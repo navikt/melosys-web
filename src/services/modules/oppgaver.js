0ee28bf2c36773876caec41fd4d9e3bf326a422c
JohanSakshaug <johan.sakshauger@nav.no>
2019-12-13 17:22:23 +0100

    Legg til oppgaver-sok endepunkt

--- src/services/modules/oppgaver.js
+++ src/services/modules/oppgaver.js
@@ -6,3 +6,5 @@ export const oversikt = () => getAsJson(`${API_BASE_URL}${OPPGAVER}/oversikt`);
 export const sendPlukk = oppgave => postAsJson(`${API_BASE_URL}${OPPGAVER}/plukk`, oppgave);
 
 export const tilbakelegg = oppgave => postAsJson(`${API_BASE_URL}${OPPGAVER}/tilbakelegg`, oppgave);
+
+export const sok = fnrDnr => getAsJson(`${API_BASE_URL}${OPPGAVER}/sok?fnr=${fnrDnr}`);
