4b05d444abb26a7ea055d38a8d7233dd638bb9f9
JohanSakshaug <johan.sakshauger@nav.no>
2020-02-04 17:07:26 +0100

    Fjerner kode relatert til form i saksopplysninger.js, fordi formen for søknad er flyttet til Soknadpaneler-komponent.

--- src/sider/saksbehandling/komponenter/saksopplysninger/saksopplysninger.js
+++ src/sider/saksbehandling/komponenter/saksopplysninger/saksopplysninger.js
@@ -2,12 +2,9 @@ import React, { Fragment } from 'react';
 import PT from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { reduxForm } from 'redux-form';
 
 import MKV from '../../../../melosyskodeverk';
 import * as Utils from '../../../../utils';
-import * as KV from '../../../../kodeverk';
-import * as Validering from '../../../../felleskomponenter/skjema/validering';
 import * as MPT from '../../../../proptypes';
 
 import Stegvelger from '../../../../felleskomponenter/stegvelger';
@@ -16,19 +13,15 @@ import Soknadpaneler from '../../../../felleskomponenter/soknadpaneler';
 
 import { fagsakSelectors } from '../../../../ducks/fagsaker';
 import { behandlingerSelectors } from '../../../../ducks/behandlinger';
-import { behandlingsperioderSelectors } from '../../../../ducks/behandlingsperioder';
 import { redigerbartSelectors } from '../../../../ducks/redigerbart';
-import { saksopplysningerSelectors } from '../../../../ducks/saksopplysninger';
 import {
   soknadOperations,
   soknadSelectors,
 } from '../../../../ducks/soknad';
 
 import { avklartefaktaSelectors } from '../../../../ducks/avklartefakta';
-import { vilkarSelectors } from '../../../../ducks/vilkar';
 import { behandlingsresultatSelectors } from '../../../../ducks/behandlingsresultat';
 import { formSelectors } from '../../../../ducks/form';
-import { formatterDatoTilNorsk } from '../../../../utils/dato';
 
 import { stegMap } from '../../stegMap';
 
@@ -134,119 +127,17 @@ Saksopplysninger.defaultProps = {
 const mapStateToProps = state => ({
   redigerbart: redigerbartSelectors.RedigerbartSelector(state),
   avklartefakta: avklartefaktaSelectors.AvklartefaktaSelector(state),
-  oppfriskning: saksopplysningerSelectors.SaksopplysningerSelector(state),
   fagsakStatusKode: fagsakSelectors.FagsakStatusSelector(state),
   bekreftelser: behandlingerSelectors.BekreftelserSelector(state),
   behandlingsresultat: behandlingsresultatSelectors.BehandlingsresultatSelector(state),
   soknad: soknadSelectors.SoknadSelector(state),
-  forretningsValidering: formSelectors.ForretningsValideringSelector(state),
   soknadForm: formSelectors.SoknadenFormSelector(state),
   inngangForm: formSelectors.InngangFormSelector(state),
   oppgittAdresseHarVerdier: formSelectors.OppgittAdresseHarVerdierSelector(state),
-  initialValues: {
-    utenlandskIdent: soknadSelectors.PersonOpplysningerSelector(state).utenlandskIdent,
-    medfolgendeFamilie: soknadSelectors.PersonOpplysningerSelector(state).medfolgendeFamilie,
-    medfolgendeAndre: soknadSelectors.PersonOpplysningerSelector(state).medfolgendeAndre,
-    inntektNorskIPerioden: soknadSelectors.ArbeidsinntektSelector(state).inntektNorskIPerioden,
-    inntektUtenlandskIPerioden: soknadSelectors.ArbeidsinntektSelector(state).inntektUtenlandskIPerioden,
-    inntektNaturalFribolig: soknadSelectors.ArbeidsinntektNaturalytelserSelector(state).friBil,
-    inntektNaturalFribil: soknadSelectors.ArbeidsinntektNaturalytelserSelector(state).friBolig,
-    inntektNaturalIAnnet: soknadSelectors.ArbeidsinntektNaturalytelserSelector(state).friAnnet,
-    inntektErInnrapporteringspliktig: soknadSelectors.ArbeidsinntektSelector(state).inntektErInnrapporteringspliktig,
-    inntektTrygdeavgiftBlirTrukket: soknadSelectors.ArbeidsinntektSelector(state).inntektTrygdeavgiftBlirTrukket,
-    arbeidsgiverBekrefterUtsendelse: soknadSelectors.ArbeidsgiversBekreftelseSelector(state).arbeidsgiverBekrefterUtsendelse,
-    arbeidstakerAnsattUnderUtsendelsen: soknadSelectors.ArbeidsgiversBekreftelseSelector(state).arbeidstakerAnsattUnderUtsendelsen,
-    erstatterArbeidstakerenUtsendte: soknadSelectors.ArbeidsgiversBekreftelseSelector(state).erstatterArbeidstakerenUtsendte,
-    arbeidstakerTidligereUtsendt24Mnd: soknadSelectors.ArbeidsgiversBekreftelseSelector(state).arbeidstakerTidligereUtsendt24Mnd,
-    arbeidsgiverBetalerArbeidsgiveravgift: soknadSelectors.ArbeidsgiversBekreftelseSelector(state).arbeidsgiverBetalerArbeidsgiveravgift,
-    trygdeavgiftTrukketGjennomSkatt: soknadSelectors.ArbeidsgiversBekreftelseSelector(state).trygdeavgiftTrukketGjennomSkatt,
-    trygdeavgiftTrukketGjennomSkattDato: formatterDatoTilNorsk(soknadSelectors.ArbeidsgiversBekreftelseSelector(state).trygdeavgiftTrukketGjennomSkattDato),
-    oppgittAdresseGatenavn: soknadSelectors.BostedAdresseSelector(state).gatenavn,
-    oppgittAdresseHusnummer: soknadSelectors.BostedAdresseSelector(state).husnummer,
-    oppgittAdresseRegion: soknadSelectors.BostedAdresseSelector(state).region,
-    oppgittAdressePostnummer: soknadSelectors.BostedAdresseSelector(state).postnummer,
-    oppgittAdressePoststed: soknadSelectors.BostedAdresseSelector(state).poststed,
-    oppgittAdresseLand: soknadSelectors.BostedAdresseSelector(state).landkode,
-    utsendteNeste12Mnd: Math.trunc(soknadSelectors.JuridiskArbeidsgiverNorgeSelector(state).utsendteNeste12Mnd) || null,
-    antallAdmAnsatte: Math.trunc(soknadSelectors.JuridiskArbeidsgiverNorgeSelector(state).antallAdmAnsatte) || null,
-    antallAnsatte: Math.trunc(soknadSelectors.JuridiskArbeidsgiverNorgeSelector(state).antallAnsatte) || null,
-    andelOmsetningINorge: Math.round(soknadSelectors.JuridiskArbeidsgiverNorgeSelector(state).andelOmsetningINorge) || null,
-    andelOppdragINorge: Math.round(soknadSelectors.JuridiskArbeidsgiverNorgeSelector(state).andelOppdragINorge) || null,
-    andelKontrakterINorge: Math.round(soknadSelectors.JuridiskArbeidsgiverNorgeSelector(state).andelKontrakterINorge) || null,
-    arbeidstakereRekruttertILand: soknadSelectors.JuridiskArbeidsgiverNorgeSelector(state).arbeidstakereRekruttertILand,
-    ekstraArbeidsgivere: soknadSelectors.JuridiskArbeidsgiverNorgeSelector(state).ekstraArbeidsgivere,
-    oppholdUtlandFom: formatterDatoTilNorsk(soknadSelectors.OppholdUtlandPeriodeSelector(state).fom),
-    oppholdUtlandTom: formatterDatoTilNorsk(soknadSelectors.OppholdUtlandPeriodeSelector(state).tom),
-    oppholdsland: soknadSelectors.OppholdUtlandSelector(state).oppholdslandkoder,
-    arbeidUtland: soknadSelectors.ArbeidUtlandSelector(state),
-    ektefelleEllerBarnINorge: soknadSelectors.OppholdUtlandSelector(state).ektefelleEllerBarnINorge,
-    studentSemester: soknadSelectors.OppholdUtlandSelector(state).studentSemester,
-    erSelvstendig: soknadSelectors.SelvstendigArbeidSelector(state).erSelvstendig,
-    selvstendigForetak: soknadSelectors.SelvstendigArbeidSelector(state).selvstendigForetak,
-    antallMaanederINorge: soknadSelectors.BostedSelector(state).antallMaanederINorge,
-    EOSBarnetrygdFraNAV: soknadSelectors.BostedSelector(state).EOSBarnetrygdFraNAV,
-    maritimtArbeid: soknadSelectors.MaritimtArbeidSelector(state),
-    soknadsperiodeFom: formatterDatoTilNorsk(soknadSelectors.SoknadsperiodeSelector(state).fom),
-    soknadsperiodeTom: formatterDatoTilNorsk(soknadSelectors.SoknadsperiodeSelector(state).tom),
-    foretakUtland: soknadSelectors.ForetakUtlandSelector(state),
-    kontaktNavn: soknadSelectors.ArbeidNorgeSelector(state).kontaktNavn,
-    kontaktEpost: soknadSelectors.ArbeidNorgeSelector(state).kontaktEpost,
-    fullmektigFirma: soknadSelectors.ArbeidNorgeSelector(state).fullmektigFirma,
-    fullmektigGateadresse: soknadSelectors.ArbeidNorgeSelector(state).fullmektigGateadresse,
-    fullmektigPostnr: soknadSelectors.ArbeidNorgeSelector(state).fullmektigPostnr,
-    fullmektigPoststed: soknadSelectors.ArbeidNorgeSelector(state).fullmektigPoststed,
-    fullmektigRegion: soknadSelectors.ArbeidNorgeSelector(state).fullmektigRegion,
-    fullmektigLand: soknadSelectors.ArbeidNorgeSelector(state).fullmektigLandkode,
-    tidligeremedlemskap: behandlingsperioderSelectors.tidligereMedlemskap(state),
-    avklartefakta: {
-      soknadsland: avklartefaktaSelectors.Soknadsland(state),
-      yrkesgruppe: avklartefaktaSelectors.Yrkesgruppe(state),
-      yrkesaktivitetAntallLand: avklartefaktaSelectors.YrkesaktivitetAntallLand(state),
-      yrkesaktivitet: avklartefaktaSelectors.Yrkesaktivitet(state),
-      sokkelSkipKonklusjon: avklartefaktaSelectors.ArbeidSokkelSkipSelector(state),
-    },
-    vilkar: {
-      vesentligVirksomhet: (vilkarSelectors.vesentligVirksomhetSelector(state).oppfylt),
-      vesentligVirksomhetBegrunnelser: (vilkarSelectors.vesentligVirksomhetSelector(state).begrunnelseKoder),
-      normaltDriverVirksomhet: (vilkarSelectors.normaltDriverVirksomhetSelector(state).oppfylt),
-      normaltDriverVirksomhetBegrunnelser: (vilkarSelectors.normaltDriverVirksomhetSelector(state).begrunnelseKoder),
-      forutgaendeMedlemskap: (vilkarSelectors.forutgaendeMedlemskap(state).oppfylt),
-      forutgaendeMedlemskapBegrunnelser: (vilkarSelectors.forutgaendeMedlemskap(state).begrunnelseKoder),
-      art11_3A: vilkarSelectors.art11_3A(state).oppfylt,
-      art11_4_1: vilkarSelectors.art11_4_1(state).oppfylt,
-      art11_4_2: vilkarSelectors.art11_4_2(state).oppfylt,
-      nis: vilkarSelectors.nis(state).oppfylt,
-      art12_1: vilkarSelectors.art12_1(state).oppfylt,
-      art12_1_begrunnelser: vilkarSelectors.art12_1(state).begrunnelseKoder,
-      art12_2: vilkarSelectors.art12_2(state).oppfylt,
-      art12_2_begrunnelser: vilkarSelectors.art12_2(state).begrunnelseKoder,
-      art16_1: vilkarSelectors.art16_1(state).oppfylt,
-      art16_1_begrunnelser: vilkarSelectors.art16_1(state).begrunnelseKoder,
-    },
-    vurderingLovvalg: avklartefaktaSelectors.AvklartefaktaLovvalgKodeSelector(state),
-    vurderingBegrunnelser: avklartefaktaSelectors.AvklartefaktaVurderingSelector(state).begrunnelser,
-  },
 });
 
 const mapDispatchToProps = dispatch => ({
   oppdaterSoknad: () => dispatch(soknadOperations.oppdaterSoknadState()),
 });
 
-const SaksopplysningerForm = reduxForm({
-  form: KV.Form.SOKNAD,
-  enableReinitialize: true,
-  destroyOnUnmount: true,
-  keepDirtyOnReinitialize: true,
-  updateUnregisteredFields: true,
-  validate: (values, props) => {
-    const settings = {
-      context: {
-        skalOppgittAdresseValideres: props.oppgittAdresseHarVerdier,
-      },
-    };
-
-    return Validering.Skjemaer.lagYupToReduxformErrorMapper(Validering.Skjemaer.saksopplysninger, settings)(values);
-  },
-})(Saksopplysninger);
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SaksopplysningerForm));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Saksopplysninger));
