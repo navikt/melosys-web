8a0dbb27d580c780bf0fef7d0e5c622698761b2a
JohanSakshaug <johan.sakshauger@nav.no>
2020-02-11 16:47:36 +0100

    Burde ikke teste antall oppgaver som eksisterer uten å spesifisere mockdata/fixture i testen.

--- cypress/integration/melosys_web/forside_spec.js
+++ cypress/integration/melosys_web/forside_spec.js
@@ -3,13 +3,7 @@ describe('Forsiden', () => {
     cy.visit('/');
   });
   it('Laste startsiden', () => {
-    cy.get('.journalOppgave__link')
-      .should('have.length', 3);
-    cy.get('.behandlingOppgave__link')
-      .should('have.length', 5);
     cy.get('[name="behandlingsortering"]:checked')
       .should('have.value', 'descending');
-
-    cy.wait(2000);
   });
 });
