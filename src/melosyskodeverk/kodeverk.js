62b52a6aa0748b9095374d3e112cc2eb77f43c6d
JohanSakshaug <johan.sakshauger@nav.no>
2020-02-13 15:21:23 +0100

    Refaktor filtrering av MKV og filtrer begrunnelse ENDRINGER_ARBEIDSITUASJON

---
+++ src/melosyskodeverk/kodeverk.js
@@ -0,0 +1,18 @@
+import { unset, get, set } from 'lodash';
+
+/* eslint-disable no-param-reassign */
+const fjernKode = (kodeverk, path, kode) => {
+  set(kodeverk.KTObjects, path, get(kodeverk.KTObjects, path).filter(KTObject => KTObject.kode !== kode));
+  const pathMedKode = `${path}.${kode}`;
+  unset(kodeverk.Koder, pathMedKode);
+  unset(kodeverk.Terms, pathMedKode);
+};
+
+const fjernFlereKoder = (kodeverk, paths) => {
+  const rensetKodeverk = { ...kodeverk };
+  paths.forEach(({ path, kode }) => fjernKode(rensetKodeverk, path, kode));
+
+  return rensetKodeverk;
+};
+
+export { fjernFlereKoder };
---
+++ src/melosyskodeverk/kodeverk.test.js
@@ -0,0 +1,13 @@
+import * as MKV from 'melosys-kodeverk';
+
+import { fjernFlereKoder } from './kodeverk';
+
+describe('fjernFlereKoder', () => {
+  it('fjerner koder', () => {
+    const rensetKodeverk = fjernFlereKoder(MKV, [{ path: 'begrunnelser.art12_1_vesentlig_virksomhet', kode: 'KONTRAKTER_IKKE_NORSK_LOV' }]);
+
+    expect(rensetKodeverk.Koder.begrunnelser.art12_1_vesentlig_virksomhet.KONTRAKTER_IKKE_NORSK_LOV).toBeUndefined();
+    expect(rensetKodeverk.Terms.begrunnelser.art12_1_vesentlig_virksomhet.KONTRAKTER_IKKE_NORSK_LOV).toBeUndefined();
+    expect(rensetKodeverk.KTObjects.begrunnelser.art12_1_vesentlig_virksomhet.find(kode => kode === MKV.Koder.begrunnelser.art12_1_vesentlig_virksomhet.KONTRAKTER_IKKE_NORSK_LOV)).toBeUndefined();
+  });
+});
