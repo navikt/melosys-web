55b7928d7493c7a556f1ea30e13778041944b8e6
JohanSakshaug <johan.sakshauger@nav.no>
2020-02-24 14:17:19 +0100

    Ikke hent behandlingsgrunnlag registrering av unntaksperioder og registrering av annmodning om unntak. (A001 & A009)

--- src/ducks/datalasting/operations.js
+++ src/ducks/datalasting/operations.js
@@ -36,7 +36,6 @@ export const lastInnSaksopplysningerSedBehandling = (saksnummer, behandlingID) =
     try {
       dispatch(fagsakOperations.hent(saksnummer));
       dispatch(behandlingerOperations.hentBehandling(behandlingID));
-      dispatch(behandlingsgrunnlagOperations.hent(behandlingID));
       dispatch(behandlingsresultatOperations.hent(behandlingID));
       dispatch(avklartefaktaOperations.hent(behandlingID));
       dispatch(vilkarOperations.hent(behandlingID));
@@ -52,7 +51,6 @@ export const lastInnSaksopplysningerBehandleMottattAOU = (behandlingID, anmodnin
   dispatch => {
     try {
       dispatch(behandlingerOperations.hentBehandling(behandlingID));
-      dispatch(behandlingsgrunnlagOperations.hent(behandlingID));
       dispatch(anmodningsperioderOperations.hent(behandlingID));
       dispatch(anmodningsperiodesvarOperations.hent(anmodningsperiodeID));
     } catch (e) {
