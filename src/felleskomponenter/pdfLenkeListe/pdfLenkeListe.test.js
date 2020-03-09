b40569e4ea2c3aec2dee244a0f58bd6d893218a6
JohanSakshaug <johan.sakshauger@nav.no>
2020-03-09 16:53:08 +0100

    Forhåndsvisning av brev og SED: Vis en generisk feilmelding dersom status er fra og med 500 og opp. Vis feilmelding fra payload dersom status er fra og med 400 til 500.

--- src/felleskomponenter/pdfLenkeListe/pdfLenkeListe.test.js
+++ src/felleskomponenter/pdfLenkeListe/pdfLenkeListe.test.js
@@ -1,18 +1,43 @@
 import React from 'react';
+import * as EKV from 'eessi-kodeverk';
+
+import * as Nav from '../../utils/navFrontend';
+import * as Utils from '../../utils';
+
 import PdfLenkeListe from '.';
+import MKV from '../../melosyskodeverk';
 
 describe('PdfLenkeListe', () => {
-  const vedKlikk = jest.fn();
+  const vedKlikk = jest.fn(() => true);
   let props = null;
 
-  beforeEach(() => {
+  const setup = () => {
     fetch.resetMocks();
     fetch.mockResponse(JSON.stringify({}));
     props = {
       behandlingID: 1,
-      dokumenter: [],
+      dokumenter: [
+        {
+          navn: 'Forhåndsvis vedtaksbrev',
+          type: MKV.Koder.brev.produserbaredokumenter.AVSLAG_MANGLENDE_OPPLYSNINGER,
+          data: {
+            begrunnelseKode: MKV.Koder.begrunnelser.folketrygdloven.avslag.MANGLENDE_OPPLYSNINGER,
+            fritekst: null,
+            mottaker: MKV.Koder.aktoersroller.BRUKER,
+          },
+        },
+        {
+          navn: 'Forhåndsvis SED A003',
+          type: EKV.Koder.sedtyper.A003,
+          erSed: true,
+        },
+      ],
       vedKlikk,
     };
+  };
+
+  beforeEach(() => {
+    setup();
   });
 
   it('viser samme antall linker som antall dokumenter passet som props ', () => {
@@ -27,5 +52,81 @@ describe('PdfLenkeListe', () => {
     ];
     liste = shallow(<PdfLenkeListe {...props} />);
     expect(liste.find('button')).toHaveLength(props.dokumenter.length);
+  });
+
+  describe('forhåndsvisning av brev', () => {
+    it('viser feilmelding ved 400-feil fra backend', async () => {
+      const liste = shallow(<PdfLenkeListe {...props} />);
+
+      const responseBody = {
+        error: null,
+        status: 400,
+        message: 'feilmelding',
+      };
+      fetch.mockResponse(JSON.stringify(responseBody), { status: 400 });
+
+      const lenke = liste.find('button').first();
+      lenke.simulate('click');
+      await Utils.delay(0);
+
+      const alertstripe = liste.find(Nav.AlertStripe);
+      expect(alertstripe.props().children).toBe(responseBody.message);
+    });
+
+    it('viser feilmelding ved 500-feil fra backend', async () => {
+      const liste = shallow(<PdfLenkeListe {...props} />);
+
+      const responseBody = {
+        error: null,
+        status: 500,
+        message: 'feilmelding',
+      };
+      fetch.mockResponse(JSON.stringify(responseBody), { status: 500 });
+
+      const lenke = liste.find('button').first();
+      lenke.simulate('click');
+      await Utils.delay(0);
+
+      const alertstripe = liste.find(Nav.AlertStripe);
+      expect(alertstripe.props().children).toBe('Det oppstod en feil da brevet skulle forhåndsvises!');
+    });
+  });
+
+  describe('forhåndsvisning av sed', () => {
+    it('viser feilmelding ved 400-feil fra backend', async () => {
+      const liste = shallow(<PdfLenkeListe {...props} />);
+
+      const responseBody = {
+        error: null,
+        status: 400,
+        message: 'feilmelding',
+      };
+      fetch.mockResponse(JSON.stringify(responseBody), { status: 400 });
+
+      const lenke = liste.find('button').last();
+      lenke.simulate('click');
+      await Utils.delay(0);
+
+      const alertstripe = liste.find(Nav.AlertStripe);
+      expect(alertstripe.props().children).toBe(responseBody.message);
+    });
+
+    it('viser feilmelding ved 500-feil fra backend', async () => {
+      const liste = shallow(<PdfLenkeListe {...props} />);
+
+      const boresponseBody = {
+        error: null,
+        status: 500,
+        message: 'feilmelding',
+      };
+      fetch.mockResponse(JSON.stringify(boresponseBody), { status: 500 });
+
+      const lenke = liste.find('button').last();
+      lenke.simulate('click');
+      await Utils.delay(0);
+
+      const alertstripe = liste.find(Nav.AlertStripe);
+      expect(alertstripe.props().children).toBe('Det oppstod en feil da SED skulle forhåndsvises!');
+    });
   });
 });
