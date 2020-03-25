e1f2a6f6c2be1484518082be81d9e96edd335841
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2020-03-25 15:10:28 +0100

    filtrering og rekkefølge av unntaksbestemmelser

--- src/melosyskodeverk/kodekombinasjoner.js
+++ src/melosyskodeverk/kodekombinasjoner.js
@@ -1,3 +1,4 @@
+import { uniqBy } from 'lodash';
 import MKV from './filtrertmelosyskodeverk';
 
 export const alleLovvalg = [
@@ -5,3 +6,25 @@ export const alleLovvalg = [
   ...MKV.KTObjects.lovvalgsbestemmelser.lovvalgbestemmelser_987_2009,
   ...MKV.KTObjects.lovvalgsbestemmelser.tilleggsbestemmelser_883_2004,
 ];
+
+const bestemmelserIkkeRelevanteForUnntak = [
+  MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART11_1,
+  MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART11_3B,
+  MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART11_3C,
+  MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART12_1,
+  MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART12_2,
+  MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART15,
+  MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART16_1,
+  MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART16_2,
+  MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ANNET,
+  MKV.Koder.lovvalgsbestemmelser.tilleggsbestemmelser_883_2004.FO_883_2004_ART87_8,
+  MKV.Koder.lovvalgsbestemmelser.tilleggsbestemmelser_883_2004.FO_883_2004_ART87A,
+  MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_987_2009.FO_987_2009_ART14_11,
+];
+
+const kodeverkComparator = ({ kode: k1 }, { kode: k2 }) => k1.localeCompare(k2);
+
+export const unntaksbestemmelser = uniqBy(
+  [...alleLovvalg].filter(({ kode }) => !bestemmelserIkkeRelevanteForUnntak.includes(kode)),
+  ({ kode }) => kode
+).sort(kodeverkComparator);
