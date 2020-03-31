09a98efc099337e8425ab8b0b3590c0f01e89073
JohanSakshaug <johan.sakshauger@nav.no>
2020-03-31 09:37:12 +0200

    Gjør Unntaksperioder.godkjenn til POST, da den sender body

--- src/services/modules/saksflyt/unntaksperioder.js
+++ src/services/modules/saksflyt/unntaksperioder.js
@@ -1,8 +1,8 @@
-import { postAsJson, putAsText, putAsJson } from '../../utils';
+import { postAsJson, putAsText } from '../../utils';
 import { API_BASE_URL, SAKSFLYT, UNNTAKSPERIODER } from '../../api-constants';
 
 // eslint-disable-next-line import/prefer-default-export
-export const godkjenn = (behandlingID, data = {}) => putAsJson(`${API_BASE_URL}${SAKSFLYT}/${UNNTAKSPERIODER}/${behandlingID}/godkjenn`, data);
+export const godkjenn = (behandlingID, data = {}) => postAsJson(`${API_BASE_URL}${SAKSFLYT}/${UNNTAKSPERIODER}/${behandlingID}/godkjenn`, data);
 export const innhentinfo = behandlingID => putAsText(`${API_BASE_URL}${SAKSFLYT}/${UNNTAKSPERIODER}/${behandlingID}/innhentinfo`);
 
 export const ikkegodkjenn = (behandlingID, data) => postAsJson(`${API_BASE_URL}${SAKSFLYT}/${UNNTAKSPERIODER}/${behandlingID}/ikkegodkjenn`, data);
