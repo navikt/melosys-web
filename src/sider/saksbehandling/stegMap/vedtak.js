d3b2484ebaadb7df55ba4645fc77093e19b13bbb
JohanSakshaug <johan.sakshauger@nav.no>
2020-04-07 15:24:44 +0200

    Fiks bug ved sjekk på behandlingstype

--- src/sider/saksbehandling/stegMap/vedtak.js
+++ src/sider/saksbehandling/stegMap/vedtak.js
@@ -20,7 +20,7 @@ class Vedtak extends Steg {
     this.komponent = VurderingVedtak;
     this.samleRelevanteData = _propsLight => {
       const formValues = _propsLight.artikkel12_vedtak_skjema;
-      const erNyVurdering = _propsLight.behandlingstype === MKV.Koder.behandlinger.behandlingstyper.NY_VURDERING;
+      const erNyVurdering = _propsLight.behandlingstype.kode === MKV.Koder.behandlinger.behandlingstyper.NY_VURDERING;
       const lovvalgSomKodeTerm = KV.finnEnkeltKodeFraListe(_propsLight.lovvalgsbestemmelse, MKV.Kodekombinasjoner.alleLovvalg);
 
       const pdfDokumenter = [
