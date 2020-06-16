15ced65e485f7f2e024026cd36ba173b68be10a7
JohanSakshaug <johan.sakshaug@nav.no>
2020-06-16 16:42:22 +0200

    test om dette bygger

--- scripts/patchnodelocal.js
+++ scripts/patchnodelocal.js
@@ -1,26 +1,26 @@
-const writePkg = require('write-pkg');
-const readPkg = require('read-pkg');
+// const writePkg = require('write-pkg');
+// const readPkg = require('read-pkg');
 
 const DEFAULT_NODE_MOCK_SERVER = 'http://localhost:3002';
 
 // Read package.json into js map.
-const pkg = readPkg.sync();
-const { proxy } = pkg;
+// const pkg = readPkg.sync();
+// const { proxy } = pkg;
 
 const patchPackageNodeLocal = () => {
-  delete pkg._id;
+  // delete pkg._id;
 
-  if (proxy['/api'].target !== DEFAULT_NODE_MOCK_SERVER) {
-    proxy['/api'].target = DEFAULT_NODE_MOCK_SERVER;
-  }
-  if (proxy['/frontendlogger'].target !== DEFAULT_NODE_MOCK_SERVER) {
-    proxy['/frontendlogger'].target = DEFAULT_NODE_MOCK_SERVER;
-  }
+  // if (proxy['/api'].target !== DEFAULT_NODE_MOCK_SERVER) {
+  //   proxy['/api'].target = DEFAULT_NODE_MOCK_SERVER;
+  // }
+  // if (proxy['/frontendlogger'].target !== DEFAULT_NODE_MOCK_SERVER) {
+  //   proxy['/frontendlogger'].target = DEFAULT_NODE_MOCK_SERVER;
+  // }
   // get value of 'disabled-hompage'
-  if (!pkg['disabled-homepage']) {
-    pkg['disabled-homepage'] = 'http://eesi2.no/melosys';
-  }
-  writePkg.sync(pkg);
+  // if (!pkg['disabled-homepage']) {
+  //   pkg['disabled-homepage'] = 'http://eesi2.no/melosys';
+  // }
+  // writePkg.sync(pkg);
 };
 patchPackageNodeLocal();
 
