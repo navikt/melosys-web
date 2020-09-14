b0ed8a49374cd9f8449db5b23ffcfdc42d340e10
JohanSakshaug <johan.sakshaug@nav.no>
2020-09-14 14:55:34 +0200

    Legg til types

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,2 +1,5 @@
 export type { default as AnmodningOmUnntakBestilling } from './anmodningomunntakbestilling';
+export type { default as Fagsak } from './fagsak';
+export type { default as Oppsummering } from './oppsummering';
+export type { default as Person } from './person';
 export type { default as Videresending } from './videresending';
--- src/global.d.ts
+++ src/global.d.ts
@@ -12,6 +12,9 @@ declare module 'AppTypes' {
 
 declare module 'Domene' {
   export type AnmodningOmUnntakBestilling = import('./@types').AnmodningOmUnntakBestilling;
+  export type Fagsak = import('./@types').Fagsak;
+  export type Oppsummering = import('./@types').Oppsummering;
+  export type Person = import('./@types').Person;
   export type Videresending = import('./@types').Videresending;
 }
 
