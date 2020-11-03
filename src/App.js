5b06d80539530cb1ee72e0673a052da9c60841a1
JohanSakshaug <johan.sakshaug@nav.no>
2020-11-03 16:08:44 +0100

    Fjern komponent for visning av serverinfo

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import PT from 'prop-types';
 
 import Rammeverk from './sider/rammeverk';
 
-import Versjon from './debug/versjon';
-
 export function App({
   children,
 }) {
   return (
     <div className="App">
       <Rammeverk>{children}</Rammeverk>
-      <Versjon />
     </div>
   );
 }
