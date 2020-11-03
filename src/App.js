bcf42d61dda3b13cff0a67c20f3aba77276ba7f2
JohanSakshaug <johan.sakshaug@nav.no>
2020-11-03 15:05:27 +0100

    Fjern duck for serverinfo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import PT from 'prop-types';
 
 import Rammeverk from './sider/rammeverk';
 
 import Versjon from './debug/versjon';
 
-import { datalastingOperations } from './ducks/datalasting';
-
 export function App({
   children,
-  lastAppdata,
 }) {
-  useEffect(() => {
-    lastAppdata();
-  }, []);
-
   return (
     <div className="App">
       <Rammeverk>{children}</Rammeverk>
@@ -26,15 +18,10 @@ export function App({
 
 App.propTypes = {
   children: PT.node,
-  lastAppdata: PT.func.isRequired,
 };
 
 App.defaultProps = {
   children: undefined,
 };
 
-const mapDispatchToProps = dispatch => ({
-  lastAppdata: () => dispatch(datalastingOperations.lastAppdata()),
-});
-
-export default connect(null, mapDispatchToProps)(App);
+export default App;
--- src/ducks/serverinfo/types.js
+++
@@ -1,3 +0,0 @@
-export const OK = 'serverinfo/OK';
-export const FEILET = 'serverinfo/FEILET';
-export const PENDING = 'serverinfo/PENDING';
