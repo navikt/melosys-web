96d64e7844c9b1b18ad90e4a6830d33e18e42a60
janitamarielle <janita.marielle.johansen@nav.no>
2020-11-11 14:19:33 +0100

    url fiks: EU_EØS før sedbehandling og registrering også

--- src/routing/url.ts
+++ src/routing/url.ts
@@ -5,9 +5,9 @@ export const lagUrl = (saksnummer: number, behandlingID: number, behandlingstema
   switch (behandlingstemaKode) {
     case MKV.Koder.behandlinger.behandlingstema.REGISTRERING_UNNTAK_NORSK_TRYGD_UTSTASJONERING:
     case MKV.Koder.behandlinger.behandlingstema.REGISTRERING_UNNTAK_NORSK_TRYGD_ØVRIGE:
-      return `/registrering/${saksnummer}/unntaksperioder/?behandlingID=${behandlingID}`;
+      return `/${MKV.Koder.sakstyper.EU_EOS}/registrering/${saksnummer}/unntaksperioder/?behandlingID=${behandlingID}`;
     case MKV.Koder.behandlinger.behandlingstema.ANMODNING_OM_UNNTAK_HOVEDREGEL:
-      return `/registrering/${saksnummer}/anmodningunntak/?behandlingID=${behandlingID}`;
+      return `/${MKV.Koder.sakstyper.EU_EOS}/registrering/${saksnummer}/anmodningunntak/?behandlingID=${behandlingID}`;
     case MKV.Koder.behandlinger.behandlingstema.UTSENDT_ARBEIDSTAKER:
     case MKV.Koder.behandlinger.behandlingstema.UTSENDT_SELVSTENDIG:
     case MKV.Koder.behandlinger.behandlingstema.ARBEID_ETT_LAND_ØVRIG:
@@ -18,7 +18,7 @@ export const lagUrl = (saksnummer: number, behandlingID: number, behandlingstema
     case MKV.Koder.behandlinger.behandlingstema.TRYGDETID:
     case MKV.Koder.behandlinger.behandlingstema.ØVRIGE_SED_MED:
     case MKV.Koder.behandlinger.behandlingstema.ØVRIGE_SED_UFM:
-      return `/sedbehandling/${saksnummer}/?behandlingID=${behandlingID}`;
+      return `/${MKV.Koder.sakstyper.EU_EOS}/sedbehandling/${saksnummer}/?behandlingID=${behandlingID}`;
     case MKV.Koder.behandlinger.behandlingstema.BESLUTNING_LOVVALG_NORGE:
     case MKV.Koder.behandlinger.behandlingstema.BESLUTNING_LOVVALG_ANNET_LAND:
       return `/vurderutpeking/${saksnummer}/?behandlingID=${behandlingID}`;
