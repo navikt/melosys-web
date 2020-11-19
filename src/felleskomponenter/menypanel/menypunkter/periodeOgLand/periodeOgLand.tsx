c2d5f59cf22c6754b496238cf8ec1feb4c6a4d24
JohanSakshaug <johan.sakshaug@nav.no>
2020-11-19 10:00:53 +0100

    Vis søknadsland i menypanel

--- src/felleskomponenter/menypanel/menypunkter/periodeOgLand/periodeOgLand.tsx
+++ src/felleskomponenter/menypanel/menypunkter/periodeOgLand/periodeOgLand.tsx
@@ -1,22 +1,39 @@
 import React from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import { RootState } from 'AppTypes';
 
 import * as Nav from '../../../../utils/navFrontend';
 import * as Etiketter from '../etiketter';
 import * as KV from '../../../../kodeverk';
+import * as Utils from '../../../../utils';
+
+import MKV from '../../../../melosyskodeverk';
+
+import { behandlingsgrunnlagSelectors } from '../../../../ducks/behandlingsgrunnlag';
 
 import Soknadsperiode from './soknadsperiode';
 
-interface PeriodeOgLandProps {
+const mapStateToProps = (state: RootState) => ({
+  soknadsland: behandlingsgrunnlagSelectors.SoknadslandSelector(state),
+});
+
+const connector = connect(mapStateToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+type PeriodeOgLandProps = PropsFromRedux & {
   visArbeidsforholdRolleEtiketter: boolean,
   redigerbart: boolean,
   lagreSoknadOgOppfriskSaksopplysninger: () => void,
-}
+};
 
 const PeriodeOgLand = ({
   visArbeidsforholdRolleEtiketter,
   redigerbart,
   lagreSoknadOgOppfriskSaksopplysninger,
+  soknadsland,
 }: PeriodeOgLandProps) => {
+  const soknadslandTerms = soknadsland.map((land: string) => KV.kodeTilTerm(land, MKV.KTObjects.landkoder));
+
   return (
     <div>
       <div style={{ marginBottom: '1em' }}>
@@ -34,9 +51,13 @@ const PeriodeOgLand = ({
             lagreSoknadOgOppfriskSaksopplysninger={lagreSoknadOgOppfriskSaksopplysninger}
           />
         </Nav.Column>
+        <Nav.Column xs="6">
+          <Nav.typo.EtikettLiten>Land</Nav.typo.EtikettLiten>
+          { Utils.streng.arrayTilKonjunksjon(soknadslandTerms) || 'Ingen søknadsland' }
+        </Nav.Column>
       </Nav.Row>
     </div>
   );
 };
 
-export default PeriodeOgLand;
+export default connector(PeriodeOgLand);
