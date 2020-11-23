866c32134961e02b896a1f98d7bd0c581ff6343c
Eivind Nilsbakken <eivind.nilsbakken@nav.no>
2020-11-23 15:56:09 +0100

    Bruk bibliotek for å bygge opp query params + riktig ikon for kopiering av fnr

--- src/services/modules/saksopplysninger.js
+++ src/services/modules/saksopplysninger.js
@@ -1,8 +1,10 @@
+import qs from 'qs';
+
 import { getAsJson } from '../utils';
 import { API_BASE_URL, SAKSOPPLYSNINGER } from '../api-constants';
 
 export const oppfrisk = (behandlingID, options = {}) => {
-  const params = Object.keys(options).map(key => `${key}=${options[key]}`).join('&');
+  const params = qs.stringify(options);
   return getAsJson(`${API_BASE_URL}${SAKSOPPLYSNINGER}/oppfriskning/${behandlingID}${params ? `?${params}` : ''}`);
 };
 
