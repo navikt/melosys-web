2b078ee74f0ee42bb059d1150011754a153df940
JohanSakshaug <johan.sakshaug@nav.no>
2020-12-05 19:44:22 +0100

    Vis melding dersom ingen medfølgende barn er oppgitt i søknaden

---
+++ src/felleskomponenter/menypanel/menypunkter/familieforhold/medfolgendeBarn/ingenData.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+import * as Nav from '../../../../../utils/navFrontend';
+
+const IngenData: FunctionComponent = () => <Nav.typo.EtikettLiten>Ingen barn oppgitt i søknaden.</Nav.typo.EtikettLiten>;
+
+export default IngenData;
