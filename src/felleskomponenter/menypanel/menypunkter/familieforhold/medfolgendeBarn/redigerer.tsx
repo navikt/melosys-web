5119b0ddb85d221e7a48eb15fd9d2e800549971c
JohanSakshaug <johan.sakshaug@nav.no>
2020-12-09 10:43:13 +0100

    Bytt om på rekkefølgen til fnr og navn for medfølgende barn

--- src/felleskomponenter/menypanel/menypunkter/familieforhold/medfolgendeBarn/redigerer.tsx
+++ src/felleskomponenter/menypanel/menypunkter/familieforhold/medfolgendeBarn/redigerer.tsx
@@ -55,6 +55,16 @@ const Redigerer = ({
     <Nav.Row className="medfolgende-barn__redigerer">
       <Nav.Column xs="5">
         <Skjema.Input
+          label="F.nr./d-nr."
+          feltNavn={`${overordnetFeltNavn}.fnr`}
+          disabled={!redigerbart}
+          bredde="fullbredde"
+          datoFelt={false}
+          onChange={idNummerChangeHandler}
+        />
+      </Nav.Column>
+      <Nav.Column xs="5">
+        <Skjema.Input
           label="Fullt navn"
           feltNavn={`${overordnetFeltNavn}.navn`}
           disabled={!redigerbart || disableNavnInput}
@@ -65,16 +75,6 @@ const Redigerer = ({
           visNavnSpinner &&
           <Nav.NavFrontendSpinner className="navn-spinner" />
         }
-      </Nav.Column>
-      <Nav.Column xs="5">
-        <Skjema.Input
-          label="F.nr./d-nr."
-          feltNavn={`${overordnetFeltNavn}.fnr`}
-          disabled={!redigerbart}
-          bredde="fullbredde"
-          datoFelt={false}
-          onChange={idNummerChangeHandler}
-        />
       </Nav.Column>
       <Symboler.Slett
         onClick={slett}
