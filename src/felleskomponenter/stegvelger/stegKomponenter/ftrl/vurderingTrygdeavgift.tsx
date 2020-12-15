2a48d4ca48aa86a5e551a3d225d62de76374dd8b
janitamarielle <janita.marielle.johansen@nav.no>
2020-12-15 07:09:19 +0100

    lint

--- src/felleskomponenter/stegvelger/stegKomponenter/ftrl/vurderingTrygdeavgift.tsx
+++ src/felleskomponenter/stegvelger/stegKomponenter/ftrl/vurderingTrygdeavgift.tsx
@@ -28,19 +28,19 @@ const PeriodeTabellComponent = ({ perioder }: { perioder: string[][] | undefined
   return (
     <table className="periode_tabell">
       <tbody>
-      <tr>
-        <th key={Utils._uuid()} style={{ width: '30%' }} scope="col">Periode</th>
-        <th key={Utils._uuid()} style={{ width: '40%' }} scope="col">Dekning</th>
-        <th key={Utils._uuid()} style={{ width: '10%' }} scope="col">Sats</th>
-        <th key={Utils._uuid()} style={{ width: '20%' }} scope="col">Avgift per måned</th>
-      </tr>
-      {perioder.map(avgiftsPeriode =>
-        <tr className="border_top" key={Utils._uuid()}>
-          {avgiftsPeriode.map((listeElement: string) =>
-            <td key={Utils._uuid()}>{listeElement}</td>)
-          }
-        </tr>)
-      }
+        <tr>
+          <th key={Utils._uuid()} style={{ width: '30%' }} scope="col">Periode</th>
+          <th key={Utils._uuid()} style={{ width: '40%' }} scope="col">Dekning</th>
+          <th key={Utils._uuid()} style={{ width: '10%' }} scope="col">Sats</th>
+          <th key={Utils._uuid()} style={{ width: '20%' }} scope="col">Avgift per måned</th>
+        </tr>
+        {perioder.map(avgiftsPeriode =>
+          <tr className="border_top" key={Utils._uuid()}>
+            {avgiftsPeriode.map((listeElement: string) =>
+              <td key={Utils._uuid()}>{listeElement}</td>)
+            }
+          </tr>)
+        }
       </tbody>
     </table>
   );
