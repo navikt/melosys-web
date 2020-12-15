e9b32bc15de9a3607a4498cfcc4d89fde7befa80
janitamarielle <janita.marielle.johansen@nav.no>
2020-12-15 11:40:48 +0100

    tar ikke med inngangsvilkårene

--- src/sider/ftrl/saksbehandling/stegMap/start.js
+++ src/sider/ftrl/saksbehandling/stegMap/start.js
@@ -5,7 +5,7 @@ import VurderingStart from '../../../../felleskomponenter/stegvelger/stegKompone
 class Start extends Steg {
   constructor(propsLight, stegPosisjon) {
     super(propsLight, stegPosisjon);
-    const harAvklaring = this.finnAvklaring(propsLight.behandlingsgrunnlag, propsLight.inngangsvilkaar.oppfylt);
+    const harAvklaring = this.finnAvklaring(propsLight.behandlingsgrunnlag);
     this.kriterier = [
       {
         exec: () => harAvklaring,
@@ -26,12 +26,12 @@ class Start extends Steg {
     };
     this.status = FANE_STATUS.OK;
   }
-  finnAvklaring = (behandlingsgrunnlag, oppfylt) => {
+  finnAvklaring = (behandlingsgrunnlag) => {
     const søknadsperiodeValid = behandlingsgrunnlag.periode ? !!behandlingsgrunnlag.periode.fom : false;
     const søknadslandValid = behandlingsgrunnlag.soeknadsland && behandlingsgrunnlag.soeknadsland.landkoder ? behandlingsgrunnlag.soeknadsland.landkoder.length > 0 : false;
     const trygdedekningValid = !!behandlingsgrunnlag.trygdedekning;
 
-    return søknadslandValid && søknadsperiodeValid && trygdedekningValid && oppfylt;
+    return søknadslandValid && søknadsperiodeValid && trygdedekningValid;
   }
 }
 export default Start;
