bc073c1434bf2b531dc1d99c3c546a7db43ec20d
JohanSakshaug <johan.sakshaug@nav.no>
2021-02-08 15:23:23 +0100

    Vis redigeringsknapp for arbeidPaaLand etter all data for arbeidPaaLand er slettet

--- src/felleskomponenter/menypanel/menypunkter/editerbartElementListe/enRedigeringsknappListe.tsx
+++ src/felleskomponenter/menypanel/menypunkter/editerbartElementListe/enRedigeringsknappListe.tsx
@@ -5,7 +5,7 @@ import classNames from "classnames";
 import * as Mui from "../../../ui";
 import * as Ikoner from "../../../../resources/images";
 
-import EditerbartElement from "../editerbartElement";
+import EditerbartElement, { SymbolsynlighetMap } from "../editerbartElement";
 
 import "./enRedigeringsknappListe.css";
 
@@ -52,6 +52,7 @@ interface EnRedigeringsKnappListeProps<T> {
   leggTil: () => void;
   leggTilTekst: string;
   onBinClick?: (index: number) => void;
+  symbolsynlighetMap?: SymbolsynlighetMap;
 }
 
 function EnRedigeringsKnappListe<T>({
@@ -71,6 +72,7 @@ function EnRedigeringsKnappListe<T>({
   leggTil,
   leggTilTekst,
   onBinClick,
+  symbolsynlighetMap,
 }: EnRedigeringsKnappListeProps<T>) {
   const RedigererKomponent = redigererKomponent;
   const RedigeringUtfortKomponent = redigeringUtfortKomponent;
@@ -115,6 +117,7 @@ function EnRedigeringsKnappListe<T>({
       tittelUnderstrek={tittelUnderstrek}
       onBinClick={slettElementerOgPreElementer}
       harData={harData()}
+      symbolsynlighetMap={symbolsynlighetMap}
       hentNyStatusVedHarData={false}
       visLagreKnappBareHvisHarData
       redigererRender={() => (
