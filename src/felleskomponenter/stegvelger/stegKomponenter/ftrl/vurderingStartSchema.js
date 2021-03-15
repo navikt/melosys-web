ef8ec68666fbdcba6ab7a4c1e322475b6837f218
janitamarielle <janita.marielle.johansen@nav.no>
2021-03-15 09:25:38 +0100

    gyldig-periode feilmeldingen skal vises i tom-dato-feltet

--- src/felleskomponenter/stegvelger/stegKomponenter/ftrl/vurderingStartSchema.js
+++ src/felleskomponenter/stegvelger/stegKomponenter/ftrl/vurderingStartSchema.js
@@ -13,20 +13,24 @@ const gyldigDatoTest = {
   test: erDatoGyldig,
 };
 
-const sjekkOmPeriodeErUgyldig = (fom, tom) => {
-  const gyldig = !fom || !tom || Utils.dato.erGyldigPeriode(fom, tom);
-  return !gyldig && erDatoGyldig(fom) && erDatoGyldig(tom);
+const erPeriodeGyldig = (fom, tom) => {
+  if (!erDatoGyldig(fom) || !erDatoGyldig(tom)) return true;
+  return !fom || !tom || Utils.dato.erGyldigPeriode(fom, tom);
 };
 
 const vurdering_start = object().shape({
   fom: string().test(gyldigDatoTest).required(FOM_FELT_KREVES),
-  tom: string().test(gyldigDatoTest).nullable(),
+  tom: string()
+    .test(gyldigDatoTest)
+    .when("fom", (fom, schema) => {
+      return schema.test({
+        test: (tom) => erPeriodeGyldig(fom, tom),
+        message: "Tidligere enn f.o.m.-dato",
+      });
+    })
+    .nullable(),
   land: string().required(ARBEIDSLAND_FELT_KREVES),
   trygdedekning: string().required(TRYGDEDEKNING_FELT_KREVES),
-  erPeriodeGyldig: string().when(["fom", "tom"], {
-    is: sjekkOmPeriodeErUgyldig,
-    then: string().required(),
-  }),
 });
 
 export default vurdering_start;
