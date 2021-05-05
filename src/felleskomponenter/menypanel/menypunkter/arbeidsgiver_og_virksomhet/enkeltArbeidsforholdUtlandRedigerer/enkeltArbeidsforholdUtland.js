68fd0890e31d6dd3e079cf00844f2a0c7d44d333
janitamarielle <janita.marielle.johansen@nav.no>
2021-05-05 15:17:36 +0200

    henter landkoderIso2 dersom sakstype er FTRL

--- src/felleskomponenter/menypanel/menypunkter/arbeidsgiver_og_virksomhet/enkeltArbeidsforholdUtlandRedigerer/enkeltArbeidsforholdUtland.js
+++ src/felleskomponenter/menypanel/menypunkter/arbeidsgiver_og_virksomhet/enkeltArbeidsforholdUtlandRedigerer/enkeltArbeidsforholdUtland.js
@@ -1,53 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PT from "prop-types";
 
+import MKV from "../../../../../melosyskodeverk";
 import * as Nav from "../../../../../utils/navFrontend";
 import * as Skjema from "../../../../skjema";
+import * as API from "../../../../../services/api";
+import * as Utils from "../../../../../utils";
 
-const EnkeltArbeidsforholdUtland = ({ redigerbart, overordnetFeltNavn, className }) => (
-  <div className={className}>
-    <Nav.Row>
-      <Nav.Column xs="6">
-        <Skjema.Input label="Navn på virksomheten" feltNavn={`${overordnetFeltNavn}.navn`} disabled={!redigerbart} />
-      </Nav.Column>
-      <Nav.Column xs="6">
-        <Skjema.Input label="Registreringsnummer" feltNavn={`${overordnetFeltNavn}.orgnr`} disabled={!redigerbart} />
-      </Nav.Column>
-    </Nav.Row>
-    <Nav.Row>
-      <Nav.Column xs="6">
-        <Skjema.Input
-          label="Adresse til arbeidsgiver"
-          feltNavn={`${overordnetFeltNavn}.adresse.gatenavn`}
-          disabled={!redigerbart}
-        />
-      </Nav.Column>
-      <Nav.Column xs="3">
-        <Skjema.Input label="Poststed" feltNavn={`${overordnetFeltNavn}.adresse.poststed`} disabled={!redigerbart} />
-      </Nav.Column>
-      <Nav.Column xs="3">
-        <Skjema.Input
-          label="Postnummer"
-          feltNavn={`${overordnetFeltNavn}.adresse.postnummer`}
-          disabled={!redigerbart}
-        />
-      </Nav.Column>
-    </Nav.Row>
-    <Nav.Row>
-      <Nav.Column xs="6">
-        <Skjema.Input label="Region" feltNavn={`${overordnetFeltNavn}.adresse.region`} disabled={!redigerbart} />
-      </Nav.Column>
-      <Nav.Column xs="6">
-        <Skjema.LandVelger
-          label="Land"
-          feltNavn={`${overordnetFeltNavn}.adresse.landkode`}
-          disabled={!redigerbart}
-          bredde="fullbredde"
-        />
-      </Nav.Column>
-    </Nav.Row>
-  </div>
-);
+const EnkeltArbeidsforholdUtland = ({ redigerbart, overordnetFeltNavn, className }) => {
+  const [alternativLandsliste, setAlternativLandsliste] = useState();
+  const erFTRL = window.location.pathname.split("/")[2] === MKV.Koder.sakstyper.FTRL;
+
+  useEffect(() => {
+    if (erFTRL) {
+      API.Kodeverk.hentLandkoderIso2()
+        .then((response) => {
+          setAlternativLandsliste(
+            response
+              .sort((a, b) => {
+                if (a.term > b.term) return 1;
+                if (b.term > a.term) return -1;
+                return 0;
+              })
+              .map((item) => ({ ...item, term: Utils.streng.storeForbokstaver(item.term) }))
+          );
+        })
+        .catch(Utils.logger.error);
+    }
+  }, []);
+
+  return (
+    <div className={className}>
+      <Nav.Row>
+        <Nav.Column xs="6">
+          <Skjema.Input label="Navn på virksomheten" feltNavn={`${overordnetFeltNavn}.navn`} disabled={!redigerbart} />
+        </Nav.Column>
+        <Nav.Column xs="6">
+          <Skjema.Input label="Registreringsnummer" feltNavn={`${overordnetFeltNavn}.orgnr`} disabled={!redigerbart} />
+        </Nav.Column>
+      </Nav.Row>
+      <Nav.Row>
+        <Nav.Column xs="6">
+          <Skjema.Input
+            label="Adresse til arbeidsgiver"
+            feltNavn={`${overordnetFeltNavn}.adresse.gatenavn`}
+            disabled={!redigerbart}
+          />
+        </Nav.Column>
+        <Nav.Column xs="3">
+          <Skjema.Input label="Poststed" feltNavn={`${overordnetFeltNavn}.adresse.poststed`} disabled={!redigerbart} />
+        </Nav.Column>
+        <Nav.Column xs="3">
+          <Skjema.Input
+            label="Postnummer"
+            feltNavn={`${overordnetFeltNavn}.adresse.postnummer`}
+            disabled={!redigerbart}
+          />
+        </Nav.Column>
+      </Nav.Row>
+      <Nav.Row>
+        <Nav.Column xs="6">
+          <Skjema.Input label="Region" feltNavn={`${overordnetFeltNavn}.adresse.region`} disabled={!redigerbart} />
+        </Nav.Column>
+        <Nav.Column xs="6">
+          <Skjema.LandVelger
+            label="Land"
+            feltNavn={`${overordnetFeltNavn}.adresse.landkode`}
+            disabled={!redigerbart}
+            bredde="fullbredde"
+            landkoder={alternativLandsliste}
+          />
+        </Nav.Column>
+      </Nav.Row>
+    </div>
+  );
+};
 
 EnkeltArbeidsforholdUtland.propTypes = {
   redigerbart: PT.bool.isRequired,
