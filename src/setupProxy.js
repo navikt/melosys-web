d1b929b5c37de137a8a943b3226f24d64271d96f
janitamarielle <janita.marielle.johansen@nav.no>
2021-07-05 14:46:38 +0200

    pathrewrite er unødvendig

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,20 +1,8 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function setupProxy(app) {
-  app.use(createProxyMiddleware("/api", { target: "http://localhost:3002/", pathRewrite: { "^/api/": "/api/" } }));
-  app.use(
-    createProxyMiddleware("/graphql", { target: "http://localhost:3002/", pathRewrite: { "^/graphql/": "/graphql/" } })
-  );
-  app.use(
-    createProxyMiddleware("/flyt", {
-      target: "http://localhost:8088/",
-      pathRewrite: { ".*/flyt/": "/flyt/" },
-    })
-  );
-  app.use(
-    createProxyMiddleware("/frontendlogger", {
-      target: "http://localhost:3002/",
-      pathRewrite: { "^/frontendlogger/": "frontendlogger/" },
-    })
-  );
+  app.use(createProxyMiddleware("/api", { target: "http://localhost:3002/" }));
+  app.use(createProxyMiddleware("/graphql", { target: "http://localhost:3002/" }));
+  app.use(createProxyMiddleware("/frontendlogger", { target: "http://localhost:3002/" }));
+  app.use(createProxyMiddleware("/flyt", { target: "http://localhost:8088/" }));
 };
