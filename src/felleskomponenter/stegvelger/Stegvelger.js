42b70c8fed4df9a3b095910dba73ef31bde64e49
martonSkjevelandNav <marton.skjeveland@nav.no>
2021-07-13 09:09:06 +0200

    Oppdaterer stegvelger når familieforhold eller arbeidsgiver/virksomhet endres i menypanel

--- src/felleskomponenter/stegvelger/Stegvelger.js
+++ src/felleskomponenter/stegvelger/Stegvelger.js
@@ -85,7 +85,18 @@ class Stegvelger extends Component {
     if (
       this.props.oppsummering.behandlingsstatus !== prevProps.oppsummering.behandlingsstatus ||
       this.props.artikkel12_vedtak_skjema !== prevProps.artikkel12_vedtak_skjema ||
-      this.props.vurder_virksomhet_valid !== prevProps.vurder_virksomhet_valid
+      this.props.vurder_virksomhet_valid !== prevProps.vurder_virksomhet_valid ||
+      // Opplysninger
+      // Fra Register og søknad: Familieforhold
+      this.props.saksopplysninger.familiemedlemmer !== prevProps.saksopplysninger.familiemedlemmer ||
+      this.props.soknad_skjema.medfolgendeBarn !== prevProps.soknad_skjema.medfolgendeBarn ||
+      // Fra Søknad: Arbeidsgiver/virksomhet
+      this.props.soknad_skjema.juridiskArbeidsgiverNorge.ekstraArbeidsgivere !==
+        prevProps.soknad_skjema.juridiskArbeidsgiverNorge.ekstraArbeidsgivere ||
+      this.props.soknad_skjema.selvstendigForetak !== prevProps.soknad_skjema.selvstendigForetak ||
+      this.props.soknad_skjema.arbeidsforholdUtland !== prevProps.soknad_skjema.arbeidsforholdUtland ||
+      this.props.soknad_skjema.selvstendigNaeringsvirksomhetUtland !==
+        prevProps.soknad_skjema.selvstendigNaeringsvirksomhetUtland
     ) {
       this.oppdaterAktuelleSteg(aktivtStegNummer);
     }
