f6b93f702e8a060b89ebd432dbed8025fa4dfd77
JohanSakshaug <johan.sakshaug@nav.no>
2021-07-28 12:24:10 +0200

    Legg til checkbox for ukjente land i periode-menypunkt

--- src/felleskomponenter/menypanel/menypunkter/utenlandsoppdraget/soknadslandvelger/redigeringUtfort.tsx
+++ src/felleskomponenter/menypanel/menypunkter/utenlandsoppdraget/soknadslandvelger/redigeringUtfort.tsx
@@ -14,7 +14,10 @@ import { behandlingsgrunnlagSelectors } from "../../../../../ducks/behandlingsgr
 import "./redigeringUtfort.css";
 
 const mapStateToProps = (state: RootState) => ({
-  soknadsland: behandlingsgrunnlagSelectors.SoknadslandSelector(state),
+  soknadsland: behandlingsgrunnlagSelectors.SoknadslandkoderSelector(state),
+  soknadslandErUkjenteEllerAlleEosLand: behandlingsgrunnlagSelectors.SoknadslandErUkjenteEllerAlleEosLandSelector(
+    state
+  ),
 });
 const connector = connect(mapStateToProps);
 type PropsFromRedux = ConnectedProps<typeof connector>;
@@ -23,17 +26,20 @@ type RedigeringUtfortProps = PropsFromRedux & {
   className?: string;
 };
 
-const RedigeringUtfort = ({ soknadsland, className }: RedigeringUtfortProps) => {
+const RedigeringUtfort = ({ soknadsland, soknadslandErUkjenteEllerAlleEosLand, className }: RedigeringUtfortProps) => {
   const cls = classNames(className, "soknadslandvelger-redigering-utfort");
 
   return (
     <div className={cls}>
       <Nav.Typo.Normaltekst className="etikett-liten">Land</Nav.Typo.Normaltekst>
-      <Nav.Typo.Element className="element">
-        {Utils.streng.arrayTilKonjunksjon(
-          soknadsland.map((land: string) => KV.kodeTilTerm(land, MKV.KTObjects.landkoder))
-        ) || "Ingen land valgt"}
-      </Nav.Typo.Element>
+      {soknadslandErUkjenteEllerAlleEosLand && "Flere EØS-land/Sveits. Ikke kjent hvilke."}
+      {!soknadslandErUkjenteEllerAlleEosLand && (
+        <Nav.Typo.Element className="element">
+          {Utils.streng.arrayTilKonjunksjon(
+            soknadsland.map((land: string) => KV.kodeTilTerm(land, MKV.KTObjects.landkoder))
+          ) || "Ingen land valgt"}
+        </Nav.Typo.Element>
+      )}
     </div>
   );
 };
