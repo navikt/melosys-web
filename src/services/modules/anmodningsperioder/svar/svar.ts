6d960104315102332314aaacfe98d464fd283b98
JohanSakshaug <johan.sakshaug@nav.no>
2021-09-01 17:22:54 +0200

    AnmodningsperiodeSvar-kall til ts

---
+++ src/services/modules/anmodningsperioder/svar/svar.ts
@@ -0,0 +1,29 @@
+import { postAsJson, getAsJson } from "../../../utils";
+import { API_BASE_URL, ANMODNINGSPERIODER, SVAR } from "../../../api-constants";
+
+interface AnmodningsperiodesvarResDto {
+  anmodningsperiodeSvarType: string | null;
+  endretPeriode: {
+    fom: string | null;
+    tom: string | null;
+  };
+  begrunnelseFritekst: string | null;
+}
+
+export const hent = (anmodningsperiodeID: number): Promise<AnmodningsperiodesvarResDto> =>
+  getAsJson(`${API_BASE_URL}${ANMODNINGSPERIODER}/${anmodningsperiodeID}/${SVAR}`);
+
+interface AnmodningsperiodesvarReqDto {
+  anmodningsperiodeSvarType: string;
+  endretPeriode: {
+    fom: string | null;
+    tom: string | null;
+  };
+  begrunnelseFritekst: string;
+}
+
+export const send = (
+  anmodningsperiodeID: number,
+  body: AnmodningsperiodesvarReqDto
+): Promise<AnmodningsperiodesvarResDto> =>
+  postAsJson(`${API_BASE_URL}${ANMODNINGSPERIODER}/${anmodningsperiodeID}/${SVAR}`, body);
