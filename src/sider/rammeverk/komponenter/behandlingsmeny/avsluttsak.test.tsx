d85c039b5b618977f616876e655566d57e271bb4
martonSkjevelandNav <marton.skjeveland@nav.no>
2021-09-14 11:00:24 +0200

    Laget bedre tester for behandlingsmeny

---
+++ src/sider/rammeverk/komponenter/behandlingsmeny/avsluttsak.test.tsx
@@ -0,0 +1,54 @@
+import React, { ComponentProps } from "react";
+import { instance, mock } from "ts-mockito";
+import { shallow } from "enzyme";
+
+import MKV from "../../../../melosyskodeverk";
+
+import AvsluttSak from "./avsluttsak";
+import Handling from "./handling";
+
+const { BESLUTNING_LOVVALG_NORGE, ARBEID_I_UTLANDET, TRYGDETID } = MKV.Koder.behandlinger.behandlingstema;
+const mockedProps = mock<ComponentProps<typeof AvsluttSak>>();
+const mockedEvent = mock<React.MouseEvent>();
+
+describe("AvsluttSak", () => {
+  let props = instance(mockedProps);
+  const event = instance(mockedEvent);
+
+  beforeEach(() => {
+    props = instance(mockedProps);
+  });
+
+  it("viser alle valg om FTRL_SAKSBEHANDLING og redigerbart", () => {
+    props.behandlingstema = ARBEID_I_UTLANDET;
+    props.redigerbart = true;
+
+    const avsluttSak = shallow(<AvsluttSak {...props} />);
+    const handlinger = avsluttSak.find(Handling);
+
+    expect(handlinger).toHaveLength(3);
+    expect(handlinger.at(0).props().tekst).toBe("Avslå søknad pga. manglende opplysninger");
+    expect(handlinger.at(1).props().tekst).toBe("Henlegg sak");
+    expect(handlinger.at(2).props().tekst).toBe("Avslutt sak som bortfalt");
+  });
+
+  it("viser bare avsluttSak om tema er trygdetid", () => {
+    props.behandlingstema = TRYGDETID;
+    props.redigerbart = true;
+
+    const avsluttSak = shallow(<AvsluttSak {...props} />);
+    const handlinger = avsluttSak.find(Handling);
+
+    expect(handlinger).toHaveLength(1);
+    expect(handlinger.props().tekst).toBe("Avslutt sak som bortfalt");
+  });
+
+  it("returerer null om EØS_VURDER_UTPEKING og ikke redigerbart", () => {
+    props.redigerbart = false;
+    props.behandlingstema = BESLUTNING_LOVVALG_NORGE;
+
+    const behandlingsmeny = shallow(<AvsluttSak {...props} />);
+
+    expect(behandlingsmeny.find(".behandlingsmeny__meny__avslutt-sak").exists()).toBeFalsy();
+  });
+});
--- src/sider/rammeverk/komponenter/behandlingsmeny/behandlingsmeny.test.tsx
+++ src/sider/rammeverk/komponenter/behandlingsmeny/behandlingsmeny.test.tsx
@@ -1,7 +1,6 @@
 import React, { ComponentProps } from "react";
-import { act } from "react-dom/test-utils";
 import { instance, mock } from "ts-mockito";
-import { mount } from "enzyme";
+import { shallow } from "enzyme";
 
 import MKV from "../../../../melosyskodeverk";
 
@@ -10,8 +9,9 @@ import LeggBehandlingTilbake from "./leggbehandlingtilbake";
 import AvsluttSak from "./avsluttsak";
 import Handling from "./handling";
 
-const { BESLUTNING_LOVVALG_NORGE, ARBEID_I_UTLANDET } = MKV.Koder.behandlinger.behandlingstema;
+const { ARBEID_I_UTLANDET } = MKV.Koder.behandlinger.behandlingstema;
 const { AVSLUTTET } = MKV.Koder.behandlinger.behandlingsstatus;
+const { NY_VURDERING } = MKV.Koder.behandlinger.behandlingstyper;
 const mockedProps = mock<ComponentProps<typeof Behandlingsmeny>>();
 const mockedEvent = mock<React.MouseEvent>();
 
@@ -23,55 +23,30 @@ describe("Behandlingsmeny", () => {
     props = instance(mockedProps);
   });
 
-  it("viser alle linjer", () => {
+  it("rendrer LeggBehandlingTilbake og AvsluttSak med riktige props", () => {
+    props.behandlingID = "12";
     props.behandlingstema = ARBEID_I_UTLANDET;
     props.behandlingsstatus = AVSLUTTET;
+    props.behandlingstype = NY_VURDERING;
     props.redigerbart = true;
-    const behandlingsmeny = mount(<Behandlingsmeny {...props} />);
+    const behandlingsmeny = shallow(<Behandlingsmeny {...props} />);
 
-    act(() => behandlingsmeny.find(".behandlingsmeny__knapp").props().onClick?.(event));
-    behandlingsmeny.update();
-    act(() => behandlingsmeny.find(LeggBehandlingTilbake).find(".ekspanderbartPanel__hode").props().onClick?.(event));
-    act(() => behandlingsmeny.find(AvsluttSak).find(".ekspanderbartPanel__hode").props().onClick?.(event));
-    behandlingsmeny.update();
+    behandlingsmeny.find(".behandlingsmeny__knapp").props().onClick?.(event);
 
-    const leggBehandlingTilbakeHandlinger = behandlingsmeny.find(LeggBehandlingTilbake).find(Handling);
-    expect(leggBehandlingTilbakeHandlinger).toHaveLength(2);
-    expect(leggBehandlingTilbakeHandlinger.at(0).text()).toEqual("Til min oppgaveliste");
-    expect(leggBehandlingTilbakeHandlinger.at(1).text()).toEqual("Til felles oppgaveliste");
-    expect(leggBehandlingTilbakeHandlinger.at(1).props().disabled).toBeFalsy();
+    const leggBehandlingTilbake = behandlingsmeny.find(LeggBehandlingTilbake);
+    expect(leggBehandlingTilbake.exists()).toBeTruthy();
+    expect(leggBehandlingTilbake.props().behandlingID).toBe(props.behandlingID);
+    expect(leggBehandlingTilbake.props().redigerbart).toBe(props.redigerbart);
 
-    const avsluttSakHandlinger = behandlingsmeny.find(AvsluttSak).find(Handling);
-    expect(avsluttSakHandlinger).toHaveLength(3);
-    expect(avsluttSakHandlinger.at(0).text()).toEqual("Avslå søknad pga. manglende opplysninger");
-    expect(avsluttSakHandlinger.at(1).text()).toEqual("Henlegg sak");
-    expect(avsluttSakHandlinger.at(2).text()).toEqual("Avslutt sak som bortfalt");
+    const avsluttSak = behandlingsmeny.find(AvsluttSak);
+    expect(avsluttSak.exists()).toBeTruthy();
+    expect(avsluttSak.props().behandlingstema).toBe(props.behandlingstema);
+    expect(avsluttSak.props().behandlingstype).toBe(props.behandlingstype);
+    expect(avsluttSak.props().redigerbart).toBe(props.redigerbart);
 
     const menyHandlinger = behandlingsmeny.find(".behandlingsmeny__meny__handlinger").find(Handling);
     expect(menyHandlinger).toHaveLength(2);
-    expect(menyHandlinger.at(0).text()).toEqual("copy.svgVis saksoversikt");
-    expect(menyHandlinger.at(1).text()).toEqual("cancel.svgVurder saken på nytt");
-  });
-
-  it("viser bare saksoversikt", () => {
-    props.behandlingstema = BESLUTNING_LOVVALG_NORGE;
-    props.redigerbart = false;
-    const behandlingsmeny = mount(<Behandlingsmeny {...props} />);
-
-    act(() => behandlingsmeny.find(".behandlingsmeny__knapp").props().onClick?.(event));
-    behandlingsmeny.update();
-    act(() => behandlingsmeny.find(LeggBehandlingTilbake).find(".ekspanderbartPanel__hode").props().onClick?.(event));
-    behandlingsmeny.update();
-
-    const tilFellesOppgaveliste = behandlingsmeny.find(LeggBehandlingTilbake).find(Handling);
-    expect(tilFellesOppgaveliste).toHaveLength(1);
-    expect(tilFellesOppgaveliste.text()).toEqual("Til felles oppgaveliste");
-    expect(tilFellesOppgaveliste.props().disabled).toBeTruthy();
-
-    expect(behandlingsmeny.find(AvsluttSak).children()).toHaveLength(0);
-
-    const visSaksoversikt = behandlingsmeny.find(".behandlingsmeny__meny__handlinger").find(Handling);
-    expect(visSaksoversikt).toHaveLength(1);
-    expect(visSaksoversikt.text()).toEqual("copy.svgVis saksoversikt");
+    expect(menyHandlinger.at(0).props().tekst).toBe("Vis saksoversikt");
+    expect(menyHandlinger.at(1).props().tekst).toBe("Vurder saken på nytt");
   });
 });
