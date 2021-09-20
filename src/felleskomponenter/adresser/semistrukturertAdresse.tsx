33cfa537a3f0fd74d9ad5cc0686da5a8c9c28dd4
JohanSakshaug <johan.sakshaug@nav.no>
2021-09-20 13:26:31 +0200

    test SemistrukturertAdresse

--- src/felleskomponenter/adresser/semistrukturertAdresse.tsx
+++ src/felleskomponenter/adresser/semistrukturertAdresse.tsx
@@ -8,16 +8,18 @@ interface SemistrukturertAdresseProps {
 
 const SemistrukturertAdresse = ({
   adresse: { adresselinje1, adresselinje2, adresselinje3, adresselinje4, postnummer, poststed, land },
-}: SemistrukturertAdresseProps) => (
-  <address>
-    <div>{adresselinje1}</div>
-    <div>{adresselinje2}</div>
-    <div>{adresselinje3}</div>
-    <div>{adresselinje4}</div>
-    <div>
-      {postnummer} {poststed}, {land}
-    </div>
-  </address>
-);
+}: SemistrukturertAdresseProps) => {
+  const postNrStedLandLinje = `${postnummer} ${poststed}, ${land}`;
+
+  return (
+    <address>
+      <div>{adresselinje1}</div>
+      <div>{adresselinje2}</div>
+      <div>{adresselinje3}</div>
+      <div>{adresselinje4}</div>
+      <div>{postNrStedLandLinje}</div>
+    </address>
+  );
+};
 
 export default SemistrukturertAdresse;
