7b9545f52593b36240fe1299d2248636e3e15be2
martonSkjevelandNav <marton.skjeveland@nav.no>
2021-09-29 15:01:50 +0200

    Fjernet debug logging

--- src/felleskomponenter/menypanel/menypunkter/editerbartElement/editerbartElement.tsx
+++ src/felleskomponenter/menypanel/menypunkter/editerbartElement/editerbartElement.tsx
@@ -95,7 +95,6 @@ const EditerbartElement = ({
     if (onLagreClick) {
       try {
         const validert = await onLagreClick(event);
-        console.log(validert);
         if (!validert) return;
       } catch (error) {
         return;
