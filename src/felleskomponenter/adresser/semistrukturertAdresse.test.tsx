9b39d8506932a470df6599670c4790b6bce1991f
JohanSakshaug <johan.sakshaug@nav.no>
2021-11-04 11:17:38 +0100

    ikke vis null-string for Semistrukturertadresse

--- src/felleskomponenter/adresser/semistrukturertAdresse.test.tsx
+++ src/felleskomponenter/adresser/semistrukturertAdresse.test.tsx
@@ -8,21 +8,25 @@ import SemistrukturertAdresse from "./semistrukturertAdresse";
 
 describe("SemistrukturertAdresse", () => {
   const mockedProps = mock<ComponentProps<typeof SemistrukturertAdresse>>();
-  const props = instance(mockedProps);
+  let props = instance(mockedProps);
 
-  props.adresse = {
-    adresselinje1: "Oslogata 1",
-    adresselinje2: "Trondheimsgata 1",
-    adresselinje3: "Bergensgata 1",
-    adresselinje4: "Tromsøgata 1",
-    land: MKV.Koder.landkoder.NO,
-    postnummer: "0000",
-    poststed: "Oslo",
-  };
-
-  const semistrukturertAdresse = shallow(<SemistrukturertAdresse {...props} />);
+  beforeEach(() => {
+    props = instance(mockedProps);
+  });
 
   it("viser alle felter for semistrukturert adresse", () => {
+    props.adresse = {
+      adresselinje1: "Oslogata 1",
+      adresselinje2: "Trondheimsgata 1",
+      adresselinje3: "Bergensgata 1",
+      adresselinje4: "Tromsøgata 1",
+      land: MKV.Koder.landkoder.NO,
+      postnummer: "0000",
+      poststed: "Oslo",
+    };
+
+    const semistrukturertAdresse = shallow(<SemistrukturertAdresse {...props} />);
+
     expect(semistrukturertAdresse.contains("Oslogata 1")).toBe(true);
     expect(semistrukturertAdresse.contains("Trondheimsgata 1")).toBe(true);
     expect(semistrukturertAdresse.contains("Bergensgata 1")).toBe(true);
@@ -31,6 +35,35 @@ describe("SemistrukturertAdresse", () => {
   });
 
   it("viser et adresse-element", () => {
+    const semistrukturertAdresse = shallow(<SemistrukturertAdresse {...props} />);
+
     expect(semistrukturertAdresse.find("address")).toHaveLength(1);
+  });
+
+  it("viser ikke null- eller undefined-stringer dersom felter er null eller undefined", () => {
+    props.adresse = {
+      adresselinje1: null,
+      adresselinje2: null,
+      adresselinje3: null,
+      adresselinje4: null,
+      land: undefined,
+      postnummer: null,
+      poststed: null,
+    };
+
+    const semistrukturertAdresse = shallow(<SemistrukturertAdresse {...props} />);
+
+    expect(semistrukturertAdresse.text()).not.toContain("null");
+    expect(semistrukturertAdresse.text()).not.toContain("undefined");
+  });
+
+  it("viser ikke komma ved land-tekst dersom land er undefined", () => {
+    props.adresse.postnummer = "1234";
+    props.adresse.poststed = "Trondheim";
+    props.adresse.land = undefined;
+
+    const semistrukturertAdresse = shallow(<SemistrukturertAdresse {...props} />);
+
+    expect(semistrukturertAdresse.text()).not.toContain(",");
   });
 });
--- src/felleskomponenter/adresser/semistrukturertAdresse.tsx
+++ src/felleskomponenter/adresser/semistrukturertAdresse.tsx
@@ -9,14 +9,15 @@ interface SemistrukturertAdresseProps {
 const SemistrukturertAdresse = ({
   adresse: { adresselinje1, adresselinje2, adresselinje3, adresselinje4, postnummer, poststed, land },
 }: SemistrukturertAdresseProps) => {
-  const postNrStedLandLinje = `${postnummer} ${poststed}, ${land}`;
+  const kommaLand = `, ${land}`;
+  const postNrStedLandLinje = `${postnummer ?? ""} ${poststed ?? ""}${land ? kommaLand : ""}`;
 
   return (
     <address>
-      <div>{adresselinje1}</div>
-      <div>{adresselinje2}</div>
-      <div>{adresselinje3}</div>
-      <div>{adresselinje4}</div>
+      {adresselinje1 && <div>{adresselinje1}</div>}
+      {adresselinje2 && <div>{adresselinje2}</div>}
+      {adresselinje3 && <div>{adresselinje3}</div>}
+      {adresselinje4 && <div>{adresselinje4}</div>}
       <div>{postNrStedLandLinje}</div>
     </address>
   );
