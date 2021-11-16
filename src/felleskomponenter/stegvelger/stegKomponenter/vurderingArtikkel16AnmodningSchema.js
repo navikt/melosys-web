9e6ae58568b3e31b1cb9fb56b2609994dc905890
Lars Løken Bjørnskau <larsbjornskau@gmail.com>
2021-11-16 10:52:23 +0100

    Legger til validering på maks 500 tegn i fritekst mot SED på artikkel 16.1 ved sending av SED A001

--- src/felleskomponenter/stegvelger/stegKomponenter/vurderingArtikkel16AnmodningSchema.js
+++ src/felleskomponenter/stegvelger/stegKomponenter/vurderingArtikkel16AnmodningSchema.js
@@ -1,6 +1,7 @@
 import { object, string, bool } from "yup";
 
 const VELG_MOTTAKERINSTITUSJON = { melding: "Velg mottakerinstitusjon" };
+import { DU_KAN_IKKE_SKRIVE_MER_ENN_500_TEGN } from "../../../kodeverk/feilmeldinger";
 
 const artikkel16_anmodning = object().shape({
   kreverMottakerinstitusjon: bool().required(),
@@ -8,6 +9,7 @@ const artikkel16_anmodning = object().shape({
     is: true,
     then: string().required(VELG_MOTTAKERINSTITUSJON),
   }),
+  fritekstSed: string().nullable().max(500, DU_KAN_IKKE_SKRIVE_MER_ENN_500_TEGN),
 });
 
 export default artikkel16_anmodning;
