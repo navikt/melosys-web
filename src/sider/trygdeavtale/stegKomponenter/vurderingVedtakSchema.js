9e75cfcefffcc4f18816fb20f78dbdd496b828e7
janitamarielle <janita.marielle.johansen@nav.no>
2021-11-23 15:49:33 +0100

    Fjerner skjema-felt uten nyttig feilmelding

--- src/sider/trygdeavtale/stegKomponenter/vurderingVedtakSchema.js
+++ src/sider/trygdeavtale/stegKomponenter/vurderingVedtakSchema.js
@@ -1,4 +1,4 @@
-import { object, string, bool } from "yup";
+import { object, string } from "yup";
 import * as KV from "../../../kodeverk";
 
 const { MAA_FYLLES_UT } = KV.Feilmeldinger;
@@ -6,9 +6,6 @@ const { MAA_FYLLES_UT } = KV.Feilmeldinger;
 const vurdering_vedtak = object().shape({
   lovvalgsperiodeFom: string().erGyldigDato().required(MAA_FYLLES_UT),
   lovvalgsperiodeTom: string().erGyldigDato().erEtterDatofelt("lovvalgsperiodeFom").required(MAA_FYLLES_UT),
-  fritekstInnledning: string().nullable(),
-  fritekstBegrunnelse: string().nullable(),
-  kopiTilArbeidsgiver: bool().nullable(),
 });
 
 export default vurdering_vedtak;
