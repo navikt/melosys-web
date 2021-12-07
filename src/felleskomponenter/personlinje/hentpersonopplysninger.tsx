bbea48bb8899be2efaee9e646c74f6ff60a42851
Marton Skjæveland <56435736+martonSkjevelandNav@users.noreply.github.com>
2021-12-07 15:58:38 +0100

    Melosys 4469 pdl personstatus utvidet visning (#1320)
    * Lagt til Personstatusmodal med dummydata
    
    * Flyttet logikk for visning av modal til selve modalen
    
    * Oppdatert graphql med ny folkeregisterpersonstatuser
    
    * Tar i bruk faktisk data fra graphql-dall
    
    * Styling og fjernet debugkode
    
    * Slått sammen sivilstand og personstatus-modalene og gjort dem mer like i strukturen
    
    * Rename modal
    
    * Lagt til enkel test for personstatusModal
    
    * Mindre endringer etter PR
    
    * Rettet opp i feil tekst

--- src/felleskomponenter/personlinje/hentpersonopplysninger.tsx
+++ src/felleskomponenter/personlinje/hentpersonopplysninger.tsx
@@ -29,10 +29,14 @@ const hentPersonopplysninger = (behandlingID: number): PersonopplysningerProps =
     .map((sivilstand) => sivilstand.type)
     .pop();
 
+  const gyldigPersonstatus = person.folkeregisterpersonstatuser
+    .filter((folkeregisterpersonstatus) => !folkeregisterpersonstatus.erHistorisk)
+    .pop();
+
   return {
     navn: PersonUtils.tilSammensattNavn(person.navn.fornavn, person.navn.mellomnavn, person.navn.etternavn),
     kjoenn: person.kjoenn,
-    erDoed: person.folkeregisterpersonstatus?.kode === MKV.Koder.personstatuser.DOED,
+    erDoed: gyldigPersonstatus?.kode === MKV.Koder.personstatuser.DOED,
     fnr: person.folkeregisteridentifikator || "",
     statsborgerskap: gyldigeStatsborgerskap || [],
     sivilstand: gyldigSivilstand || "",
