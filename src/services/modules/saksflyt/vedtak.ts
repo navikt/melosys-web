ab81d7671d30b6ff09a473e7bac8e6afca529dbe
janitamarielle <janita.marielle.johansen@nav.no>
2021-12-14 11:29:25 +0100

    Endring i kontroller-path

--- src/services/modules/saksflyt/vedtak.ts
+++ src/services/modules/saksflyt/vedtak.ts
@@ -44,4 +44,4 @@ export const endre = (behandlingID: number, data: EndreVedtakReqDto) =>
   postAsJson(`${API_BASE_URL}${SAKSFLYT}/${VEDTAK}/${behandlingID}/endre`, data);
 
 export const kontroller = (behandlingID: number, oppdater: boolean, data: FattVedtakReqDto) =>
-  postAsJson(`${API_BASE_URL}${SAKSFLYT}/${VEDTAK}/${behandlingID}/kontroll?oppdater=${oppdater}`, data);
+  postAsJson(`${API_BASE_URL}${SAKSFLYT}/${VEDTAK}/${behandlingID}/kontroller?oppdater=${oppdater}`, data);
