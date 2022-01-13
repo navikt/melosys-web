5f645e64f5291b8be821ac3070601be6a64ff952
Isa Salem Hame <isa.salem.hame@nav.no>
2022-01-13 11:34:26 +0100

    Fikser endepunkt etter endring i API
    MELOSYS-4815

--- src/services/modules/behandlinger/behandling/behandling.ts
+++ src/services/modules/behandlinger/behandling/behandling.ts
@@ -7,4 +7,4 @@ export const hentBehandling = (behandlingID: number): Promise<BehandlingResDto> 
   getAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}`);
 
 export const ferdigbehandleNyVurdering = (behandlingID: number) =>
-  putAsText(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/ferdigbehandleNyVurdering`);
+  putAsText(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/avslutt-uten-endring`);
