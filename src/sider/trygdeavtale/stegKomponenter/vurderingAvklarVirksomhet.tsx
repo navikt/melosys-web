4c3dd71725b8f21210c7fde20c03567632fa921a
Janita Johansen <janita.marielle.johansen@nav.no>
2022-01-14 15:28:50 +0100

    Skrivefeil i feilmelding

--- src/sider/trygdeavtale/stegKomponenter/vurderingAvklarVirksomhet.tsx
+++ src/sider/trygdeavtale/stegKomponenter/vurderingAvklarVirksomhet.tsx
@@ -61,7 +61,7 @@ const VurderingAvklarVirksomhet = ({
   const hjelpetekst =
     'Velg virksomhet søker er ansatt av og arbeider for i søknadsperioden. Du kan kun velge én virksomhet. Hvis søker arbeider for en virksomhet som ikke er synlig her, må du legge den til i sidemenyen under "Arbeidsgiver/virksomhet".';
   const ingenVirksomheterTekst =
-    'Det er ingen virksomhet registret. Du må legge til virksomhet under "Arbeidsgiver/virksomhet"';
+    'Det er ingen virksomhet registrert. Du må legge til virksomhet under "Arbeidsgiver/virksomhet".';
 
   useEffect(() => {
     if (redigerbart && formValues) {
