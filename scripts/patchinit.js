a51ded10f207e98b12bf75d1cd206610480ba7c5
Isa Salem Hame <isa.salem.hame@nav.no>
2022-01-25 21:29:35 +0100

    Endrer env-variabelnavn til et mer beskrivende navn

--- scripts/patchinit.js
+++ scripts/patchinit.js
@@ -10,4 +10,4 @@ dotenv.set("REACT_APP_API_BASE_URL", "/api/");
 dotenv.set("REACT_APP_TRYGDEAVTALE_FLYT_BASE_URL", "/trygdeavtale-flyt/");
 dotenv.set("REACT_APP_GRAPHQL_URL", "/graphql/");
 dotenv.set("REACT_APP_LOCAL_CONTEXT", "");
-dotenv.set("REACT_APP_API_PORT", DEFAULT_MOCK_SERVER_PORT);
+dotenv.set("REACT_APP_LOCAL_API_PORT", DEFAULT_MOCK_SERVER_PORT);
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function setupProxy(app) {
   app.use(
     createProxyMiddleware(["/api", "/graphql", "/frontendlogger"], {
-      target: `http://localhost:${process.env.REACT_APP_API_PORT}/`,
+      target: `http://localhost:${process.env.REACT_APP_LOCAL_API_PORT}/`,
     })
   );
   app.use(createProxyMiddleware("/flyt", { target: "http://localhost:8088/" }));
