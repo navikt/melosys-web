5b0de6dda5344f7f575830fce9ddc98e59d36e49
Isa Salem Hame <isa.salem.hame@nav.no>
2022-01-26 00:07:29 +0100

    Legger på mellomrom for å fikse klager fra Eslint

--- scripts/patchnginxlocal.js
+++ scripts/patchnginxlocal.js
@@ -12,7 +12,7 @@ dotenv.set("REACT_APP_BUILD_VERSION", "java_local");
 dotenv.set("REACT_APP_LOCAL_CONTEXT", "/melosys");
 dotenv.set("REACT_APP_GRAPHQL_URL", "/graphql/");
 dotenv.set("REACT_APP_LOCAL_API_PORT", DEFAULT_NGINX_SERVER_PORT);
-dotenv.set("PORT", "3001"); //Endrer port fordi Nginx kjører på port 3000
+dotenv.set("PORT", "3001"); // Endrer port fordi Nginx kjører på port 3000
 
 if (proxy) {
   pkg.proxy = proxy;
