616b9109ab4fe49efee1ac1b45db1c0927518c11
Janita Johansen <janita.marielle.johansen@nav.no>
2022-02-04 14:44:24 +0100

    Sender med nyVurderingBakgrunn ved forhåndsvisning også.

--- src/sider/trygdeavtale/stegKomponenter/vurderingVedtak.tsx
+++ src/sider/trygdeavtale/stegKomponenter/vurderingVedtak.tsx
@@ -213,6 +213,10 @@ const VurderingVedtak = ({
           institusjonId: muligMottaker?.institusjonId || null,
           ektefelleFritekst: familieFormValues?.ektefelle?.fritekst || null,
           barnFritekst: familieFormValues?.barn?.fritekst || null,
+          nyVurderingBakgrunn:
+            formValues?.nyVurderingBakgrunn === FRITEKST
+              ? formValues?.nyVurderingBakgrunnFritekst
+              : formValues?.nyVurderingBakgrunn,
         },
       },
     ];
