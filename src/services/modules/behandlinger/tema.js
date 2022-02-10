424ecf64c3b35345ecd9e290ddc696f8873d44c1
martonSkjevelandNav <marton.skjeveland@nav.no>
2022-02-10 10:02:55 +0100

    Oppdatert endepunkt med - for mellomrom

--- src/services/modules/behandlinger/tema.js
+++ src/services/modules/behandlinger/tema.js
@@ -2,7 +2,7 @@ import { getAsJson, postAsJson } from "../../utils";
 import { API_BASE_URL, BEHANDLINGER } from "../../api-constants";
 
 export const hentMuligeBehandlingstema = (behandlingID) =>
-  getAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/muligebehandlingstema`);
+  getAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/mulige-behandlingstema`);
 
 export const endreBehandlingstema = (behandlingID, behandlingstema) =>
   postAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/endreBehandlingstema`, { behandlingstema });
--- src/services/modules/behandlinger/type.ts
+++ src/services/modules/behandlinger/type.ts
@@ -2,4 +2,4 @@ import { getAsJson } from "../../utils";
 import { API_BASE_URL, BEHANDLINGER } from "../../api-constants";
 
 export const hentMuligeBehandlingstyper = (behandlingID: string) =>
-  getAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/muligetyper`);
+  getAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/mulige-typer`);
