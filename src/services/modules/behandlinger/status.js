89756e5baf7be607b43a00748200e2ba5a6a3d18
martonSkjevelandNav <marton.skjeveland@nav.no>
2022-02-10 09:04:32 +0100

    Oppdatert endepunkt til lowercase

--- src/services/modules/behandlinger/status.js
+++ src/services/modules/behandlinger/status.js
@@ -2,7 +2,7 @@ import { getAsJson, postAsJson } from "../../utils";
 import { API_BASE_URL, BEHANDLINGER } from "../../api-constants";
 
 export const hentMuligeBehandlingsstatuser = (behandlingID) =>
-  getAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/muligeStatuser`);
+  getAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/muligestatuser`);
 
 export const oppdaterStatus = (behandlingID, status) =>
   postAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/status`, { behandlingsstatus: status });
--- src/services/modules/behandlinger/tema.js
+++ src/services/modules/behandlinger/tema.js
@@ -2,7 +2,7 @@ import { getAsJson, postAsJson } from "../../utils";
 import { API_BASE_URL, BEHANDLINGER } from "../../api-constants";
 
 export const hentMuligeBehandlingstema = (behandlingID) =>
-  getAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/muligeBehandlingstema`);
+  getAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/muligebehandlingstema`);
 
 export const endreBehandlingstema = (behandlingID, behandlingstema) =>
   postAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/endreBehandlingstema`, { behandlingstema });
