869db4b66411eba123437ec697ef9e701ba29a6b
Marton Skjæveland <56435736+martonSkjevelandNav@users.noreply.github.com>
2022-02-17 13:17:32 +0100

    Fjernet ANMODNING_OM_UNNTAK_HOVEDREGEL fra nySak i journalføring etter at funksjonalitet er fjernet i backend (#1384)

--- src/sider/journalforing/komponenter/opprettSak.js
+++ src/sider/journalforing/komponenter/opprettSak.js
@@ -18,7 +18,6 @@ const euEosBehandlingstemaer = MKV.KTObjects.behandlinger.behandlingstema.filter
     kode === MKV.Koder.behandlinger.behandlingstema.ARBEID_ETT_LAND_ØVRIG ||
     kode === MKV.Koder.behandlinger.behandlingstema.IKKE_YRKESAKTIV ||
     kode === MKV.Koder.behandlinger.behandlingstema.ARBEID_FLERE_LAND ||
-    kode === MKV.Koder.behandlinger.behandlingstema.ANMODNING_OM_UNNTAK_HOVEDREGEL ||
     kode === MKV.Koder.behandlinger.behandlingstema.ØVRIGE_SED_MED ||
     kode === MKV.Koder.behandlinger.behandlingstema.ØVRIGE_SED_UFM ||
     kode === MKV.Koder.behandlinger.behandlingstema.TRYGDETID
