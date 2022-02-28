01e0e70643be2717f61c5b40f0b90854708437e3
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2022-02-28 15:21:38 +0100

    Slettet overflødig hook

--- src/felleskomponenter/menypanel/menypunkter/editerbartElement/editerbartElement.tsx
+++ src/felleskomponenter/menypanel/menypunkter/editerbartElement/editerbartElement.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ReactNode, MouseEventHandler, MouseEvent, ElementType, useEffect } from "react";
+import React, { ElementType, MouseEvent, MouseEventHandler, ReactNode, useState } from "react";
 import classnames from "classnames";
 
 import * as Nav from "../../../../navFrontend";
