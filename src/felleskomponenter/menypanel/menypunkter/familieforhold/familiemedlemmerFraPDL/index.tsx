03e8f988204c43c94bc9e891099252f7668d9477
Marton Skjæveland <56435736+martonSkjevelandNav@users.noreply.github.com>
2022-03-18 11:51:30 +0100

    Melosys 5064 fjerne pdl toggle (#1441)
    * Fjernet pld-toggle og gammel kode fra person og familieforhold sidemeny
    
    * Fjernet pdl toggle fra personstatus og saksoversikt
    
    * Fjernet toggle melosys.web.fjern_annen_adresse
    
    * Lint feil
    
    * Fikset feilende test
    
    * La tilbake toggle, da den fjernes i annen PR

--- src/felleskomponenter/menypanel/menypunkter/familieforhold/familiemedlemmerFraPDL/index.tsx
+++
@@ -1,3 +0,0 @@
-import FamiliemedlemmerFraPDL from "./familiemedlemmerFraPDL";
-
-export default FamiliemedlemmerFraPDL;
--- src/felleskomponenter/saksoversiktLenke/saksoversiktLenke.tsx
+++ src/felleskomponenter/saksoversiktLenke/saksoversiktLenke.tsx
@@ -8,7 +8,6 @@ import * as Ikon from "../../resources/images";
 import * as Nav from "../../navFrontend";
 
 import { behandlingerOperations, behandlingerSelectors } from "../../ducks/behandlinger";
-import { useFeatureToggle } from "../../featuretoggle";
 import useHentPersonopplysninger from "../personlinje/useHentpersonopplysninger";
 import "./saksoversiktLenke.css";
 
@@ -25,16 +24,13 @@ const connector = connect(mapStateToProps, mapDispatchToProps);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
 const SaksoversiktLenke = ({ behandlingID, hentSaksoversikt }: PropsFromRedux) => {
-  const pdlToggle = useFeatureToggle("melosys.pdl.aktiv");
   const personopplysninger = useHentPersonopplysninger(behandlingID, false);
-
-  const fnr = pdlToggle === "enabled" ? personopplysninger?.fnr : undefined;
 
   return (
     <div className="saksoversiktLenke">
       <Nav.Panel>
         Vis saksoversikt:
-        <Nav.Lenker href="#" onClick={() => hentSaksoversikt(fnr)}>
+        <Nav.Lenker href="#" onClick={() => hentSaksoversikt(personopplysninger?.fnr)}>
           <Ikon.ExternalLink className="ikon" />
           Åpnes i nytt vindu
         </Nav.Lenker>
