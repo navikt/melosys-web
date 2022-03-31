b13bb3ccb6b3ab660a8a0b22b437385acb784e3d
Janita Johansen <janita.marielle.johansen@nav.no>
2022-03-31 11:53:39 +0200

    Skal alltid være liste - uansett om det bare er én feil

---
+++ src/utils/feilmelding.test.tsx
@@ -0,0 +1,40 @@
+import { mount } from "enzyme";
+import { syncErrorsTilFeilmelding } from "./feilmelding";
+
+describe("syncErrorsTilFeilmelding", () => {
+  test("Vanlige sync-errors blir mappet riktig", () => {
+    const syncErrors = {
+      gatenavn: {
+        melding: "Mangler gatenavn",
+      },
+      postkode: {
+        melding: "Mangler postkode",
+      },
+    };
+
+    // @ts-ignore
+    const feilmeldinger = mount(syncErrorsTilFeilmelding(syncErrors));
+    const feilmeldingListe = feilmeldinger.find("li");
+
+    expect(feilmeldingListe).toHaveLength(2);
+    expect(feilmeldingListe.get(0).props.children).toBe("Mangler gatenavn");
+    expect(feilmeldingListe.get(1).props.children).toBe("Mangler postkode");
+  });
+
+  test("form-wide errors blir mappet riktig", () => {
+    const syncErrors = {
+      adresse: {
+        _error: {
+          melding: "Mangler gatenavn",
+        },
+      },
+    };
+
+    // @ts-ignore
+    const feilmeldinger = mount(syncErrorsTilFeilmelding(syncErrors));
+    const feilmeldingListe = feilmeldinger.find("li");
+
+    expect(feilmeldingListe).toHaveLength(1);
+    expect(feilmeldingListe.get(0).props.children).toBe("Mangler gatenavn");
+  });
+});
--- src/utils/feilmelding.tsx
+++ src/utils/feilmelding.tsx
@@ -2,25 +2,18 @@ import React from "react";
 import * as Utils from "./index";
 
 export function syncErrorsTilFeilmelding(
-  syncErrors: { [key: string]: { melding: string; _error: { melding: string } } },
+  syncErrors: { [key: string]: { melding?: string; _error?: { melding: string } } },
   tittel: string = "Følgende feil ble funnet"
 ) {
   if (Utils._isEmpty(syncErrors)) return null;
-
-  const keyArray = Object.keys(syncErrors);
-
-  if (keyArray.length === 1) {
-    const syncError = syncErrors[keyArray[0]];
-    return <p>{Utils._isObject(syncError._error) ? syncError._error.melding : syncError.melding}</p>;
-  }
 
   return (
     <>
       <p>{tittel}</p>
       <ul>
-        {keyArray.map((key) => {
+        {Object.keys(syncErrors).map((key) => {
           const syncError = syncErrors[key];
-          return <li>{Utils._isObject(syncError._error) ? syncError._error.melding : syncError.melding}</li>;
+          return <li key={key}>{Utils._isObject(syncError._error) ? syncError._error.melding : syncError.melding}</li>;
         })}
       </ul>
     </>
