5968998bccadb3075e9f3d1c11bba8a3d8e97477
Janita Johansen <56385061+janitamarielle@users.noreply.github.com>
2022-05-11 17:08:26 +0200

    4766 Feilmelding dersom man ikke kan sende BUC (#1491)
    Løste spinner med omskriving til doThenDispatch. Resten kom på kjøpet etter eksisterende fiks.

--- src/ducks/utpek/operations.js
+++ src/ducks/utpek/operations.js
@@ -1,7 +1,6 @@
 import { doThenDispatch } from "../../services/utils";
 import * as Api from "../../services/api";
 import * as Types from "./types";
-import { behandlingerSelectors } from "../behandlinger";
 import { navigeringOperations } from "../navigering";
 
 export function utpek(saksnummer, body) {
@@ -20,11 +19,18 @@ export function utpek(saksnummer, body) {
   );
 }
 
-export function avvis(body) {
-  return async (dispatch, getState) => {
-    const behandlingID = behandlingerSelectors.BehandlingIDSelector(getState());
-
-    await Api.Saksflyt.Utpeking.avvis(behandlingID, body);
-    dispatch(navigeringOperations.tilForsiden());
-  };
+export function avvis(behandlingID, body) {
+  return doThenDispatch(
+    () => Api.Saksflyt.Utpeking.avvis(behandlingID, body),
+    {
+      OK: Types.OK,
+      FEILET: Types.FEILET,
+      PENDING: Types.PENDING,
+    },
+    {
+      success: (dispatch) => {
+        dispatch(navigeringOperations.tilForsiden());
+      },
+    }
+  );
 }
