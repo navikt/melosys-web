1e1cb0b5d2f45e148f935a0e6f265381b59380d5
Janita Johansen <janita.marielle.johansen@nav.no>
2022-05-12 10:01:39 +0200

    Vi viser ikke "Avslått søknad" dersom det er ny vurdering.

--- src/sider/eu_eøs/saksbehandling/komponenter/saksopplysninger/saksopplysninger.js
+++ src/sider/eu_eøs/saksbehandling/komponenter/saksopplysninger/saksopplysninger.js
@@ -58,9 +58,10 @@ const Saksopplysninger = ({
   }
 
   const erHenlagtSak = fagsakStatusKode === MKV.Koder.saksstatuser.HENLAGT;
+  const erNyVurdering = behandlingstype === MKV.Koder.behandlinger.behandlingstyper.NY_VURDERING;
   const erAvslaattSoknad =
     behandlingsresultat.behandlingsresultatTypeKode ===
-    MKV.Koder.behandlinger.behandlingsresultattyper.AVSLAG_MANGLENDE_OPPL;
+      MKV.Koder.behandlinger.behandlingsresultattyper.AVSLAG_MANGLENDE_OPPL && !erNyVurdering;
   const visAvslaattSoknad = erAvslaattSoknad && !erHenlagtSak;
   const behandlingsgrunnlagErKlart = !(
     Object.keys(soknadForm).length === 0 || Object.keys(behandlingsgrunnlag).length === 0
