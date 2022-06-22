57b5f3095644bf50eb2877b96c3602872531cfa9
Janita Johansen <56385061+janitamarielle@users.noreply.github.com>
2022-06-22 13:54:24 +0200

    5127 DAPI Støtte for virksomhet i Opprett ny sak (#1510)
    * La til støtte for virksomhet i opprettnysak:
    - Henting av oppgaver tar inn orgnr i tillegg til fnrdnr
    - BRUKER og VIRKSOMHET blir hentet fra kodeverk istedenfor egendefinert kode i journalforingform.js
    - Bytter ut BrukerNavnSkjema med egen komponent+useEffects som støtter både bruker og virksomhet
    - Generell renaming av variabler/funksjoner for å tilpasse både bruker og virksomhet
    - Legger til hovedpart for å speile journalføring
    - Feilmeldinger og varsler til saksbehandler endres
    - Skjema tar hensyn til virksomhet
    
    * fnrdnr -> personIdent etter QA

--- src/services/modules/oppgaver.js
+++ src/services/modules/oppgaver.js
@@ -7,5 +7,8 @@ export const sendPlukk = (oppgave) => postAsJson(`${API_BASE_URL}${OPPGAVER}/plu
 
 export const tilbakelegg = (oppgave) => postAsJson(`${API_BASE_URL}${OPPGAVER}/tilbakelegg`, oppgave);
 
-export const sok = (fnrDnr, cacheDuration = 30) =>
-  cachedGetAsJson(`${API_BASE_URL}${OPPGAVER}/sok?fnr=${fnrDnr}`, cacheDuration);
+export const sok = (personIdent, orgnr, cacheDuration = 30) =>
+  cachedGetAsJson(
+    `${API_BASE_URL}${OPPGAVER}/sok?personIdent=${personIdent || ""}&orgnr=${orgnr || ""}`,
+    cacheDuration
+  );
