b073dfca255e299b7dd6476a8be1a8fdc279d63b
Thang Phan <thang.quoc.ly.phan@nav.no>
2022-07-15 10:47:30 +0200

    5189: Fiks av feilmelding i valg av bestemmelse (#1527)
    Legger inn nullable på bestemmelse i vurderingBestemmelseSchema.js. 
    
    ref: https://jira.adeo.no/browse/MELOSYS-5189

--- src/sider/trygdeavtale/stegKomponenter/vurderingBestemmelse/vurderingBestemmelseSchema.js
+++ src/sider/trygdeavtale/stegKomponenter/vurderingBestemmelse/vurderingBestemmelseSchema.js
@@ -6,7 +6,7 @@ const { MAA_FYLLES_UT } = KV.Feilmeldinger;
 const vurdering_bestemmelse = object().shape({
   vedtak: string().required(MAA_FYLLES_UT),
   innvilgelse: string().required(MAA_FYLLES_UT),
-  bestemmelse: string().required(MAA_FYLLES_UT),
+  bestemmelse: string().nullable().required(MAA_FYLLES_UT),
 });
 
 export default vurdering_bestemmelse;
