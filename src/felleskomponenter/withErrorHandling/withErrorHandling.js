3bd493eadabdf308bacd58239a77807af7babbc4
Janita Johansen <janita.marielle.johansen@nav.no>
2022-08-10 13:41:10 +0200

    Fjerner unødvendige kommentarer

--- src/felleskomponenter/withErrorHandling/withErrorHandling.js
+++ src/felleskomponenter/withErrorHandling/withErrorHandling.js
@@ -73,16 +73,12 @@ const ErrorComponentInner = ({ kontekster, WrappedComponent, reduxRootState, ...
     }
   });
 
-  // Dersom ingen feilstatus er funnet returner wrappet component.
   if (feilSamling.length === 0) {
     return <WrappedComponent {...props} />;
   }
 
-  // Finn hvilke feil kode(r) som finnes og legg til kun 1 alert stripe.
-  // Sorter feilkoder med synkende verdi
   feilSamling.sort((a, b) => b.status - a.status);
 
-  // Dersom 404 så skal både alertstripe og kompoonent vises.
   if (feilSamling[0].status === 404) {
     return (
       <div {...props} className="errorContainer">
@@ -90,7 +86,7 @@ const ErrorComponentInner = ({ kontekster, WrappedComponent, reduxRootState, ...
       </div>
     );
   }
-  // alle andre feilkoder gir full stopp uten å vise komponenten.
+
   return (
     <div className="errorContainer">
       <FeilKomponent feilobjekt={feilSamling[0]} />
