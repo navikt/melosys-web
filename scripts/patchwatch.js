ef8406bfb0a9a15987b1d3ea8daa8f5c8d489d48
Isa Salem Hame <wanghattan@gmail.com>
2022-08-17 09:50:53 +0200

    Introduserer en ny måte å legge inn environments på

--- scripts/patchwatch.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require("dotenv-save");
-
-/*
-cra-build-watch setter ikke  PUBLIC_URL for ressurser i index.html(for eksempel frontendlogger, favicon).
-Setter derfor PUBLIC_URL her.
-*/
-
-dotenv.set("PUBLIC_URL", "/melosys");
