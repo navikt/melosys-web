a5e1d687770b56407f103c185ac419eed7a28c04
Janita Johansen <janita.marielle.johansen@nav.no>
2022-09-02 15:22:37 +0200

    Justerer tester

--- src/felleskomponenter/informasjonlinje/behandlingsmeny/avsluttsak.test.tsx
+++ src/felleskomponenter/informasjonlinje/behandlingsmeny/avsluttsak.test.tsx
@@ -17,7 +17,7 @@ const {
   REGISTRERING_UNNTAK_NORSK_TRYGD_UTSTASJONERING,
 } = MKV.Koder.behandlinger.behandlingstema;
 const { NY_VURDERING, FØRSTEGANG, HENVENDELSE, KLAGE } = MKV.Koder.behandlinger.behandlingstyper;
-const { FTRL, TRYGDEAVTALE } = MKV.Koder.sakstyper;
+const { FTRL, TRYGDEAVTALE, EU_EOS } = MKV.Koder.sakstyper;
 const { MEDLEMSKAP_LOVVALG, UNNTAK } = MKV.Koder.sakstemaer;
 const { VURDER_DOKUMENT } = MKV.Koder.behandlinger.behandlingsstatus;
 
@@ -35,9 +35,18 @@ describe("AvsluttSak", () => {
     props = instance(mockedProps);
   });
 
-  it("viser alle valg om FTRL_SAKSBEHANDLING og redigerbart", () => {
+  const setupProps = (initialProps: typeof mockedProps) => {
+    initialProps.redigerbart = true;
+    initialProps.sakstype = EU_EOS;
+    initialProps.sakstema = MEDLEMSKAP_LOVVALG;
+    initialProps.behandlingstema = UTSENDT_ARBEIDSTAKER;
+    initialProps.behandlingstype = FØRSTEGANG;
+  };
+
+  it("viser alle valg dersom behandlingskategori FTRL_SAKSBEHANDLING og redigerbart", () => {
+    setupProps(props);
+    props.sakstype = FTRL;
     props.behandlingstema = ARBEID_I_UTLANDET;
-    props.redigerbart = true;
 
     const avsluttSak = shallow(<AvsluttSak {...props} />);
     const handlinger = avsluttSak.find(Handling);
@@ -48,9 +57,9 @@ describe("AvsluttSak", () => {
     expect(handlinger.at(2).props().tekst).toBe("Behandlingen er bortfalt");
   });
 
-  it("viser bare avsluttSak om tema er trygdetid", () => {
+  it("viser bare avsluttSak dersom tema er trygdetid", () => {
+    setupProps(props);
     props.behandlingstema = TRYGDETID;
-    props.redigerbart = true;
 
     const avsluttSak = shallow(<AvsluttSak {...props} />);
     const handlinger = avsluttSak.find(Handling);
@@ -59,19 +68,20 @@ describe("AvsluttSak", () => {
     expect(handlinger.props().tekst).toBe("Behandlingen er bortfalt");
   });
 
-  it("viser ferdigBehandlet om tema er yrkesaktiv", () => {
+  it("viser ferdigBehandlet dersom tema er yrkesaktiv og type er ny vurdering", () => {
+    setupProps(props);
     props.behandlingstema = YRKESAKTIV;
-    props.redigerbart = true;
     props.behandlingstype = NY_VURDERING;
 
     const avsluttSak = shallow(<AvsluttSak {...props} />);
     const handlinger = avsluttSak.find(Handling);
 
-    expect(handlinger).toHaveLength(5);
-    expect(handlinger.at(2).props().tekst).toBe("Ferdigbehandlet");
+    expect(handlinger).toHaveLength(7);
+    expect(handlinger.at(4).props().tekst).toBe("Ferdigbehandlet");
   });
 
   it("returerer null om EØS_VURDER_UTPEKING og ikke redigerbart", () => {
+    setupProps(props);
     props.redigerbart = false;
     props.behandlingstema = BESLUTNING_LOVVALG_NORGE;
 
@@ -81,7 +91,7 @@ describe("AvsluttSak", () => {
   });
 
   it(`viser ikke 'Avslutt sak som bortfalt' dersom behandlingstema er ${UTSENDT_ARBEIDSTAKER} og behandlingstype er ${NY_VURDERING}`, () => {
-    props.redigerbart = true;
+    setupProps(props);
     props.behandlingstema = UTSENDT_ARBEIDSTAKER;
     props.behandlingstype = NY_VURDERING;
 
@@ -95,6 +105,7 @@ describe("AvsluttSak", () => {
 
   describe("Behandlingen er bortfalt", () => {
     it(`viser 'Behandlingen er bortfalt' dersom behandlingstema er ${REGISTRERING_UNNTAK_NORSK_TRYGD_ØVRIGE} og behandlingsstatus er ${VURDER_DOKUMENT}`, () => {
+      setupProps(props);
       props.behandlingstema = REGISTRERING_UNNTAK_NORSK_TRYGD_ØVRIGE;
       props.behandlingsstatus = VURDER_DOKUMENT;
 
@@ -106,6 +117,7 @@ describe("AvsluttSak", () => {
     });
 
     it(`viser 'Behandlingen er bortfalt' dersom behandlingstema er ${REGISTRERING_UNNTAK_NORSK_TRYGD_UTSTASJONERING} og behandlingsstatus er ${VURDER_DOKUMENT}`, () => {
+      setupProps(props);
       props.behandlingstema = REGISTRERING_UNNTAK_NORSK_TRYGD_UTSTASJONERING;
       props.behandlingsstatus = VURDER_DOKUMENT;
 
@@ -119,21 +131,19 @@ describe("AvsluttSak", () => {
 
   describe("Søknaden er avslått", () => {
     it(`viser 'Søknaden er avslått' dersom behandlingstype er ${FØRSTEGANG} og behandlingstema er ${YRKESAKTIV}`, () => {
-      props.redigerbart = true;
-      props.sakstema = MEDLEMSKAP_LOVVALG;
+      setupProps(props);
       props.behandlingstype = FØRSTEGANG;
       props.behandlingstema = YRKESAKTIV;
 
       const avsluttSak = shallow(<AvsluttSak {...props} />);
       const handlinger = avsluttSak.find(Handling);
 
-      expect(handlinger).toHaveLength(4);
-      expect(handlinger.at(0).props().tekst).toBe("Søknaden er avslått");
+      expect(handlinger).toHaveLength(5);
+      expect(handlinger.at(1).props().tekst).toBe("Søknaden er avslått");
     });
 
     it(`viser ikke 'Søknaden er avslått' dersom behandlingstype er ${HENVENDELSE} og behandlingstema er ${YRKESAKTIV}`, () => {
-      props.redigerbart = true;
-      props.sakstema = MEDLEMSKAP_LOVVALG;
+      setupProps(props);
       props.behandlingstype = HENVENDELSE;
       props.behandlingstema = YRKESAKTIV;
 
@@ -146,10 +156,8 @@ describe("AvsluttSak", () => {
     });
 
     it(`viser ikke 'Søknaden er avslått' dersom sakstema er ${UNNTAK}`, () => {
-      props.redigerbart = true;
+      setupProps(props);
       props.sakstema = UNNTAK;
-      props.behandlingstype = FØRSTEGANG;
-      props.behandlingstema = YRKESAKTIV;
 
       const avsluttSak = shallow(<AvsluttSak {...props} />);
       const soknadenErAvslatt = avsluttSak.findWhere(
@@ -160,10 +168,8 @@ describe("AvsluttSak", () => {
     });
 
     it(`viser ikke 'Søknaden er avslått' dersom redigerbart er false`, () => {
+      setupProps(props);
       props.redigerbart = false;
-      props.sakstema = MEDLEMSKAP_LOVVALG;
-      props.behandlingstype = FØRSTEGANG;
-      props.behandlingstema = YRKESAKTIV;
 
       const avsluttSak = shallow(<AvsluttSak {...props} />);
       const handlinger = avsluttSak.find(Handling);
@@ -174,8 +180,7 @@ describe("AvsluttSak", () => {
 
   describe("Søknaden er innvilget", () => {
     it(`viser 'Søknaden er innvilget' dersom sakstype er ${FTRL} og behandlingstype er ${FØRSTEGANG} og behandlingstema er ${YRKESAKTIV}`, () => {
-      props.redigerbart = true;
-      props.sakstema = MEDLEMSKAP_LOVVALG;
+      setupProps(props);
       props.sakstype = FTRL;
       props.behandlingstype = FØRSTEGANG;
       props.behandlingstema = YRKESAKTIV;
@@ -188,8 +193,7 @@ describe("AvsluttSak", () => {
     });
 
     it(`viser 'Søknaden er innvilget' dersom sakstype er ${TRYGDEAVTALE} og behandlingstype er ${FØRSTEGANG} og behandlingstema er ${YRKESAKTIV}`, () => {
-      props.redigerbart = true;
-      props.sakstema = MEDLEMSKAP_LOVVALG;
+      setupProps(props);
       props.sakstype = TRYGDEAVTALE;
       props.behandlingstype = FØRSTEGANG;
       props.behandlingstema = YRKESAKTIV;
@@ -202,8 +206,7 @@ describe("AvsluttSak", () => {
     });
 
     it(`viser ikke 'Søknaden er innvilget' dersom sakstype er ${TRYGDEAVTALE} og behandlingstype er ${HENVENDELSE} og behandlingstema er ${YRKESAKTIV}`, () => {
-      props.redigerbart = true;
-      props.sakstema = MEDLEMSKAP_LOVVALG;
+      setupProps(props);
       props.sakstype = TRYGDEAVTALE;
       props.behandlingstype = HENVENDELSE;
       props.behandlingstema = YRKESAKTIV;
@@ -217,11 +220,8 @@ describe("AvsluttSak", () => {
     });
 
     it(`viser ikke 'Søknaden er innvilget' dersom sakstema er ${UNNTAK}`, () => {
-      props.redigerbart = true;
+      setupProps(props);
       props.sakstema = UNNTAK;
-      props.sakstype = TRYGDEAVTALE;
-      props.behandlingstype = FØRSTEGANG;
-      props.behandlingstema = YRKESAKTIV;
 
       const avsluttSak = shallow(<AvsluttSak {...props} />);
       const soknadenErInnvilget = avsluttSak.findWhere(
@@ -232,11 +232,8 @@ describe("AvsluttSak", () => {
     });
 
     it(`viser ikke 'Søknaden er innvilget' dersom redigerbart er false`, () => {
+      setupProps(props);
       props.redigerbart = false;
-      props.sakstema = MEDLEMSKAP_LOVVALG;
-      props.sakstype = TRYGDEAVTALE;
-      props.behandlingstype = FØRSTEGANG;
-      props.behandlingstema = YRKESAKTIV;
 
       const avsluttSak = shallow(<AvsluttSak {...props} />);
       const handlinger = avsluttSak.find(Handling);
@@ -247,8 +244,7 @@ describe("AvsluttSak", () => {
 
   describe("Klage-handlinger", () => {
     it(`viser Klage-handlinger dersom behandlingstype er ${KLAGE}`, () => {
-      props.redigerbart = true;
-      props.behandlingstema = YRKESAKTIV;
+      setupProps(props);
       props.behandlingstype = KLAGE;
 
       const avsluttSak = shallow(<AvsluttSak {...props} />);
@@ -261,8 +257,7 @@ describe("AvsluttSak", () => {
     });
 
     it(`viser ikke Klage-handlinger dersom behandlingstype er ${FØRSTEGANG}`, () => {
-      props.redigerbart = true;
-      props.behandlingstema = YRKESAKTIV;
+      setupProps(props);
       props.behandlingstype = FØRSTEGANG;
 
       const avsluttSak = shallow(<AvsluttSak {...props} />);
@@ -285,8 +280,7 @@ describe("AvsluttSak", () => {
 
   describe("Vedtaket er omgjort (fvl § 35)", () => {
     it(`viser 'Vedtaket er omgjort (fvl § 35)' dersom behandlingstype er ${NY_VURDERING}`, () => {
-      props.redigerbart = true;
-      props.behandlingstema = YRKESAKTIV;
+      setupProps(props);
       props.behandlingstype = NY_VURDERING;
 
       const avsluttSak = shallow(<AvsluttSak {...props} />);
@@ -297,8 +291,7 @@ describe("AvsluttSak", () => {
     });
 
     it(`viser ikke 'Vedtaket er omgjort (fvl § 35)' dersom behandlingstype er ${FØRSTEGANG}`, () => {
-      props.redigerbart = true;
-      props.behandlingstema = YRKESAKTIV;
+      setupProps(props);
       props.behandlingstype = FØRSTEGANG;
 
       const avsluttSak = shallow(<AvsluttSak {...props} />);
