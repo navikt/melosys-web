9eb6a1f049e813ac78b6d871cab9c34251a426c5
Thang Phan <thang.quoc.ly.phan@nav.no>
2022-09-13 12:28:24 +0200

    Endre til saksbehandling url

--- src/services/api-constants.js
+++ src/services/api-constants.js
@@ -35,4 +35,4 @@ export const UTPEKINGSPERIODER = "utpekingsperioder";
 export const VEDTAK = "vedtak";
 export const VILKAAR = "vilkaar";
 export const INNGANGSVILKAAR = "inngangsvilkaar";
-export const LOVLIGE_KOMBINASJONER = "lovligekombinasjoner";
+export const SAKSBEHANDLING = "saksbehandling";
