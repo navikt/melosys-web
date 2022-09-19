2e404a143c72785b545a106c13fc1b0e92e884da
Ivan Skodje <ivanskodje@protonmail.com>
2022-09-19 10:50:40 +0200

    Tatt vekk logger som ser ut til å ødelegge for Build regel?

--- src/ducks/lovvalgsperioder/operations.js
+++ src/ducks/lovvalgsperioder/operations.js
@@ -169,11 +169,8 @@ const skalByggeLovvalgsperiodeForArtikkel16_1 = (reduxState) => {
 };
 
 const byggLovvalgsPeriodeArtikkel16_1 = (stegState, reduxState) => {
-  console.log("byggLovvalgsPeriodeARtikkel16_1 start");
   erFeatureToggleEnabled("melosys.5278.art16lovvalgsperiodefiks").then((toggleEnabled) => {
-    console.log("byggLovvalgsPeriodeARtikkel16_1: Har mottatt promise, fortsetter med å sjekke toggle");
     if (!toggleEnabled) {
-      console.log("byggLovvalgsPeriodeARtikkel16_1: Toggle er IKKE på!");
       const erAnmodningsperiodeSendtUtland =
         anmodningsperioderSelectors.AnmodningsperioderErSendtUtlandetSelector(reduxState);
       const erBehandlingsstatusUnderBehandlingEllerAvsluttet = [
@@ -186,8 +183,6 @@ const byggLovvalgsPeriodeArtikkel16_1 = (stegState, reduxState) => {
       }
       return [];
     }
-
-    console.log("byggLovvalgsPeriodeARtikkel16_1: Toggle er PÅ!");
 
     if (!skalByggeLovvalgsperiodeForArtikkel16_1(reduxState)) {
       return [];
