6149074d8f9827d264a55f3b9da310111e58ee8e
Janita Johansen <janita.marielle.johansen@nav.no>
2022-09-30 10:03:02 +0200

    Fotknapper får egen css

--- src/sider/journalforing/komponenter/fotknapper.tsx
+++ src/sider/journalforing/komponenter/fotknapper.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import * as Mui from "../../../felleskomponenter/ui";
 
+import "./fotknapper.css";
+
 interface FotknapperProps {
   avbrytJournalforing: () => void;
   kanSubmittes: boolean;
@@ -9,11 +11,11 @@ interface FotknapperProps {
 }
 
 const Fotknapper = ({ avbrytJournalforing, kanSubmittes, spinner = false }: FotknapperProps) => (
-  <div className="journalforing__fotknapper">
+  <div className="fotknapper">
     <Mui.Knapp type="hoved" htmlType="submit" disabled={!kanSubmittes} spinner={spinner} autoDisableVedSpinner>
       Journalfør
     </Mui.Knapp>
-    <Mui.Knapp type="flat" className="journalforing__fotknapper__avbryt" onClick={avbrytJournalforing}>
+    <Mui.Knapp type="flat" className="fotknapper__avbryt" onClick={avbrytJournalforing}>
       Avbryt
     </Mui.Knapp>
   </div>
