4307c1a8b116b424f15425c7979a0d3a64876faf
Fredrik Olsen Hauge <45845087+fredrik-oh@users.noreply.github.com>
2022-10-11 09:44:17 +0200

    5375 Horisontal scrolling i <table> elementer (#1626)
    * La til en wrapper div rundt alle forekomster av <table> elementer og la på overflow-y: auto
    
    * Endret overflow-y til overflow-x for table wrapper elements, oops
    
    Co-authored-by: Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>

--- src/felleskomponenter/menypanel/menypunkter/arbeidssteder/skip/redigeringUtfort.tsx
+++ src/felleskomponenter/menypanel/menypunkter/arbeidssteder/skip/redigeringUtfort.tsx
@@ -12,26 +12,30 @@ import "./redigeringUtfort.css";
 const cls = classNames("tabell", "arbeidssted__utland__redigeringutfort");
 
 const RedigeringUtfort = ({ verdier }: EnRedigeringsknappListeRedigeringUtfort<KV.Form.ArbeidsstedSkip>) => (
-  <table className={cls}>
-    <thead>
-      <tr>
-        <th>Navn på skip</th>
-        <th>Fartsområde</th>
-        <th>Flaggstat/lands territorialfarvann</th>
-      </tr>
-    </thead>
-    <tbody>
-      {verdier.map((element, index) => (
-        /* eslint-disable-next-line react/no-array-index-key */
-        <tr key={index}>
-          <td>{element.enhetNavn}</td>
-          <td>{KV.kodeTilTerm(element.fartsomradeKode, MKV.KTObjects.begrunnelser.fartsomrader)}</td>
-          {element.flaggLandkode && <td>{KV.kodeTilTerm(element.flaggLandkode, MKV.KTObjects.landkoder)}</td>}
-          {element.territorialfarvann && <td>{KV.kodeTilTerm(element.territorialfarvann, MKV.KTObjects.landkoder)}</td>}
+  <div className="arbeidssted__utland__redigeringutfort-wrapper">
+    <table className={cls}>
+      <thead>
+        <tr>
+          <th>Navn på skip</th>
+          <th>Fartsområde</th>
+          <th>Flaggstat/lands territorialfarvann</th>
         </tr>
-      ))}
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        {verdier.map((element, index) => (
+          /* eslint-disable-next-line react/no-array-index-key */
+          <tr key={index}>
+            <td>{element.enhetNavn}</td>
+            <td>{KV.kodeTilTerm(element.fartsomradeKode, MKV.KTObjects.begrunnelser.fartsomrader)}</td>
+            {element.flaggLandkode && <td>{KV.kodeTilTerm(element.flaggLandkode, MKV.KTObjects.landkoder)}</td>}
+            {element.territorialfarvann && (
+              <td>{KV.kodeTilTerm(element.territorialfarvann, MKV.KTObjects.landkoder)}</td>
+            )}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
 );
 
 export default RedigeringUtfort;
