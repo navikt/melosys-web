0be17be78739d78a2a9a585ae26ab671d4055ea3
Janita Johansen <janita.marielle.johansen@nav.no>
2022-10-17 10:35:51 +0200

    Skrev om til typescript

--- src/felleskomponenter/menypanel/menypunkter/utenlandsoppdraget/soknadsperiode/soknadsperiode.tsx
+++ src/felleskomponenter/menypanel/menypunkter/utenlandsoppdraget/soknadsperiode/soknadsperiode.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { connect } from "react-redux";
-
-import PT from "prop-types";
+import { connect, ConnectedProps } from "react-redux";
+import { RootState } from "AppTypes";
+import { ThunkDispatch } from "redux-thunk";
+import { Action } from "redux";
 
 import MKV from "../../../../../melosyskodeverk";
 import * as Utils from "../../../../../utils";
 import * as Nav from "../../../../../navFrontend";
 import * as Symboler from "../../symboler";
 
+import { Periode } from "../../../../../services/modules/behandlingsgrunnlag/types";
 import { behandlingsgrunnlagSelectors, behandlingsgrunnlagOperations } from "../../../../../ducks/behandlingsgrunnlag";
 import { formSelectors } from "../../../../../ducks/form";
 import { erFeatureToggleEnabled } from "../../../../../featuretoggle";
@@ -15,6 +17,28 @@ import { erFeatureToggleEnabled } from "../../../../../featuretoggle";
 import SoknadsperiodeEndring from "./soknadsperiodeEndring";
 
 import "./soknadsperiode.css";
+
+const mapStateToProps = (state: RootState) => ({
+  soknadsperiodeFom: Utils.dato.formatterDatoTilNorsk(behandlingsgrunnlagSelectors.PeriodeSelector(state).fom),
+  soknadsperiodeTom: Utils.dato.formatterDatoTilNorsk(behandlingsgrunnlagSelectors.PeriodeSelector(state).tom),
+  soknadsperiodeFomErrors: formSelectors.SoknadsperiodeFomErrorsSelector(state),
+  soknadsperiodeTomErrors: formSelectors.SoknadsperiodeTomErrorsSelector(state),
+  behandlingHarLand: behandlingsgrunnlagSelectors.HarLandSelector(state),
+});
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<RootState, unknown, Action>) => ({
+  oppdaterPeriode: (periode: Periode) => dispatch(behandlingsgrunnlagOperations.oppdaterPeriode(periode)),
+});
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+type SoknadsperiodeProps = PropsFromRedux & {
+  redigerbart: boolean;
+  lagreSoknadOgOppfriskSaksopplysninger: () => void;
+  tittel: string;
+};
 
 export const Soknadsperiode = ({
   redigerbart,
@@ -24,14 +48,14 @@ export const Soknadsperiode = ({
   lagreSoknadOgOppfriskSaksopplysninger,
   behandlingHarLand,
   ...props
-}) => {
+}: SoknadsperiodeProps) => {
   const [erEndrePeriodeSynlig, setErEndrePeriodeSynlig] = useState(false);
   const [soknadsperiodeFom, setSoknadsperiodeFom] = useState(props.soknadsperiodeFom);
   const [soknadsperiodeTom, setSoknadsperiodeTom] = useState(props.soknadsperiodeTom);
   const [soknadsperiodeGammelFom, setSoknadsperiodeGammelFom] = useState(props.soknadsperiodeFom);
   const [soknadsperiodeGammelTom, setSoknadsperiodeGammelTom] = useState(props.soknadsperiodeTom);
 
-  const oppdaterPeriode = (fom, tom) => {
+  const oppdaterPeriode = (fom: string, tom: string) => {
     props.oppdaterPeriode({
       fom: fom ? Utils.dato.formatterDatoTilISO(fom) : "",
       tom: tom ? Utils.dato.formatterDatoTilISO(tom) : "",
@@ -108,7 +132,6 @@ export const Soknadsperiode = ({
           soknadsperiodeTom={soknadsperiodeTom}
           soknadsperiodeFomErrors={soknadsperiodeFomErrors}
           soknadsperiodeTomErrors={soknadsperiodeTomErrors}
-          skjulEndrePeriode={skjulEndrePeriode}
           lagre={lagre}
           setSoknadsperiodeFom={setSoknadsperiodeFom}
           setSoknadsperiodeTom={setSoknadsperiodeTom}
@@ -119,33 +142,4 @@ export const Soknadsperiode = ({
   );
 };
 
-Soknadsperiode.propTypes = {
-  redigerbart: PT.bool.isRequired,
-  oppdaterPeriode: PT.func.isRequired,
-  lagreSoknadOgOppfriskSaksopplysninger: PT.func.isRequired,
-  soknadsperiodeFom: PT.string.isRequired,
-  soknadsperiodeTom: PT.string.isRequired,
-  soknadsperiodeFomErrors: PT.string,
-  soknadsperiodeTomErrors: PT.string,
-  tittel: PT.string.isRequired,
-  behandlingHarLand: PT.bool.isRequired,
-};
-
-Soknadsperiode.defaultProps = {
-  soknadsperiodeFomErrors: undefined,
-  soknadsperiodeTomErrors: undefined,
-};
-
-const mapStateToProps = (state) => ({
-  soknadsperiodeFom: Utils.dato.formatterDatoTilNorsk(behandlingsgrunnlagSelectors.PeriodeSelector(state).fom),
-  soknadsperiodeTom: Utils.dato.formatterDatoTilNorsk(behandlingsgrunnlagSelectors.PeriodeSelector(state).tom),
-  soknadsperiodeFomErrors: formSelectors.SoknadsperiodeFomErrorsSelector(state),
-  soknadsperiodeTomErrors: formSelectors.SoknadsperiodeTomErrorsSelector(state),
-  behandlingHarLand: behandlingsgrunnlagSelectors.HarLandSelector(state),
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  oppdaterPeriode: (periode) => dispatch(behandlingsgrunnlagOperations.oppdaterPeriode(periode)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Soknadsperiode);
+export default connector(Soknadsperiode);
