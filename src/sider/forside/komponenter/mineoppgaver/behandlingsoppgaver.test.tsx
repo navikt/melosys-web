7ba6e53efb58601f09e7b80e8961820bfd61785a
barstadEkstern <oystein.madsen.barstad@nav.no>
2022-10-28 12:34:02 +0200

    Lag mock for dispatch

--- src/sider/forside/komponenter/mineoppgaver/behandlingsoppgaver.test.tsx
+++ src/sider/forside/komponenter/mineoppgaver/behandlingsoppgaver.test.tsx
@@ -3,6 +3,7 @@ import { shallow } from "enzyme";
 import SorterbarListe from "../../../../felleskomponenter/sorterbarListe";
 import { BehandlingOppgaver } from "./behandlingOppgaver";
 import BehandlingOppgave from "../../../../felleskomponenter/oppgaveliste/behandlingOppgave";
+import * as redux from "react-redux";
 
 describe("Behandlingsoppgaver", () => {
   const saksbehandling = [
@@ -53,12 +54,18 @@ describe("Behandlingsoppgaver", () => {
   };
 
   it("viser en OppgaverMedSortering for journalføringsoppgaver", () => {
+    const useDispatchSpy = jest.spyOn(redux, "useDispatch");
+    const mockDispatchFn = jest.fn();
+    useDispatchSpy.mockReturnValue(mockDispatchFn);
+
     // @ts-ignore
-    const saksbehandlingsOppgaver = shallow(<BehandlingOppgaver {...props} />);
+    const saksbehandlingsOppgaver = shallow(<BehandlingOppgaver dispatch={mockDispatchFn} {...props} />);
     const behandlingsOppgaver = saksbehandlingsOppgaver.find(SorterbarListe).first();
 
     const journalforingOppgaverProps = behandlingsOppgaver.props();
     expect(journalforingOppgaverProps.component).toBe(BehandlingOppgave);
     expect(journalforingOppgaverProps.elementer).toBe(props.mineSaker.saksbehandling);
+
+    useDispatchSpy.mockClear();
   });
 });
