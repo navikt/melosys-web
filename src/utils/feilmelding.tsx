a61e19a85e83be121467b7060b3bb49f957f7e7e
Janita Johansen <janita.marielle.johansen@nav.no>
2022-11-02 07:40:07 +0100

    Når feil er en Array har den ikke verdien "melding". Det var dette som skapte felilen.

--- src/utils/feilmelding.tsx
+++ src/utils/feilmelding.tsx
@@ -9,7 +9,7 @@ export function syncErrorsTilFeilmelding(
   if (Utils._isEmpty(syncErrors)) return null;
 
   const finnFeilmelding = (feil: any): any => {
-    if (Utils._isString(feil.melding)) {
+    if (Utils._isString(feil?.melding)) {
       return <li key={feil.melding}>{feil.melding}</li>;
     }
 
