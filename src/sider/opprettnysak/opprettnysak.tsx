29c54b872f217fd80b21c31fb2bb914242d4e8c1
Janita Johansen <janita.marielle.johansen@nav.no>
2022-11-03 12:16:57 +0100

    Feilmeldinge skal resettes / nullstilles etter bytte fra opprett sak til eksisterende sak

--- src/sider/opprettnysak/opprettnysak.tsx
+++ src/sider/opprettnysak/opprettnysak.tsx
@@ -270,6 +270,7 @@ const OpprettNySak = ({
     nullstillFelt("periodeTilOgMed");
     nullstillFelt("soknadslandUkjenteEllerAlleEosLand");
     nullstillFelt("soknadsland", []);
+    setVisFeilmeldinger(false);
   };
 
   const hovedpartErBruker = hovedpart === BRUKER;
