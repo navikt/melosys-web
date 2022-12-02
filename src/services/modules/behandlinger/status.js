2fcb1a917d143af088abb91b0494c4aac02e5503
Janita Johansen <janita.marielle.johansen@nav.no>
2022-12-02 14:46:43 +0100

    Fjerner ubrukte endepunkt

--- src/services/modules/behandlinger/status.js
+++ src/services/modules/behandlinger/status.js
@@ -1,8 +1,5 @@
-import { getAsJson, postAsJson } from "../../utils";
+import { getAsJson } from "../../utils";
 import { API_BASE_URL, BEHANDLINGER } from "../../api-constants";
 
 export const hentMuligeBehandlingsstatuser = (behandlingID) =>
   getAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/mulige-statuser`);
-
-export const oppdaterStatus = (behandlingID, status) =>
-  postAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/status`, { behandlingsstatus: status });
