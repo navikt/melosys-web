68ce386eb0438bcd9bd4878ea83c3164ef82c75c
Janita Johansen <janita.marielle.johansen@nav.no>
2022-12-09 12:45:11 +0100

    Hadde sendt objekt istedenfor kode ned til endringerErBegrenset som sjekker om man kan gjøre endringer med visse behandlingstema.

--- src/felleskomponenter/oppsummering/endreBehandlingModal.tsx
+++ src/felleskomponenter/oppsummering/endreBehandlingModal.tsx
@@ -199,7 +199,10 @@ function EndreBehandlingModal({
   };
 
   const viserAlert = behandlingEndret || generellFeil?.length > 0;
-  const endringerErBegrenset = erBehandlingstemaMedBegrensetRettigheter(oppsummering.behandlingstema, fagsak.sakstype);
+  const endringerErBegrenset = erBehandlingstemaMedBegrensetRettigheter(
+    oppsummering.behandlingstema?.kode,
+    fagsak.sakstype?.kode
+  );
 
   const nullstillSak = (steg: FeltVerdier): void => {
     switch (steg) {
