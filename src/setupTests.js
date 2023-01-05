ce5d71ee9c3229cb5bce5b342c75f81cb7327ff3
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2023-01-05 14:51:43 +0100

    Tester eslint sjekk av testfiler

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 import Enzyme, { shallow, mount } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import each from "jest-each";
 
 // Feiler tester som kaster en proptype-error
