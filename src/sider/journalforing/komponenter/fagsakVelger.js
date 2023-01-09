673c9e9a3a7c4f6a7e39923d2ade3a4e25401679
Janita Johansen <janita.marielle.johansen@nav.no>
2023-01-09 12:53:50 +0100

    Logikk-feil ved fjerning av toggle

--- src/sider/journalforing/komponenter/fagsakVelger.js
+++ src/sider/journalforing/komponenter/fagsakVelger.js
@@ -90,21 +90,19 @@ const FagsakVelger = (props) => {
         </div>
       )}
 
-      {!erOpprettNySak && (
-        <>
-          {valgtVisning === EKSISTERENDE && (
-            <Skjema.CustomRadioPanelGruppe
-              feltNavn="saksnummer"
-              radios={radioValg}
-              notify={notifier}
-              begrensVisteRadios
-              onChange={nullstillFormVerdier}
-              className="marginMellomCustomRadioPaneler"
-            />
-          )}
-          {valgtVisning === OPPRETT && <OpprettSak formValues={formValues} feltNavn={feltNavn} />}
-        </>
-      )}
+      <>
+        {valgtVisning === EKSISTERENDE && (
+          <Skjema.CustomRadioPanelGruppe
+            feltNavn="saksnummer"
+            radios={radioValg}
+            notify={notifier}
+            begrensVisteRadios
+            onChange={nullstillFormVerdier}
+            className="marginMellomCustomRadioPaneler"
+          />
+        )}
+        {valgtVisning === OPPRETT && <OpprettSak formValues={formValues} feltNavn={feltNavn} />}
+      </>
     </div>
   );
 };
