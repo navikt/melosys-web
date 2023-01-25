292474a4fccb5673228d8978f898b957d3cb2e93
barstadEkstern <oystein.madsen.barstad@nav.no>
2023-01-25 16:23:54 +0100

    wip

--- src/sider/ikkeYrkesaktivFlyt/stegKomponenter/index.ts
+++
@@ -1,5 +0,0 @@
-import VurderingBestemmelse from "./vurderingBestemmelse/vurderingBestemmelse";
-import VurderingInngang from "./vurderingInngang/vurderingInngang";
-import VurderingVedtak from "./vurderingVedtak/vurderingVedtak";
-
-export { VurderingBestemmelse, VurderingInngang, VurderingVedtak };
