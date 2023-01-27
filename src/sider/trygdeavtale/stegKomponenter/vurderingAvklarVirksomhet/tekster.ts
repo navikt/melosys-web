5415aad0ea13cab70c958219a84537f67a11f6bb
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2023-01-27 09:17:32 +0100

    Flyttet tekstkonstanter ut av tsx filer i trygdeavtale\stegkomponenter

---
+++ src/sider/trygdeavtale/stegKomponenter/vurderingAvklarVirksomhet/tekster.ts
@@ -0,0 +1,4 @@
+export const HJELPETEKST =
+  'Velg virksomhet søker er ansatt av og arbeider for i søknadsperioden. Du kan kun velge én virksomhet. Hvis søker arbeider for en virksomhet som ikke er synlig her, må du legge den til i sidemenyen under "Arbeidsgiver/virksomhet".';
+export const INGEN_VIRKSOMHETER_TEKST =
+  'Det er ingen virksomhet registrert. Du må legge til virksomhet under "Arbeidsgiver/virksomhet".';
---
+++ src/sider/trygdeavtale/stegKomponenter/vurderingFamilie/tekster.ts
@@ -0,0 +1,6 @@
+export const OBS_TEKST =
+  '* Hvis dette ikke stemmer, må du legge inn nødvendig informasjon i menypunktet "Familieforhold".';
+export const HJELPETEKST =
+  "Teksten du skriver her vil vises etter begrunnelsen du har valgt. Eksempel:\n\n" +
+  '"Kari Nordmann f.nr xxxxxx xxxxx er over 18 år, og må derfor sende inn egen søknad om medlemskap i folketrygden."\n\n' +
+  "Teksten din kommer etter dette.";
