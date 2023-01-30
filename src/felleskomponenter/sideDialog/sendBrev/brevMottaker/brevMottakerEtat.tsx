4bdfde251107b781462497783f77af675a600cf6
Janita Johansen <janita.marielle.johansen@nav.no>
2023-01-30 09:15:08 +0100

    Sette etater fra formvalues

--- src/felleskomponenter/sideDialog/sendBrev/brevMottaker/brevMottakerEtat.tsx
+++ src/felleskomponenter/sideDialog/sendBrev/brevMottaker/brevMottakerEtat.tsx
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from "react";
+import { connect, ConnectedProps, useDispatch } from "react-redux";
+import { change, getFormValues } from "redux-form";
+import { RootState } from "AppTypes";
 
-import { useDispatch } from "react-redux";
-import { change } from "redux-form";
 import * as KV from "../../../../kodeverk";
 import * as Api from "../../../../services/api";
 import * as Nav from "../../../../navFrontend";
+import { SendBrevFormValues } from "../types";
 
 export const erEtat = (rolle: string | undefined) => rolle === KV.Koder.MottakerRolle.ETAT;
 
-const BrevMottakerEtat = () => {
+const mapStateToProps = (state: RootState) => ({
+  formValues: getFormValues(KV.Form.SEND_BREV)(state) as SendBrevFormValues,
+});
+const connector = connect(mapStateToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+const BrevMottakerEtat = ({ formValues }: PropsFromRedux) => {
   const [tilgjengeligeEtater, setTilgjengeligeEtater] = useState<Api.DokumenterV2.TilgjengeligeEtaterResDto>();
-  const [valgteEtater, setValgteEtater] = useState<string[]>([]);
+  const [valgteEtater, setValgteEtater] = useState<string[]>(formValues?.etater || []);
 
   const dispatch = useDispatch();
 
@@ -35,14 +43,18 @@ const BrevMottakerEtat = () => {
       <Nav.Typo.Element className="brevmottaker__etat">Hvilke etater skal brevet sendes til?</Nav.Typo.Element>
       <Nav.Row>
         <Nav.Column xs="12">
-          {tilgjengeligeEtater &&
-            tilgjengeligeEtater.map((etat) => (
-              <Nav.Checkbox label={etat.navn} key={etat.orgnr} onChange={() => handleCheckboxChange(etat.orgnr)} />
-            ))}
+          {tilgjengeligeEtater?.map((etat) => (
+            <Nav.Checkbox
+              label={etat.navn}
+              key={etat.orgnr}
+              checked={valgteEtater.includes(etat.orgnr)}
+              onChange={() => handleCheckboxChange(etat.orgnr)}
+            />
+          ))}
         </Nav.Column>
       </Nav.Row>
     </>
   );
 };
 
-export default BrevMottakerEtat;
+export default connector(BrevMottakerEtat);
