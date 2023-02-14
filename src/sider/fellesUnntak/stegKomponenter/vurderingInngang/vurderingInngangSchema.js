1355c6b6b3e83cdaeb8f6555239d42cc6809de97
Isa Salem Hame <wanghattan@gmail.com>
2023-02-14 15:29:26 +0100

    Changes by janitamariellejohansen

--- src/sider/fellesUnntak/stegKomponenter/vurderingInngang/vurderingInngangSchema.js
+++ src/sider/fellesUnntak/stegKomponenter/vurderingInngang/vurderingInngangSchema.js
@@ -1,13 +1,19 @@
 import { object, string } from "yup";
+import MKV from "../../../../melosyskodeverk";
 import * as KV from "../../../../kodeverk";
 
 const { MAA_FYLLES_UT, TIDLIGERE_ENN_FOM } = KV.Feilmeldinger;
 const ARBEIDSLAND_FELT_KREVES = { melding: "Du må velge avsenderland" };
+const LOVVALGSLAND_FELT_KREVES = { melding: "Du må velge lovvalgsland" };
 
 const vurdering_inngang = object().shape({
   fom: string().erGyldigDato().required(MAA_FYLLES_UT),
   tom: string().erGyldigDato().erEtterDatofelt("fom", TIDLIGERE_ENN_FOM).nullable(),
   avsenderland: string().required(ARBEIDSLAND_FELT_KREVES),
+  lovvalgsland: string().when("$sakstype", {
+    is: MKV.Koder.sakstyper.EU_EOS,
+    then: string().required(LOVVALGSLAND_FELT_KREVES),
+  }),
 });
 
 export default vurdering_inngang;
--- src/sider/fellesUnntak/stegVelger.tsx
+++ src/sider/fellesUnntak/stegVelger.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import StegLinje from "../../felleskomponenter/stegLinje";
 import { FANE_STATUS } from "../../felleskomponenter/stegvelger";
