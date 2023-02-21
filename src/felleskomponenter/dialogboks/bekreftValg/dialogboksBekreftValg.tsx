8450eec4389aa00bbc7d87f502bb7cf3f325c32e
Isa Salem Hame <wanghattan@gmail.com>
2023-02-21 09:59:33 +0100

    5650: Gjør dialogboksBekreftValg til Typescript og bruker useSelectors

---
+++ src/felleskomponenter/dialogboks/bekreftValg/dialogboksBekreftValg.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import PT from "prop-types";
+import { useSelector } from "react-redux";
+
+import * as Nav from "../../../navFrontend";
+
+import Knapperad from "../../knapperad";
+
+import { redigerbartSelectors } from "../../../ducks/redigerbart";
+
+import "./dialogboksBekreftValg.css";
+
+interface DialogboksBekreftValgProps {
+  handleBekreft: () => void;
+  handleAvbryt: () => void;
+  tittel: string;
+  tekst: string;
+  redigerbart: boolean;
+  ariaHideApp: boolean;
+}
+export const DialogboksBekreftValg = ({
+  handleBekreft,
+  handleAvbryt,
+  tittel,
+  tekst,
+  ariaHideApp,
+}: DialogboksBekreftValgProps) => {
+  const erBehandlingRedigerbart = useSelector(redigerbartSelectors.BehandlingsmenyRedigerbartSelector);
+
+  return (
+    <Nav.Modal
+      className="dialogboksBekreftValg"
+      isOpen
+      contentLabel={tittel}
+      onRequestClose={handleAvbryt}
+      closeButton={false}
+      shouldCloseOnOverlayClick
+      ariaHideApp={ariaHideApp}
+    >
+      <Nav.Typo.Systemtittel>{tittel}</Nav.Typo.Systemtittel>
+      <Nav.Typo.Normaltekst className="normaltekst">{tekst}</Nav.Typo.Normaltekst>
+      <Knapperad
+        bekreft={handleBekreft}
+        bekreftTekst="Bekreft"
+        avbryt={handleAvbryt}
+        avbrytTekst="Avbryt"
+        redigerbart={erBehandlingRedigerbart}
+      />
+    </Nav.Modal>
+  );
+};
+
+DialogboksBekreftValg.propTypes = {
+  handleBekreft: PT.func.isRequired,
+  handleAvbryt: PT.func.isRequired,
+  tittel: PT.string.isRequired,
+  tekst: PT.string.isRequired,
+  ariaHideApp: PT.bool,
+  redigerbart: PT.bool.isRequired,
+};
+
+DialogboksBekreftValg.defaultProps = {
+  ariaHideApp: true,
+};
+
+export default DialogboksBekreftValg;
