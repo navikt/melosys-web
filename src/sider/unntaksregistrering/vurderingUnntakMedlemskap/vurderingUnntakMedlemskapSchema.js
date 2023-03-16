65ea9f2e80368e28bad1e878f4532c8bcc776bd2
Isa Salem Hame <wanghattan@gmail.com>
2023-03-16 09:28:31 +0100

    5684: Gjør forbedringer rundt validering av dato på steg 2

--- src/sider/unntaksregistrering/vurderingUnntakMedlemskap/vurderingUnntakMedlemskapSchema.js
+++ src/sider/unntaksregistrering/vurderingUnntakMedlemskap/vurderingUnntakMedlemskapSchema.js
@@ -15,32 +15,20 @@ const erIkkeInnvilgetMedÅpenSluttDatoGodkjentUtfall = {
   },
 };
 
-const erIkkeInnvilgetMedÅpenSluttDatoDelvisGodkjentUtfall = {
-  name: "Ikke godkjenn åpen sluttdato for DELVIS_GODKJENT utfall",
-  message: { feilmelding: "Du kan ikke godkjenne en periode med åpen sluttdato" },
-  test() {
-    const resultat = this.parent.utfallRegistreringUnntak;
-    const sluttdato = this.parent.tom;
-    return !(resultat === MKV.Koder.utfallregistreringunntak.DELVIS_GODKJENT && Utils._isEmpty(sluttdato));
-  },
-};
-
 const vurdering_unntak_medlemskap = object().shape({
   utfallRegistreringUnntak: string().test(erIkkeInnvilgetMedÅpenSluttDatoGodkjentUtfall).required(MAA_FYLLES_UT),
-  fom: string().when("utfallRegistreringUnntak", {
+  fom: string().when("$utfallRegistreringUnntak", {
     is: MKV.Koder.innvilgelsesResultat.DELVIS_GODKJENT,
     then: string().erGyldigDato().required(MAA_FYLLES_UT),
   }),
-  tom: string()
-    .when("utfallRegistreringUnntak", {
-      is: MKV.Koder.innvilgelsesResultat.DELVIS_GODKJENT,
-      then: string().erGyldigDato().erEtterDatofelt("fom", TIDLIGERE_ENN_FOM).required(MAA_FYLLES_UT),
-    })
-    .test(erIkkeInnvilgetMedÅpenSluttDatoDelvisGodkjentUtfall),
+  tom: string().when("utfallRegistreringUnntak", {
+    is: (utfall) => utfall === MKV.Koder.utfallregistreringunntak.DELVIS_GODKJENT,
+    then: string().erGyldigDato().erEtterDatofelt("fom", TIDLIGERE_ENN_FOM).required(MAA_FYLLES_UT),
+  }),
   bestemmelse: string().when("utfallRegistreringUnntak", {
-    is: (vurdering) =>
-      vurdering === MKV.Koder.utfallregistreringunntak.GODKJENT ||
-      vurdering === MKV.Koder.utfallregistreringunntak.DELVIS_GODKJENT,
+    is: (utfall) =>
+      utfall === MKV.Koder.utfallregistreringunntak.GODKJENT ||
+      utfall === MKV.Koder.utfallregistreringunntak.DELVIS_GODKJENT,
     then: string().required(MAA_FYLLES_UT),
   }),
 });
