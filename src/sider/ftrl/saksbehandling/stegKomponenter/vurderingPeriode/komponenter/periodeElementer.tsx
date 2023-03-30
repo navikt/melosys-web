b079a8d95a8306971813c6ed54fb9660c651b982
Janita Johansen <56385061+janitamarielle@users.noreply.github.com>
2023-03-30 08:16:38 +0200

    5645 TOGGLE Småpirk etter test (#1830)
    * Småpirk etter test:
    Datovelger skal ikke ha placeholder. Dette gjelder alle felt i kodebasen.
    Hjelpetekst flyttes noen hakk ned.
    Info-elementene skal ikke ha fult så mye luft mellom seg.
    
    * Fikser tester
    
    * Matcher feilmelding-css til NAV/Aksel design på feilmeldinger slik at vi er konsekvente

--- src/sider/ftrl/saksbehandling/stegKomponenter/vurderingPeriode/komponenter/periodeElementer.tsx
+++ src/sider/ftrl/saksbehandling/stegKomponenter/vurderingPeriode/komponenter/periodeElementer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Control } from "react-hook-form";
 import { KTObject } from "@navikt/melosys-kodeverk";
 import * as Nav from "../../../../../../navFrontend";
+import LabelMedHjelpetekst from "../../../../../../felleskomponenter/labelMedHjelpetekst";
 import { MedlemskapsperiodeProp } from "../vurderingPerioder";
 import { PeriodeElement } from "./periodeElement";
 
@@ -18,7 +19,15 @@ export interface PeriodeElementerProps {
 
 export const PeriodeElementer = (props: PeriodeElementerProps) => {
   return (
-    <Nav.Fieldset legend="Medlemskapsperiode(r)">
+    <Nav.Fieldset
+      legend={
+        <LabelMedHjelpetekst
+          label="Medlemskapsperiode(r)"
+          hjelpetekst="Melosys har foreslått medlemskapsperioder på bakgrunn av periode og dekning det er søkt for, og tidspunktet søknaden ble mottatt. Du har mulighet til å gjøre endringer. Hvis du har mottatt opplysninger om at søknadsperiode eller trygdedekning det er søkt om er endret, må du endre dette i det inngangssteget «Inngang»."
+          hjelpetekstClassName="hjelpetekst"
+        />
+      }
+    >
       <Nav.Row className="labelRad">
         <Nav.Column xs="2">
           <Nav.Typo.Element>Fra og med</Nav.Typo.Element>
