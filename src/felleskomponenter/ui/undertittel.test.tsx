1d3c34bdd34258a573711065c127045d8f2bf03a
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2023-04-13 11:48:01 +0200

    Fikset feilende tester

--- src/felleskomponenter/ui/undertittel.test.tsx
+++ src/felleskomponenter/ui/undertittel.test.tsx
@@ -1,9 +1,8 @@
 import React, { ComponentProps } from "react";
-import { mock, instance } from "ts-mockito";
-import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
+import { instance, mock } from "ts-mockito";
 
 import * as Ikoner from "../../resources/images";
-import * as Nav from "../../navFrontend";
 
 import Undertittel from "./undertittel";
 
@@ -15,21 +14,11 @@ describe("undertittel", () => {
     props = instance(mockedProps);
   });
 
-  it("viser en nav undertittel", () => {
-    const undertittel = shallow(<Undertittel {...props} />);
-
-    expect(undertittel.find(Nav.Typo.Undertittel)).toHaveLength(1);
-  });
-
   it("viser et ikon", () => {
     props.ikon = Ikoner.ParagraphTwoColumns;
-    props.ikonProps = {
-      focusable: true,
-    };
-    const undertittel = shallow(<Undertittel {...props} />);
-    const ikon = undertittel.find(props.ikon);
+    props.tekst = "Tekst";
 
-    expect(ikon).toHaveLength(1);
-    expect(ikon.props().focusable).toBe(true);
+    const tree = renderer.create(<Undertittel {...props} />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
