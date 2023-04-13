2ee39f7ff5ed001e8839afbbaf766cf4d2145a53
Janita Johansen <56385061+janitamarielle@users.noreply.github.com>
2023-04-13 14:06:37 +0200

    5730 TOGGLE Buggy sortering skapte problemer i validering (#1843)
    * Fikser sortering som viste seg å ikke fungere oprtimalt.
    
    * Oppdaterer finnesOverlappIInnvilgedePerioder-funksjon
    
    * Validering for at minst en periode må starte samtidig som søknadsperiodefomdato
    
    * Dette er feilmeldinger som stopper videre behandling. Setter de derfor som Advarsel istedenfor Info. Endrer samtidig styling-navn da det ikke lengre matchet.

--- src/sider/ftrl/saksbehandling/stegKomponenter/vurderingPeriode/komponenter/feilmeldinger.tsx
+++ src/sider/ftrl/saksbehandling/stegKomponenter/vurderingPeriode/komponenter/feilmeldinger.tsx
@@ -3,58 +3,70 @@ import MKV from "../../../../../../melosyskodeverk";
 import * as Nav from "../../../../../../navFrontend";
 import * as Utils from "../../../../../../utils";
 import { FlytFinnesIkke } from "../../felles/flytFinnesIkke";
-import { MedlemskapsperiodeProp, sorterPerioder } from "../vurderingPerioder";
+import { MedlemskapsperiodeProp } from "../vurderingPerioder";
 
 const { AVSLAATT, INNVILGET } = MKV.Koder.innvilgelsesResultat;
 
 const IngenMedlemskapsperioder = (
-  <Nav.AlertStripeAdvarsel className="infomelding">
+  <Nav.AlertStripeAdvarsel className="alertstripe_feilmelding">
     Du må legge inn minst én periode før du kan fortsette.
   </Nav.AlertStripeAdvarsel>
 );
 
 const OppholdIPeriodene = (
-  <Nav.AlertStripeAdvarsel className="infomelding">Det er opphold mellom innvilgede perioder.</Nav.AlertStripeAdvarsel>
+  <Nav.AlertStripeAdvarsel className="alertstripe_feilmelding">
+    Det er opphold mellom innvilgede perioder.
+  </Nav.AlertStripeAdvarsel>
 );
 
 const OverlappIInnvilgedePerioder = (
-  <Nav.AlertStripeAdvarsel className="infomelding">Innvilgede perioder overlapper.</Nav.AlertStripeAdvarsel>
+  <Nav.AlertStripeAdvarsel className="alertstripe_feilmelding">Innvilgede perioder overlapper.</Nav.AlertStripeAdvarsel>
 );
 
 const OverlappMenIkkeLikPeriode = (
-  <Nav.AlertStripeAdvarsel className="infomelding">
+  <Nav.AlertStripeAdvarsel className="alertstripe_feilmelding">
     Innvilget og avslått periode som overlapper må ha lik periode.
   </Nav.AlertStripeAdvarsel>
 );
 
 const IngenSluttdato = (
-  <Nav.AlertStripeInfo className="infomelding">
+  <Nav.AlertStripeAdvarsel className="alertstripe_feilmelding">
     Du må oppgi sluttdato for å kunne angi resultat. Dette blir sluttdatoen på vedtaket.
-  </Nav.AlertStripeInfo>
+  </Nav.AlertStripeAdvarsel>
+);
+
+const MåStartePåSøknadsperiodeFom = (
+  <Nav.AlertStripeAdvarsel className="alertstripe_feilmelding">
+    Minst én periode må starte samme dato som søknadsperioden .
+  </Nav.AlertStripeAdvarsel>
 );
 
 const erIkkeStøttetIMelosys = (medlemskapsperioder: MedlemskapsperiodeProp[]) =>
   medlemskapsperioder.every((periode) => periode.innvilgelsesResultat === AVSLAATT);
 
+const finnesPeriodeSomStarterSamtidigSomSøknadsperioden = (
+  medlemskapsperioder: MedlemskapsperiodeProp[],
+  søknadsperiodeFomDato: string
+) => medlemskapsperioder.some((periode) => periode.fomDato === Utils.dato.formatterDatoTilNorsk(søknadsperiodeFomDato));
+
 const perioderErLike = (periode1: MedlemskapsperiodeProp, periode2: MedlemskapsperiodeProp) =>
   periode1.fomDato === periode2.fomDato && periode1.tomDato === periode2.tomDato;
 
+const sorterPerioder = (a: MedlemskapsperiodeProp, b: MedlemskapsperiodeProp) =>
+  (Utils.dato.norskStringTilDate(a.fomDato)?.getTime() ?? 0) -
+  (Utils.dato.norskStringTilDate(b.fomDato)?.getTime() ?? 0);
+
 const finnesOverlappIInnvilgedePerioder = (medlemskapsperioder: MedlemskapsperiodeProp[]) => {
-  const innvilgedePerioder = [...medlemskapsperioder]
+  return [...medlemskapsperioder]
     ?.filter((periode) => periode.innvilgelsesResultat === INNVILGET)
-    .sort(sorterPerioder);
-
-  if (!innvilgedePerioder?.length || innvilgedePerioder.length < 2) return false;
-
-  for (let i = 0; i < innvilgedePerioder.length - 1; i += 1) {
-    const periode = innvilgedePerioder[i];
-    const nestePeriode = innvilgedePerioder[i + 1];
-
-    if (Utils.dato.perioderOverlapper(periode.fomDato, periode.tomDato, nestePeriode.fomDato, nestePeriode.tomDato)) {
-      return true;
-    }
-  }
-  return false;
+    .sort(sorterPerioder)
+    .some((periode, index, perioder) =>
+      perioder
+        .slice(index + 1)
+        .some((nestePeriode) =>
+          Utils.dato.perioderOverlapper(periode.fomDato, periode.tomDato, nestePeriode.fomDato, nestePeriode.tomDato)
+        )
+    );
 };
 
 const finnesInnvilgetOgAvslåttPeriodeSomOverlapperMenIkkeHarLikPeriode = (
@@ -111,9 +123,13 @@ enum TypeFeilmelding {
   OVERLAPP_I_INNVILGEDE_PERIODER = "OVERLAPP_I_INNVILGEDE_PERIODER",
   OVERLAPP_MEN_FORSKJELLIG_PERIODE = "OVERLAPP_MEN_FORSKJELLIG_PERIODE",
   OPPHOLD_I_PERIODENE = "OPPHOLD_I_PERIODENE",
+  MÅ_STARTE_PÅ_SØKNADSFOM = "MÅ_STARTE_PÅ_SØKNADSFOM",
 }
 
-export function finnAktivFeilmelding(medlemskapsperioder: MedlemskapsperiodeProp[]): string | undefined {
+export function finnAktivFeilmelding(
+  medlemskapsperioder: MedlemskapsperiodeProp[],
+  søknadsperiodeFomDato: string
+): string | undefined {
   const ingenMedlemskapsperioder = medlemskapsperioder?.length === undefined || medlemskapsperioder?.length === 0;
   if (ingenMedlemskapsperioder) {
     return TypeFeilmelding.INGEN_MEDLEMSKAPSPERIODER;
@@ -126,6 +142,10 @@ export function finnAktivFeilmelding(medlemskapsperioder: MedlemskapsperiodeProp
   const manglerSluttdato = Utils._isEmpty(medlemskapsperioder[medlemskapsperioder.length - 1].tomDato);
   if (manglerSluttdato) {
     return TypeFeilmelding.INGEN_SLUTTDATO;
+  }
+
+  if (!finnesPeriodeSomStarterSamtidigSomSøknadsperioden(medlemskapsperioder, søknadsperiodeFomDato)) {
+    return TypeFeilmelding.MÅ_STARTE_PÅ_SØKNADSFOM;
   }
 
   if (finnesOverlappIInnvilgedePerioder(medlemskapsperioder)) {
@@ -151,6 +171,8 @@ export const Feilmelding = ({ type }: { type?: string }) => {
       return <FlytFinnesIkke />;
     case TypeFeilmelding.INGEN_SLUTTDATO:
       return IngenSluttdato;
+    case TypeFeilmelding.MÅ_STARTE_PÅ_SØKNADSFOM:
+      return MåStartePåSøknadsperiodeFom;
     case TypeFeilmelding.OVERLAPP_I_INNVILGEDE_PERIODER:
       return OverlappIInnvilgedePerioder;
     case TypeFeilmelding.OVERLAPP_MEN_FORSKJELLIG_PERIODE:
