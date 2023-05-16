9f9d824f245675a21b7fef9156f1e230f0d9727a
Janita Johansen <janita.marielle.johansen@nav.no>
2023-05-16 12:08:33 +0200

    Fjerner død kode etter FTRL-omskriving til react hook. Gikk også over å la til hentBestemmelse siden denne nå er dratt ut av medlemskapsperioder

--- src/services/modules/trygdeavgift.ts
+++ src/services/modules/trygdeavgift.ts
@@ -1,29 +1,5 @@
-import { Avgiftsberegning, Avgiftsgrunnlag, AvgiftsgrunnlagInfo } from "./types";
 import { getAsJson, putAsJson } from "../utils";
 import { API_BASE_URL, TRYGDEAVGIFT } from "../api-constants";
-
-export type OppdaterAvgiftsgrunnlag = {
-  lønnsforhold: string | null;
-  trygdeavgiftsgrunnlagNorge: AvgiftsgrunnlagInfo | null;
-  trygdeavgiftsgrunnlagUtland: AvgiftsgrunnlagInfo | null;
-};
-
-export type OppdaterAvgiftsberegning = {
-  avgiftspliktigLønnNorge: number | null;
-  avgiftspliktigLønnUtland: number | null;
-};
-
-export const hentBeregning = (behandlingID: number): Promise<Avgiftsberegning> =>
-  getAsJson(`${API_BASE_URL}/behandlinger/${behandlingID}/${TRYGDEAVGIFT}/beregning`);
-
-export const sendBeregning = (behandlingID: number, beregning: OppdaterAvgiftsberegning): Promise<Avgiftsberegning> =>
-  putAsJson(`${API_BASE_URL}/behandlinger/${behandlingID}/${TRYGDEAVGIFT}/beregning`, beregning);
-
-export const hentGrunnlag = (behandlingID: number): Promise<Avgiftsgrunnlag> =>
-  getAsJson(`${API_BASE_URL}/behandlinger/${behandlingID}/${TRYGDEAVGIFT}/grunnlag`);
-
-export const sendGrunnlag = (behandlingID: number, grunnlag: OppdaterAvgiftsgrunnlag): Promise<Avgiftsgrunnlag> =>
-  putAsJson(`${API_BASE_URL}/behandlinger/${behandlingID}/${TRYGDEAVGIFT}/grunnlag`, grunnlag);
 
 export type Inntektskilde = {
   type: string;
