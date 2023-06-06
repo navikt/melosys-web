b55f544d668abf0c592339ddcd163ddf102517cb
Marton Skjæveland <56435736+martonSkjevelandNav@users.noreply.github.com>
2023-06-06 15:33:33 +0200

    5946 TOGGLE Lagt til CAN_ART_11 og USA_ART_5_9 til ikke_pensjon trygdedekning (#1891)

--- src/sider/unntaksregistrering/vurderingUnntakMedlemskap/vurderingUnntakMedlemskap.tsx
+++ src/sider/unntaksregistrering/vurderingUnntakMedlemskap/vurderingUnntakMedlemskap.tsx
@@ -139,13 +139,23 @@ const VurderingUnntakMedlemskap = ({ oppdaterStatus, tilbake, aktivtSteg }: Vurd
       sakstype === MKV.Koder.sakstyper.TRYGDEAVTALE &&
       [
         MKV.Koder.lovvalgsbestemmelser.trygdeavtale.lovvalgsbestemmelser_trygdeavtale_ca.CAN_ART7,
+        MKV.Koder.lovvalgsbestemmelser.trygdeavtale.lovvalgsbestemmelser_trygdeavtale_ca.CAN_ART11,
         MKV.Koder.lovvalgsbestemmelser.trygdeavtale.lovvalgsbestemmelser_trygdeavtale_us.USA_ART5_2,
+        MKV.Koder.lovvalgsbestemmelser.trygdeavtale.lovvalgsbestemmelser_trygdeavtale_us.USA_ART5_9,
       ].includes(values.bestemmelse);
 
-    const trygdedekningUnntatt =
-      MKV.Koder.lovvalgsbestemmelser.trygdeavtale.lovvalgsbestemmelser_trygdeavtale_ca.CAN_ART7 === values.bestemmelse
-        ? UNNTATT_CAN_7_5_B
-        : UNNTATT_USA_5_2_G;
+    const trygdedekningUnntatt = () => {
+      switch (values.bestemmelse) {
+        case MKV.Koder.lovvalgsbestemmelser.trygdeavtale.lovvalgsbestemmelser_trygdeavtale_ca.CAN_ART7:
+        case MKV.Koder.lovvalgsbestemmelser.trygdeavtale.lovvalgsbestemmelser_trygdeavtale_ca.CAN_ART11:
+          return UNNTATT_CAN_7_5_B;
+        case MKV.Koder.lovvalgsbestemmelser.trygdeavtale.lovvalgsbestemmelser_trygdeavtale_us.USA_ART5_2:
+        case MKV.Koder.lovvalgsbestemmelser.trygdeavtale.lovvalgsbestemmelser_trygdeavtale_us.USA_ART5_9:
+          return UNNTATT_USA_5_2_G;
+        default:
+          return null;
+      }
+    };
 
     dispatch(
       lovvalgsperioderOperations.oppdaterLovvalgsperioderState({
@@ -157,7 +167,7 @@ const VurderingUnntakMedlemskap = ({ oppdaterStatus, tilbake, aktivtSteg }: Vurd
         lovvalgsbestemmelse: values.bestemmelse,
         lovvalgsland: lovvalgsland === MKV.Koder.land_iso2.CA_QC ? MKV.Koder.land_iso2.CA : lovvalgsland,
         medlemskapstype: harMedlemskapstypeDelvisUnntatt ? DELVIS_UNNTATT : UNNTATT,
-        trygdeDekning: harMedlemskapstypeDelvisUnntatt ? trygdedekningUnntatt : UTEN_DEKNING,
+        trygdeDekning: harMedlemskapstypeDelvisUnntatt ? trygdedekningUnntatt() : UTEN_DEKNING,
       })
     );
     debouncedLagreLovvalgsperiode(skalRegisteropplysningerOppdateres);
