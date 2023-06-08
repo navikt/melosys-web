99e87a1d4c3e8fd038bb1adb8dac534ea49c420d
Isa Salem Hame <wanghattan@gmail.com>
2023-06-08 16:48:25 +0200

    Gjør "til og med dato"-nonnullable

--- src/sider/ikkeYrkesaktiv/stegKomponenter/vurderingVedtakSchema.js
+++ src/sider/ikkeYrkesaktiv/stegKomponenter/vurderingVedtakSchema.js
@@ -5,7 +5,7 @@ const { MAA_FYLLES_UT } = KV.Feilmeldinger;
 
 const vurdering_vedtak = object().shape({
   fom: string().erGyldigDato().erInnenforSoknadsperioden().required(MAA_FYLLES_UT),
-  tom: string().erGyldigDato().erInnenforSoknadsperioden().erEtterDatofelt("fom").nullable(),
+  tom: string().erGyldigDato().erInnenforSoknadsperioden().erEtterDatofelt("fom").required(MAA_FYLLES_UT),
 });
 
 export default vurdering_vedtak;
