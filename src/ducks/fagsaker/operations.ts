53adcfd636a9ccd19b3e28ea656eb974189e6f94
Janita Johansen <janita.marielle.johansen@nav.no>
2023-06-13 14:01:00 +0200

    Oppdaterer melding etter ønske fra UX

--- src/ducks/fagsaker/operations.ts
+++ src/ducks/fagsaker/operations.ts
@@ -43,7 +43,7 @@ export function lagNySak(body: Api.Fagsaker.fagsak.OpprettReqDto) {
     },
     {
       success: (dispatch: ThunkDispatch<RootState, unknown, Types.Action>) => {
-        dispatch(tilbakemeldingOperations.tilForsidenOgVisTilbakemelding("Handling vellykket!"));
+        dispatch(tilbakemeldingOperations.tilForsidenOgVisTilbakemelding("Ok. Saken ble opprettet."));
       },
     }
   );
@@ -60,7 +60,7 @@ export function lagNyBehandlingForSak(saksnummer: string, body: Api.Fagsaker.fag
     },
     {
       success: (dispatch: ThunkDispatch<RootState, unknown, Types.Action>) => {
-        dispatch(tilbakemeldingOperations.tilForsidenOgVisTilbakemelding("Handling vellykket!"));
+        dispatch(tilbakemeldingOperations.tilForsidenOgVisTilbakemelding("Ok. Behandlingen ble opprettet."));
       },
     }
   );
