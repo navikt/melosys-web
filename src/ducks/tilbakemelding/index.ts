2b764121fd8d82c08d5cb257a66d0ca087f15ca4
Janita Johansen <janita.marielle.johansen@nav.no>
2023-06-13 14:02:38 +0200

    Fjerner unødvendig export

--- src/ducks/tilbakemelding/index.ts
+++ src/ducks/tilbakemelding/index.ts
@@ -1,8 +1,7 @@
-import * as tilbakemeldingTypes from "./types";
 import * as tilbakemeldingSelectors from "./selectors";
 import * as tilbakemeldingOperations from "./operations";
 import tilbakemeldingReducers from "./reducers";
 
-export { tilbakemeldingTypes, tilbakemeldingOperations, tilbakemeldingSelectors };
+export { tilbakemeldingOperations, tilbakemeldingSelectors };
 
 export default tilbakemeldingReducers;
