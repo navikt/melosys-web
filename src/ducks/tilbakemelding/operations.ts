078772d8af413307872dee2e5840f1dab20667aa
Janita Johansen <janita.marielle.johansen@nav.no>
2023-06-13 13:57:30 +0200

    Toggler visningen

--- src/ducks/tilbakemelding/operations.ts
+++ src/ducks/tilbakemelding/operations.ts
@@ -1,8 +1,12 @@
 import { ThunkDispatch } from "redux-thunk";
 import { RootState } from "AppTypes";
 import { Action } from "redux";
+
 import * as Actions from "./actions";
+
 import { navigeringOperations } from "../navigering";
+import { erFeatureToggleEnabled } from "../../featuretoggle";
+import { MELOSYS_TILBAKEMELDING_TIL_SAKSBEHANDLER } from "../../featuretoggle/toggleNavn";
 
 export const visTilbakemelding =
   (tekst: string = "Handling vellykket!") =>
@@ -13,8 +17,11 @@ export const skjulTilbakemelding = () => (dispatch: ThunkDispatch<RootState, unk
   dispatch(Actions.oppdater({ synlig: false, tekst: "" }));
 
 export const tilForsidenOgVisTilbakemelding =
-  (tekst: string = "Handling vellykket!") =>
-  (dispatch: ThunkDispatch<RootState, unknown, Action>) => {
-    dispatch(navigeringOperations.tilForsiden());
-    dispatch(Actions.oppdater({ synlig: true, tekst }));
+  (tekst?: string) => (dispatch: ThunkDispatch<RootState, unknown, Action>, getState: () => RootState) => {
+    if (erFeatureToggleEnabled(MELOSYS_TILBAKEMELDING_TIL_SAKSBEHANDLER, getState())) {
+      dispatch(navigeringOperations.tilForsiden());
+      visTilbakemelding(tekst);
+    } else {
+      dispatch(navigeringOperations.tilForsiden());
+    }
   };
