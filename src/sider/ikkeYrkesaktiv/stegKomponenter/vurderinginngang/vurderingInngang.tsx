87ca96ee38a1e57d5bd0c000bdc22c8795e79311
Isa Salem Hame <isa.salem.hame@nav.no>
2023-06-30 11:55:38 +0200

    5980 TOGGLE: Vis feilmelding hvis trygdeavtale og valgt land er frankrike eller italia (#1930)
    * 5980: Vis feilmelding dersom valgt land i Ikke-yrkesaktiv er Frankrike eller Italia
    
    * 5980: Vis feilmelding kun for trygdeavtale
    
    * Flytter landvalidering til schema
    
    * Fjerner unødvendig sjekk etter å legge inn landUtenStøtteValgt i schema

--- src/sider/ikkeYrkesaktiv/stegKomponenter/vurderinginngang/vurderingInngang.tsx
+++ src/sider/ikkeYrkesaktiv/stegKomponenter/vurderinginngang/vurderingInngang.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { KTObject } from "@navikt/melosys-kodeverk";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -9,6 +9,7 @@ import * as Mui from "../../../../felleskomponenter/ui";
 import * as Utils from "../../../../utils";
 
 import LabelMedHjelpetekst from "../../../../felleskomponenter/labelMedHjelpetekst";
+import { TomFlytMelding } from "../../../../felleskomponenter/alertmeldinger";
 import { FellesHandlersContext } from "../../../../contexts";
 
 import { mottatteOpplysningerOperations, mottatteOpplysningerSelectors } from "../../../../ducks/mottatteOpplysninger";
@@ -18,6 +19,7 @@ import { landkoderSelectors } from "../../../../ducks/landkoder";
 
 import vurderingInngangSchema from "./vurderingInngangSchema";
 import { behandlingerSelectors } from "../../../../ducks/behandlinger";
+import { fagsakSelectors } from "../../../../ducks/fagsaker";
 
 interface Props {
   bekreft: () => void;
@@ -34,9 +36,10 @@ export const VurderingInngang = ({ bekreft, aktivtSteg, oppdaterStatus }: Props)
   const landkoder = useSelector(landkoderSelectors.LandkoderFraSakstypeSelector);
   const redigerbart = useSelector(redigerbartSelectors.RedigerbartSelector);
   const registeropplysningerHentet = useSelector(behandlingerSelectors.SisteOpplysningerHentetDatoSelector);
+  const sakstype = useSelector(fagsakSelectors.SakstypeKodeSelector);
 
   const { control, watch, formState, trigger } = useForm({
-    resolver: yupResolver(vurderingInngangSchema),
+    resolver: yupResolver(vurderingInngangSchema(sakstype)),
     mode: "all",
     defaultValues: {
       fom: periodeFom,
@@ -53,6 +56,8 @@ export const VurderingInngang = ({ bekreft, aktivtSteg, oppdaterStatus }: Props)
     formValues?.land !== søknadsland;
 
   const [visSpinner, setVisSpinner] = useState(false);
+
+  const landUtenStøtteValgt = formState?.errors?.land?.type === "landValidering";
 
   const { lagreMottatteOpplysningerOgOppfriskSaksopplysninger } = useContext(FellesHandlersContext) as any;
 
@@ -129,6 +134,8 @@ export const VurderingInngang = ({ bekreft, aktivtSteg, oppdaterStatus }: Props)
           </Nav.Column>
         </Nav.Row>
       </Nav.Fieldset>
+
+      {landUtenStøtteValgt && <TomFlytMelding />}
 
       <Mui.StegKnapper
         bekreftKnappProps={{
