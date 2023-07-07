bd825820c1f25eaf20fa33fd4245f2e94c40649e
Isa Salem Hame <wanghattan@gmail.com>
2023-07-07 14:32:11 +0200

    Fjerner unødvendige nullables

--- src/sider/ikkeYrkesaktiv/stegKomponenter/vurderingvedtak/vurderingVedtakSchema.js
+++ src/sider/ikkeYrkesaktiv/stegKomponenter/vurderingvedtak/vurderingVedtakSchema.js
@@ -4,26 +4,20 @@ import * as KV from "../../../../kodeverk";
 const { MAA_FYLLES_UT } = KV.Feilmeldinger;
 
 const vurderingVedtakSchema = object().shape({
-  nyVurderingBakgrunnValg: string()
-    .when("$erNyVurdering", {
-      is: true,
-      then: string().required(MAA_FYLLES_UT),
+  nyVurderingBakgrunnValg: string().when("$erNyVurdering", {
+    is: true,
+    then: string().required(MAA_FYLLES_UT),
+    otherwise: string().nullable(),
+  }),
+  nyVurderingBakgrunnFritekst: string().when("$erNyVurdering", {
+    is: true,
+    then: string().when("nyVurderingBakgrunnValg", {
+      is: "Fritekst",
+      then: string().erIkkeBlankHtml().required(MAA_FYLLES_UT),
       otherwise: string().nullable(),
-    })
-    .nullable(),
-  nyVurderingBakgrunnFritekst: string()
-    .when("$erNyVurdering", {
-      is: true,
-      then: string()
-        .when("nyVurderingBakgrunnValg", {
-          is: "Fritekst",
-          then: string().erIkkeBlankHtml().required(MAA_FYLLES_UT),
-          otherwise: string().nullable(),
-        })
-        .nullable(),
-      otherwise: string().nullable(),
-    })
-    .nullable(),
+    }),
+    otherwise: string().nullable(),
+  }),
 });
 
 export default vurderingVedtakSchema;
