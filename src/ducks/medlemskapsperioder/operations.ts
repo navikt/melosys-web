2c9d0997cf161f475bfe222eb48d1bd01e2bb04f
Janita Johansen <janita.marielle.johansen@nav.no>
2023-07-07 10:28:13 +0200

    Skriver operations om til typescript

--- src/ducks/medlemskapsperioder/operations.ts
+++ src/ducks/medlemskapsperioder/operations.ts
@@ -1,3 +1,6 @@
+import { ThunkDispatch } from "redux-thunk";
+import { RootState } from "AppTypes";
+
 import { doThenDispatch } from "../../services/utils";
 import * as Api from "../../services/api";
 import * as Types from "./types";
@@ -6,7 +9,7 @@ import * as Selectors from "./selectors";
 
 import { behandlingerSelectors } from "../behandlinger";
 
-export function hentMedlemskapsperioder(behandlingID) {
+export function hentMedlemskapsperioder(behandlingID: number) {
   return doThenDispatch(() => Api.Medlemskapsperioder.getMedlemskapsperioder(behandlingID), {
     OK: Types.OK_MEDLEMSKAPSPERIODE,
     FEILET: Types.FEILET,
@@ -14,7 +17,7 @@ export function hentMedlemskapsperioder(behandlingID) {
   });
 }
 
-export function hentBestemmelse(behandlingID) {
+export function hentBestemmelse(behandlingID: number) {
   return doThenDispatch(() => Api.Medlemskapsperioder.getBestemmelse(behandlingID), {
     OK: Types.OK_BESTEMMELSE,
     FEILET: Types.FEILET,
@@ -22,7 +25,7 @@ export function hentBestemmelse(behandlingID) {
   });
 }
 
-function opprettMedlemskapsperiode(behandlingID, bestemmelse) {
+function opprettMedlemskapsperiode(behandlingID: number, bestemmelse: string) {
   return doThenDispatch(
     () => Api.Medlemskapsperioder.opprettMedlemskapsperioderFraBestemmelse(behandlingID, bestemmelse),
     {
@@ -33,12 +36,13 @@ function opprettMedlemskapsperiode(behandlingID, bestemmelse) {
   );
 }
 
-export function oppdaterBestemmelse(bestemmelse) {
-  return (dispatch) => dispatch(Actions.oppdaterBestemmelse(bestemmelse));
+export function oppdaterBestemmelse(bestemmelse: string) {
+  return (dispatch: ThunkDispatch<RootState, unknown, Types.Action>) =>
+    dispatch(Actions.oppdaterBestemmelse(bestemmelse));
 }
 
 export function opprettMedlemskapsperiodeFraBestemmelse() {
-  return (dispatch, getState) => {
+  return (dispatch: ThunkDispatch<RootState, unknown, Types.Action>, getState: () => RootState) => {
     const bestemmelse = Selectors.BestemmelseSelector(getState());
     const bid = behandlingerSelectors.BehandlingIDSelector(getState());
     dispatch(opprettMedlemskapsperiode(bid, bestemmelse));
