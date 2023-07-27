afebd496f81db135107414fefc1b141015fc32b3
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2023-07-27 15:21:12 +0200

    Liten justering på koden i vurderingPerioder.debouncedLagreMedlemskapsperioder for å unngå å måtte ignorere enda en eslint regel

--- src/sider/ftrl/saksbehandling/stegKomponenter/vurderingPeriode/vurderingPerioder.tsx
+++ src/sider/ftrl/saksbehandling/stegKomponenter/vurderingPeriode/vurderingPerioder.tsx
@@ -149,11 +149,11 @@ export const VurderingPerioder = ({ bekreft, tilbake, aktivtSteg, oppdaterStatus
   const debouncedLagreMedlemskapsperioder = useCallback(
     Utils._debounce(async (medlemskapsperioder, isValid, overskrevetIndex) => {
       if (isValid) {
-        // eslint-disable-next-line no-restricted-syntax
-        for (const periode of medlemskapsperioder) {
+        const promises = medlemskapsperioder.map((periode: MedlemskapsperiodeProp) => {
           const index = overskrevetIndex !== undefined ? overskrevetIndex : medlemskapsperioder.indexOf(periode);
-          await lagreMedlemskapsperiode(periode, index);
-        }
+          return lagreMedlemskapsperiode(periode, index);
+        });
+        await Promise.all(promises);
       }
     }, 500),
     []
