57d18d26c4a578e01726343fe90c94ef3985eec2
Isa Salem Hame <isa.salem.hame@nav.no>
2023-08-17 09:31:27 +0200

    VAKT: Fikser toggle i ikke-yrkesaktiv flyt (#1983)

--- src/sider/ikkeYrkesaktiv/saksbehandling.tsx
+++ src/sider/ikkeYrkesaktiv/saksbehandling.tsx
@@ -32,7 +32,7 @@ import { MatchParams } from "../../@types";
 import { alleSteg } from "./initialStegArray";
 import "./saksbehandling.css";
 import { lovvalgsperioderOperations, lovvalgsperioderSelectors } from "../../ducks/lovvalgsperioder";
-import { MELOSYS_FOLKETRYGDEN_MVP } from "../../featuretoggle/toggleNavn";
+import { MELOSYS_IKKEYRKESAKTIV_FORENKLETFLYT } from "../../featuretoggle/toggleNavn";
 import { kontrollOperations } from "../../ducks/kontroll";
 
 interface Props extends RouteComponentProps<MatchParams> {
@@ -50,7 +50,7 @@ const Saksbehandling = ({
 }: Props) => {
   const [behandlingID, setBehandlingID] = useState(-1);
   const [saksopplysningerLastet, setSaksopplysningerLastet] = useState(false);
-  const folketrygdenToggle = useFeatureToggle(MELOSYS_FOLKETRYGDEN_MVP);
+  const ikkeYrkesaktivToggle = useFeatureToggle(MELOSYS_IKKEYRKESAKTIV_FORENKLETFLYT);
 
   const dispatch = useDispatch();
 
@@ -129,7 +129,7 @@ const Saksbehandling = ({
   if (Utils._isNil(redigerbart)) return null;
   if (!behandlingID || behandlingID < 0) return null;
   if (!saksopplysningerLastet) return null;
-  if (!folketrygdenToggle) return null;
+  if (!ikkeYrkesaktivToggle) return null;
 
   const erHenlagtSak = fagsakStatusKode === MKV.Koder.saksstatuser.HENLAGT;
   const erNyVurdering = behandlingstype === MKV.Koder.behandlinger.behandlingstyper.NY_VURDERING;
