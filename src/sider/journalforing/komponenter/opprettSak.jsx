df2628415e14ce4347771d7747a7ff2f503c0543
barstadEkstern <oystein.madsen.barstad@nav.no>
2023-08-23 13:55:47 +0200

    hotfix medlemskapsperiode

--- src/sider/journalforing/komponenter/opprettSak.jsx
+++ src/sider/journalforing/komponenter/opprettSak.jsx
@@ -41,6 +41,7 @@ export const skalViseSoknadsperiodeOgLand = (sakstype, sakstema, behandlingstema
   behandlingstema &&
   behandlingstype &&
   behandlingstema !== MKV.Koder.behandlinger.behandlingstema.IKKE_YRKESAKTIV &&
+  behandlingstema !== MKV.Koder.behandlinger.behandlingstema.A1_ANMODNING_OM_UNNTAK_PAPIR &&
   !skalViseTomFlyt(sakstype, sakstema, behandlingstema, behandlingstype);
 
 export const OpprettSak = (props) => {
