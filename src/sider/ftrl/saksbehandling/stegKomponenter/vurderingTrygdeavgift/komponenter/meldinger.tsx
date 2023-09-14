b6d5935fcf5f7fecaf159cd59fa6f6fdb996efcd
Eirik Bergande <eirik.bergande@nav.no>
2023-09-14 11:58:54 +0200

    6010 TOGGLE Sorterer inntektsperioder riktig

--- src/sider/ftrl/saksbehandling/stegKomponenter/vurderingTrygdeavgift/komponenter/meldinger.tsx
+++ src/sider/ftrl/saksbehandling/stegKomponenter/vurderingTrygdeavgift/komponenter/meldinger.tsx
@@ -44,12 +44,16 @@ const finnesInntektskildeperiodeUtenforMedlemskapsperiode = (
   medlemskapsperioder: Medlemskapsperiode[]
 ) => {
   if (inntektskilder.length === 0) return false;
-  const sortertInntekstkilder = inntektskilder.sort(
-    (a, b) => new Date(a.fomDato!).getTime() - new Date(b.fomDato!).getTime()
-  );
-  const sortertMedlemskapsperioder = medlemskapsperioder.sort(
-    (a, b) => new Date(a.fomDato!).getTime() - new Date(b.fomDato!).getTime()
-  );
+  const sortertInntekstkilder = [...inntektskilder].sort((a, b) => {
+    const fomA = Utils.dato.formatterDatoTilISO(a.fomDato);
+    const fomB = Utils.dato.formatterDatoTilISO(b.fomDato);
+    return new Date(fomA).getTime() - new Date(fomB).getTime();
+  });
+  const sortertMedlemskapsperioder = [...medlemskapsperioder].sort((a, b) => {
+    const fomA = Utils.dato.formatterDatoTilISO(a.fomDato);
+    const fomB = Utils.dato.formatterDatoTilISO(b.fomDato);
+    return new Date(fomA!).getTime() - new Date(fomB!).getTime();
+  });
   return (
     Utils.dato.erFør(sortertInntekstkilder[0].fomDato, sortertMedlemskapsperioder[0].fomDato) ||
     Utils.dato.erEtter(
