831f3881e2266112e35dc706df9eb67807ad0fa5
Janita Johansen <janita.marielle.johansen@nav.no>
2023-09-15 09:48:50 +0200

    25 år tilbake i tid og 15 år fremover i tid etter diskusjon med fag

--- src/felleskomponenter/datovelger/datovelger.tsx
+++ src/felleskomponenter/datovelger/datovelger.tsx
@@ -33,7 +33,7 @@ const Datovelger = ({
 }: DatovelgerProps) => {
   const [erUgyldigDato, setErUgyldigDato] = useState<boolean>(false);
   const { datepickerProps, inputProps } = useDatepicker({
-    fromDate: minDate ?? new Date(moment(moment.now()).subtract(15, "years").toDate()),
+    fromDate: minDate ?? new Date(moment(moment.now()).subtract(25, "years").toDate()),
     toDate: maxDate ?? new Date(moment(moment.now()).add(15, "years").toDate()),
     locale: "nb",
     defaultSelected: value,
