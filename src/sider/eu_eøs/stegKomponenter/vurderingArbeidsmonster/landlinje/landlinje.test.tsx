7e8a31b89f00c0e90aba0be41c3b4c62513800ac
Janita Johansen <janita.marielle.johansen@nav.no>
2023-09-15 16:02:09 +0200

    Skriver om litt enzyme tester

--- src/sider/eu_eøs/stegKomponenter/vurderingArbeidsmonster/landlinje/landlinje.test.tsx
+++ src/sider/eu_eøs/stegKomponenter/vurderingArbeidsmonster/landlinje/landlinje.test.tsx
@@ -1,39 +1,22 @@
-import { ComponentProps } from "react";
-import { shallow } from "enzyme";
-import { mock, instance } from "ts-mockito";
-
-import * as Mui from "../../../../../felleskomponenter/ui";
-
 import LandLinje from "./landlinje";
+import { renderWithProviders } from "../../../../../ducks/test-utils/renderWithProviders";
+import { expect } from "vitest";
+import { screen } from "@testing-library/react";
 
 describe("LandLinje", () => {
-  const mockedProps = mock<ComponentProps<typeof LandLinje>>();
-  const props = instance(mockedProps);
-
-  props.land = "DK";
-  props.checkbox = {
-    checked: true,
-    onCheck: vi.fn(),
-    redigerbart: true,
-    value: "Testvalue",
+  const props = {
+    land: "DK",
+    checkbox: {
+      checked: true,
+      onCheck: vi.fn(),
+      redigerbart: true,
+      value: "Testvalue",
+    },
   };
 
-  const landLinje = shallow(<LandLinje {...props} />);
-
-  it("viser land", () => {
-    expect(landLinje.contains("DK")).toBe(true);
-  });
-
-  it("viser checkbox", () => {
-    const checkbox = landLinje.find(Mui.Checkbox);
-
-    expect(checkbox).toHaveLength(1);
-
-    const checkboxProps = checkbox.props();
-
-    expect(checkboxProps.checked).toBe(props.checkbox.checked);
-    expect(checkboxProps.onCheck).toBe(props.checkbox.onCheck);
-    expect(checkboxProps.disabled).toBe(!props.checkbox.redigerbart);
-    expect(checkboxProps.value).toBe(props.checkbox.value);
+  it("viser land og checkbox", () => {
+    renderWithProviders(<LandLinje {...props} />);
+    expect(screen.getByText("DK")).toBeInTheDocument();
+    expect(screen.getByRole("checkbox")).toBeInTheDocument();
   });
 });
