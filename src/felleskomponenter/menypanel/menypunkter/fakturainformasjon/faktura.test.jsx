fd571a031c533d5a7c582b957996e534801d2664
Thang Phan <thang.quoc.ly.phan@nav.no>
2023-09-20 14:45:01 +0200

    Mock endepunkt resolve fortest

--- src/felleskomponenter/menypanel/menypunkter/fakturainformasjon/faktura.test.jsx
+++ src/felleskomponenter/menypanel/menypunkter/fakturainformasjon/faktura.test.jsx
@@ -1,6 +1,10 @@
 import { Faktura } from "./faktura";
 import { renderWithProviders } from "../../../../ducks/test-utils/renderWithProviders";
 
+vi.mock("../../../../services/modules/faktureringskomponenten", () => ({
+  hentFakturainfo: () => Promise.resolve([]),
+}));
+
 vi.mock("../../../../utils", async () => {
   const actual = await vi.importActual("../../../../utils");
   return {
--- src/felleskomponenter/menypanel/menypunkter/fakturainformasjon/faktura.tsx
+++ src/felleskomponenter/menypanel/menypunkter/fakturainformasjon/faktura.tsx
@@ -5,6 +5,7 @@ import { _isEmpty, _uuid } from "../../../../utils";
 import { Table } from "@navikt/ds-react";
 import moment from "moment";
 import KopierbarTekst from "../../../kopierbarTekst";
+import { FakturaLinjeContainer } from "./fakturalinjecontainer";
 
 interface FakturaProps {
   faktura: any;
@@ -71,53 +72,8 @@ export const Faktura = ({ faktura }: FakturaProps) => {
     }
   }, [faktura]);
 
-  const renderFakturaLinjer = () => {
-    return (
-      <div className="fakturalinje">
-        <div className="fakturanr_wrapper">
-          Fakturanr:&nbsp;
-          <KopierbarTekst hovertekst="">{faktura.id}</KopierbarTekst>
-        </div>
-
-        <Table>
-          <Table.Header>
-            <Table.Row shadeOnHover={false}>
-              <Table.HeaderCell scope="col" />
-              <Table.HeaderCell scope="col">Antall</Table.HeaderCell>
-              <Table.HeaderCell scope="col">Enhetspris</Table.HeaderCell>
-              <Table.HeaderCell scope="col">Beløp</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {faktura.fakturaLinje.map((fakturaLinje: any) => (
-              <Table.Row key={_uuid()} shadeOnHover={false}>
-                <Table.DataCell>{fakturaLinje.beskrivelse}</Table.DataCell>
-                <Table.DataCell>1</Table.DataCell>
-                <Table.DataCell>{fakturaLinje.belop.toFixed(2)}</Table.DataCell>
-                <Table.DataCell>{fakturaLinje.belop.toFixed(2)}</Table.DataCell>
-              </Table.Row>
-            ))}
-            {!_isEmpty(faktura.fakturaLinje) && (
-              <Table.Row shadeOnHover={false}>
-                <Table.HeaderCell className="uten-border" />
-                <Table.HeaderCell className="uten-border" />
-                <Table.HeaderCell className="uten-border">Totalt</Table.HeaderCell>
-                <Table.DataCell className="uten-border">
-                  {faktura.fakturaLinje
-                    .map((linje: any) => linje.belop)
-                    .reduce((a: any, b: any) => a + b)
-                    .toFixed(2)}
-                </Table.DataCell>
-              </Table.Row>
-            )}
-          </Table.Body>
-        </Table>
-      </div>
-    );
-  };
-
   return (
-    <Table.ExpandableRow key={faktura.id} content={renderFakturaLinjer()}>
+    <Table.ExpandableRow key={faktura.id} content={<FakturaLinjeContainer faktura={faktura} />}>
       <Table.DataCell>{faktura.datoBestilt}</Table.DataCell>
       <Table.DataCell>{mapPeriodeTilKvartalString(faktura.periodeFra, faktura.periodeTil)}</Table.DataCell>
       <Table.DataCell>
