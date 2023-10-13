142ac6adfa5acf5234eef246a3c27cf4e27336c2
Janita Johansen <janita.marielle.johansen@nav.no>
2023-10-13 07:49:28 +0200

    En liten justering på hva og når man trigger feilmeldingene. Siden debouncedLagreTrygdeavgiftsgrunnlag kun kjører lagre-kallet dersom andre parameter er true kan skrive det slik uten en spesifikk if i selve useEffecten.

--- src/sider/ftrl/saksbehandling/stegKomponenter/vurderingTrygdeavgift/vurderingTrygdeavgift.tsx
+++ src/sider/ftrl/saksbehandling/stegKomponenter/vurderingTrygdeavgift/vurderingTrygdeavgift.tsx
@@ -166,19 +166,16 @@ export const VurderingTrygdeavgift = ({ bekreft, tilbake, aktivtSteg, oppdaterSt
   }, [stegErGyldig, isValidating, formValues?.inntektskilder?.length, formValues?.skatteforholdsperioder?.length]);
 
   useEffect(() => {
-    if (aktivtSteg) {
-      if (redigerbart && formIsValid) {
-        debouncedLagreTrygdeavgiftsgrunnlag(formValues, stegErGyldig);
-      } else {
-        formValues?.skatteforholdsperioder?.forEach((_periode: any, index: number) => {
-          trigger(`skatteforholdsperioder[${index}].fomDato`);
-          trigger(`skatteforholdsperioder[${index}].tomDato`);
-        });
-        formValues?.inntektskilder?.forEach((_periode: any, index: number) => {
-          trigger(`inntektskilder[${index}].fomDato`);
-          trigger(`inntektskilder[${index}].tomDato`);
-        });
-      }
+    if (redigerbart && aktivtSteg) {
+      debouncedLagreTrygdeavgiftsgrunnlag(formValues, formIsValid && !feilMeldingBlokkerer(aktivFeilmeldingType));
+      formValues?.skatteforholdsperioder?.forEach((_periode: any, index: number) => {
+        trigger(`skatteforholdsperioder[${index}].fomDato`);
+        trigger(`skatteforholdsperioder[${index}].tomDato`);
+      });
+      formValues?.inntektskilder?.forEach((_periode: any, index: number) => {
+        trigger(`inntektskilder[${index}].fomDato`);
+        trigger(`inntektskilder[${index}].tomDato`);
+      });
     }
   }, [aktivtSteg, innvilgetMedlemskapsperiode]);
 
