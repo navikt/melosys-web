f04b032829e83c8120f21c215e869f6d9a9aca47
Eirik Bergande <eirik.bergande@nav.no>
2023-10-16 10:02:26 +0200

    qa

--- src/sider/journalforing/komponenter/sendForvaltningsMelding.tsx
+++ src/sider/journalforing/komponenter/sendForvaltningsMelding.tsx
@@ -60,7 +60,7 @@ const SendForvaltningsMelding = ({
         )}
       </Skjema.RadioGruppe>
 
-      {harRegistrertAdresse && (
+      {!harRegistrertAdresse && (
         <Nav.AlertStripe className="feilmelding" type="advarsel">
           <Nav.Typo.Element>Melding kan ikke sendes automatisk pga. manglende eller ugyldig adresse</Nav.Typo.Element>
           <ul>
