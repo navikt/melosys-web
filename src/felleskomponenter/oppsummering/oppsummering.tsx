382db32ee1a610257ba199226153a08c491cfeed
Isa Salem Hame <wanghattan@gmail.com>
2023-10-19 12:38:13 +0200

    Fjern mottatteopplysningerselector

--- src/felleskomponenter/oppsummering/oppsummering.tsx
+++ src/felleskomponenter/oppsummering/oppsummering.tsx
@@ -23,7 +23,6 @@ import KopierbarTekst from "../kopierbarTekst";
 import OppsummeringVerdiPar from "./verdiPar/oppsummeringVerdiPar";
 import EndreBehandlingModal from "./endreBehandlingModal";
 import "./oppsummering.css";
-import { mottatteOpplysningerSelectors } from "../../ducks/mottatteOpplysninger";
 import { useAsyncCallbackState } from "../../hooks";
 import { useFeatureToggle } from "../../featuretoggle";
 import { MELOSYS_FOLKETRYGDEN_MVP } from "../../featuretoggle/toggleNavn";
