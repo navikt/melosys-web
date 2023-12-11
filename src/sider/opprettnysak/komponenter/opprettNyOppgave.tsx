8af02f4aa93202e93f91c1415f016393077cf892
Janita Johansen <janita.marielle.johansen@nav.no>
2023-12-11 13:21:19 +0100

    Gjemmer behandlingsårsaktype bak toggle

--- src/sider/opprettnysak/komponenter/opprettNyOppgave.tsx
+++ src/sider/opprettnysak/komponenter/opprettNyOppgave.tsx
@@ -4,11 +4,15 @@ import MKV from "../../../melosyskodeverk";
 import * as Skjema from "../../../felleskomponenter/skjema";
 import * as Nav from "../../../navFrontend";
 import { formSelectors } from "../../../ducks/form";
+import { useFeatureToggle } from "../../../featuretoggle";
+import { MELOSYS_SAKSBEHANDLING_MANGLENDE_INNBETALING } from "../../../featuretoggle/toggleNavn";
 
-const { FRITEKST } = MKV.Koder.behandlinger.behandlingsaarsaktyper;
+const { FRITEKST, MELDING_OM_MANGLENDE_INNBETALING } = MKV.Koder.behandlinger.behandlingsaarsaktyper;
 
 export const OpprettNyOppgave = () => {
   const { behandlingsaarsakType } = useSelector(formSelectors.OpprettNySakFormValuesSelector);
+  const manglendeInnbetalingToggleEnabled = useFeatureToggle(MELOSYS_SAKSBEHANDLING_MANGLENDE_INNBETALING);
+
   const labelTilÅrsak = (aarsak: KTObject) => {
     if (aarsak.kode === FRITEKST) {
       return `${aarsak.term} (Maks 25 tegn)`;
@@ -16,11 +20,22 @@ export const OpprettNyOppgave = () => {
     return aarsak.term || "";
   };
 
+  const gyldigeBehandlingsårsaker = () => {
+    // TODO: MELOSYS-6312 medfører bedre logikk. Kanskje også samtidig legge dette i lovligeKombonasjoner i backend?
+    if (manglendeInnbetalingToggleEnabled) {
+      return MKV.KTObjects.behandlinger.behandlingsaarsaktyper;
+    } else {
+      return MKV.KTObjects.behandlinger.behandlingsaarsaktyper.filter(
+        (kt: KTObject) => kt.kode !== MELDING_OM_MANGLENDE_INNBETALING
+      );
+    }
+  };
+
   return (
     <div className="opprettnyoppgave">
       <Skjema.Datovelger feltNavn="mottaksdato" label={<Nav.Typo.Element>Mottaksdato</Nav.Typo.Element>} />
       <Skjema.Select feltNavn="behandlingsaarsakType" label="Årsak" className="aarsakfelt">
-        {MKV.KTObjects.behandlinger.behandlingsaarsaktyper.map((aarsak: KTObject) => (
+        {gyldigeBehandlingsårsaker().map((aarsak: KTObject) => (
           <option key={aarsak.kode} value={aarsak.kode} label={labelTilÅrsak(aarsak)} />
         ))}
       </Skjema.Select>
