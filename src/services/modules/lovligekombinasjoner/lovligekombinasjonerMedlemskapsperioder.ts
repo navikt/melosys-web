4ec9b03327724352273a780a77b6f3f8538b6094
Janita Johansen <janita.marielle.johansen@nav.no>
2024-02-15 09:03:23 +0100

    Ubrukt endepunkt

--- src/services/modules/lovligekombinasjoner/lovligekombinasjonerMedlemskapsperioder.ts
+++ src/services/modules/lovligekombinasjoner/lovligekombinasjonerMedlemskapsperioder.ts
@@ -1,8 +1,5 @@
 import { getAsJson } from "../../utils";
 import { API_BASE_URL, MEDLEMSKAPSPERIODER } from "../../api-constants";
 
-export const hentBestemmelser = (trygdedekning: string) =>
-  getAsJson(`${API_BASE_URL}${MEDLEMSKAPSPERIODER}/bestemmelse/lovlige-kombinasjoner?trygdedekning=${trygdedekning}`);
-
 export const hentTrygdedekninger = (bestemmelse: string) =>
   getAsJson(`${API_BASE_URL}${MEDLEMSKAPSPERIODER}/trygdedekning/lovlige-kombinasjoner?bestemmelse=${bestemmelse}`);
