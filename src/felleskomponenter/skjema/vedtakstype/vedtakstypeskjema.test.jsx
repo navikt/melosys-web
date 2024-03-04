7a6121052b3c0a1d89bb9e7a36fdd9f2b20f8eda
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2024-03-04 16:54:46 +0100

    vedtakstypeskjema.test.jsx enzyme rewrite

--- src/felleskomponenter/skjema/vedtakstype/vedtakstypeskjema.test.jsx
+++ src/felleskomponenter/skjema/vedtakstype/vedtakstypeskjema.test.jsx
@@ -1,76 +1,30 @@
-import * as MKV from "@navikt/melosys-kodeverk";
-
-import * as Skjema from "..";
-
 import VedtakstypeSkjema from "./vedtakstypeskjema";
+import { reduxForm } from "redux-form";
+import { renderWithProviders } from "../../../ducks/test-utils/renderWithProviders";
+
+vi.mock("../../../utils", async () => {
+  const actual = await vi.importActual("../../../utils");
+  return {
+    ...actual,
+    _uuid: () => "123",
+  };
+});
 
 describe("Vedtaketypeskjema", () => {
-  describe("radiogruppe", () => {
-    const props = {
+  let props;
+  const WrappedVedtakstypeSkjema = reduxForm({ form: "test" })(VedtakstypeSkjema);
+
+  beforeEach(() => {
+    props = {
       className: "artikkel",
       redigerbart: true,
       feltNavn: "feltNavn",
       label: "label",
     };
-
-    const vedtakstypeSkjema = shallow(<VedtakstypeSkjema {...props} />);
-    const radiogruppe = vedtakstypeSkjema.find(Skjema.RadioGruppe);
-
-    it("vises", () => {
-      expect(radiogruppe).toHaveLength(1);
-    });
-
-    it("setter className", () => {
-      expect(radiogruppe.props().className).toBe(props.className);
-    });
-
-    it("setter label", () => {
-      expect(radiogruppe.props().label).toBe(props.label);
-    });
-
-    it("setter feltNavn", () => {
-      expect(radiogruppe.props().feltNavn).toBe(props.feltNavn);
-    });
   });
 
-  describe("radioknapper", () => {
-    let props = null;
-    let vedtakstypeSkjema = null;
-    let radioknapper = null;
-
-    beforeEach(() => {
-      props = {
-        className: "artikkel",
-        redigerbart: true,
-        feltNavn: "feltNavn",
-        label: "label",
-      };
-      vedtakstypeSkjema = shallow(<VedtakstypeSkjema {...props} />);
-      radioknapper = vedtakstypeSkjema.find(Skjema.Radio);
-    });
-
-    it("viser 2", () => {
-      expect(radioknapper).toHaveLength(2);
-    });
-
-    it("sin feltNavn settes", () => {
-      radioknapper.forEach((radioKnapp) => {
-        expect(radioKnapp.props().feltNavn).toBe(props.feltNavn);
-      });
-    });
-
-    describe("sin disabled", () => {
-      it("settes av redigerbart for korrigeringsvedtak", () => {
-        expect(radioknapper.find({ value: MKV.Koder.vedtakstyper.KORRIGERT_VEDTAK }).props().disabled).toBe(false);
-
-        vedtakstypeSkjema = shallow(<VedtakstypeSkjema {...props} redigerbart={false} />);
-        radioknapper = vedtakstypeSkjema.find(Skjema.Radio);
-        expect(radioknapper.find({ value: MKV.Koder.vedtakstyper.KORRIGERT_VEDTAK }).props().disabled).toBe(true);
-      });
-
-      it("er disabled for omgjøringsvedtak", () => {
-        expect(radioknapper.find({ value: MKV.Koder.vedtakstyper.OMGJØRINGSVEDTAK }).props().disabled).toBe(true);
-      });
-    });
+  it("snapshot test", () => {
+    const { container } = renderWithProviders(<WrappedVedtakstypeSkjema {...props} />);
+    expect(container).toMatchSnapshot();
   });
 });
