ac0c54c51ed371b074e9d8f12fe5a52f61a28189
Janita Johansen <56385061+janitamarielle@users.noreply.github.com>
2024-03-05 11:28:48 +0100

    6337 TOGGLE Periodesteg bugs fra test (#2235)
    https://jira.adeo.no/browse/MELOSYS-6337
    
    - Endrer tekst på en feilmelding (vises kun for visse bestemmelser som
    er togglet)
    - Støtter lagring av medlemskapsperiode med tom tom-dato (kun tillatt
    for bestemmelse som er togglet)

--- src/sider/ftrl/saksbehandling/stegKomponenter/vurderingPeriode/vurderingPerioder.tsx
+++ src/sider/ftrl/saksbehandling/stegKomponenter/vurderingPeriode/vurderingPerioder.tsx
@@ -144,8 +144,8 @@ export const VurderingPerioder = ({ bekreft, tilbake, aktivtSteg, oppdaterStatus
 
   const lagreMedlemskapsperiode = async (medlemskapsperiode: MedlemskapsperiodeProp, index: number) => {
     const periodeRequest = {
-      fomDato: Utils.dato.formatterDatoTilISO(medlemskapsperiode.fomDato),
-      tomDato: Utils.dato.formatterDatoTilISO(medlemskapsperiode.tomDato),
+      fomDato: Utils.dato.formatterDatoTilISO(medlemskapsperiode.fomDato, false, ""),
+      tomDato: Utils.dato.formatterDatoTilISO(medlemskapsperiode.tomDato, false, ""),
       trygdedekning: medlemskapsperiode.trygdedekning,
       bestemmelse:
         medlemskapsperiode.innvilgelsesResultat === OPPHØRT
