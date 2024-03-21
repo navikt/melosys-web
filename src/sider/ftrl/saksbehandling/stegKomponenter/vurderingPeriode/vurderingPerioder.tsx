d8b78593049a4007e939f5adbbd8e7b39418d1da
Fredrik Olsen Hauge <45845087+fredrik-oh@users.noreply.github.com>
2024-03-21 15:55:12 +0100

    6516 La til validering om at medlemskapsperiode ikke kan overskride 1… (#2253)
    …2 mnd
    https://jira.adeo.no/browse/MELOSYS-6515
    
    Dersom bestemmelse FTRL_KAP2_2_1 og arbeidssituasjon er midlertidig
    arbeid i utlandet
    
    ---------
    
    Co-authored-by: Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>

--- src/sider/ftrl/saksbehandling/stegKomponenter/vurderingPeriode/vurderingPerioder.tsx
+++ src/sider/ftrl/saksbehandling/stegKomponenter/vurderingPeriode/vurderingPerioder.tsx
@@ -87,6 +87,7 @@ export const VurderingPerioder = ({ bekreft, tilbake, aktivtSteg, oppdaterStatus
   const soknadsland = useSelector(mottatteOpplysningerSelectors.SoknadslandkoderSelector);
   const ikkeyrkesaktivOppholdstype = useSelector(oppsummertfaktaSelectors.IkkeYrkesaktivOppholdSelector);
   const medlemskapsTypeErPliktig = lagredeMedlemskapsperioder.some((periode) => periode.medlemskapstype === PLIKTIG);
+  const arbeidssituasjonType = useSelector(oppsummertfaktaSelectors.ArbeidssituasjonSelector);
 
   const {
     control,
@@ -121,7 +122,8 @@ export const VurderingPerioder = ({ bekreft, tilbake, aktivtSteg, oppdaterStatus
     manglendeInnbetalingToggleEnabled,
     soknadsperiode.fom,
     soknadsperiode.tom,
-    ikkeyrkesaktivOppholdstype
+    ikkeyrkesaktivOppholdstype,
+    arbeidssituasjonType
   );
 
   const stegErGyldig = formIsValid && !feilMeldingBlokkerer(aktivFeilmeldingType);
