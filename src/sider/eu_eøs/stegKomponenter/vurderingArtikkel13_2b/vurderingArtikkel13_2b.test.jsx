af55f66b65de39be8a45898be460a96ce02550a5
Janita Johansen <janita.marielle.johansen@nav.no>
2024-03-25 13:34:03 +0100

    vurderingArtikkel13_2b.test.jsx

--- src/sider/eu_eøs/stegKomponenter/vurderingArtikkel13_2b/vurderingArtikkel13_2b.test.jsx
+++ src/sider/eu_eøs/stegKomponenter/vurderingArtikkel13_2b/vurderingArtikkel13_2b.test.jsx
@@ -1,42 +1,48 @@
+import { render, screen } from "@testing-library/react";
 import MKV from "../../../../melosyskodeverk";
-
 import VurderingArtikkel13_2b from "./vurderingArtikkel13_2b";
-import EnkeltLandPure from "../../../../felleskomponenter/skjema/landvelger/enkeltLandPure";
 
+vi.mock("../../../../utils", async () => {
+  const actual = await vi.importActual("../../../../utils");
+  return {
+    ...actual,
+    _uuid: () => "123",
+  };
+});
+vi.mock("nav-frontend-js-utils", async () => {
+  const actual = await vi.importActual("nav-frontend-js-utils");
+  return {
+    ...actual,
+    guid: () => "123",
+  };
+});
+
+// TODO: Skriv om når aksel tas inn i prosjektet. MELOSYS-6021
 /* eslint-disable react/jsx-pascal-case */
-describe("VurderingArtikkel13_2b", () => {
-  let props = null;
-
-  beforeEach(() => {
-    props = {
-      tilstand: {
-        omfattesILandFakta: {
-          fakta: [MKV.Koder.landkoder.CY],
-        },
-        harAvklaring: true,
+describe.skip("VurderingArtikkel13_2b", () => {
+  const props = (omfattesILandFaktaKode = MKV.Koder.landkoder.CY) => ({
+    tilstand: {
+      omfattesILandFakta: {
+        fakta: [omfattesILandFaktaKode],
       },
-      redigerbart: true,
-      oppdaterData: vi.fn(),
-      slettData: vi.fn(),
-      bekreftOgFortsett: vi.fn(),
-      tilbake: vi.fn(),
-    };
+      harAvklaring: true,
+    },
+    redigerbart: true,
+    oppdaterData: vi.fn(),
+    slettData: vi.fn(),
+    bekreftOgFortsett: vi.fn(),
+    tilbake: vi.fn(),
   });
 
-  it("vises uten å krasje", () => {
-    shallow(<VurderingArtikkel13_2b {...props} />);
+  it("snapshot test dersom Norge ikke er valgt", () => {
+    const { container } = render(<VurderingArtikkel13_2b {...props()} />);
+    expect(screen.getByText("Velg land:")).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 
-  it("viser landvelger dersom Norge ikke er valgt", () => {
-    const vurderingArtikkel13_2b = shallow(<VurderingArtikkel13_2b {...props} />);
-
-    expect(vurderingArtikkel13_2b.find(EnkeltLandPure)).toHaveLength(1);
-  });
-
-  it("viser ikke landvelger dersom Norge er valgt", () => {
-    props.tilstand.omfattesILandFakta.fakta = [MKV.Koder.landkoder.NO];
-    const vurderingArtikkel13_2b = shallow(<VurderingArtikkel13_2b {...props} />);
-
-    expect(vurderingArtikkel13_2b.find(EnkeltLandPure)).toHaveLength(0);
+  it("snapshot test dersom Norge er valgt", () => {
+    const { container } = render(<VurderingArtikkel13_2b {...props(MKV.Koder.landkoder.NO)} />);
+    expect(screen.queryByText("Velg land:")).toBeNull();
+    expect(container).toMatchSnapshot();
   });
 });
