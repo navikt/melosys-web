a217f31947f032df790b8faf14cb7c545b2c5be3
Fredrik Olsen Hauge <45845087+fredrik-oh@users.noreply.github.com>
2024-04-05 14:53:52 +0200

    6398 La tilbake htmlType i nullstill knapp (#2269)
    Co-authored-by: Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>

--- src/sider/forside/komponenter/saksplukker/saksplukker.tsx
+++ src/sider/forside/komponenter/saksplukker/saksplukker.tsx
@@ -184,7 +184,9 @@ export const Saksplukker = ({
         )}
         <Nav.Row className="saksplukker__knapperad">
           <Nav.Knapp className="saksplukker__knapp">Behandle sak</Nav.Knapp>
-          <Nav.Flatknapp onClick={nullstill}>Nullstill</Nav.Flatknapp>
+          <Nav.Flatknapp htmlType="button" onClick={nullstill}>
+            Nullstill
+          </Nav.Flatknapp>
         </Nav.Row>
       </form>
     </Nav.Panel>
