d003e3fd4b23ddf2c251a4a81e89e784c6d53bdc
barstadEkstern <oystein.madsen.barstad@nav.no>
2024-04-15 13:40:41 +0200

    fjern ikke eksisterende parametre i skjema

--- src/sider/årsavregning/stegKomponenter/vurderingInngang/vurderingInngangSchema.js
+++ src/sider/årsavregning/stegKomponenter/vurderingInngang/vurderingInngangSchema.js
@@ -1,12 +1,5 @@
-import { object, string } from "yup";
-import * as KV from "../../../../kodeverk";
+import { object } from "yup";
 
-const { MAA_FYLLES_UT, VELG_LAND } = KV.Feilmeldinger;
-
-const vurderingInngang = object().shape({
-  fom: string().erGyldigDato().required(MAA_FYLLES_UT),
-  tom: string().erGyldigDato().erEtterDatofelt("fom").nullable(),
-  land: string().required(VELG_LAND),
-});
+const vurderingInngang = object().shape({});
 
 export default vurderingInngang;
