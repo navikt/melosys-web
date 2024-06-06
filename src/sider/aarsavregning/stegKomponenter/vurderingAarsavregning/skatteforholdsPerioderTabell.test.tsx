c7c258541126470147a715204765ec1caa499773
barstadEkstern <oystein.madsen.barstad@nav.no>
2024-06-06 13:41:22 +0200

    test

--- src/sider/aarsavregning/stegKomponenter/vurderingAarsavregning/skatteforholdsPerioderTabell.test.tsx
+++ src/sider/aarsavregning/stegKomponenter/vurderingAarsavregning/skatteforholdsPerioderTabell.test.tsx
@@ -2,14 +2,38 @@ import SkatteforholdsPerioderTabell from "./skatteforholdsPerioderTabell";
 import { render, screen } from "@testing-library/react";
 
 describe("SkatteforholdsPerioderTabell", () => {
-  it("renders render tabell uten data", () => {
-    render(<SkatteforholdsPerioderTabell perioder={[]} />);
+  it("ikke rendrer tabell når perioder prop er undefined", () => {
+    render(<SkatteforholdsPerioderTabell />);
+    expect(screen.queryByText(/Skatteforhold/i)).toBeNull();
+    expect(screen.queryByText(/Skattepliktig/i)).toBeNull();
   });
 
-  it("Rendrer tabell med data", () => {
+  it("rendrer tabell med data", () => {
     const perioder = [{ fom: "2022-01-01", tom: "2022-12-31", skatteplikttype: "true" }];
     render(<SkatteforholdsPerioderTabell perioder={perioder} />);
     expect(screen.getByText(/Skatteforhold/i)).toBeInTheDocument();
     expect(screen.getByText(/Skattepliktig/i)).toBeInTheDocument();
+  });
+
+  it("rendrer tabell med flere perioder", () => {
+    const perioder = [
+      { fom: "2022-01-01", tom: "2022-12-31", skatteplikttype: "true" },
+      { fom: "2023-01-01", tom: "2023-12-31", skatteplikttype: "false" },
+    ];
+    render(<SkatteforholdsPerioderTabell perioder={perioder} />);
+    const rows = screen.getAllByRole("row");
+    expect(rows.length - 1).toBe(perioder.length); // -1 for header
+  });
+
+  it("formaterer og viser riktig fom og tom datoer", () => {
+    const perioder = [{ fom: "2022-01-01", tom: "2022-12-31", skatteplikttype: "true" }];
+    render(<SkatteforholdsPerioderTabell perioder={perioder} />);
+    expect(screen.getByText("01.01.2022 - 31.12.2022")).toBeInTheDocument();
+  });
+
+  it("viser riktig skatteplikttype verdi", () => {
+    const perioder = [{ fom: "2022-01-01", tom: "2022-12-31", skatteplikttype: "true" }];
+    render(<SkatteforholdsPerioderTabell perioder={perioder} />);
+    expect(screen.getByText("true")).toBeInTheDocument();
   });
 });
