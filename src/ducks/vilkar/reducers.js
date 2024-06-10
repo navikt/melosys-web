848337cb50bcce91ccd04ec5b4d5e6a46fc96a58
Janita Johansen <56385061+janitamarielle@users.noreply.github.com>
2024-06-10 12:15:17 +0200

    6584 bugs (#2343)
    https://jira.adeo.no/browse/MELOSYS-6584
    
    Bugs som dukket opp i ettekant av merging. Hoved-PRen burde ikke gå ut
    uten denne.

--- src/ducks/vilkar/reducers.js
+++ src/ducks/vilkar/reducers.js
@@ -80,7 +80,7 @@ export default function reducer(state = initialState, action = {}) {
           action.data.vilkar.vesentligVirksomhet_begrunnelser
         ),
         vilkarTilObjekt(
-          MKV.Koder.vilkaar.FORUTGAAENDE_MEDLEMSKAP,
+          MKV.Koder.vilkaar.NORMALT_DRIVER_VIRKSOMHET,
           action.data.vilkar.normaltDriverVirksomhet,
           action.data.vilkar.normaltDriverVirksomhet_begrunnelser
         ),
@@ -88,7 +88,7 @@ export default function reducer(state = initialState, action = {}) {
         vilkarTilObjekt(
           MKV.Koder.vilkaar.FO_883_2004_ART12_1,
           action.data.vilkar.art12_1,
-          action.data.vilkar.valgte_utsendt_arbeidstaker_begrunnelser
+          action.data.vilkar.art12_1_begrunnelser
         ),
         vilkarTilObjekt(
           MKV.Koder.vilkaar.FO_883_2004_ART12_2,
