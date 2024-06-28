fe9c0398a79b462d8485208a7b8795c8c2c7c62e
Isa Salem Hame <wanghattan@gmail.com>
2024-06-28 20:09:53 +0200

    6520 DAPI: Bruk riktig endepunkt for å se mulige behandlingstyper

--- src/sider/journalforing/komponenter/knyttTilSak.jsx
+++ src/sider/journalforing/komponenter/knyttTilSak.jsx
@@ -96,13 +96,10 @@ export const KnyttTilSak = (props) => {
 
   useEffect(() => {
     if (sakstype.kode && sakstema.kode && behandlingstema) {
-      Api.LovligeKombinasjoner.hentBehandlingstyper(
+      Api.LovligeKombinasjoner.hentBehandlingstyperForKnyttTilSak(
         journalforingGjelder,
-        sakstype.kode,
-        sakstema.kode,
-        behandlingstema,
-        null,
-        sisteBehandling.behandlingID
+        sak.saksnummer,
+        behandlingstema
       ).then((alleMuligeBehandlingstyper) => {
         setMuligeBehandlingstyper(alleMuligeBehandlingstyper);
       });
