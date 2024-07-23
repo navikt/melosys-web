d13a4b4fbdf13ce5bb0f1ccd799e8fe588bbb0db
Isa Salem Hame <wanghattan@gmail.com>
2024-07-23 14:02:54 +0200

    Fiks test etter merge

--- src/sider/aarsavregning/stegKomponenter/vurderingAarsavregning/vurderingAarsavregning.test.tsx
+++ src/sider/aarsavregning/stegKomponenter/vurderingAarsavregning/vurderingAarsavregning.test.tsx
@@ -1,11 +1,61 @@
+import { reduxForm } from "redux-form";
 import { VurderingAarsavregning } from "./vurderingAarsavregning";
+import { STATUS } from "../../../../services";
 import { renderWithProviders } from "../../../../ducks/test-utils/renderWithProviders";
 
-vi.mock("uuid");
+describe("VurderingAarsavreging", () => {
+  const initialReduxState = {
+    behandlinger: {
+      data: {
+        behandlingID: 4,
+        oppsummering: {},
+        redigerbart: true,
+      },
+    },
+    dokumenter: {
+      data: {
+        dokumentOversikt: [
+          {
+            journalpostID: "321",
+            journalforingDato: null,
+            mottattDato: null,
+            avsenderEllerMottaker: "avsendernavn",
+            mottaksretning: { kode: "INN", term: "Inngående" },
+            hoveddokument: {
+              tittel: "tittel",
+              dokumentID: "123",
+              logiskeVedlegg: [],
+            },
+            vedlegg: [],
+          },
+        ],
+      },
+    },
+    fagsaker: {
+      data: {
+        saksnummer: "333",
+        sakstype: {},
+      },
+      status: STATUS.OK,
+    },
+  };
+  // @ts-ignore
+  const WrappedVurderingAarsavregning = reduxForm({ form: "test" })(VurderingAarsavregning);
 
-describe("VurderingAarsavregning", () => {
-  it("snapshot test", () => {
-    const { asFragment } = renderWithProviders(<VurderingAarsavregning />);
-    expect(asFragment()).toMatchSnapshot();
+  beforeEach(() => {
+    // @ts-ignore
+    fetch.resetMocks();
+    // @ts-ignore
+    fetch.mockResponse(JSON.stringify({}));
+  });
+
+  describe("VurderingAarsavregning", () => {
+    it("snapshot test", () => {
+      // @ts-ignore
+      const { container } = renderWithProviders(<WrappedVurderingAarsavregning />, {
+        preloadedState: initialReduxState,
+      });
+      expect(container).toMatchSnapshot();
+    });
   });
 });
