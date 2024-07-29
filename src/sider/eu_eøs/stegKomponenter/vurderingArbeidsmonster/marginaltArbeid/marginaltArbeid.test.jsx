7224e33dd6006d0f4acac88bfc1d870f72bef009
thomasdolplads <thomas.dolplads@twoday.com>
2024-07-29 17:12:49 +0200

    noe opprydding

--- src/sider/eu_eøs/stegKomponenter/vurderingArbeidsmonster/marginaltArbeid/marginaltArbeid.test.jsx
+++ src/sider/eu_eøs/stegKomponenter/vurderingArbeidsmonster/marginaltArbeid/marginaltArbeid.test.jsx
@@ -2,44 +2,26 @@ import userEvent from "@testing-library/user-event";
 import { renderWithProviders } from "../../../../../ducks/test-utils/renderWithProviders";
 import { testReduxState } from "../../../../journalforing/komponenter/journalforingform/testReduxState";
 import MarginaltArbeid from "./marginaltArbeid";
-import { getAllByRole, prettyDOM } from "@testing-library/dom";
 import { lagAvklartfakta } from "../../../../../felleskomponenter/stegvelger";
 import MKV from "../../../../../melosyskodeverk";
 import { BOOLSK_STRING } from "../../../../../constants";
-import { string } from "yup";
-//import { useSelector } from "react-redux";
-/*
-vi.mock("react-redux", async () => {
-  const actual = await vi.importActual("react-redux");
-  return {
-    ...actual,
-    useSelector: () => true,
-  };
-});
 
- */
+const opprettLand = (k, t) => {
+  return {
+    land: {
+      kode: k,
+      term: t,
+    },
+    erLonnetArbeid: false,
+    erSelvstendigNaeringsvirksomhet: false,
+  };
+};
 
 describe("MarginaltArbeid", () => {
   const user = userEvent.setup();
   const oppdaterDataMock = vi.fn();
 
   let props = null;
-  const sveOption = {
-    land: {
-      kode: "SVE",
-      term: "SVERIGE",
-    },
-    erLonnetArbeid: false,
-    erSelvstendigNaeringsvirksomhet: false,
-  };
-  const norOption = {
-    land: {
-      kode: "NOR",
-      term: "NORGE",
-    },
-    erLonnetArbeid: false,
-    erSelvstendigNaeringsvirksomhet: false,
-  };
 
   beforeAll(() => {
     props = {
@@ -71,15 +53,4 @@ describe("MarginaltArbeid", () => {
       lagAvklartfakta(MKV.Koder.avklartefaktatyper.MARGINALT_ARBEID, "SVE", BOOLSK_STRING.SANN)
     );
   });
-
-  const opprettLand = (kode, term) => {
-    return {
-      land: {
-        kode: kode,
-        term: term,
-      },
-      erLonnetArbeid: false,
-      erSelvstendigNaeringsvirksomhet: false,
-    };
-  };
 });
