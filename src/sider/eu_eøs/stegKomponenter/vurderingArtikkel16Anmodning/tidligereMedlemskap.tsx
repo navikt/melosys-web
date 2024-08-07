1a3c4ee6ffb47ada1c28f9280aa2222b463453c0
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2024-08-07 15:25:28 +0200

    NOJIRA Fikset en bug med checkboxes i anmodning om unntak

--- src/sider/eu_eøs/stegKomponenter/vurderingArtikkel16Anmodning/tidligereMedlemskap.tsx
+++ src/sider/eu_eøs/stegKomponenter/vurderingArtikkel16Anmodning/tidligereMedlemskap.tsx
@@ -1,5 +1,4 @@
 import { formatterDatoTilNorsk } from "../../../../utils/dato";
-import * as Mui from "../../../../felleskomponenter/ui";
 import * as Nav from "../../../../navFrontend";
 import { v4 as uuid } from "uuid";
 import { FieldArray, WrappedFieldArrayProps } from "redux-form";
@@ -43,11 +42,9 @@ const TidligereMedlemskapsperioder = ({ medlemskap, redigerbart, fields, land }:
       {medlemskap?.perioderMed?.map((periodeMed) => {
         const { periodeID, periode } = periodeMed;
         return (
-          <Mui.Checkbox
-            key={uuid()}
-            onCheck={() => onChange(periodeID)}
-            label={`Periode: ${formatterDatoTilNorsk(periode.fom)} - ${formatterDatoTilNorsk(periode.tom)}`}
-          />
+          <Nav.Checkbox key={uuid()} value={periodeID} onChange={() => onChange(periodeID)}>
+            {`Periode: ${formatterDatoTilNorsk(periode.fom)} - ${formatterDatoTilNorsk(periode.tom)}`}
+          </Nav.Checkbox>
         );
       })}
     </Nav.CheckboxGroup>
