5734efe0736f2cc1979ffb30dc76151082be0770
Fredrik Olsen Hauge <45845087+fredrik-oh@users.noreply.github.com>
2024-08-21 15:12:50 +0200

    6761 TOGGLE forenklet vedtakssteg aarsavregning (#2410)
    https://jira.adeo.no/browse/MELOSYS-6761
    
    api: https://github.com/navikt/melosys-api/pull/2496
    dokgen: https://github.com/navikt/melosys-dokgen/pull/394
    
    ---------
    
    Co-authored-by: Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>

---
+++ src/sider/aarsavregning/stegKomponenter/vurderingVedtak/vurderingVedtakSchema.js
@@ -0,0 +1,13 @@
+// noinspection ES6PreferShortImport
+
+import { object, string } from "yup";
+import * as KV from "../../../../kodeverk";
+
+const { DU_KAN_IKKE_SKRIVE_MER_ENN_4000_TEGN } = KV.Feilmeldinger;
+
+const vurdering_vedtak = object().shape({
+  begrunnelseFritekst: string().nullable().max(4000, DU_KAN_IKKE_SKRIVE_MER_ENN_4000_TEGN),
+  innledningFritekst: string().nullable().max(4000, DU_KAN_IKKE_SKRIVE_MER_ENN_4000_TEGN),
+});
+
+export default vurdering_vedtak;
