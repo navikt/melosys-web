42b0f384f2aba4c2b561fb5e6df63ab52764674f
Øystein Barstad <111733896+barstadEkstern@users.noreply.github.com>
2024-08-26 11:24:02 +0200

    6787 PRODFIX fix undefined arbeidsland (#2419)
    https://jira.adeo.no/browse/MELOSYS-6787
    Legg til optional

--- src/sider/eu_eøs/saksbehandling/stegMap/virksomheter.jsx
+++ src/sider/eu_eøs/saksbehandling/stegMap/virksomheter.jsx
@@ -21,7 +21,7 @@ class SaksbehandlingVirksomheter extends Virksomheter {
       KV.Koder.VurderingYrkesgruppeTyper.ORDINAER_UTEN_ART12
     );
 
-    const arbeidslandErNorge = propsLight.arbeidsland[0].kode === MKV.Koder.landkoder.NO;
+    const arbeidslandErNorge = propsLight.arbeidsland[0]?.kode === MKV.Koder.landkoder.NO;
 
     const yrkesgruppe = hentFakta(KV.Koder.avklartefaktaKoder.YRKESGRUPPE, propsLight.avklartefakta);
     const erYrkesaktiv = yrkesgruppe.fakta?.includes(KV.Koder.VurderingYrkesgruppeTyper.ORDINAER) || false;
