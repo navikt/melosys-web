d5590ae6553539f1066a5fb1bb74999b5a379b98
Thang Phan <thang.quoc.ly.phan@nav.no>
2024-09-09 12:43:27 +0200

    4000 tegn

--- src/sider/eu_eøs/stegKomponenter/vurderingVedtak11_3_og_13_3a/vurderingVedtak11_3_og_13_3aSchema.js
+++ src/sider/eu_eøs/stegKomponenter/vurderingVedtak11_3_og_13_3a/vurderingVedtak11_3_og_13_3aSchema.js
@@ -5,7 +5,7 @@ import { MAA_FYLLES_UT } from "../../../../kodeverk/feilmeldinger";
 const { DU_KAN_IKKE_SKRIVE_MER_ENN_4000_TEGN } = KV.Feilmeldinger;
 
 const vurderingVedtak_11_3_og_13_3aSchema = object().shape({
-  begrunnelseFritekst: string().nullable().max(10, DU_KAN_IKKE_SKRIVE_MER_ENN_4000_TEGN),
+  begrunnelseFritekst: string().nullable().max(4000, DU_KAN_IKKE_SKRIVE_MER_ENN_4000_TEGN),
   fom: string().erGyldigDato().required(MAA_FYLLES_UT),
   tom: string().erGyldigDato().erEtterDatofelt("fom").required(MAA_FYLLES_UT),
 });
