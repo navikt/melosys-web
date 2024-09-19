23b87cac3f6361a75c40d0b6fac81d17eee9dedc
thomasdolplads <thomas.dolplads@twoday.com>
2024-09-19 12:31:18 +0200

    6841 disable fattvedtak etter ferdigstillelse

--- src/sider/aarsavregning/stegKomponenter/vurderingVedtak/vurderingVedtak.tsx
+++ src/sider/aarsavregning/stegKomponenter/vurderingVedtak/vurderingVedtak.tsx
@@ -209,7 +209,7 @@ export const VurderingVedtak = ({ tilbake, aktivtSteg }: Props) => {
       <Mui.StegKnapper
         bekreftKnappProps={{
           onClick: fattVedtakOnClick,
-          disabled: !stegErGyldig,
+          disabled: !stegErGyldig || !formIsValid,
           loading: vedtakPending,
         }}
         bekreftTekst="Fatt vedtak"
