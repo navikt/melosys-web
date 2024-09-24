a4c10b021926a555a1b80bae45d138584f6564f3
barstadEkstern <oystein.madsen.barstad@nav.no>
2024-09-24 14:13:10 +0200

    fjern ubrukt kode

--- src/felleskomponenter/stegvelger/Stegvelger.jsx
+++ src/felleskomponenter/stegvelger/Stegvelger.jsx
@@ -764,7 +764,6 @@ const mapStateToProps = (state) => ({
 /* eslint no-alert:off */
 const mapDispatchToProps = (dispatch) => ({
   hentVilkar: (behandlingID) => dispatch(vilkarOperations.hent(behandlingID)),
-  endreVedtak: (behandlingID, body) => dispatch(vedtakOperations.endre(behandlingID, body)),
   kontrollerFerdigbehandling: (data) => dispatch(kontrollOperations.kontrollerFerdigbehandling(data)),
   videresend: (saksnummer, videresending) => dispatch(videresendingOperations.send(saksnummer, videresending)),
   hentAvklartefakta: (behandlingID) => dispatch(avklartefaktaOperations.hent(behandlingID)),
