0527a4f64354b629248c4aea8dff87dfb8c00188
thomasdolplads <thomas.dolplads@twoday.com>
2024-10-08 10:57:14 +0200

    6858 [TOGGLE] validering (#2470)
    https://jira.adeo.no/browse/MELOSYS-6858

--- src/sider/eu_eøs/stegKomponenter/vurderingVedtak11_3_og_13_3a/vurderingVedtak11_3_og_13_3aSchema.js
+++ src/sider/eu_eøs/stegKomponenter/vurderingVedtak11_3_og_13_3a/vurderingVedtak11_3_og_13_3aSchema.js
@@ -6,8 +6,14 @@ const { DU_KAN_IKKE_SKRIVE_MER_ENN_4000_TEGN } = KV.Feilmeldinger;
 
 const vurderingVedtak_11_3_og_13_3aSchema = object().shape({
   begrunnelseFritekst: string().nullable().max(4000, DU_KAN_IKKE_SKRIVE_MER_ENN_4000_TEGN),
-  fom: string().erGyldigDato().required(MAA_FYLLES_UT),
-  tom: string().erGyldigDato().erEtterDatofelt("fom").required(MAA_FYLLES_UT),
+  fom: string().when("korterePeriodeChecked", {
+    is: true,
+    then: string().erInnenforSoknadsperioden().erGyldigDato().required(MAA_FYLLES_UT),
+  }),
+  tom: string().when("korterePeriodeChecked", {
+    is: true,
+    then: string().erInnenforSoknadsperioden().erEtterDatofelt("fom").erGyldigDato().required(MAA_FYLLES_UT),
+  }),
 });
 
 export default vurderingVedtak_11_3_og_13_3aSchema;
