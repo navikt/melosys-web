08d6150d78dfe4a6cd6a8bc6e631d7fac7903e59
thomasdolplads <thomas.dolplads@twoday.com>
2024-10-16 10:43:24 +0200

    ubrukt import

--- src/sider/trygdeavtale/stegKomponenter/vurderingBestemmelse/bestemmelseHjelpetekst/bestemmelseHjelpetekst.tsx
+++ src/sider/trygdeavtale/stegKomponenter/vurderingBestemmelse/bestemmelseHjelpetekst/bestemmelseHjelpetekst.tsx
@@ -1,6 +1,5 @@
 import MKV from "../../../../../melosyskodeverk";
 import * as Hjelpetekster from "./hjelpetekster";
-import { List } from "@navikt/ds-react";
 import { MelList, MelListItem } from "../../../../../felleskomponenter/liste/melListe";
 
 const BestemmelseHjelpetekst = ({ bestemmelse }: { bestemmelse?: string }) => {
