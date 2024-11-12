93d90e690de8a760d0262fb2d8743f1d73f768c9
Isa Salem Hame <isa.salem.hame@nav.no>
2024-11-12 11:32:44 +0100

    mapDokumenter

--- src/sider/eu_eøs/stegKomponenter/vurderingVedtak/vurderingVedtak.tsx
+++ src/sider/eu_eøs/stegKomponenter/vurderingVedtak/vurderingVedtak.tsx
@@ -354,7 +354,7 @@ const VurderingVedtak = ({
                 behandlingID={behandlingID}
                 dokumenter={
                   bucLukketOgLovvalgNorge
-                    ? pdfDokumenter.filter((dok) => !("sedType" in dok))
+                    ? mapDokumenter(pdfDokumenter.filter((dok) => !("sedType" in dok)) as BrevDokumentMetadataType[])
                     : mapDokumenter(pdfDokumenter as BrevDokumentMetadataType[])
                 }
               />
