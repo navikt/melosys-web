83dadaf7ef47ae7110c37f77c75fb860206f52d7
barstadEkstern <oystein.madsen.barstad@nav.no>
2024-11-22 13:45:43 +0100

    Fjern ubrukelig test

--- src/hooks/useCallbackState.test.ts
+++ src/hooks/useCallbackState.test.ts
@@ -13,18 +13,7 @@ describe("useCallbackState", () => {
 });
 
 describe("useAsyncCallbackState", () => {
-  it("tar imot et async callback og lagrer resultatet til state", async () => {
-    const asyncSum = async (a: number, b: number) => a + b;
 
-    const rh = renderHook(() => useAsyncCallbackState(() => asyncSum(1, 2), 0, []));
-
-    await act(async () => {
-      await rh.waitForNextUpdate();
-    });
-
-    const [state] = rh.result.current;
-    expect(state).toBe(3);
-  });
 
   it("kaller errorHandler når callback rejectes", async () => {
     const rejectionReason = new Error();
