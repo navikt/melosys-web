d4ff295800c765aba9375432ae6b6339530d4148
Isa Salem Hame <isa.salem.hame@nav.no>
2024-12-03 13:31:14 +0100

    MELOSYS-6847: Legg til 11.3A dersom Norge er utpekt

--- src/sider/eu_eøs/stegKomponenter/vurderingUtpekt/vurderingUtpekt.jsx
+++ src/sider/eu_eøs/stegKomponenter/vurderingUtpekt/vurderingUtpekt.jsx
@@ -33,19 +33,8 @@ import { lagYupToReduxformErrorMapper } from "../../../../yup";
 import vurderingUtpektSchema from "./vurderingUtpektSchema";
 
 import "./vurderingUtpekt.css";
-
-const lovvalgsbestemmelserStottetAvBrevVedNorgeUtpekt = MKV.Kodekombinasjoner.alleEØSLovvalg.filter(
-  ({ kode }) =>
-    kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_1A ||
-    kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_1B1 ||
-    kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_1B2 ||
-    kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_1B3 ||
-    kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_1B4 ||
-    kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_2A ||
-    kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_2B ||
-    kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_3 ||
-    kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_4
-);
+import { useFeatureToggle } from "../../../../featuretoggle";
+import { MELOSYS_11_3_A_NORGE_ER_UTPEKT } from "../../../../featuretoggle/toggleNavn";
 
 export const VurderingUtpekt = ({
   vurderingBegrunnelser,
@@ -61,6 +50,23 @@ export const VurderingUtpekt = ({
   behandlingstema,
   behandlingID,
 }) => {
+  const erToggle11_3_A_NorgeErUtpektEnabled = useFeatureToggle(MELOSYS_11_3_A_NORGE_ER_UTPEKT);
+
+  const lovvalgsbestemmelserStottetAvBrevVedNorgeUtpekt = MKV.Kodekombinasjoner.alleEØSLovvalg.filter(
+    ({ kode }) =>
+      kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_1A ||
+      kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_1B1 ||
+      kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_1B2 ||
+      kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_1B3 ||
+      kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_1B4 ||
+      kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_2A ||
+      kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_2B ||
+      kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_3 ||
+      kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART13_4 ||
+      (erToggle11_3_A_NorgeErUtpektEnabled &&
+        kode === MKV.Koder.lovvalgsbestemmelser.lovvalgbestemmelser_883_2004.FO_883_2004_ART11_3A)
+  );
+
   const [erBucAapen, setErBucAapen] = useState(true);
   const dispatch = useDispatch();
 
