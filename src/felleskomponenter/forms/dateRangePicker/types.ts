2930415dc3d1f1f932be06e9f7f481be25f62d8e
thomasdolplads <thomas.dolplads@twoday.com>
2024-12-18 09:39:25 +0100

    opprydding props

--- src/felleskomponenter/forms/dateRangePicker/types.ts
+++ src/felleskomponenter/forms/dateRangePicker/types.ts
@@ -1,19 +1,11 @@
-import { Control, UseControllerProps } from "react-hook-form";
+import { Control } from "react-hook-form";
 import { DatePickerProps, DateInputProps } from "@navikt/ds-react";
-import { ComponentProps } from "react";
-import * as Nav from "../../../navFrontend";
 
 interface DateRangeText {
   from?: string;
   to?: string;
 }
 
-type InputProps = Omit<ComponentProps<typeof Nav.TextField>, "onChange"> &
-  UseControllerProps & {
-    onChange?: (value: string) => void;
-  };
-
-//DateInputProps:: label, hidelabel
 interface Props {
   control: Control;
   onRangeChange: (dateRange: DateRangeText) => void;
@@ -23,5 +15,4 @@ interface Props {
 
 export type { DateRangeText };
 export type DateRangePickerProps = Props & DatePickerProps & DateInputProps;
-//export type ExternalDateRangePickerProps = Omit<Props & DatePickerProps, "onRangeChange" | "fieldError">;
-export type ExternalDateRangePickerProps = Omit<DateRangePickerProps, "mode" | "onRangeChange" | "fieldError">;
+export type ExternalDateRangePickerProps = Omit<DateRangePickerProps, "mode" | "onRangeChange">;
