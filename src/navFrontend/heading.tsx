94589ac36d483109ae92e5e6066e3e74f3817dcf
Marius Bråthen <marius.brathen@me.com>
2024-12-19 23:46:47 +0100

    Edit to wrapper component

--- src/navFrontend/heading.tsx
+++ src/navFrontend/heading.tsx
@@ -3,16 +3,13 @@ import { Heading as NavHeading } from "@navikt/ds-react";
 type HeadingLevel = "1" | "2" | "3";
 type HeadingSize = "medium" | "small" | "xsmall";
 
-export const Heading = ({
-  level,
-  size,
-  children,
-  ...props
-}: {
+type HeadingProps = React.ComponentProps<typeof NavHeading> & {
   level: HeadingLevel;
   size?: HeadingSize;
   children: React.ReactNode;
-}) => {
+};
+
+export const Heading = ({ level, size, children, ...props }: HeadingProps) => {
   const sizeMapping: Record<HeadingLevel, HeadingSize> = {
     "1": "medium",
     "2": "small",
