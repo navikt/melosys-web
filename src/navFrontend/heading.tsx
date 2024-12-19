11ae8b70149b7b333706cb73cdd4b163a332f4d8
Marius Bråthen <marius.brathen@me.com>
2024-12-20 00:20:20 +0100

    Prop blunder

--- src/navFrontend/heading.tsx
+++ src/navFrontend/heading.tsx
@@ -1,17 +1,14 @@
 import { Heading as NavHeading } from "@navikt/ds-react";
 
 type HeadingLevel = "1" | "2" | "3";
-type HeadingSize = "medium" | "small" | "xsmall";
 
 type HeadingProps = Omit<React.ComponentProps<typeof NavHeading>, "as"> & {
   as?: React.ElementType;
   level?: HeadingLevel;
-  size?: HeadingSize;
-  children: React.ReactNode;
 };
 
 export const Heading = ({ level = "1", size, children, ...props }: HeadingProps) => {
-  const sizeMapping: Record<HeadingLevel, HeadingSize> = {
+  const sizeMapping: Record<HeadingLevel, React.ComponentProps<typeof NavHeading>["size"]> = {
     "1": "medium",
     "2": "small",
     "3": "xsmall",
