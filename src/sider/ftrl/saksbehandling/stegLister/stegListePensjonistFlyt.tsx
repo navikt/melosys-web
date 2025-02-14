464a34810f0794313b2d04b4782fb61edb97f855
Thang Phan <thang.quoc.ly.phan@nav.no>
2025-02-14 11:59:53 +0100

    7105 TOGGLE Inngangssteg pensjonist (#2592)
    https://jira.adeo.no/browse/MELOSYS-7105

---
+++ src/sider/ftrl/saksbehandling/stegLister/stegListePensjonistFlyt.tsx
@@ -0,0 +1,64 @@
+import { FANE_STATUS } from "../../../../felleskomponenter/stegvelger";
+import { VurderingInngang } from "../stegKomponenter/vurderingInngang/vurderingInngang";
+import { VurderingPerioder } from "../stegKomponenter/vurderingPeriode/vurderingPerioder";
+import { VurderingTrygdeavgift } from "../stegKomponenter/vurderingTrygdeavgift/vurderingTrygdeavgift";
+import { VurderingVedtak } from "../stegKomponenter/vurderingVedtak/vurderingVedtak";
+import { VurderingBestemmelse } from "../stegKomponenter/vurderingBestemmelse/vurderingBestemmelse";
+
+const initialInngangSteg = {
+  id: "Inngang",
+  tittel: "Inngang",
+  stegPosisjon: 0,
+  status: FANE_STATUS.UBEHANDLET,
+  aktivtSteg: true,
+  vedtakSteg: false,
+  komponent: VurderingInngang,
+};
+
+const initialBestemmelseSteg = {
+  id: "Bestemmelse",
+  tittel: "Bestemmelse",
+  stegPosisjon: 1,
+  status: FANE_STATUS.UBEHANDLET,
+  aktivtSteg: false,
+  vedtakSteg: false,
+  komponent: VurderingBestemmelse,
+};
+
+const initialPeriodeSteg = {
+  id: "Perioder",
+  tittel: "Perioder",
+  stegPosisjon: 2,
+  status: FANE_STATUS.UBEHANDLET,
+  aktivtSteg: false,
+  vedtakSteg: false,
+  komponent: VurderingPerioder,
+};
+
+const initialTrygdeavgiftSteg = {
+  id: "Trygdeavgift",
+  tittel: "Trygdeavgift",
+  stegPosisjon: 3,
+  status: FANE_STATUS.UBEHANDLET,
+  aktivtSteg: false,
+  vedtakSteg: false,
+  komponent: VurderingTrygdeavgift,
+};
+
+const initialVedtakSteg = {
+  id: "Vedtak",
+  tittel: "Vedtak",
+  stegPosisjon: 4,
+  status: FANE_STATUS.UBEHANDLET,
+  aktivtSteg: false,
+  vedtakSteg: true,
+  komponent: VurderingVedtak,
+};
+
+export const alleStegPensjonistFlyt = [
+  initialInngangSteg,
+  initialBestemmelseSteg,
+  initialPeriodeSteg,
+  initialTrygdeavgiftSteg,
+  initialVedtakSteg,
+];
