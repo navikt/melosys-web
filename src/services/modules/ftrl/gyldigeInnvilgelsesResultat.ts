d9c6c39e1f5ebc4e0a7beab8373de31147f50dc0
Thang Phan <thang.quoc.ly.phan@nav.no>
2025-02-19 12:41:10 +0100

    Fjern noe jeg lagde

--- src/services/modules/ftrl/gyldigeInnvilgelsesResultat.ts
+++ src/services/modules/ftrl/gyldigeInnvilgelsesResultat.ts
@@ -3,9 +3,3 @@ import { API_BASE_URL, FTRL } from "../../api-constants";
 
 export const hentGyldigeInnvilgelsesresultat = (behandlingstype: string): Promise<string[]> =>
   getAsJson(`${API_BASE_URL}${FTRL}/innvilgelsesresultat/?behandlingstype=${behandlingstype}`);
-
-export const hentGyldigeInnvilgelsesresultatPensjonist = (
-  bestemmelse: string,
-  trygdedekning: string,
-): Promise<string[]> =>
-  getAsJson(`${API_BASE_URL}${FTRL}/innvilgelsesresultat/?bestemmelse=${bestemmelse}&trygdedekning=${trygdedekning}`);
