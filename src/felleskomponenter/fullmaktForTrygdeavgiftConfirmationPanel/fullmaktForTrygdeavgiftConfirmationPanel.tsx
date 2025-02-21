9c23e60db13bc359695d28e553ab2bdf1a95d32d
ck-hui <Chak.Kwan.Hui@nav.no>
2025-02-21 13:38:49 +0100

    La til logikk for å vise riktig tekst for pensjonist flyten

--- src/felleskomponenter/fullmaktForTrygdeavgiftConfirmationPanel/fullmaktForTrygdeavgiftConfirmationPanel.tsx
+++ src/felleskomponenter/fullmaktForTrygdeavgiftConfirmationPanel/fullmaktForTrygdeavgiftConfirmationPanel.tsx
@@ -1,11 +1,13 @@
 import * as Nav from "../../navFrontend";
 
 interface FullmaktForTrygdeavgiftConfirmationPanelProps {
+  erPensjonist: boolean;
   harBekreftet: boolean;
   onChange: (val: boolean) => void;
 }
 
 function FullmaktForTrygdeavgiftConfirmationPanel({
+  erPensjonist,
   harBekreftet,
   onChange,
 }: FullmaktForTrygdeavgiftConfirmationPanelProps) {
@@ -15,15 +17,30 @@ function FullmaktForTrygdeavgiftConfirmationPanel({
       onChange={() => onChange(!harBekreftet)}
       label="Jeg bekrefter at fullmektig for betaling er riktig"
     >
-      <Nav.BodyLong weight="semibold" size="small">
-        Husk at det vanligvis er arbeidsgiver som skal motta faktura
-      </Nav.BodyLong>
-      <Nav.BodyLong size="small">
-        Hvis bruker har oppgitt en annen fullmektig for betaling, skal du spørre bruker om det er riktig.
-        <br />
-        <br />
-        Kontroller at
-      </Nav.BodyLong>
+      {erPensjonist ? (
+        <>
+          <Nav.BodyLong weight="semibold" size="small">
+            Husk at det vanligvis er arbeidsgiver som skal motta faktura
+          </Nav.BodyLong>
+          <Nav.BodyLong size="small">
+            Hvis bruker har oppgitt en annen fullmektig for betaling, skal du spørre bruker om det er riktig.
+            <br />
+            <br />
+            Kontroller at
+          </Nav.BodyLong>
+        </>
+      ) : (
+        <>
+          <Nav.BodyLong weight="semibold" size="small">
+            Fullmektig for betaling av trygdeavgift er en virksomhet
+          </Nav.BodyLong>
+          <Nav.BodyLong size="small">
+            <br />
+            Kontroller at
+          </Nav.BodyLong>
+        </>
+      )}
+
       <Nav.List>
         <Nav.List.Item>organisasjonsnummeret som er oppgitt tilhører riktig enhet i virksomheten</Nav.List.Item>
         <Nav.List.Item>organisasjonsnummeret er registrert i OeBS</Nav.List.Item>
