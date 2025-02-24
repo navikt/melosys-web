59561ca92034ab8297be90210b586ec59e188bef
barstadEkstern <oystein.madsen.barstad@nav.no>
2025-02-24 09:30:57 +0100

    console log

--- src/sider/aarsavregning/stegKomponenter/vurderingAarsavregning/komponenter/beregnetTrygdeavgiftDetaljer.tsx
+++ src/sider/aarsavregning/stegKomponenter/vurderingAarsavregning/komponenter/beregnetTrygdeavgiftDetaljer.tsx
@@ -30,8 +30,6 @@ export function BeregnetTrygdeavgiftDetaljer({
   tittel: string;
 }) {
   const hentDetaljer = (data: Grunnlagsopplysninger | undefined): DetaljerInterface[] => {
-    console.log("Data: ");
-    console.log(data);
     if (data === undefined) return [];
     return data.avgift.trygdeavgiftsperioder
       .map((period) => {
