7a6f85ac34fa8e43596546c613427a40410bc3ef
ck-hui <Chak.Kwan.Hui@nav.no>
2025-02-26 13:25:28 +0100

    Endret på validerings schema

--- src/sider/eu_eøs/stegKomponenter/vurderingVedtak11_3_og_13_3a/vurderingVedtak11_3_og_13_3aSchema.js
+++ src/sider/eu_eøs/stegKomponenter/vurderingVedtak11_3_og_13_3a/vurderingVedtak11_3_og_13_3aSchema.js
@@ -1,10 +1,14 @@
 import { object, string } from "yup";
 import * as KV from "../../../../kodeverk";
 import { MAA_FYLLES_UT } from "../../../../kodeverk/feilmeldinger";
+import { Lovvalgsbestemmelse } from "../../../../proptypes";
+import { notNull } from "ts-mockito";
 
 const { DU_KAN_IKKE_SKRIVE_MER_ENN_4000_TEGN } = KV.Feilmeldinger;
+const { KORTERE_PERIODE } = MKV.Lovvalgsbestemmelser;
 
 const vurderingVedtak_11_3_og_13_3aSchema = object().shape({
+  lovvalgsbestemmelse: notNull,
   begrunnelseFritekst: string().nullable().max(4000, DU_KAN_IKKE_SKRIVE_MER_ENN_4000_TEGN),
   fom: string().when("korterePeriodeChecked", {
     is: true,
