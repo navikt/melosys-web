b3e35df18a7ff5790300b648d6d08278a92ec751
thomasdolplads <thomas.dolplads@twoday.com>
2025-02-28 07:36:18 +0100

    refaktorisert 1 akseptansekriterie

--- tests/aarsavregningFlyt/aarsavregningT509MedGrunnlag.spec.ts
+++ tests/aarsavregningFlyt/aarsavregningT509MedGrunnlag.spec.ts
@@ -6,10 +6,10 @@ import { aapneBehandling, forkastAarsavregning, opprettAarsavregning } from "../
 const SKATTEPLIKTIG = "SKATTEPLIKTIG";
 const IKKE_SKATTEPLIKTIG = "IKKE_SKATTEPLIKTIG";
 
-test.only("MELOSYS-6528 aarsavregning viser tidligere grunnlag ved valg av år", async ({ page }) => {
+test("@1 MELOSYS-6528 aarsavregning viser tidligere grunnlag ved valg av år", async ({ page }) => {
   const options = {
-    fromDate: "11.01.2024",
-    toDate: "12.01.2024",
+    fromDate: "18.01.2024",
+    toDate: "19.01.2024",
     trygdeavgiftOptions: {
       skatteplikttype: IKKE_SKATTEPLIKTIG,
     },
@@ -20,6 +20,22 @@ test.only("MELOSYS-6528 aarsavregning viser tidligere grunnlag ved valg av år",
   await aapneBehandling(page, 0);
 
   await page.selectOption("#aarVelger", "2024");
+
+  // Verify
+  await expect(page.getByRole("heading", { name: "Inntekts- og skatteopplysninger" })).toBeVisible({ timeout: 10000 });
+  await page.getByRole("button", { name: "Vis mer" }).click();
+
   await page.pause();
+  await expect(page.getByLabel("trygdeavgiftdetaljer")).toBeVisible({ timeout: 10000 });
+  const matchingRows = await page
+    .locator("tr", {
+      has: page.locator("td", { hasText: "7 800 kr" }),
+    })
+    .filter({
+      has: page.locator("td", { hasText: "100 000 kr" }),
+    });
+
+  await expect(matchingRows).toHaveCount(1);
+
   await forkastAarsavregning(page);
 });
--- tests/opprettForstegangsbehandling.spec.ts
+++ tests/opprettForstegangsbehandling.spec.ts
@@ -37,32 +37,6 @@ test("opprettvedtak", async ({ page }) => {
 // 21075114491
 const saksnummer = "1";
 const aar = "2024";
-test("MELOSYS-6528 aarsavregning viser tidligere grunnlag ved valg av år", async ({ page }) => {
-  await page.goto("http://localhost:3000/melosys");
-  await page.getByRole("button", { name: "Opprett ny sak/behandling" }).click();
-  await page.getByRole("textbox", { name: "Brukers f.nr. eller d-nr.:" }).click();
-  await page.getByRole("textbox", { name: "Brukers f.nr. eller d-nr.:" }).fill("21075114491");
-  await page.locator(`#saksnummer-MEL-${saksnummer}`).check();
-  await page.locator('input[value="ÅRSAVREGNING"]').check();
-  await page.getByLabel("Årsak").selectOption("SØKNAD");
-  await page.getByRole("checkbox", { name: "Legg behandlingen i mine" }).check();
-  await page.getByRole("button", { name: "Opprett ny behandling" }).click();
-
-  await page.getByRole("link", { name: "GYNGEHEST BRÅKETE -" }).first().click();
-  await page.getByLabel("", { exact: true }).selectOption(aar);
-  await expect(page.getByRole("heading", { name: "Inntekts- og skatteopplysninger" })).toBeVisible({ timeout: 10000 });
-  await page.getByRole("button", { name: "Vis mer" }).click();
-  await expect(page.getByLabel("trygdeavgiftdetaljer")).toBeVisible({ timeout: 10000 });
-  const matchingRows = await page
-    .locator("tr", {
-      has: page.locator("td", { hasText: "7 800 kr" }),
-    })
-    .filter({
-      has: page.locator("td", { hasText: "100 000 kr" }),
-    });
-
-  await expect(matchingRows).toHaveCount(1);
-});
 
 test("MELOSYS-6528 aarsavregning viser feilmelding ved annen åpen årsavregning", async ({ page }) => {
   await page.goto("http://localhost:3000/melosys");
