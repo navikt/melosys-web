d35075e44ef615f55f1947779fa977219fd5c7b3
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2025-03-04 12:16:32 +0100

    7165 Skjuler fullmektig for trygdeavgift varsel dersom beløp er under minstegrensen
    Og dersom det skal refunderes

--- src/sider/aarsavregning/stegKomponenter/vurderingVedtak/vurderingVedtak.tsx
+++ src/sider/aarsavregning/stegKomponenter/vurderingVedtak/vurderingVedtak.tsx
@@ -195,7 +195,7 @@ export function VurderingVedtak({ tilbake, aktivtSteg }: Props) {
   const erNullKroner = trygdeavgiftDiff === 0;
   const skalFaktureres = trygdeavgiftDiff > 0;
 
-  const stegErGyldig = formIsValid && (!harFullmaktForTrygdeavgift || harBekreftetFullmaktForTrygdeavgift);
+  const stegErGyldig = formIsValid && (!harFullmaktForTrygdeavgift || erDifferanseUnderMinstebeløp || harBekreftetFullmaktForTrygdeavgift);
 
   return (
     <div className="vurderingVedtak">
@@ -229,7 +229,7 @@ export function VurderingVedtak({ tilbake, aktivtSteg }: Props) {
         </Nav.Row>
       ) : null}
 
-      {harFullmaktForTrygdeavgift && redigerbart ? (
+      {redigerbart && skalFaktureres && !erDifferanseUnderMinstebeløp && harFullmaktForTrygdeavgift ? (
         <FullmaktForTrygdeavgiftConfirmationPanel
           harBekreftet={harBekreftetFullmaktForTrygdeavgift}
           onChange={setHarBekreftetFullmaktForTrygdeavgift}
