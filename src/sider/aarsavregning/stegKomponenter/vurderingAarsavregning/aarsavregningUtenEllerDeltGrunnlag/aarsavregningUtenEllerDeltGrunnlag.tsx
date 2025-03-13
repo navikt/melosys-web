181d4dee57527a3d9676eda6f0ed85e109e41485
Fredrik Olsen Hauge <45845087+fredrik-oh@users.noreply.github.com>
2025-03-13 12:10:04 +0100

    NOJIRA TOGGLE Fikser en bug ved lagring av medlemskapsperioder (#2634)
    I delt grunnlag.
    TODO Vi gjør unødvendig lagring og/eller oppretting av
    medlemskapsperioder. Ser videre på det i morgen 13.03
    
    Co-authored-by: Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>

--- src/sider/aarsavregning/stegKomponenter/vurderingAarsavregning/aarsavregningUtenEllerDeltGrunnlag/aarsavregningUtenEllerDeltGrunnlag.tsx
+++ src/sider/aarsavregning/stegKomponenter/vurderingAarsavregning/aarsavregningUtenEllerDeltGrunnlag/aarsavregningUtenEllerDeltGrunnlag.tsx
@@ -248,6 +248,7 @@ export function AarsavregningUtenEllerDeltGrunnlag({ bekreft, oppdaterStatus, ha
   }, [formIsValid]);
 
   const lagreMedlemskapsperiode = async (medlemskapsperiode: Medlemskapsperiode) => {
+    // TODO: Fjern unødvendig lagring/oppretting av medlemskapsperioder som er identiske til eksisterende perioder på behandlingsresultat)
     const periodeRequest = {
       fomDato: Utils.dato.formatterDatoTilISO(medlemskapsperiode.fomDato, "") as string,
       tomDato: Utils.dato.formatterDatoTilISO(medlemskapsperiode.tomDato, "") as string,
@@ -266,11 +267,6 @@ export function AarsavregningUtenEllerDeltGrunnlag({ bekreft, oppdaterStatus, ha
 
     if (response.type === medlemskapsperioderTypes.FEILET) {
       setMedlemskapsperiodeFeilmelding(response?.data?.data?.message);
-    } else if (medlemskapsperiode.id === -1) {
-      /* eslint-disable no-param-reassign */
-      medlemskapsperiode.id = response.id;
-      medlemskapsperiode.medlemskapstype = response.medlemskapstype;
-      /* eslint-enable no-param-reassign */
     }
   };
 
@@ -278,7 +274,10 @@ export function AarsavregningUtenEllerDeltGrunnlag({ bekreft, oppdaterStatus, ha
     Utils._debounce(async (medlemskapsperioderFormValues) => {
       const isValid = await trigger("medlemskapsperioder");
       if (isValid) {
-        await Promise.all(medlemskapsperioderFormValues.map(lagreMedlemskapsperiode));
+        /* eslint-disable-next-line no-restricted-syntax */
+        for (const periode of medlemskapsperioderFormValues) {
+          await lagreMedlemskapsperiode(periode);
+        }
 
         dispatch(medlemskapsperioderOperations.hentMedlemskapsperioder(behandlingID));
         if (initiellBeregningUtført && (await trigger())) {
