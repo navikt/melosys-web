dfc29ce46389086109e8b982538a1449dfb3042e
ck-hui <Chak.Kwan.Hui@nav.no>
2025-03-18 10:04:08 +0100

    Rydding

--- src/felleskomponenter/menypanel/linkgroups/linksBuilder.tsx
+++ src/felleskomponenter/menypanel/linkgroups/linksBuilder.tsx
@@ -42,7 +42,6 @@ class LinksBuilder implements ILinksBuilder {
   }
 
   public addPerson() {
-    console.log(this.contentProps);
     this.links.push({
       label: "Person",
       active: false,
