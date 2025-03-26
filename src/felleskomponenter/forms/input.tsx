236fdbe290f2bd9a997eb26edd20aed96bafb11a
Isa Salem Hame <isa.salem.hame@nav.no>
2025-03-26 14:23:03 +0100

    7236: Erstatt input type number med filter (#2654)
    https://jira.adeo.no/browse/MELOSYS-7236
    
    Av [ulike
    årsaker](https://stackoverflow.blog/2022/12/26/why-the-number-input-is-the-worst-input/)
    ønsker vi ikke bruke input type "number". Bruken av type number er også
    noe [Aksel
    fraråder](https://aksel.nav.no/komponenter/core/textfield#5cd0fe4b76a3).v
    
    Erstatter type="number" med pattern + filter

--- src/felleskomponenter/forms/input.tsx
+++ src/felleskomponenter/forms/input.tsx
@@ -11,29 +11,65 @@ function InputInnerComponent({ ...props }: InputInnerComponentProps) {
   return <Nav.TextField {...props} />;
 }
 
+const filterIkkeNumeriskeTegn = (verdi: string, tillattNegativeTall: boolean) => {
+  if (tillattNegativeTall) {
+    let nyVerdi = verdi.replace(/[^0-9-]/g, "");
+
+    if (nyVerdi.indexOf("-") !== -1) {
+      nyVerdi = (nyVerdi.startsWith("-") ? "-" : "") + nyVerdi.replace(/-/g, "");
+    }
+
+    return nyVerdi;
+  }
+
+  return verdi.replace(/[^0-9]/g, "");
+};
+
+const onChangeHandler = (
+  event: any,
+  numeric: boolean | undefined,
+  tillattNegativeTall: boolean | undefined,
+  field: any,
+  rest: any,
+) => {
+  if (numeric && event?.target?.value) {
+    const klonetEvent = { ...event, target: { ...event.target, value: event.target.value } };
+    klonetEvent.target.value = filterIkkeNumeriskeTegn(event.target.value, tillattNegativeTall ?? false);
+
+    field.onChange(klonetEvent);
+    if (rest.onChange) rest.onChange(klonetEvent.target.value);
+  } else {
+    field.onChange(event);
+    if (rest.onChange) rest.onChange(event?.target?.value);
+  }
+};
+
 type InputProps = Omit<ComponentProps<typeof Nav.TextField>, "onChange"> &
   UseControllerProps & {
     onChange?: (value: string) => void;
+    numeric?: boolean;
+    tillattNegativeTall?: boolean;
   };
 
-const Input = forwardRef<HTMLInputElement, InputProps>(({ name, control, error, ...rest }: InputProps, _ref: any) => {
-  return (
-    <Controller
-      name={name}
-      control={control}
-      render={({ field, formState }) => (
-        <InputInnerComponent
-          {...field}
-          {...rest}
-          onChange={(event: any) => {
-            field.onChange(event);
-            if (rest.onChange) rest.onChange(event?.target?.value);
-          }}
-          error={error ?? getErrorMessage(field, formState)}
-        />
-      )}
-    />
-  );
-});
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ name, control, error, numeric, tillattNegativeTall, ...rest }: InputProps, _ref: any) => {
+    return (
+      <Controller
+        name={name}
+        control={control}
+        render={({ field, formState }) => (
+          <InputInnerComponent
+            {...field}
+            {...rest}
+            inputMode={numeric ? "numeric" : rest.inputMode}
+            pattern={numeric ? "^-?[0-9]+$" : rest.pattern}
+            onChange={(event: any) => onChangeHandler(event, numeric, tillattNegativeTall, field, rest)}
+            error={error ?? getErrorMessage(field, formState)}
+          />
+        )}
+      />
+    );
+  },
+);
 
 export default Input;
