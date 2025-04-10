fa45b059462978370d4afd83e97b2ca8d07e4a7b
thomasdolplads <thomas.dolplads@twoday.com>
2025-04-10 10:30:54 +0200

    7255 DAPI ikke gjenbruk eksisterende oppgaver (#2683)
    [JIRA](https://jira.adeo.no/browse/MELOSYS-7255)

--- src/sider/opprettnysak/komponenter/oppgaveVelger.tsx
+++ src/sider/opprettnysak/komponenter/oppgaveVelger.tsx
@@ -1,9 +1,5 @@
-import * as Api from "../../../services/api";
-import * as KV from "../../../kodeverk";
 import * as Nav from "../../../navFrontend";
 import * as Skjema from "../../../felleskomponenter/skjema";
-
-import EnkeltDato from "../../../felleskomponenter/enkeltDato";
 import { OpprettNySakFormData } from "../opprettnysak";
 import { OpprettNyOppgave } from "./opprettNyOppgave";
 
@@ -12,25 +8,10 @@ import { HStack } from "@navikt/ds-react";
 import { EnkelNavBox } from "../../../felleskomponenter/enkelNavBox";
 
 interface OppgaveVelgerProps {
-  oppgaverForsoktHentet: boolean;
-  oppgaver: Api.Oppgaver.SokOppgaveResDto[];
-  change: (field: string, value: any) => void;
   formValues: OpprettNySakFormData;
 }
 
-export function OppgaveVelger({
-  oppgaverForsoktHentet,
-  formValues: { saksnummer, oppretterOppgave },
-  oppgaver,
-  change,
-}: OppgaveVelgerProps) {
-  const oppgaverFinnes = oppgaver.length > 0;
-
-  const settJournalpostID = (oppgaveID: string) => {
-    const oppgave = oppgaver.find((o) => o.oppgaveID === oppgaveID);
-    change("journalpostID", oppgave?.journalpostID);
-  };
-
+export function OppgaveVelger({ formValues: { saksnummer, oppretterOppgave } }: OppgaveVelgerProps) {
   if (saksnummer !== "-1") {
     return (
       <div className="oppgaveVelger">
@@ -42,35 +23,6 @@ export function OppgaveVelger({
     );
   }
 
-  if (!oppgaverFinnes && oppgaverForsoktHentet) {
-    return (
-      <div className="oppgaveVelger">
-        <Nav.Alert variant="info">Ingen eksisterende oppgaver funnet. Det blir opprettet en ny</Nav.Alert>
-        <OpprettNyOppgave />
-      </div>
-    );
-  }
-
-  const radioValg = oppgaver.map((oppgave) => {
-    const tema = KV.Koder.Tema[oppgave.tema];
-    const innhold = (
-      <Skjema.CustomRadioPanelElement
-        tittel={tema}
-        data={[
-          { term: "Oppgavetype:", description: oppgave.oppgavetype },
-          { term: "Registrert dato:", description: <EnkeltDato dato={oppgave.registrertDato} /> },
-          { term: "Saksid:", description: oppgave.sakID },
-          { term: "Frist:", description: <EnkeltDato dato={oppgave.frist} /> },
-        ]}
-      />
-    );
-
-    return {
-      value: oppgave.oppgaveID,
-      innhold,
-    };
-  });
-
   return (
     <div className="oppgaveVelger">
       <Skjema.RadioGroup legend="" hideLegend name="oppretterOppgave" size="medium">
@@ -78,23 +30,9 @@ export function OppgaveVelger({
           <EnkelNavBox focused={oppretterOppgave}>
             <Nav.Radio value>Opprett ny oppgave</Nav.Radio>
           </EnkelNavBox>
-          <EnkelNavBox focused={!oppretterOppgave}>
-            <Nav.Radio value={false}>Eksisterende oppgave</Nav.Radio>
-          </EnkelNavBox>
         </HStack>
       </Skjema.RadioGroup>
-      {oppretterOppgave ? (
-        <OpprettNyOppgave />
-      ) : (
-        oppgaverFinnes && (
-          <>
-            <Nav.Alert className="alertstripe" variant="info">
-              Du kan kun velge mellom følgende oppgaver som er knyttet til et inngående dokument
-            </Nav.Alert>
-            <Skjema.CustomRadioPanelGruppe feltNavn="oppgaveID" radios={radioValg} notify={settJournalpostID} />
-          </>
-        )
-      )}
+      {oppretterOppgave && <OpprettNyOppgave />}
     </div>
   );
 }
