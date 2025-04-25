6ea9505fe02e7d64f36fe58a8feda452d40de983
barstadEkstern <oystein.madsen.barstad@nav.no>
2025-04-25 11:49:47 +0200

    css og refaktorering til mindre komponenter

--- src/sider/journalforing/komponenter/behandlingerListe.tsx
+++ src/sider/journalforing/komponenter/behandlingerListe.tsx
@@ -3,6 +3,7 @@ import * as Nav from "../../../navFrontend";
 import * as Ikon from "../../../resources/images";
 import { BehandlingsstatusMedSvarfrist } from "../../../felleskomponenter/behandlingsstatus";
 import { KTObject } from "@navikt/melosys-kodeverk";
+import "./behandlingerListe.less";
 
 interface BehandlingOversikt {
   behandlingID: string;
@@ -15,6 +16,47 @@ interface BehandlingerListeProps {
   behandlingOversikter: BehandlingOversikt[];
 }
 
+interface BehandlingRadProps {
+  behandling: BehandlingOversikt;
+}
+
+function BehandlingRad({ behandling }: BehandlingRadProps) {
+  return (
+    <Nav.Row key={behandling.behandlingID} className="behandling-rad">
+      <Nav.Column lg="6" className="behandling-tittel">
+        {behandling.tittel}
+      </Nav.Column>
+      <Nav.Column lg="6">
+        <BehandlingsstatusMedSvarfrist
+          behandlingsstatus={behandling.behandlingsstatus}
+          svarFrist={behandling.svarFrist || null}
+        />
+      </Nav.Column>
+    </Nav.Row>
+  );
+}
+
+interface ToggleButtonProps {
+  onClick: () => void;
+  showAll: boolean;
+  disabled?: boolean;
+}
+
+function ToggleButton({ onClick, showAll, disabled }: ToggleButtonProps) {
+  return (
+    <div className="toggle-button-container">
+      <Nav.Button
+        onClick={onClick}
+        variant="tertiary"
+        icon={showAll ? <Ikon.ChevronUp /> : <Ikon.ChevronDown />}
+        disabled={disabled}
+      >
+        {showAll ? "Skjul avsluttede behandlinger" : "Vis avsluttede behandlinger"}
+      </Nav.Button>
+    </div>
+  );
+}
+
 function BehandlingerListe({ behandlingOversikter }: BehandlingerListeProps) {
   const [showAllBehandlinger, setShowAllBehandlinger] = useState<boolean>(false);
 
@@ -23,59 +65,26 @@ function BehandlingerListe({ behandlingOversikter }: BehandlingerListeProps) {
   );
 
   const alleBehandlingerErAvsluttet = pågåendeBehandlinger.length === 0;
-
   const initialBehandlinger = alleBehandlingerErAvsluttet
     ? [behandlingOversikter[behandlingOversikter.length - 1]]
     : pågåendeBehandlinger;
-
   const harFlereBehandlinger = behandlingOversikter.length > initialBehandlinger.length;
 
-  return (
-    <div className="behandlingsstatusSvarfrist-wrapper">
-      {!showAllBehandlinger && (
-        <div className="behandlinger-liste">
-          {initialBehandlinger.map((behandling) => (
-            <Nav.Row key={behandling.behandlingID} className="behandling-rad">
-              <Nav.Column lg="6" className="behandling-tittel">
-                {behandling.tittel}
-              </Nav.Column>
-              <Nav.Column lg="6">
-                <BehandlingsstatusMedSvarfrist
-                  behandlingsstatus={behandling.behandlingsstatus}
-                  svarFrist={behandling.svarFrist || null}
-                />
-              </Nav.Column>
-            </Nav.Row>
-          ))}
-          {harFlereBehandlinger && (
-            <Nav.Button onClick={() => setShowAllBehandlinger(true)} variant="tertiary" icon={<Ikon.ChevronDown />}>
-              Vis mer
-            </Nav.Button>
-          )}
-        </div>
-      )}
+  const behandlingerToShow = showAllBehandlinger ? behandlingOversikter : initialBehandlinger;
 
-      {showAllBehandlinger && (
-        <div className="behandlinger-liste">
-          {behandlingOversikter.map((behandling) => (
-            <Nav.Row key={behandling.behandlingID} className="behandling-rad">
-              <Nav.Column lg="6" className="behandling-tittel">
-                {behandling.tittel}
-              </Nav.Column>
-              <Nav.Column lg="6">
-                <BehandlingsstatusMedSvarfrist
-                  behandlingsstatus={behandling.behandlingsstatus}
-                  svarFrist={behandling.svarFrist || null}
-                />
-              </Nav.Column>
-            </Nav.Row>
-          ))}
-          <Nav.Button onClick={() => setShowAllBehandlinger(false)} variant="tertiary" icon={<Ikon.ChevronUp />}>
-            Vis mindre
-          </Nav.Button>
-        </div>
-      )}
-    </div>
+  return (
+    <Nav.Column>
+      <Nav.Row className="behandlinger-liste">
+        {behandlingerToShow.map((behandling) => (
+          <BehandlingRad key={behandling.behandlingID} behandling={behandling} />
+        ))}
+        <ToggleButton
+          onClick={() => setShowAllBehandlinger(!showAllBehandlinger)}
+          showAll={showAllBehandlinger}
+          disabled={!showAllBehandlinger && !harFlereBehandlinger}
+        />
+      </Nav.Row>
+    </Nav.Column>
   );
 }
 
