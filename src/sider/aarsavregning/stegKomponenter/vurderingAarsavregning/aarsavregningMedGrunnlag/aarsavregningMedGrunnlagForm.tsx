a41700ca9353f7f48564b89beba304c7f472910f
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2025-04-30 14:11:16 +0200

    Tester precommit

--- src/sider/aarsavregning/stegKomponenter/vurderingAarsavregning/aarsavregningMedGrunnlag/aarsavregningMedGrunnlagForm.tsx
+++ src/sider/aarsavregning/stegKomponenter/vurderingAarsavregning/aarsavregningMedGrunnlag/aarsavregningMedGrunnlagForm.tsx
@@ -189,7 +189,7 @@ export function AarsavregningMedGrunnlagForm({ initiellData, bekreft, oppdaterSt
     if (debouncedBeregningRef.current?.cancel)  {
       console.log('Testing pre-commit hook');
       debouncedBeregningRef.current.cancel();
-    }
+    }              
     if (debouncedBeregningRef.current) {
       if (redigerbart && aarsavregningID && erAvvik === true && !endrerAvvik) {
         const currentFormState = mapFormState(getValues("skatteforholdsperioder"), getValues("inntektskilder"));
