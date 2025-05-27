917448808b51d2b6c268c76511d6522453962fc2
Fredrik Olsen Hauge <45845087+fredrik-oh@users.noreply.github.com>
2025-05-27 13:58:17 +0200

    7346 Fikser innsynsmodusfeil i FTRL flyt (#2731)
    Co-authored-by: Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>

--- src/sider/ftrl/saksbehandling/stegKomponenter/vurderingTrygdeavgift/vurderingTrygdeavgift.tsx
+++ src/sider/ftrl/saksbehandling/stegKomponenter/vurderingTrygdeavgift/vurderingTrygdeavgift.tsx
@@ -272,10 +272,10 @@ export function VurderingTrygdeavgift({ bekreft, tilbake, aktivtSteg, oppdaterSt
           trigger(`inntektskilder[${index}].fomDato`);
           trigger(`inntektskilder[${index}].tomDato`);
         });
-      }
-      if (!erÅpenSluttDato && (feil || harEndretInnvilgetMedlemskapsperiode)) {
-        debounceBeregnTrygdeavgiftsperioder(formValues, formIsValid && !feilMeldingBlokkerer(aktivFeilmeldingType));
-        setHarEndretInnvilgetMedlemskapsperiode(false);
+        if (!erÅpenSluttDato && (feil || harEndretInnvilgetMedlemskapsperiode)) {
+          debounceBeregnTrygdeavgiftsperioder(formValues, formIsValid && !feilMeldingBlokkerer(aktivFeilmeldingType));
+          setHarEndretInnvilgetMedlemskapsperiode(false);
+        }
       }
     }
   }, [aktivtSteg, harEndretInnvilgetMedlemskapsperiode]);
