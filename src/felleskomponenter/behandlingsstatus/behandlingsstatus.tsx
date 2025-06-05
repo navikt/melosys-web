b638d92cc1e8656b53473d6e4ac6fde122d6680f
ragnarwestad <ragnar.westad@nav.no>
2025-06-05 10:01:59 +0200

    Revertert, bør gjøres på egen PR

--- src/felleskomponenter/behandlingsstatus/behandlingsstatus.tsx
+++ src/felleskomponenter/behandlingsstatus/behandlingsstatus.tsx
@@ -37,25 +37,24 @@ export function BehandlingsstatusMedSvarfrist({
 }
 
 const getIkon = (status: string) => {
-  const statusTerm = KV.objektTilTerm({ kode: status });
   switch (status) {
     case MKV.Koder.behandlinger.behandlingsstatus.OPPRETTET:
     case MKV.Koder.behandlinger.behandlingsstatus.UNDER_BEHANDLING:
     case MKV.Koder.behandlinger.behandlingsstatus.SVAR_ANMODNING_MOTTATT:
-      return <Ikoner.LockOpenFilled color="#0056B4" aria-label={`Status: ${statusTerm}`} title={statusTerm} />;
+      return <Ikoner.LockOpenFilled color="#0056B4" />;
     case MKV.Koder.behandlinger.behandlingsstatus.VURDER_DOKUMENT:
-      return <Ikoner.LockOpenFilled color="#D47B00" aria-label={`Status: ${statusTerm}`} title={statusTerm} />;
+      return <Ikoner.LockOpenFilled color="#D47B00" />;
     case MKV.Koder.behandlinger.behandlingsstatus.AVVENT_DOK_UTL:
     case MKV.Koder.behandlinger.behandlingsstatus.AVVENT_DOK_PART:
     case MKV.Koder.behandlinger.behandlingsstatus.AVVENT_FAGLIG_AVKLARING:
     case MKV.Koder.behandlinger.behandlingsstatus.ANMODNING_UNNTAK_SENDT:
-      return <Ikoner.ClockFilled color="#0056B4" aria-label={`Status: ${statusTerm}`} title={statusTerm} />;
+      return <Ikoner.ClockFilled color="#0056B4" />;
     case MKV.Koder.behandlinger.behandlingsstatus.TIDSFRIST_UTLOEPT:
-      return <Ikoner.ClockFilled color="#D47B00" aria-label={`Status: ${statusTerm}`} title={statusTerm} />;
+      return <Ikoner.ClockFilled color="#D47B00" />;
     case MKV.Koder.behandlinger.behandlingsstatus.IVERKSETTER_VEDTAK:
     case MKV.Koder.behandlinger.behandlingsstatus.MIDLERTIDIG_LOVVALGSBESLUTNING:
     case MKV.Koder.behandlinger.behandlingsstatus.AVSLUTTET:
-      return <Ikoner.LockClosedFilled color="#6A6A6A" aria-label={`Status: ${statusTerm}`} title={statusTerm} />;
+      return <Ikoner.LockClosedFilled color="#6A6A6A" />;
     default:
       return null;
   }
