61c9aa37826e05f3a13ae0966adeb3deca55c948
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2025-06-09 14:25:44 +0200

    Erstatter redux-mock-store med createTestStore.js. Skriver om operations.test.js filer til nytt pattern for selectors/actions testing

--- src/ducks/test-utils/renderWithProviders.tsx
+++ src/ducks/test-utils/renderWithProviders.tsx
@@ -1,13 +1,10 @@
 import { PropsWithChildren, ReactElement } from "react";
 import type { RenderOptions } from "@testing-library/react";
 import { render } from "@testing-library/react";
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import { RootState } from "AppTypes";
-import rootReducer from "../../reducer";
+import { createTestStore } from "./createTestStore";
 
-// This type interface extends the default options for render from RTL, as well
-// as allows the user to specify other things such as initialState, store.
 interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
   preloadedState?: Partial<RootState>;
   store?: any;
@@ -15,16 +12,7 @@ interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
 
 export function renderWithProviders(
   ui: ReactElement,
-  {
-    preloadedState = {},
-    // Automatically create a store instance if no store was passed in
-    store = configureStore({
-      // @ts-expect-error Forenklet reducerstruktur i test skaper ts krøll
-      reducer: rootReducer,
-      preloadedState,
-    }),
-    ...renderOptions
-  }: ExtendedRenderOptions = {},
+  { preloadedState = {}, store = createTestStore(preloadedState), ...renderOptions }: ExtendedRenderOptions = {},
 ) {
   function Wrapper({ children }: PropsWithChildren<object>): React.JSX.Element {
     return <Provider store={store}>{children}</Provider>;
