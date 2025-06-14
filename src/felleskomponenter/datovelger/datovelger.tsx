0e5083c91eb587fbc6bb0672d61344320554b582
Fredrik Olsen Hauge <45845087+fredrik-oh@users.noreply.github.com>
2025-06-14 17:18:48 +0200

    NOJIRA Legger til onBlur formatting på datovelger (#2765)
    Co-authored-by: Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>

--- src/felleskomponenter/datovelger/datovelger.tsx
+++ src/felleskomponenter/datovelger/datovelger.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FocusEventHandler, ReactNode, useId, useState } from "react";
+import { ChangeEvent, FocusEvent, FocusEventHandler, ReactNode, useId, useState } from "react";
 import classNames from "classnames";
 import { DatePicker, useDatepicker } from "@navikt/ds-react";
 import * as Utils from "../../utils";
@@ -63,6 +63,27 @@ function Datovelger({
     onChange(event.target.value);
   };
 
+  const handleOnBlur = (event: FocusEvent<HTMLInputElement>) => {
+    const currentValue = event.target.value.trim();
+
+    if (currentValue && !readOnly) {
+      const formattedValue = Utils.dato.vaskOgFormatterDatoTilNorsk(currentValue, "");
+      if (formattedValue && formattedValue !== currentValue) {
+        if (inputProps?.onChange) {
+          const syntheticEvent = {
+            ...event,
+            target: { ...event.target, value: formattedValue },
+          } as ChangeEvent<HTMLInputElement>;
+          inputProps.onChange(syntheticEvent);
+        }
+      }
+    }
+
+    if (onBlur) {
+      onBlur(event);
+    }
+  };
+
   const datovelgerID = useId();
   return (
     <div className="datovelger">
@@ -77,7 +98,7 @@ function Datovelger({
             datovelger__input_feil: feil || erUgyldigDato,
           })}
           size="small"
-          onBlur={onBlur}
+          onBlur={handleOnBlur}
           readOnly={readOnly}
           onChange={handleOnChange}
         />
