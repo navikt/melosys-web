4d5cc2d1992ea86b49c4e673678ba60cd1b6b72c
Thang Phan <thang.quoc.ly.phan@nav.no>
2025-06-24 13:31:58 +0200

    ny dispatch

--- src/sider/eu_eøs/pensjonist/stegKomponenter/vurderingOpplysninger/vurderingOpplysninger.tsx
+++ src/sider/eu_eøs/pensjonist/stegKomponenter/vurderingOpplysninger/vurderingOpplysninger.tsx
@@ -1,7 +1,8 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import * as Nav from "../../../../../navFrontend";
 import * as Mui from "../../../../../felleskomponenter/ui";
 import * as Utils from "../../../../../utils";
+import { useDispatch } from "../../../../../hooks";
 
 import { PeriodeOgLandVelger } from "./komponenter/periodeVelger/periodeVelger";
 import { yupResolver } from "@hookform/resolvers/yup";
