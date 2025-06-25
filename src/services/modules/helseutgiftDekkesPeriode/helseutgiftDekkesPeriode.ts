2d4e485dbe1c3d9a1177dbc694036a2ab7573fbe
Thang Phan <thang.quoc.ly.phan@nav.no>
2025-06-25 11:42:37 +0200

    7258 DAPI TOGGLE Endre helseutgift dekkes periode (#2785)
    https://jira.adeo.no/browse/MELOSYS-7258
    
    Støtte for endringer i helseutgift dekkes periode - tabellen

--- src/services/modules/helseutgiftDekkesPeriode/helseutgiftDekkesPeriode.ts
+++ src/services/modules/helseutgiftDekkesPeriode/helseutgiftDekkesPeriode.ts
@@ -1,4 +1,4 @@
-import { getAsJson, postAsJson } from "../../utils";
+import { getAsJson, postAsJson, putAsJson } from "../../utils";
 import { API_BASE_URL, HELSEUTGIFTDEKKESPERIODE, BEHANDLINGER } from "../../api-constants";
 
 export interface HelseutgiftDekkesPeriodeDto {
@@ -9,6 +9,9 @@ export interface HelseutgiftDekkesPeriodeDto {
 
 export const opprettHelseutgiftDekkesPeriode = (behandlingID: number, data: HelseutgiftDekkesPeriodeDto) =>
   postAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/${HELSEUTGIFTDEKKESPERIODE}`, data);
+
+export const oppdaterHelseutgiftDekkesPeriode = (behandlingID: number, data: HelseutgiftDekkesPeriodeDto) =>
+  putAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/${HELSEUTGIFTDEKKESPERIODE}`, data);
 
 export const hentHelseutgiftDekkesPeriode = (behandlingID: number): Promise<HelseutgiftDekkesPeriodeDto> =>
   getAsJson(`${API_BASE_URL}${BEHANDLINGER}/${behandlingID}/${HELSEUTGIFTDEKKESPERIODE}`);
