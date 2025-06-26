f0ac8a3d2e1fba2c5d430a351e582b62f4332efc
Ragnar Westad <ragnar.westad@gmail.com>
2025-06-26 14:41:57 +0200

    Litt bedre typer

--- src/ducks/test-utils/renderWithProviders.tsx
+++ src/ducks/test-utils/renderWithProviders.tsx
@@ -1,12 +1,10 @@
 import React, { PropsWithChildren, ReactElement } from "react";
-import { act, RenderOptions } from "@testing-library/react";
-import { render } from "@testing-library/react";
+import { render, RenderOptions, act } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { RootState } from "AppTypes";
 import { createTestStore } from "./createTestStore";
 
-interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
-  preloadedState?: Partial<RootState>;
+interface ExtendedRenderOptions extends Omit<RenderOptions, "wrapper"> {
+  preloadedState?: object | undefined;
   store?: any;
 }
 
