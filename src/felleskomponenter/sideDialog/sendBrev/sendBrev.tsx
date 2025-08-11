35f625ae80df11fa8eba746ff1b95974f0a79337
Ragnar Westad <ragnar.westad@gmail.com>
2025-08-11 11:38:40 +0200

    Lagt inn igjen linje som ble fjernet ved uhell

--- src/felleskomponenter/sideDialog/sendBrev/sendBrev.tsx
+++ src/felleskomponenter/sideDialog/sendBrev/sendBrev.tsx
@@ -346,6 +346,7 @@ function SendBrev({
     manglerFritekst: hentFormVerdi("MANGLER_FRITEKST"),
     fritekstTittel: hentFormVerdi("BREV_TITTEL", true),
     fritekst: hentFormVerdi("FRITEKST"),
+    skalViseStandardTekstOmOpplysninger: hentFormVerdi("STANDARDTEKST_INNTEKTSOPPLYSNINGER") === "true",
     kopiMottakere: hentKopiMottakere() || [],
     skalViseStandardTekstOmkontaktopplysninger: hentFormVerdi("STANDARDTEKST_KONTAKTINFORMASJON") === "true",
     saksvedlegg: valgteVedlegg?.saksvedlegg.map((vedlegg) => ({
