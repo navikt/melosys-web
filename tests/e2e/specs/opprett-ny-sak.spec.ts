9109014816f2233bc38476de6b80c647b5cadb88
Ragnar Westad <ragnar.westad@gmail.com>
2025-08-30 12:31:08 +0200

    Korrigering av tester

--- tests/e2e/specs/opprett-ny-sak.spec.ts
+++ tests/e2e/specs/opprett-ny-sak.spec.ts
@@ -1,21 +1,7 @@
-import { expect, test } from "@playwright/test";
+import { test } from "@playwright/test";
 import { runAxeAnalyze } from "../utils/axeUtils";
 import { MelosysHovedsidePage } from "../pages/melosys-hovedside.page";
 import { OpprettNySakPage } from "../pages/opprett-ny-sak.page";
-
-test("clicking on a task navigates to the details page", async ({ page }, testInfo) => {
-  const mainPage = new MelosysHovedsidePage(page);
-
-  await mainPage.goto();
-
-  await mainPage.verifyMainPage();
-
-  const taskLinkHref = await mainPage.clickFirstTaskLink();
-
-  expect(page.url(), "Expected URL to contain the taskLink's href after clicking").toContain(taskLinkHref);
-
-  await runAxeAnalyze(mainPage.page, testInfo.title);
-});
 
 test("clicking the 'Opprett ny sak/behandling' button navigates to the create new case page", async ({
   page,
