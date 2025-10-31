a1e1936021f5757761c979ce7545f7bca49dadc3
ck-hui <Chak.Kwan.Hui@nav.no>
2025-10-31 10:07:13 +0100

    Koblet på Trygdeavgift steget i stegvelger

---
+++ src/sider/eu_eøs/saksbehandling/stegMap/trygdeavgift.js
@@ -0,0 +1,41 @@
+import Steg from "../../../../felleskomponenter/stegvelger/stegMotor/steg";
+import { FANE_STATUS, STEG } from "../../../../felleskomponenter/stegvelger";
+import { VurderingTrygdeavgift } from "../../stegKomponenter/vurderingTrygdeavgift/vurderingTrygdeavgift";
+
+class Trygdeavgift extends Steg {
+  constructor(propsLight, stegPosisjon) {
+    super(propsLight, stegPosisjon);
+
+    this.kriterier = [
+      {
+        exec: () => true,
+        nesteSteg: STEG.ARBEID_TJENESTEPERSON_ELLER_FLY_VEDTAK,
+      },
+    ];
+
+    this.id = STEG.TRYGDEAVGIFT;
+    this.tittel = "Trygdeavgift";
+    this.komponent = VurderingTrygdeavgift;
+
+    this.samleRelevanteData = (_propsLight) => ({
+      behandlingID: _propsLight.behandlingID,
+      redigerbart: _propsLight.generiskStegRedigerbart,
+    });
+
+    this.beregnRelevantUI = (_propsLight) => ({
+      harAvklaring: true,
+    });
+
+    this.handlers = {
+      bekreft: propsLight.tilgjengeligeHandlers.bekreftOgFortsett,
+      tilbake: propsLight.tilgjengeligeHandlers.tilbake,
+      oppdaterData: (felt, verdi) => this._propsLight.tilgjengeligeHandlers.oppdaterStegData(this.id, felt, verdi),
+      slettData: (data) => this._propsLight.tilgjengeligeHandlers.slettStegData(this.id, data),
+      oppdaterStatus: () => {}, // No-op: Step validity is managed internally by the component
+    };
+
+    this.status = FANE_STATUS.OK;
+  }
+}
+
+export default Trygdeavgift;
