1f770cd473a64d2f20763de79410e73b0d825b1a
ck-hui <Chak.Kwan.Hui@nav.no>
2025-11-06 11:33:36 +0100

    Navn endring

--- src/sider/eu_eøs/saksbehandling/stegMap/medfolgende_barn.js
+++ src/sider/eu_eøs/saksbehandling/stegMap/medfolgende_barn.js
@@ -31,7 +31,7 @@ class VesentligVirksomhet extends Steg {
       },
       {
         exec: () => harAvklaring && propsLight.erArbeidTjenestepersonEllerFly,
-        nesteSteg: STEG.VURDERING_PERIODE_OFFENTLIG_ANSATT,
+        nesteSteg: STEG.VURDER_PERIODE,
       },
     ];
 
