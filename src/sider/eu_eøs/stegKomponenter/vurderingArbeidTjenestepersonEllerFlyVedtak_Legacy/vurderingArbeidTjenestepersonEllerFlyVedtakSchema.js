0a6d3df7d7725454c93e4e2007172bc1f9a00d45
CK-Hui <chak.kwan.hui@nav.no>
2025-11-10 15:14:36 +0100

    7659 periode steg i eu eøs (#2935)
    https://jira.adeo.no/browse/MELOSYS-7659
    
    Nå ligger det flere PR, men denne skal være den som vi pusher ut med
    bare endringer som er relevant for oppgaven.
    
    Filer som ligger i denne stien er ikke nye filer, her har jeg bare
    endret mappenavn men Github tolker da alt inni som ny.
    Men de kan man altså ignoreres.
    
    `src/sider/eu_eøs/stegKomponenter/vurderingArbeidTjenestepersonEllerFlyVedtak_Legacy`

--- src/sider/eu_eøs/stegKomponenter/vurderingArbeidTjenestepersonEllerFlyVedtak_Legacy/vurderingArbeidTjenestepersonEllerFlyVedtakSchema.js
+++ src/sider/eu_eøs/stegKomponenter/vurderingArbeidTjenestepersonEllerFlyVedtak_Legacy/vurderingArbeidTjenestepersonEllerFlyVedtakSchema.js
@@ -3,6 +3,7 @@ import { object, string, bool, array } from "yup";
 import MKV from "../../../../melosyskodeverk";
 import * as KV from "../../../../kodeverk";
 
+const VELG_EN_BESTEMMELSE = "Velg en bestemmelse.";
 const VELG_EN_VEDTAKSTYPE = { melding: "Velg en vedtakstype" };
 const OPPGI_BEGRUNNELSE = { melding: "Oppgi begrunnelse" };
 const MOTTAKERINSTITUSJON_KREVES = { melding: "Mottakerinstitusjon kreves" };
@@ -10,9 +11,20 @@ const VELG_OM_UTENLANDSK_TRYGDEMYNDIGHET_SKAL_INFORMERES = {
   melding: "Velg om utenlandsk trygdemyndighet skal informeres",
 };
 const VELG_LAND = { melding: "Velg land" };
+const { MAA_FYLLES_UT } = KV.Feilmeldinger;
 
 const arbeid_tjenesteperson_eller_fly_vedtak = object().shape({
-  // Lovvalgsbestemmelse og periode valideres nå i periode-steget
+  lovvalgsbestemmelse: string().nullable().required(VELG_EN_BESTEMMELSE),
+  forkortLovvalgsperiode: bool().required(),
+  fomDato: string().when("forkortLovvalgsperiode", {
+    is: true,
+    then: (schema) => schema.erInnenforSoknadsperioden().erGyldigDato().required(MAA_FYLLES_UT),
+  }),
+  tomDato: string().when("forkortLovvalgsperiode", {
+    is: true,
+    then: (schema) =>
+      schema.erInnenforSoknadsperioden().erEtterDatofelt("fomDato").erGyldigDato().required(MAA_FYLLES_UT),
+  }),
   vedtakstype: string()
     .nullable()
     .when("$behandlingstype", {
