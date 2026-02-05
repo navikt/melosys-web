af39f350c9ee762442fceb028dc555dc3eb49301
Ragnar Westad <ragnar.westad@gmail.com>
2026-02-05 09:00:01 +0100

    Rettet feil property-navn for trygdedekning i vurderingUnntakMedlemskap   - Endret lovvalgsperiode.trygdedekning til lovvalgsperiode.trygdeDekning (stor D)   - Ble glemt under revertering av LovvalgsperiodeDto-endringer (b3d14086)   - Lovvalgsperiode-interfacet og backend bruker trygdeDekning (camelCase)   - Feilen ga undefined runtime-verdi, ble ikke fanget av TypeScript pga .js-selector

--- src/sider/unntaksregistrering/vurderingUnntakMedlemskap/vurderingUnntakMedlemskap.tsx
+++ src/sider/unntaksregistrering/vurderingUnntakMedlemskap/vurderingUnntakMedlemskap.tsx
@@ -64,7 +64,7 @@ function VurderingUnntakMedlemskap({ oppdaterStatus, tilbake, aktivtSteg }: Vurd
       fom: Utils.dato.formatterDatoTilNorsk(lovvalgsperiode.fomDato || mottatteOpplysningerPeriode.fom),
       tom: Utils.dato.formatterDatoTilNorsk(lovvalgsperiode.tomDato || mottatteOpplysningerPeriode.tom),
       bestemmelse: lovvalgsperiode.lovvalgsbestemmelse || "",
-      trygdedekning: lovvalgsperiode.trygdedekning,
+      trygdedekning: lovvalgsperiode.trygdeDekning,
     } as FieldValues,
   });
   const formValues = watch();
