f2248223ac5eb1b5469d7d24e58d47684af69912
ck-hui <Chak.Kwan.Hui@nav.no>
2026-03-23 14:28:46 +0100

    Test fix

--- src/sider/aarsavregning/stegKomponenter/vurderingAarsavregning/komponenter/innbetaltTrygdeavgiftInput.test.tsx
+++ src/sider/aarsavregning/stegKomponenter/vurderingAarsavregning/komponenter/innbetaltTrygdeavgiftInput.test.tsx
@@ -16,7 +16,7 @@ import { InnbetaltTrygdeavgiftInput } from "./innbetaltTrygdeavgiftInput";
 describe("InnbetaltTrygdeavgiftInput", () => {
   it("rendrer label", () => {
     render(<InnbetaltTrygdeavgiftInput control={{} as any} redigerbart={true} erNyAarsavregning={false} />);
-    expect(screen.getByText("Innbetalt trygdeavgift")).toBeDefined();
+    expect(screen.getByText("Trygdeavgift fra Avgiftssystemet")).toBeDefined();
   });
 
   it("viser beskrivelse for ny årsavregning", () => {
