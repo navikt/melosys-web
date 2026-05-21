bbf16d4a0992ca4b5beeb94277cf186c77170410
Ragnar Westad <ragnar.westad@gmail.com>
2026-05-21 11:46:28 +0200

    8027 Rettet feilmelding for bostedsland og hindret for tidlig validering på EØS pensjonist-inngang
    Endret feilmeldingen for bostedsland fra den rå Yup-meldingen til "Du må velge bostedsland".
    
    Byttet valideringsmodus fra "all" til "onTouched" og autolagringen fra trigger() til stille schema-validering, slik at feilmeldinger ikke vises for felt brukeren ennå ikke har rørt. La til schema-test.

--- src/sider/eu_eøs/pensjonist/stegKomponenter/vurderingOpplysninger/vurderingOpplysningerSchema.jsx
+++ src/sider/eu_eøs/pensjonist/stegKomponenter/vurderingOpplysninger/vurderingOpplysningerSchema.jsx
@@ -1,12 +1,12 @@
 import { object, string } from "yup";
 import * as KV from "../../../../../kodeverk";
 
-const { MAA_FYLLES_UT } = KV.Feilmeldinger;
+const { MAA_FYLLES_UT, MAA_VELGE_BOSTEDSLAND } = KV.Feilmeldinger;
 
 const vurdering_opplysninger = object().shape({
   fomDato: string().erGyldigDato().required(MAA_FYLLES_UT),
   tomDato: string().erGyldigDato().erEtterDatofelt("fomDato").required(MAA_FYLLES_UT),
-  bostedLandkode: string().required(),
+  bostedLandkode: string().required(MAA_VELGE_BOSTEDSLAND),
 });
 
 export default vurdering_opplysninger;
