9bc065ec804182c29e181acf331df2d8c417e201
Isa Salem Hame <isa.salem.hame@nav.no>
2026-05-26 09:02:05 +0200

    8088 G4P Uthev placeholders i tekstblokk-forhåndsvisning (#3077)
    Placeholders ([NAVN], [DATO_FRA] osv.) vises nå rødt/fet i
    forhåndsvisningen av tekstblokker – både i Send brev-popoveren og
    admin-listen – på samme måte som ved redigering i editoren.
    
    Lagret HTML har placeholders som ren tekst; forhåndsvisningen wrapper
    dem i span.bracketed-text før render. CSS-stilen fantes allerede.

--- src/felleskomponenter/htmlEditor/tekstblokkForhandsvisning.tsx
+++ src/felleskomponenter/htmlEditor/tekstblokkForhandsvisning.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import { useMemo } from "react";
 
 import "./tekstblokkForhandsvisning.less";
 
@@ -7,9 +8,16 @@ interface Props {
   className?: string;
 }
 
+const uthevPlaceholders = (html: string): string =>
+  html.replace(/\[[^[\]<>]*\]/g, (token) => `<span class="bracketed-text">${token}</span>`);
+
 function TekstblokkForhandsvisning({ html, className }: Props) {
+  const uthevet = useMemo(() => uthevPlaceholders(html), [html]);
   return (
-    <div className={classNames("tekstblokk-forhandsvisning", className)} dangerouslySetInnerHTML={{ __html: html }} />
+    <div
+      className={classNames("tekstblokk-forhandsvisning", className)}
+      dangerouslySetInnerHTML={{ __html: uthevet }}
+    />
   );
 }
 
