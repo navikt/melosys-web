0adf3bb472b9a39450a26c34fdece3202742a344
Isa Salem Hame <isa.salem.hame@nav.no>
2026-05-26 08:41:15 +0200

    Uthev [PLACEHOLDER] rødt i tekstblokk-forhåndsvisning
    Lagret innhold har placeholders som ren tekst (Quill highlighter dem kun
    runtime i editoren). Forhåndsvisningen wrapper nå [..]-tokens i
    span.bracketed-text, så de vises rødt/fet i både Send brev-popoveren og
    admin-listen – likt som ved redigering. CSS-stilen fantes allerede.

--- src/felleskomponenter/htmlEditor/tekstblokkForhandsvisning.tsx
+++ src/felleskomponenter/htmlEditor/tekstblokkForhandsvisning.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import { useMemo } from "react";
 
 import "./tekstblokkForhandsvisning.less";
 
@@ -7,9 +8,19 @@ interface Props {
   className?: string;
 }
 
+// Wrapper [PLACEHOLDER]-tokens i en span med samme uthevingsstil som Quill-editoren,
+// slik at forhåndsvisningen markerer placeholders rødt på samme måte som ved redigering.
+// Mønsteret krysser ikke tag-grenser (ingen <, > eller nøstede klammer).
+const uthevPlaceholders = (html: string): string =>
+  html.replace(/\[[^[\]<>]*\]/g, (token) => `<span class="bracketed-text">${token}</span>`);
+
 function TekstblokkForhandsvisning({ html, className }: Props) {
+  const uthevet = useMemo(() => uthevPlaceholders(html), [html]);
   return (
-    <div className={classNames("tekstblokk-forhandsvisning", className)} dangerouslySetInnerHTML={{ __html: html }} />
+    <div
+      className={classNames("tekstblokk-forhandsvisning", className)}
+      dangerouslySetInnerHTML={{ __html: uthevet }}
+    />
   );
 }
 
