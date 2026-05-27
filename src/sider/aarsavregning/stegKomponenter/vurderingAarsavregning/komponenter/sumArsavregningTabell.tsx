5c493280658c70ed6d87b8ed35702875a4c4cf35
CK-Hui <chak.kwan.hui@nav.no>
2026-05-27 15:09:13 +0200

    8052 TOGGLE Oppdater visning av innbetalt trygdeavgift (#3083)
    Oppdaterer årsavregningstabellen slik at innbetalt trygdeavgift vises
    med riktig logikk og tekst når EØS-pensjonist-toggle er aktiv.
    
    Endringen sikrer at årsavregningen bruker riktig begrep for innbetalt
    trygdeavgift og ikke viser tidligere innbetalt trygdeavgift uten aktiv
    toggle.
    [MELOSYS-8052](https://jira.adeo.no/browse/MELOSYS-8052)
    
    - Viser innbetalt trygdeavgift kun når EØS-pensjonist-toggle er aktiv
    - Oppdaterer label for tidligere beregnet trygdeavgift ved aktiv toggle
    
    ## Testing
    - [ ] Enhetstester
    - [x] Manuell testing lokalt
    - [ ] E2E-tester

--- src/sider/aarsavregning/stegKomponenter/vurderingAarsavregning/komponenter/sumArsavregningTabell.tsx
+++ src/sider/aarsavregning/stegKomponenter/vurderingAarsavregning/komponenter/sumArsavregningTabell.tsx
@@ -21,11 +21,8 @@ export function SumArsavregningTabell({
 }) {
   const erÅrsavregningEøsPensjonistToggleEnabled = useFeatureToggle(ÅRSAVREGNING_EØS_PENSJONIST);
 
-  const skalViseInnbetaltTrygdeavgift = erÅrsavregningEøsPensjonistToggleEnabled
-    ? tidligereInnbetaltTrygdeavgift !== undefined &&
-      tidligereInnbetaltTrygdeavgift !== null &&
-      tidligereInnbetaltTrygdeavgift !== 0
-    : tidligereInnbetaltTrygdeavgift !== undefined && tidligereInnbetaltTrygdeavgift !== null;
+  const skalViseInnbetaltTrygdeavgift =
+    tidligereInnbetaltTrygdeavgift !== undefined && tidligereInnbetaltTrygdeavgift !== null;
 
   const skalViseTidligereBeregnetTrygdeavgift = erÅrsavregningEøsPensjonistToggleEnabled
     ? !skalViseInnbetaltTrygdeavgift && (harGrunnlagIMelosys || tidligereTrygdeavgift) !== undefined
@@ -53,6 +50,10 @@ export function SumArsavregningTabell({
     ? "Tidligere innbetalt trygdeavgift"
     : "Tidligere trygdeavgift fra Avgiftssystemet";
 
+  const tidligereBeregnetTrygdeavgiftLabel = erÅrsavregningEøsPensjonistToggleEnabled
+    ? "Innbetalt trygdeavgift"
+    : "Tidligere beregnet trygdeavgift";
+
   return (
     <Nav.Box className="sumArsavregningTabell" background="surface-subtle">
       <Nav.Table size="small" width={500} className="periode_tabell">
@@ -69,7 +70,7 @@ export function SumArsavregningTabell({
           {skalViseTidligereBeregnetTrygdeavgift && (
             <Nav.Table.Row>
               <Nav.Table.DataCell scope="col">-</Nav.Table.DataCell>
-              <Nav.Table.DataCell scope="col">Tidligere beregnet trygdeavgift</Nav.Table.DataCell>
+              <Nav.Table.DataCell scope="col">{tidligereBeregnetTrygdeavgiftLabel}</Nav.Table.DataCell>
               <Nav.Table.DataCell align="right" key={Utils._uuid()}>
                 {formaterTilNorskBelop(tidligereTrygdeavgift || 0)} kr
               </Nav.Table.DataCell>
