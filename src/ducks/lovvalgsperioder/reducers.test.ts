ace7db145edc5daa24010cc61a50e0ba8b787c08
Thang Phan <thang.quoc.ly.phan@nav.no>
2026-06-25 12:30:30 +0200

    8149 Revert pr for feil i offentlig tjenesteperson (#3101)
    https://nav.atlassian.net/jira/software/c/projects/MELOSYS/boards/1704?selectedIssue=MELOSYS-8149
    
    
    Revert pr for https://github.com/navikt/melosys-web/pull/3097/changes
    
    Fiks for feil blir fikset i API istedet.

--- src/ducks/lovvalgsperioder/reducers.test.ts
+++ src/ducks/lovvalgsperioder/reducers.test.ts
@@ -12,13 +12,10 @@ describe("lovvalgsperioder reducer", () => {
     expect(reducer(initialState, { type: Types.PENDING }).status).toBe(STATUS.PENDING);
   });
 
-  it("setter FEILET uten å overskrive periode-arrayet", () => {
-    const state = { data: [{ periodeID: 1, fomDato: "2024-01-01" }], status: STATUS.OK } as any;
-    const next = reducer(state, { type: Types.FEILET, data: { response: {}, data: "error" } }) as any;
+  it("setter FEILET med data", () => {
+    const next = reducer(initialState, { type: Types.FEILET, data: "error" });
     expect(next.status).toBe(STATUS.ERROR);
-    // data (periode-arrayet) skal bevares slik at en påfølgende lagre() ikke POSTer et objekt
-    expect(next.data).toEqual(state.data);
-    expect(next.error).toEqual({ response: {}, data: "error" });
+    expect(next.data).toBe("error");
   });
 
   it("setter OK med data", () => {
