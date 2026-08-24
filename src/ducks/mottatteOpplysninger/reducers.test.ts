d06339d2e080afba99d1c3f31ef272c7b955acc1
Odin Eilertsen <odin.eilertsen@nav.no>
2026-08-24 14:20:52 +0200

    MELOSYS-8224 Bevar varsel om ulik periode ved oppdatering av state

--- src/ducks/mottatteOpplysninger/reducers.test.ts
+++ src/ducks/mottatteOpplysninger/reducers.test.ts
@@ -127,6 +127,19 @@ describe("mottatteOpplysninger reducer", () => {
   });
 
   it("Oppdaterer mottatteOpplysninger ved action.type OPPDATER_MOTTATTE_OPPLYSNINGER", () => {
+    /**
+     * arbeidsgiverOgArbeidstakerHarUlikPeriode er utledet av melosys-api og finnes ikke i
+     * skjemaverdiene. Den ligger derfor kun i eksisterende state, og skal bevares av reduceren.
+     */
+    const eksisterendeState: any = {
+      ...initialState,
+      data: {
+        data: {
+          arbeidsgiverOgArbeidstakerHarUlikPeriode: true,
+        },
+      },
+    };
+
     const action: MockAction = {
       type: Types.OPPDATER_MOTTATTE_OPPLYSNINGER,
       dokument: {
@@ -497,6 +510,7 @@ describe("mottatteOpplysninger reducer", () => {
             fom: "2011-11-11",
             tom: "2011-11-11",
           },
+          arbeidsgiverOgArbeidstakerHarUlikPeriode: true,
           representantIUtlandet: {
             representantNavn: "Representant I Danmark",
             adresselinjer: ["Adresselinje 1", "Adresselinje 2"],
@@ -539,7 +553,7 @@ describe("mottatteOpplysninger reducer", () => {
       },
     };
 
-    const nextState = reducer(initialState, action);
+    const nextState = reducer(eksisterendeState, action);
 
     expect(nextState).toEqual(expectedState);
   });
