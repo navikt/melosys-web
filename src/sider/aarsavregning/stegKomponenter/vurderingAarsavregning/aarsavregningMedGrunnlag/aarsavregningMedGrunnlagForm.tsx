a1a2fdf729bf61ab5ded43f7c14cbf57b94a012d
Isa Salem Hame <isa.salem.hame@nav.no>
2026-09-24 16:32:34 +0200

    8006 Skjul valg av endelig avgift når ny vurdering har fjernet avgiftspliktig periode

--- src/sider/aarsavregning/stegKomponenter/vurderingAarsavregning/aarsavregningMedGrunnlag/aarsavregningMedGrunnlagForm.tsx
+++ src/sider/aarsavregning/stegKomponenter/vurderingAarsavregning/aarsavregningMedGrunnlag/aarsavregningMedGrunnlagForm.tsx
@@ -670,12 +670,14 @@ export function AarsavregningMedGrunnlagForm({ initiellData, bekreft, oppdaterSt
 
   return (
     <>
-      <EndeligAvgiftValgRadioGroup
-        control={control}
-        redigerbart={redigerbart}
-        handleEndeligAvgiftValgChange={handleEndeligAvgiftValgChange}
-        endeligAvgiftValg={endeligAvgiftValg}
-      />
+      {!nyVurderingHarFjernetAvgiftspliktigperiode && (
+        <EndeligAvgiftValgRadioGroup
+          control={control}
+          redigerbart={redigerbart}
+          handleEndeligAvgiftValgChange={handleEndeligAvgiftValgChange}
+          endeligAvgiftValg={endeligAvgiftValg}
+        />
+      )}
 
       {(endeligAvgiftValg === OPPLYSNINGER_ENDRET ||
         endeligAvgiftValg === OPPLYSNINGER_ENDRET_MED_PERIODE_FRA_AVGIFTSSYSTEMET) &&
